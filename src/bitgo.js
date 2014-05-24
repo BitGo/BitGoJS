61f3ff02e06ae5f48dd224b1800a54be2f43e00b
mike belshe <mike@belshe.com>
2014-05-24 14:21:47 -0700

    Add a simple market data API

--- src/bitgo.js
+++ src/bitgo.js
@@ -4,11 +4,43 @@
 // Copyright 2014, BitGo, Inc.  All Rights Reserved.
 //
 
-var BitGo = function() {
+var request = require('superagent');
+
+//
+// Constructor for BitGo Object
+// arguments:
+//   @useProduction: flag to use the production bitcoin network rather than the testnet network.
+//
+var BitGo = function(useProduction) {
+  if (useProduction) {
+    this.baseUrl = 'https://beer.bitgo.com/api/v1';
+  } else {
+    this.baseUrl = 'https://www.bitgo.com/api/v1';
+  }
 };
 
+// 
+// version
+// Gets the version of the BitGoJS API
+// 
 BitGo.prototype.version = function() {
   return '0.1.0';
-}
+};
+
+//
+// market
+// Get the latest bitcoin prices.
+//
+BitGo.prototype.market = function(callback) {
+  var url = this.baseUrl + '/market/latest';
+  request
+    .get(url)
+    .end(function(err, res) {
+      if (err) {
+        throw err;
+      }
+      callback(res.body);
+    });
+};
 
 module.exports = BitGo;
--- test/bitgo.js
+++ test/bitgo.js
@@ -10,12 +10,27 @@ var should = require('should');
 var assert = require('assert');
 
 describe('BitGo', function() {
-  describe('bitgo methods', function() {
-    it('has version', function() {
+
+  describe('methods', function() {
+    it('includes version', function() {
       var bitgo = new BitGoJS.BitGo();
       bitgo.should.have.property('version');
       var version = bitgo.version();
       assert.equal(typeof(version), 'string');
+    });
+
+    it('includes market', function(done) {
+      var bitgo = new BitGoJS.BitGo();
+      bitgo.should.have.property('market');
+      bitgo.market(function(marketData) {
+        marketData.should.have.property('last');
+        marketData.should.have.property('bid');
+        marketData.should.have.property('ask');
+        marketData.should.have.property('volume');
+        marketData.should.have.property('high');
+        marketData.should.have.property('low');
+        done();
+      });
     });
   });
 });
