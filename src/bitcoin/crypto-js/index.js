a4c2bd06a85bc116fc4f639f54725847e75c809b
mike belshe <mike@belshe.com>
2014-05-26 15:50:55 -0700

    Fix case of ripemd160.

--- src/bitcoin/crypto-js/index.js
+++ src/bitcoin/crypto-js/index.js
@@ -4,24 +4,9 @@
 // Copyright 2014, BitGo, Inc.  All Rights Reserved.
 //
 
-/*
-    $SRCDIR/src/crypto-js/src/Crypto.js \
-    $SRCDIR/src/crypto-js/src/CryptoMath.js \
-    $SRCDIR/src/crypto-js/src/BlockModes.js \
-    $SRCDIR/src/crypto-js/src/SHA256.js \
-    $SRCDIR/src/crypto-js/src/AES.js \
-    $SRCDIR/src/crypto-js/src/PBKDF2.js \
-    $SRCDIR/src/crypto-js/src/HMAC.js \
-    $SRCDIR/src/sha512.js \
-
-    $SRCDIR/src/crypto-js-etc/ripemd160.js \
-
-*/
-
 var Crypto = require('./Crypto');
 var SHA256 = require('./SHA256');
-var RIPEMD160 = require('./RIPEMD160');
-
+var RIPEMD160 = require('./ripemd160');
 
 module.exports = {
   RIPEMD160: RIPEMD160,
