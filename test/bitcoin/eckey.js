dd5009ca092381639aed3aab2b343cd2933af166
Mike Belshe <mike@bitgo.com>
2014-05-30 16:25:39 -0700

    Flesh out more ECKey tests.

--- test/bitcoin/eckey.js
+++ test/bitcoin/eckey.js
@@ -59,7 +59,10 @@ describe('ECKey', function() {
     assert.equal(true, newkey.verify(hash, signature2), 'key2 can verify its own sig');
     assert.notEqual(true, newkey.verify(hash, signature1), 'key2 cannot verify key1\'s sig');
 
+
     // Now, can we derive the same public key by chaining just the public key
+    assert.throws(function() { Bitcoin.ECKey.createPubKeyFromChain(eckey.getPub(), Bitcoin.Util.bytesToHex(chainCode)); });
+
     var pubkeyChain = Bitcoin.ECKey.createPubKeyFromChain(eckey.getPub(), chainCode);
     assert.ok(pubkeyChain, 'created chain from pubkey');
     assert.deepEqual(pubkeyChain, newkey.getPub(), 'chained public key derived from parent\'s public key matches that dervived from parent\'s private key');
@@ -83,8 +86,16 @@ describe('ECKey', function() {
     for (var index = 0; index < kNumParallelChains; ++index) {
       var chain = new Array(32);
       random.nextBytes(chain);
+      assert.throws(function() { Bitcoin.ECKey.createECKeyFromChain(eckey.priv.toByteArrayUnsigned(), Bitcoin.Util.bytesToHex(chain)); });
       var newkey = Bitcoin.ECKey.createECKeyFromChain(eckey.priv.toByteArrayUnsigned(), chain);
       chainedKeys[newkey.getBitcoinAddress().toString()] = { chain: chain, key: newkey };
+
+      // Verify alternate forms of the same process also work.
+      eckey.setCompressed(true);
+      var newkey2 = Bitcoin.ECKey.createECKeyFromChain(eckey, chain);
+      assert.equal(eckey.compressed, true);
+      assert.equal(newkey.toString(), newkey2.toString());
+      eckey.setCompressed(false);
     }
     assert.equal(Object.associativeArraySize(chainedKeys), kNumParallelChains, "generated unique keys");
 
@@ -127,6 +138,43 @@ describe('ECKey', function() {
        chainHead = chainedPubKey;
     }
   });
+
+  it('toString', function() {
+    var eckey = new Bitcoin.ECKey();
+    var wifKey = eckey.toString('wif');
+    assert(new Bitcoin.ECKey(wifKey).toString(), wifKey);
+    var hexKey = eckey.toString();
+    assert(new Bitcoin.ECKey(hexKey).toString(), wifKey);
+  });
+
+  it('compressed key', function() {
+    var eckey = new Bitcoin.ECKey();
+    var pubKey = eckey.getPubKeyHex();
+    assert.equal(eckey.getPubKeyHex(), pubKey);  // Test the cache
+
+    eckey.setCompressed(true);
+    var pubKeyCompressed = eckey.getPubKeyHex();
+    assert.equal(eckey.getPubKeyHex(), pubKeyCompressed);  // Test the cache
+
+    assert.notEqual(pubKey, pubKeyCompressed);
+  });
+
+  it('wallet import format', function() {
+    var key = '93U4Fwa2TLmE9b1MQ2UUtioRHzTC1NSe6Y5hesNsyneNhGM1AFm';
+    var eckey = new Bitcoin.ECKey(key);
+    assert.equal(eckey.getBitcoinAddress().toString(), 'myJWF6uiMfcsMxfdBssvm2CfZ7p3JPvJBc');
+    assert.equal(eckey.getPubKeyHex(), '041AAC2A6C3861E7008A02ADDD9F65139B07C427C165ECC55194A371715C62706AAFD798E39DE61061773AF091B404AC7A934E16AF36DAB44811BA0691C19803F4');
+    assert.equal(eckey.toString('wif'), '93U4Fwa2TLmE9b1MQ2UUtioRHzTC1NSe6Y5hesNsyneNhGM1AFm');
+
+    assert.throws(function() { Bitcoin.ECKey.decodeWalletImportFormat('93U4Fwa2TLmE9b1MQ2UUtioRHzTC1NSe6Y5hesNsyneNhGM1AFn'); });
+    assert.throws(function() { Bitcoin.ECKey.decodeWalletImportFormat('a3U4Fwa2TLmE9b1MQ2UUtioRHzTC1NSe6Y5hesNsyneNhGM1AFm'); });
+
+    eckey.compressed = true;
+    assert.equal(eckey.getBitcoinAddress().toString(), 'mgcKn22R7QvQojNdmHxAvQ293tbmkjsKQb');
+    assert.equal(eckey.getPubKeyHex(), '021AAC2A6C3861E7008A02ADDD9F65139B07C427C165ECC55194A371715C62706A');
+    assert.equal(eckey.toString('wif'), 'cVtRNwrmRGED2u6Z2dWJ7HVtDzabadeAjSBkS9Ma1eTxkHQ5M1WQ');
+
+    assert.throws(function() { Bitcoin.ECKey.decodeCompressedWalletImportFormat('cVtRNwrmRGED2u6Z2dWJ7HVtDzabadeAjSBkS9Ma1eTxkHQ5M1WR'); });
+    assert.throws(function() { Bitcoin.ECKey.decodeCompressedWalletImportFormat('dVtRNwrmRGED2u6Z2dWJ7HVtDzabadeAjSBkS9Ma1eTxkHQ5M1WQ'); });
+  });
 });
-
-
