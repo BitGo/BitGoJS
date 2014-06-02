03e9c8ef0f8285e5650bafaf5b4a9616e4ae517f
Mike Belshe <mike@bitgo.com>
2014-06-01 22:11:40 -0700

    Update auth example to do simple authentication.

--- example/auth.js
+++ example/auth.js
@@ -7,5 +7,20 @@
 var BitGoJS = require('../src/index.js');
 
 var bitgo = new BitGoJS.BitGo();
-console.log(bitgo.version());
+if (process.argv.length < 5) {
+  console.log("usage:\n\t" + process.argv[0] + " " + process.argv[1] + " <user> <pass> <otp>");
+  process.exit(-1);
+}
+
+var user = process.argv[2];
+var password = process.argv[3];
+var otp = process.argv[4];
+
+console.log("BitGoJS library version: " + bitgo.version());
+bitgo.authenticate(user, password, otp, function(err, result) {
+  if (err) {
+    return console.dir(err);
+  }
+  console.dir(result);
+});
 
