BitGo/BitGoJS

commit 1248530b2c628e15a8b4bf3eddb59e7e699c3739

calculate fees using tx bytes without rounding up to the nearest kb
Summary: we were rounding up to the nearest kb, which was causing calculation of higher fees than needed.

Reviewers: ben

Reviewed By: ben

Subscribers: ben

Differential Revision: https://phabricator.bitgo.com/D1298

src/transactionBuilder.js
@@ -92,9 +92,6 @@ exports.createTransaction = function(params) {
   // Flag indicating whether this class will compute the fee
   var shouldComputeBestFee = (typeof(fee) == 'undefined');
 
-  if (typeof(fee) == 'undefined') {
-    fee = DEFAULT_FEE;
-  }
   if (fee > MAX_FEE) {
     throw new Error('fee too generous');  // Protection against bad inputs
   }
@@ -187,13 +184,13 @@ exports.createTransaction = function(params) {
     var outputSizeKb = recipients.length * 34 / 1000;
     var inputSizeKb = (transaction.ins.length * signaturesPerInput * 170) / 1000;
 
-    return Math.ceil(inputSizeKb + outputSizeKb);
+    return inputSizeKb + outputSizeKb;
   };
 
   // Approximate the fee based on number of inputs
   var calculateApproximateFee = function () {
     var feeRateToUse = typeof(feeRate) !== 'undefined' ? feeRate : FEE_PER_KB;
-    return estimateTxSizeKb() * feeRateToUse;
+    return Math.ceil(estimateTxSizeKb() * feeRateToUse);
   };
 
   // Iterate unspents, sum the inputs, and save _inputs with the total
@@ -211,9 +208,11 @@ exports.createTransaction = function(params) {
 
     if (shouldComputeBestFee) {
       var approximateFee = calculateApproximateFee();
-      if (approximateFee > fee) {
-        fee = approximateFee;
-        totalAmount = fee + totalOutputAmount;
+      var shouldRecurse = typeof(fee) === 'undefined' || approximateFee > fee;
+      fee = approximateFee;
+      totalAmount = fee + totalOutputAmount;
+      if (shouldRecurse) {
+        // if fee changed, re-collect inputs
         inputAmount = 0;
         transaction.ins = [];
         return collectInputs();

test/bitgoExpress.js
@@ -192,6 +192,7 @@ describe('Bitgo Express', function() {
           res.body.should.have.property('tx');
           res.body.should.have.property('hash');
           res.body.should.have.property('fee');
+          res.body.should.have.property('feeRate');
           done();
         });
       });

test/wallet.js
@@ -833,7 +833,7 @@ describe('Wallet', function() {
           var feeUsed = result.fee;
           // Note that the transaction size here will be fairly small, because the signatures have not
           // been applied.  But we had to estimate our fees already.
-          assert.equal(feeUsed, 974400);
+          assert.equal(feeUsed, 971421);
           result.feeRate.should.eql(0.000112 * 1e8);
           result.walletId = wallet1.id;
         });
@@ -847,7 +847,7 @@ describe('Wallet', function() {
           var feeUsed = result.fee;
           // Note that the transaction size here will be fairly small, because the signatures have not
           // been applied.  But we had to estimate our fees already.
-          assert.equal(feeUsed, 1740000);
+          assert.equal(feeUsed, 1734681);
         });
       });
 
@@ -867,7 +867,7 @@ describe('Wallet', function() {
         return TransactionBuilder.createTransaction({wallet: wallet1, recipients: recipients, feeTxConfirmTarget: 1})
         .then(function(result) {
           var feeUsed = result.fee;
-          assert.equal(feeUsed, 1200600); // tx size will be 87kb * 0.000138 * 1e8
+          assert.equal(feeUsed, 1196930); // tx size will be 87kb * 0.000138 * 1e8
         });
       });
 
@@ -877,7 +877,7 @@ describe('Wallet', function() {
         return TransactionBuilder.createTransaction({wallet: wallet1, recipients: recipients, feeTxConfirmTarget: 3})
         .then(function(result) {
           var feeUsed = result.fee;
-          assert.equal(feeUsed, 135720); // tx size will be 87kb * 0.0000156 * 1e8
+          assert.equal(feeUsed, 135306); // tx size will be 87kb * 0.0000156 * 1e8
         });
       });
 
@@ -887,7 +887,7 @@ describe('Wallet', function() {
         return TransactionBuilder.createTransaction({wallet: wallet1, recipients: recipients, feeTxConfirmTarget: 4})
         .then(function(result) {
           var feeUsed = result.fee;
-          assert.equal(feeUsed, 870000); // tx size will be 87kb * 0.0001
+          assert.equal(feeUsed, 867341); // tx size will be 87kb * 0.0001
         });
       });
 
@@ -1239,7 +1239,8 @@ describe('Wallet', function() {
           result.should.have.property('tx');
           result.should.have.property('hash');
           result.should.have.property('fee');
-          result.fee.should.equal(0.0001 * 1e8);
+          result.should.have.property('feeRate');
+          result.fee.should.be.lessThan(result.feeRate);
           done();
         }
         );
@@ -1255,7 +1256,8 @@ describe('Wallet', function() {
           result.should.have.property('tx');
           result.should.have.property('hash');
           result.should.have.property('fee');
-          result.fee.should.equal(0.0001 * 1e8);
+          result.should.have.property('feeRate');
+          result.fee.should.be.lessThan(result.feeRate);
           done();
         }
         );

commit ae1fabeb2e8454fcb75705b928c8d60d9da7109b

fix unspents selection target
Summary:
commit 1248530b2c6 introduced a bug where we would get all unspents instead of setting a target
this fixes it

Reviewers: ben

Reviewed By: ben

Subscribers: ben

Differential Revision: https://phabricator.bitgo.com/D1300

src/transactionBuilder.js
@@ -121,7 +121,7 @@ exports.createTransaction = function(params) {
   });
 
   // The total amount needed for this transaction.
-  var totalAmount = fee + totalOutputAmount;
+  var totalAmount = totalOutputAmount + (fee || 0);
 
   // The list of unspent transactions being used in this transaction.
   var unspents;