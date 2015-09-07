36f3a20f273bd82e923e71168c77240e3c6d8547
Ben Chan <benchan@bitgo.com>
2015-09-06 22:28:14 -0700

    example of how to use token auth
    Summary: example of how to use token auth with sending op_return messages
    
    Reviewers: mason
    
    Subscribers: ben
    
    Differential Revision: https://phabricator.bitgo.com/D1569

---
+++ example/transactionOPReturnTokenAuth.js
@@ -0,0 +1,99 @@
+//
+// Advanced transaction with OP_RETURN output
+// This transaction will send out an OP_RETURN message onto the blockchain
+//
+// Copyright 2015, BitGo, Inc.  All Rights Reserved.
+//
+
+var BitGoJS = require('../src/index.js');
+var BitcoinJSLib = require('bitcoinjs-lib');
+
+if (process.argv.length < 6) {
+  console.log("usage:\n\t" + process.argv[0] + " " + process.argv[1] +
+  " <token> <walletId> <walletPassphrase> <message>");
+
+  console.log("token: the developer access token from bitgo.com settings");
+  console.log("walletId: wallet ID (first address on the wallet)");
+  console.log("walletPassphrase: passphrase to decrypt the user key");
+  console.log("message: message to be sent into the blockchain");
+  process.exit(-1);
+}
+
+var token = process.argv[2];
+var walletId = process.argv[3];
+var walletPassphrase = process.argv[4];
+var message = process.argv[5];
+
+var bitgo = new BitGoJS.BitGo({accessToken: token});
+
+var sendBitcoin = function() {
+  console.log("Getting wallet..");
+
+  // Now get the wallet
+  bitgo.wallets().get({id: walletId}, function(err, wallet) {
+    if (err) {
+      console.log("Error getting wallet!");
+      console.dir(err);
+      return process.exit(-1);
+    }
+    console.log("Balance is: " + (wallet.balance() / 1e8).toFixed(4));
+
+    wallet.getEncryptedUserKeychain({}, function(err, keychain) {
+      if (err) {
+        console.log("Error getting encrypted keychain!");
+        console.dir(err);
+        return process.exit(-1);
+      }
+      console.log("Got encrypted user keychain");
+
+      // Decrypt the user key with the passphrase
+      keychain.xprv = bitgo.decrypt({ password: walletPassphrase, input: keychain.encryptedXprv });
+
+      var data = new Buffer(message);
+      var outputScript = BitcoinJSLib.scripts.nullDataOutput(data);
+
+      // Set recipients
+      var recipients = [];
+      recipients.push({ script: outputScript, amount: 0.0001 * 1e8 });
+
+      console.log("Creating transaction");
+      wallet.createTransaction({
+        recipients: recipients
+      },
+      function(err, transaction) {
+        if (err) {
+          console.log("Failed to create transaction!");
+          console.dir(err);
+          return process.exit(-1);
+        }
+        console.dir(transaction);
+        console.log("Signing transaction");
+        wallet.signTransaction({
+          transactionHex: transaction.transactionHex,
+          unspents: transaction.unspents,
+          keychain: keychain
+        },
+        function (err, transaction) {
+          if (err) {
+            console.log("Failed to sign transaction!");
+            console.dir(err);
+            return process.exit(-1);
+          }
+          console.dir(transaction);
+          console.log("Sending transaction");
+          wallet.sendTransaction({tx: transaction.tx}, function (err, callback) {
+            if (err) {
+              console.log("Failed to send transaction to BitGo!");
+              console.dir(err);
+              return process.exit(-1);
+            }
+            console.log("Transaction sent!");
+            console.dir(callback);
+          });
+        });
+      });
+    });
+  });
+};
+
+sendBitcoin();
