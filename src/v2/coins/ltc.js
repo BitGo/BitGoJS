19bc8aa281cda9ef3f0925716e49800dd2ecb319
Arik Sosman <arik@bitgo.com>
2017-06-14 01:24:18 -0700

    fix the litecoin network version
    Reviewers: alex, benchan
    
    Reviewed By: benchan
    
    Subscribers: ben
    
    Differential Revision: https://phabricator.bitgo.com/D5704

--- src/v2/coins/ltc.js
+++ src/v2/coins/ltc.js
@@ -7,7 +7,19 @@ var Ltc = function() {
   // replace the BaseCoin prototype with the local override prototype, which inherits from BaseCoin
   // effectively, move the BaseCoin prototype one level away
   this.__proto__ = Ltc.prototype;
-  this.network = bitcoin.networks.litecoin;
+  this.network = {
+    messagePrefix: '\x19Litecoin Signed Message:\n',
+    bip32: {
+      public: 0x0488b21e,
+      private: 0x0488ade4
+    },
+    pubKeyHash: 0x30,
+    scriptHash: 0x32,
+    wif: 0xb0,
+    dustThreshold: 0, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L360-L365
+    dustSoftThreshold: 100000, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.h#L53
+    feePerKb: 100000 // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L56
+  };
 };
 
 Ltc.prototype.__proto__ = Btc.prototype;
