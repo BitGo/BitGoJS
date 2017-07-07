6bd5456b30b3e7ddd15f7c8257af2cfdc9cd2868
Jameson Lopp <jameson@bitgo.com>
2017-07-07 09:17:44 -0400

    declare rootExtKey var to prevent undefined var errors
    Summary: received a report from wirex that they can't send via web client to due a "rootExtKey is not defined" error. Doubt this will fix it, but good practice nonetheless.
    
    Reviewers: arik, alex
    
    Reviewed By: alex
    
    Subscribers: ben
    
    Differential Revision: https://phabricator.bitgo.com/D5845

--- src/transactionBuilder.js
+++ src/transactionBuilder.js
@@ -757,7 +757,7 @@ exports.signTransaction = function(params) {
 
   var hdPath;
   if (keychain) {
-    rootExtKey = bitcoin.HDNode.fromBase58(keychain.xprv);
+    var rootExtKey = bitcoin.HDNode.fromBase58(keychain.xprv);
     hdPath = bitcoin.hdPath(rootExtKey);
   }
   var txb;
