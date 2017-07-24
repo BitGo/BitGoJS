bc07e1c85f3750fa0d0e6c660ac8fd110947095b
Anton Mishyn <aaxmshn@gmail.com>
2017-07-24 18:06:08 +0300

    fixed compact param

--- src/wallet.js
+++ src/wallet.js
@@ -618,6 +618,12 @@ Wallet.prototype.transactions = function(params, callback) {
     }
     args.push('minHeight=' + params.minHeight);
   }
+  if (params.compact) {
+    if (typeof (params.compact) != 'boolean') {
+      throw new Error('invalid compact argument, expecting boolean');
+    }
+    args.push('compact=' + params.compact);
+  }
   var query = '';
   if (args.length) {
     query = '?' + args.join('&');
