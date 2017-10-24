2894dd25503b10a516429cf3e44b0582e8324261
Arik Sosman <arik@bitgo.com>
2017-10-24 10:10:04 -0700

    add latest config for CI deployment
    Summary: Signed-off-by: Arik Sosman <arik@bitgo.com>
    
    Reviewers: kevin, ron
    
    Reviewed By: ron
    
    Subscribers: ben
    
    Differential Revision: https://phabricator.bitgo.com/D6941

--- src/common.js
+++ src/common.js
@@ -85,6 +85,18 @@ exports.Environments = {
     serverXpub: 'xpub661MyMwAqRbcErFqVXGiUFv9YeoPbhN72UiNCUdj9nj3T6M8h7iKNmbCYpMVWVZP7LA2ma3HWcPngz1gRTm4FPdtm9mHfrNvU93MCoszsGL',
     blockrApiBaseUrl: 'https://tbtc.blockr.io/api/v1'
   },
+  latest: {
+    uri: 'https://latest.bitgo.com',
+    networks: {
+      tbtc: bitcoin.networks.testnet
+    },
+    network: 'testnet',
+    ethNetwork: 'ethereum',
+    rmgNetwork: 'rmgTest',
+    signingAddress: 'msignBdFXteehDEgB6DNm7npRt7AcEZJP3',
+    serverXpub: 'xpub661MyMwAqRbcErFqVXGiUFv9YeoPbhN72UiNCUdj9nj3T6M8h7iKNmbCYpMVWVZP7LA2ma3HWcPngz1gRTm4FPdtm9mHfrNvU93MCoszsGL',
+    blockrApiBaseUrl: 'https://tbtc.blockr.io/api/v1'
+  },
   rmgDev: {
     uri: 'https://rmgwebdev.bitgo.com',
     networks: {
