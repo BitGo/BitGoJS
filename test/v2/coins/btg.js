c67ad17771ed3a15a1d7ff83fee9aa4a528a5c54
Arik Sosman <arik@bitgo.com>
2017-12-13 18:00:20 -0800

    add BTG address generation unit tests
    Summary:
    Copy the LTC address unit tests to BTG, and modify accordingly.
    
    Signed-off-by: Arik Sosman <arik@bitgo.com>
    
    Reviewers: alex, thomas, andrew
    
    Reviewed By: andrew
    
    Subscribers: ben
    
    Differential Revision: https://phabricator.bitgo.com/D7411

--- test/v2/coins/btg.js
+++ test/v2/coins/btg.js
@@ -68,22 +68,23 @@ describe('BTG:', function() {
           txPrebuild: {
             txHex: '010000000144dea5cb05425f94976e887ccba5686a9a12a3f49710b021508d3d9cd8de16b80100000000ffffffff02e803000000000000116a0f426974476f2070327368207465737440a107000000000017a914d039cb3344294a5a384a5508a006444c420cbc118700000000',
             txInfo: {
-              unspents: [{
-                address: '2NCEDmmKNNnqKvnWw7pE3RLzuFe5aHHVy1X',
-                chain: 0,
-                index: 13,
-                value: 504422,
-                txid: 'b816ded89c3d8d5021b01097f4a3129a6a68a5cb7c886e97945f4205cba5de44',
-                redeemScript: '5221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253ae'
-              },
-              {
-                address: '2NCEDmmKNNnqKvnWw7pE3RLzuFe5aHHVy1X',
-                chain: 0,
-                index: 13,
-                value: 504422,
-                txid: 'b816ded89c3d8d5021b01097f4a3129a6a68a5cb7c886e97945f4205cba5de44',
-                redeemScript: '5221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253ae'
-              }
+              unspents: [
+                {
+                  address: '2NCEDmmKNNnqKvnWw7pE3RLzuFe5aHHVy1X',
+                  chain: 0,
+                  index: 13,
+                  value: 504422,
+                  txid: 'b816ded89c3d8d5021b01097f4a3129a6a68a5cb7c886e97945f4205cba5de44',
+                  redeemScript: '5221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253ae'
+                },
+                {
+                  address: '2NCEDmmKNNnqKvnWw7pE3RLzuFe5aHHVy1X',
+                  chain: 0,
+                  index: 13,
+                  value: 504422,
+                  txid: 'b816ded89c3d8d5021b01097f4a3129a6a68a5cb7c886e97945f4205cba5de44',
+                  redeemScript: '5221031cd227e40ad61b4e137109cb2845eb6f5a584ed5c67d9d3135cdaa5045a842ea2103a2e7b54c7b2da0992555353b8e26c6acff4248f4351f08787bf3e2efc94b658321025c2a6cde33c2d73ccf12eecf64c54f08f722c2f073824498950695e9883b141253ae'
+                }
               ]
             }
           },
@@ -167,4 +168,75 @@ describe('BTG:', function() {
     });
 
   });
+
+  describe('Should test address generation', () => {
+
+    const keychains = [
+      {
+        pub: 'xpub661MyMwAqRbcGiQhVk1J7cD1YodF9tc5Y1B8vpTjjB1pcB1J1m1QX8fMtYP2sYqFmW6J2ra69tNoARKjvTGo9cGUrbPbJdjwrSzGGzPzWWS',
+        prv: 'xprv9s21ZrQH143K4ELEPiUHkUGGzmnkkRtEAnFY8S48AqUqjNg9UDh9yLLt3FcfATyCjbsMB9JCGHAD8MeBTAK1P7trFppkoswu5ZAsHYASfbk'
+      },
+      {
+        pub: 'xpub661MyMwAqRbcFzLXuganogQvd7MrefQQqCcJP2ZDumnCdQecf5cw1P1nD5qBz8SNS1yCLSC9VqpNUWnQU3V6qmnPt2r21oXhicQFzPA6Lby',
+        prv: 'xprv9s21ZrQH143K3WG4of3nSYUC55XNFCgZTyghae9cMSFDkcKU7YJgTahJMpdTY9CjCcjgSo2TJ635uUVx176BufUMBFpieKYVJD9J3VvrGRm'
+      },
+      {
+        pub: 'xpub661MyMwAqRbcFHpwWrzPB61U2CgBmdD21WNVM1JKUn9rEExkoGE4yafUVFbPSd78vdX8tWcEUQWaALFkU9fUbUM4Cc49DKEJSCYGRnbzCym',
+        prv: 'xprv9s21ZrQH143K2okUQqTNox4jUAqhNAVAeHStYcthvScsMSdcFiupRnLzdxzfJithak5Zs92FQJeeJ9Jiya63KfUNxawuMZDCp2cGT9cdMKs'
+      }
+    ];
+
+    let coin;
+    before(() => {
+      coin = bitgo.coin('btg');
+    });
+
+    it('should generate standard non-segwit address', () => {
+      const generatedAddress = coin.generateAddress({ keychains });
+      generatedAddress.chain.should.equal(0);
+      generatedAddress.index.should.equal(0);
+      generatedAddress.coinSpecific.outputScript.should.equal('a9141e57a925dd863a86af341037e700862bf66bf7b687');
+      generatedAddress.coinSpecific.redeemScript.should.equal('5221037acffd52bb7c39a4ac3d4c01af33ce0367afec45347e332edca63a38d1fb2e472102658831a87322b3583515ca8725841335505755ada53ee133c70a6b4b8d3978702102641ee6557561c9038242cafa7f538070d7646a969bcf6169f9950abfcfefd6b853ae');
+      generatedAddress.address.should.equal('AJYJw2ZqTEgJEYRGswDUf5CTkeH2D2zFPC');
+    });
+
+    it('should generate custom chain non-segwit address', () => {
+      const generatedAddress = coin.generateAddress({ keychains, chain: 1, index: 113 });
+      generatedAddress.chain.should.equal(1);
+      generatedAddress.index.should.equal(113);
+      generatedAddress.coinSpecific.outputScript.should.equal('a91443457880e5e29555d6ad16bc82ef53891d6512b087');
+      generatedAddress.coinSpecific.redeemScript.should.equal('522103dc94182103c93690c2bca3fe013c19c956b940645b11b0a752e0e56b156bf4e22103b5f4aa0348bf339400ed7e16c6e960a4a46a1ea4c4cbe21abf6d0403161dc4f22103706ff6b11a8d9e3d63a455788d5d96738929ca642f1f3d8f9acedb689e759f3753ae');
+      generatedAddress.address.should.equal('AMua9G6EN3GnxzHAbzzNR5W6XMEQayvF77');
+    });
+
+    it('should generate standard segwit address', () => {
+      const generatedAddress = coin.generateAddress({ keychains, segwit: true });
+      generatedAddress.chain.should.equal(0);
+      generatedAddress.index.should.equal(0);
+      generatedAddress.coinSpecific.outputScript.should.equal('a91426e34781478f08fff903cb70ae67311c3f9bc6a987');
+      generatedAddress.coinSpecific.redeemScript.should.equal('00209a10eb58331e95333f4a6eafd5f03e442e17e0986c824e392642e872f431b7ef');
+      generatedAddress.coinSpecific.witnessScript.should.equal('5221037acffd52bb7c39a4ac3d4c01af33ce0367afec45347e332edca63a38d1fb2e472102658831a87322b3583515ca8725841335505755ada53ee133c70a6b4b8d3978702102641ee6557561c9038242cafa7f538070d7646a969bcf6169f9950abfcfefd6b853ae');
+      generatedAddress.address.should.equal('AKKVbRzGRgDNL5V1MDrF8PHhHLU4n9Vwuq');
+    });
+
+    it('should generate 3/3 non-segwit address', () => {
+      const generatedAddress = coin.generateAddress({ keychains, threshold: 3 });
+      generatedAddress.chain.should.equal(0);
+      generatedAddress.index.should.equal(0);
+      generatedAddress.coinSpecific.outputScript.should.equal('a91476dce7beb23d0e0d53edf5895716d4c80dce609387');
+      generatedAddress.coinSpecific.redeemScript.should.equal('5321037acffd52bb7c39a4ac3d4c01af33ce0367afec45347e332edca63a38d1fb2e472102658831a87322b3583515ca8725841335505755ada53ee133c70a6b4b8d3978702102641ee6557561c9038242cafa7f538070d7646a969bcf6169f9950abfcfefd6b853ae');
+      generatedAddress.address.should.equal('AScN1uBFToY8L8HiGD8Gc1sv8dWdw4NYN1');
+    });
+
+    it('should generate 3/3 custom chain segwit address', () => {
+      const generatedAddress = coin.generateAddress({ keychains, threshold: 3, segwit: true, chain: 20, index: 756 });
+      generatedAddress.chain.should.equal(20);
+      generatedAddress.index.should.equal(756);
+      generatedAddress.coinSpecific.outputScript.should.equal('a91424ba55e2753970236fae8593ca2b49654bf9f4c487');
+      generatedAddress.coinSpecific.redeemScript.should.equal('0020c8fc4f071770e15f21a13ba48c6f32421daed431a74e00e13d0187990964bbce');
+      generatedAddress.coinSpecific.witnessScript.should.equal('532103db7ec7ef3c549705582d6bb5ee258b3bc14d147ec3b069dfd4fd80adb4e9373e210387b1f7cacb6e0c78b79062e94ed0aee691bdfa34a0d1b522103c434205587ad52102044a9f965fd9b54d82e5afe9d4338d0f59027a4e11cff3a39b90fbf5978ae7e753ae');
+      generatedAddress.address.should.equal('AK85CDm4wUcVnsX46becFcXjucn5ptct7F');
+    });
+  });
+
 });
