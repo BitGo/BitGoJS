2672035b625e04e888b41f60d8631ec53a37ae8f
Tyler Levine <tyler@bitgo.com>
2018-01-12 12:57:15 -0800

    Add SDKv2 example demonstrating how to list wallets
    Signed-off-by: Tyler Levine <tyler@bitgo.com>

---
+++ example/v2/list-wallets.js
@@ -0,0 +1,29 @@
+//
+// List all multi-sig wallets at BitGo for the given coin.
+// This makes use of the convenience function wallets().list()
+//
+// This tool will help you see how to use the BitGo API to easily list your 
+// BitGo wallets.
+//
+// Copyright 2018, BitGo, Inc.  All Rights Reserved.
+//
+
+const BitGoJS = require('../../src/index.js');
+const bitgo = new BitGoJS.BitGo({ env: 'test' });
+const Promise = require('bluebird');
+
+// TODO: set your access token here
+const accessToken = null;
+
+const coin = 'tltc';
+
+Promise.coroutine(function *() {
+  bitgo.authenticateWithAccessToken({ accessToken });
+
+  const wallets = yield bitgo.coin(coin).wallets().list({});
+
+  for (const wallet of wallets.wallets) {
+    console.log(`Wallet label: ${wallet.label()}`);
+    console.log(`Wallet ID: ${wallet.id()}`);
+  }
+})();
