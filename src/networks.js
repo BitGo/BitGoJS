0ff91d078c550f4be08bc69c9f991bda85ec509a
David Cruz <davidcruz@bitgo.com>
2018-06-15 11:13:52 -0700

    [BG-5259] Add BCH testnet network properties
    Summary: The BCH testnet does not vary much from the BTC testnet, but the coin property needs to be updated to 'BCH'. This is what caused the SDK BCH transaction tests to fail.
    
    Reviewers: kevin, arik, zacleids
    
    Reviewed By: zacleids
    
    Differential Revision: https://phabricator.bitgo.com/D8907

--- src/networks.js
+++ src/networks.js
@@ -16,6 +16,18 @@ module.exports = {
     coin: coins.BCH,
     forkId: 0x00
   },
+  bitcoincashTestnet: {
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'tb',
+    bip32: {
+      public: 0x043587cf,
+      private: 0x04358394
+    },
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef,
+    coin: coins.BCH
+  },
   zcash: {
     messagePrefix: '\x18ZCash Signed Message:\n',
     bech32: 'bc',
