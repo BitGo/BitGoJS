54939eeab630e7bd8f8067566f1cf83ec59069c0
Tyler Levine <tyler@bitgo.com>
2018-10-05 09:28:55 -0700

    BG-7908: Convert test results to numbers instead of comparing strings
    Summary:
    When the leading digit of inputArr is 0, it is dropped from
    the string representation. This causes test failures with probability
    exactly 1/16.
    
    Signed-off-by: Tyler Levine <tyler@bitgo.com>
    
    Reviewers: bigly, zacleids, danny, leo
    
    Reviewed By: danny, leo
    
    Subscribers: tyler
    
    Differential Revision: https://phabricator.bitgo.com/D9993

--- test/v2/unit/internal.js
+++ test/v2/unit/internal.js
@@ -1,3 +1,5 @@
+require('should');
+
 const { randomBytes } = require('crypto');
 const { bytesToWord } = require('../../../src/v2/internal');
 
@@ -43,11 +45,13 @@ describe('Internal:', () => {
       res.should.equal(Math.pow(2, 16));
     });
 
-    it('should convert a random number', () => {
-      const inputArr = randomBytes(4);
-
-      const res = bytesToWord(inputArr);
-      res.toString(16).should.equal(inputArr.toString('hex'));
+    it('should convert 1000 random numbers', () => {
+      for (let i = 0; i < 1000; i++) {
+        const inputArr = randomBytes(4);
+        const resStr = bytesToWord(inputArr).toString(16);
+        const arrStr = inputArr.toString('hex');
+        parseInt(resStr, 16).should.equal(parseInt(arrStr, 16));
+      }
     });
   });
 });
