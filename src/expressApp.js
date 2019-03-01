bd2a26fe16f12a0f8c2578456626a1df3c10e45e
Danny Diekroeger <danny@bitgo.com>
2019-03-01 14:09:21 -0800

    BG-11092 proxy unblock oauth/token route

--- src/expressApp.js
+++ src/expressApp.js
@@ -117,7 +117,7 @@ function configureProxy(app, { env }) {
   });
 
   app.use(function(req, res) {
-    if (req.url && /^\/api\/v[12]\/.*$/.test(req.url)) {
+    if (req.url && (/^\/api\/v[12]\/.*$/.test(req.url) || /^\/oauth\/token.*$/.test(req.url))) {
       req.isProxy = true;
       proxy.web(req, res, { target: common.Environments[env].uri, changeOrigin: true });
       return;
