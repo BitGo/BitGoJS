12b13b92ab80c3991aba9e7e959bea836d489aed
Tyler Levine <tyler@bitgo.com>
2019-06-05 16:14:36 -0700

    CT-200: Fix package versions, remove package-lock.json for core module

--- modules/express/scripts/update-package-lock.js
+++ modules/express/scripts/update-package-lock.js
@@ -8,6 +8,4 @@ const pkg = require('../package.json');
 lockfile.version = pkg.version;
 
 const file = path.join(path.dirname(require.main.path), 'package-lock.json')
-console.dir(file);
-console.dir(lockfile.version);
 fs.writeFileSync(file, JSON.stringify(lockfile, undefined, 2) + "\n");
