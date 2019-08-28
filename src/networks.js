51a31086c46bb00933dfde398f931bc73eb02bba
Jorge <jorge@bitgo.com>
2019-08-28 13:32:25 -0700

    Update Zcash tesnet branch id (#47)

--- src/networks.js
+++ src/networks.js
@@ -109,7 +109,8 @@ module.exports = {
       1: 0x00,
       2: 0x00,
       3: 0x5ba81b19,
-      4: 0x76b809bb
+      // 4: 0x76b809bb (old Sapling branch id)
+      4: 0x2bb40e60
     },
     coin: coins.ZEC
   },
