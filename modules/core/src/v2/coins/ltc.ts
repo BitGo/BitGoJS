cd4ac63332fee5fdbcedbbaa2ae4d04fba656bf5
Tyler Levine <tyler@bitgo.com>
2019-08-28 14:48:53 -0700

    CT-942: Respond to review comments

--- modules/core/src/v2/coins/ltc.ts
+++ modules/core/src/v2/coins/ltc.ts
@@ -2,11 +2,10 @@ import { BitGo } from '../../bitgo';
 import { InvalidAddressError } from '../../errors';
 import { AbstractUtxoCoin } from './abstractUtxoCoin';
 import { BaseCoin } from '../baseCoin';
-import * as bitcoin from 'bitgo-utxo-lib';
 import * as Bluebird from 'bluebird';
-const co = Bluebird.coroutine;
 import * as common from '../../common';
-const request = require('superagent');
+import * as request from 'superagent';
+const co = Bluebird.coroutine;
 
 export class Ltc extends AbstractUtxoCoin {
   constructor(bitgo: BitGo, network?) {
@@ -15,7 +14,7 @@ export class Ltc extends AbstractUtxoCoin {
       messagePrefix: '\x19Litecoin Signed Message:\n',
       bip32: {
         public: 0x0488b21e,
-        private: 0x0488ade4
+        private: 0x0488ade4,
       },
       bech32: 'ltc',
       pubKeyHash: 0x30,
@@ -24,10 +23,10 @@ export class Ltc extends AbstractUtxoCoin {
       dustThreshold: 0, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L360-L365
       dustSoftThreshold: 100000, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.h#L53
       feePerKb: 100000, // https://github.com/litecoin-project/litecoin/blob/v0.8.7.2/src/main.cpp#L56
-      coin: 'ltc'
+      coin: 'ltc',
     });
     // use legacy script hash version, which is the current Bitcoin one
-    this.altScriptHash = bitcoin.networks.bitcoin.scriptHash;
+    this.altScriptHash = this.getCoinLibrary().networks.bitcoin.scriptHash;
     // do not support alt destinations in prod
     this.supportAltScriptDestination = false;
   }
@@ -73,14 +72,14 @@ export class Ltc extends AbstractUtxoCoin {
 
     try {
       // try deserializing as bech32
-      bitcoin.address.fromBech32(address);
+      this.getCoinLibrary().address.fromBech32(address);
       // address may be all uppercase, but canonical bech32 addresses are all lowercase
       return address.toLowerCase();
     } catch (e) {
       // not a valid bech32, try to decode as base58
     }
 
-    const addressDetails = bitcoin.address.fromBase58Check(address);
+    const addressDetails = this.getCoinLibrary().address.fromBase58Check(address);
     if (addressDetails.version === this.network.pubKeyHash) {
       // the pub keys never changed
       return address;
@@ -96,11 +95,11 @@ export class Ltc extends AbstractUtxoCoin {
       2: this.network.scriptHash
     };
     const newScriptHash = scriptHashMap[scriptHashVersion];
-    return bitcoin.address.toBase58Check(addressDetails.hash, newScriptHash);
+    return this.getCoinLibrary().address.toBase58Check(addressDetails.hash, newScriptHash);
   }
 
   calculateRecoveryAddress(scriptHashScript: Buffer): string {
-    const bitgoAddress = bitcoin.address.fromOutputScript(scriptHashScript, this.network);
+    const bitgoAddress = this.getCoinLibrary().address.fromOutputScript(scriptHashScript, this.network);
     const blockrAddress = this.canonicalAddress(bitgoAddress, 1);
     return blockrAddress;
   }
--- modules/core/test/v2/unit/coins/ltc.ts
+++ modules/core/test/v2/unit/coins/ltc.ts
@@ -21,62 +21,93 @@ describe('LTC:', function() {
   });
 
   describe('Canonicalize address', function() {
+
     it('base58 mainnet address', function() {
-      const oldAddress = '3GBygsGPvTdfKMbq4AKZZRu1sPMWPEsBfd';
-      const newAddress = ltc.canonicalAddress(oldAddress, 2);
-      newAddress.should.equal('MNQ7zkgMsaV67rsjA3JuP59RC5wxRXpwgE');
-      const sameAddress = ltc.canonicalAddress(oldAddress, 1);
-      oldAddress.should.equal(sameAddress);
-      const newOldAddress = ltc.canonicalAddress(newAddress, 1);
-      oldAddress.should.equal(newOldAddress);
+      const standardBase58Address = '3GBygsGPvTdfKMbq4AKZZRu1sPMWPEsBfd';
+      const litecoinBase58Address = 'MNQ7zkgMsaV67rsjA3JuP59RC5wxRXpwgE';
+
+      // convert from new format to old
+      const downgradedAddress = ltc.canonicalAddress(litecoinBase58Address, 1);
+      downgradedAddress.should.equal(standardBase58Address);
+
+      // convert from new format to new (no-op)
+      const unmodifiedAddress = ltc.canonicalAddress(litecoinBase58Address, 2);
+      unmodifiedAddress.should.equal(litecoinBase58Address);
+
+      // convert from old format to new
+      const upgradedAddress = ltc.canonicalAddress(standardBase58Address, 2);
+      upgradedAddress.should.equal(litecoinBase58Address);
     });
 
     it('base58 testnet address', function() {
-      const newAddress = 'QLc2RwpX2rFtZzoZrexLibcAgV6Nsg74Jn';
-      const oldAddress = tltc.canonicalAddress(newAddress, 1);
-      oldAddress.should.equal('2MsFGJvxH1kCoRp3XEYvKduAjY6eYz9PJHz');
-      const sameAddress = tltc.canonicalAddress(newAddress, 2);
-      newAddress.should.equal(sameAddress);
-      const newNewAddress = tltc.canonicalAddress(oldAddress, 2);
-      newAddress.should.equal(newNewAddress);
+      const standardBase58Address = '2MsFGJvxH1kCoRp3XEYvKduAjY6eYz9PJHz';
+      const litecoinBase58Address = 'QLc2RwpX2rFtZzoZrexLibcAgV6Nsg74Jn';
+
+      // convert from new format to old
+      const downgradedAddress = tltc.canonicalAddress(litecoinBase58Address, 1);
+      downgradedAddress.should.equal(standardBase58Address);
+
+      // convert from new format to new (no-op)
+      const unmodifiedAddress = tltc.canonicalAddress(litecoinBase58Address, 2);
+      unmodifiedAddress.should.equal(litecoinBase58Address);
+
+      // convert from old format to new
+      const upgradedAddress = tltc.canonicalAddress(standardBase58Address, 2);
+      upgradedAddress.should.equal(litecoinBase58Address);
     });
 
     it('bech32 mainnet address', function() {
-      // canonicalAddress is a no-op for bech32 addresses - they are already in canonical format,
-      // and the script hash version is not relevant
+      // canonicalAddress will only lower case bech32 addresses - they are already
+      // in canonical format, and the script hash version is not relevant
       const bech32Address = 'ltc1qgrl8zpndsklaa9swgd5vevyxmx5x63vcrl7dk4';
-      const newAddress = ltc.canonicalAddress(bech32Address, 2);
-      newAddress.should.equal(bech32Address);
+      const version1Address = ltc.canonicalAddress(bech32Address, 1);
+      version1Address.should.equal(bech32Address);
+      const version2Address = ltc.canonicalAddress(bech32Address, 2);
+      version2Address.should.equal(bech32Address);
     });
 
     it('upper case bech32 mainnet address', function() {
-      // casing is not relevant for bech32 addresses, but canonical bech32 addresses should be lowercase
+      // bech32 addresses which are all upper case or all lower case are potentially valid,
+      // but mixed case is always invalid. Canonical bech32 addresses should be all lower case however
       const bech32Address = 'LTC1QGRL8ZPNDSKLAA9SWGD5VEVYXMX5X63VCRL7DK4';
       const newAddress = ltc.canonicalAddress(bech32Address);
       newAddress.should.equal(bech32Address.toLowerCase());
     });
 
     it('bech32 testnet address', function() {
-      const newAddress = 'tltc1qu78xur5xnq6fjy83amy0qcjfau8m367defyhms';
-      const oldAddress = tltc.canonicalAddress(newAddress, 1);
-      oldAddress.should.equal(newAddress);
-      const sameAddress = tltc.canonicalAddress(newAddress, 2);
-      newAddress.should.equal(sameAddress);
-      const newNewAddress = tltc.canonicalAddress(oldAddress, 2);
-      newAddress.should.equal(newNewAddress);
+      const bech32Address = 'tltc1qu78xur5xnq6fjy83amy0qcjfau8m367defyhms';
+      const version1Address = tltc.canonicalAddress(bech32Address, 1);
+      version1Address.should.equal(bech32Address);
+      const version2Address = tltc.canonicalAddress(bech32Address, 2);
+      version2Address.should.equal(bech32Address);
     });
   });
 
   describe('should validate addresses', () => {
     it('should validate base58 addresses', () => {
+      // known valid main and testnet base58 address are valid
       ltc.isValidAddress('MH6J1PzpsAfapZek7QGHv2mheUxnP8Kdek').should.be.true();
       tltc.isValidAddress('QWC1miKKHFikbwg2iyt8KZBGsTSEBKr21i').should.be.true();
+
+      // malformed base58 addresses are invalid
       ltc.isValidAddress('MH6J1PzpsAfapZek7QGHv2mheUxnP8Kder').should.be.false();
       tltc.isValidAddress('QWC1miKKHFikbwg2iyt8KZBGsTSEBKr21l').should.be.false();
     });
+
     it('should validate bech32 addresses', () => {
+      // all lower case is valid
       ltc.isValidAddress('ltc1qq7fzt3ek5ege3v92wh0q6wzcjr39pqswlpe36mu28f6yufark3wspfryg7').should.be.true();
       tltc.isValidAddress('tltc1qq7fzt3ek5ege3v92wh0q6wzcjr39pqswlpe36mu28f6yufark3ws2x86ht').should.be.true();
+
+      // all upper case is also valid
+      ltc.isValidAddress('LTC1QQ7FZT3EK5EGE3V92WH0Q6WZCJR39PQSWLPE36MU28F6YUFARK3WSPFRYG7').should.be.true();
+      tltc.isValidAddress('TLTC1QQ7FZT3EK5EGE3V92WH0Q6WZCJR39PQSWLPE36MU28F6YUFARK3WS2X86HT').should.be.true();
+
+      // mixed case is invalid
+      ltc.isValidAddress('LTC1QQ7FZT3EK5EGE3V92WH0Q6WZCJR39PQSWLPE36MU28F6YUFARK3WSPFRYg7').should.be.false();
+      tltc.isValidAddress('TLTC1QQ7FZT3EK5EGE3V92WH0Q6WZCJR39PQSWLPE36MU28F6YUFARK3WS2X86Ht').should.be.false();
+
+      // malformed addresses are invalid
       ltc.isValidAddress('ltc1qq7fzt3ek5ege3v92wh0q6wzcjr39pqswlpe36mu28f6yufark3wspfryg9').should.be.false();
       tltc.isValidAddress('tltc1qq7fzt3ek5ege3v92wh0q6wzcjr39pqswlpe36mu28f6yufark3ws2x86hl').should.be.false();
     });
