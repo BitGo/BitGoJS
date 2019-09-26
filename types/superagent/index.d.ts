276aace99db178e581984f1ad027d603fd6c994f
Tyler Levine <tyler@bitgo.com>
2019-09-26 14:50:46 -0700

    SDKT-32: Improve usability for typescript dependents
    * Add @types/bluebird and @types/superagent to the deps list from dev deps,
      since these types are exported by the BitGoJS public API
    * Export BitGo object interfaces
    * Return custom `BitGoRequest` instead of raw superagent Request.
      Methods patched onto the superagent Request object will be declared on
      this interface instead of augmenting superagent.Request.
    * Separate source files from type declarations in dist/
    
    This commit covers the following tickets:
    * SDKT-32
    * SDKT-33
    * SDKT-31

--- types/superagent/index.d.ts
+++ types/superagent/index.d.ts
@@ -5,8 +5,6 @@ declare module 'superagent' {
   interface Request {
     result: (optionalField?: string) => bluebird<any>;
     proxy: (proxyUrl: string) => this;
-    // can't redefine return type of end() ...  makes sense
-    // end: (callback?: NodeCallback<superagent.Response>) => Bluebird<superagent.Response>;
     verifyResponse: (response: superagent.Response) => superagent.Response;
     forceV1Auth: boolean;
     authenticationToken?: string;
