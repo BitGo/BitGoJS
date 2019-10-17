37420b269b7caae6f32a30793e675fc328ec8bc6
Gloria Wang <gloria@bitgo.com>
2019-10-17 13:00:41 -0700

    TRON-43: Explain a Tron transaction from txHex * Deserialize a txHex to extract information to display to customer   before a tx is sent

--- modules/core/test/v2/unit/coins/trx.ts
+++ modules/core/test/v2/unit/coins/trx.ts
@@ -1,6 +1,8 @@
 import * as Promise from 'bluebird';
 import * as should from 'should';
 import { Trx } from '../../../../src/v2/coins/trx';
+import * as bitgoAccountLib from '@bitgo/account-lib';
+import { mockTx } from '../../fixtures/coins/trx';
 
 const co = Promise.coroutine;
 import { TestBitGo } from '../../../lib/test_bitgo';
@@ -19,6 +21,55 @@ describe('TRON:', function() {
     const basecoin = bitgo.coin('trx');
     basecoin.should.be.an.instanceof(Trx);
   });
+
+  it('explain a txHex', co(function *() {
+    const txHex = JSON.stringify(mockTx);
+    const explainParams = {
+      txHex,
+      feeInfo: { fee: 1 },
+      txID: mockTx.txID,
+    };
+    const explanation = yield basecoin.explainTransaction(explainParams);
+    const toAddress = bitgoAccountLib.Trx.Utils.getBase58AddressFromHex(mockTx.raw_data.contract[0].parameter.value.to_address);
+    explanation.id.should.equal(mockTx.txID);
+    explanation.outputs.amount.should.equal('10');
+    explanation.outputAmount.should.equal('10');
+    explanation.changeAmount.should.equal(0);
+    explanation.changeOutputs.length.should.equal(0);
+    explanation.fee.should.equal(1);
+    explanation.expiration.should.equal(mockTx.raw_data.expiration);
+    explanation.timestamp.should.equal((mockTx.raw_data.timestamp));
+    explanation.outputs.address.should.equal(toAddress);
+  }));
+
+  it('should throw if the params object is missing parameters', co(function *() {
+    const explainParams = {
+      feeInfo: { fee: 1 },
+      txID: mockTx.txID,
+      txHex: null,
+    };
+    yield basecoin.explainTransaction(explainParams).should.be.rejectedWith('missing explain tx parameters');
+  }));
+
+  it('explain an half-signed/fully signed transaction', co(function *() {
+    const txHex = JSON.stringify(mockTx);
+    const explainParams = {
+      halfSigned: { txHex },
+      feeInfo: { fee: 1 },
+      txID: mockTx.txID,
+    };
+    const explanation = yield basecoin.explainTransaction(explainParams);
+    const toAddress = bitgoAccountLib.Trx.Utils.getBase58AddressFromHex(mockTx.raw_data.contract[0].parameter.value.to_address);
+    explanation.id.should.equal(mockTx.txID);
+    explanation.outputs.amount.should.equal('10');
+    explanation.outputAmount.should.equal('10');
+    explanation.changeAmount.should.equal(0);
+    explanation.changeOutputs.length.should.equal(0);
+    explanation.fee.should.equal(1);
+    explanation.expiration.should.equal(mockTx.raw_data.expiration);
+    explanation.timestamp.should.equal((mockTx.raw_data.timestamp));
+    explanation.outputs.address.should.equal(toAddress);
+  }));
 
   describe('Keypairs:', () => {
     it('should generate a keypair from random seed', function() {
