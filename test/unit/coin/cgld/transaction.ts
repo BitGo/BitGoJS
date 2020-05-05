409014b1b3158fccd5662eacec7e608a2fb313e1
Gonzalo Alvarez <gonzaalvarez70@gmail.com>
2020-05-05 10:19:31 -0300

    Moved big strings to resources file to cleanup tests.
    Ticket: BGX-45

--- test/unit/coin/cgld/transaction.ts
+++ test/unit/coin/cgld/transaction.ts
@@ -1,18 +1,7 @@
 import should from 'should';
 import { coins } from '@bitgo/statics';
 import { Transaction } from '../../../../src/coin/cgld/transaction';
-import { KeyPair } from '../../../../src/coin/cgld';
-
-const TXDATA = {
-  nonce: 1,
-  data: '0x111111111111111111111111',
-  gasLimit: 12000,
-  gasPrice: 10,
-  chainId: 44786,
-};
-const TEST_KEYPAIR = new KeyPair({ prv: '8CAA00AE63638B0542A304823D66D96FF317A576F692663DB2F85E60FAB2590C' });
-const ENCODED_TRANSACTION =
-  '0xf85d010a822ee080808080808c11111111111111111111111183015e07a0cfdddecd7bd8aba329b6ad47deaeb0384815540a7c5f45a52bc159eef71f4389a00b195d91de4e57e14c91482e4b6655ada71e188997a3f149460d0b5d252fb300';
+import * as testData from '../../../resources/cgld/cgld';
 
 describe('Celo Transaction', function() {
   describe('should throw empty transaction', function() {
@@ -27,8 +16,8 @@ describe('Celo Transaction', function() {
 
   describe('should return valid transaction', function() {
     const tx = new Transaction(coins.get('cgld'));
-    tx.setTransactionData(TXDATA);
-    should.equal(tx.toJson(), TXDATA);
+    tx.setTransactionData(testData.TXDATA);
+    should.equal(tx.toJson(), testData.TXDATA);
     should.throws(() => {
       tx.toBroadcastFormat();
     });
@@ -37,21 +26,21 @@ describe('Celo Transaction', function() {
   describe('should sign', function() {
     it('invalid', function() {
       const tx = new Transaction(coins.get('cgld'));
-      return tx.sign(TEST_KEYPAIR).should.be.rejected();
+      return tx.sign(testData.KEYPAIR_PRV).should.be.rejected();
     });
     it('valid', function() {
       const tx = new Transaction(coins.get('cgld'));
-      tx.setTransactionData(TXDATA);
-      return tx.sign(TEST_KEYPAIR).should.be.fulfilled();
+      tx.setTransactionData(testData.TXDATA);
+      return tx.sign(testData.KEYPAIR_PRV).should.be.fulfilled();
     });
   });
 
   describe('should return encoded tx', function() {
     it('valid sign', async function() {
       const tx = new Transaction(coins.get('cgld'));
-      tx.setTransactionData(TXDATA);
-      await tx.sign(TEST_KEYPAIR);
-      should.equal(tx.toBroadcastFormat(), ENCODED_TRANSACTION);
+      tx.setTransactionData(testData.TXDATA);
+      await tx.sign(testData.KEYPAIR_PRV);
+      should.equal(tx.toBroadcastFormat(), testData.ENCODED_TRANSACTION);
     });
   });
 });
--- test/unit/coin/etc/transaction.ts
+++ test/unit/coin/etc/transaction.ts
@@ -1,18 +1,7 @@
 import should from 'should';
 import { coins } from '@bitgo/statics';
 import { Transaction } from '../../../../src/coin/etc/transaction';
-import { KeyPair } from '../../../../src/coin/etc';
-
-const TXDATA = {
-  nonce: 1,
-  data: '0x111111111111111111111111',
-  gasLimit: 12000,
-  gasPrice: 10,
-  chainId: 44786,
-};
-const TEST_KEYPAIR = new KeyPair({ prv: '8CAA00AE63638B0542A304823D66D96FF317A576F692663DB2F85E60FAB2590C' });
-const ENCODED_TRANSACTION =
-  '0xf858010a822ee080808c11111111111111111111111181a1a009c5dbd33801c8ae0f44429a02fc77ed2232ac1d9db88fe11aeb83659aa2c933a0069ee40c549c37b4f745a2f6c10b3d394dbb6e2d9821ac4fcc3e2782c6712ef5';
+import * as testData from '../../../resources/etc/etc';
 
 describe('Etc Transaction', function() {
   describe('should throw empty transaction', function() {
@@ -27,8 +16,8 @@ describe('Etc Transaction', function() {
 
   describe('should return valid transaction', function() {
     const tx = new Transaction(coins.get('etc'));
-    tx.setTransactionData(TXDATA);
-    should.equal(tx.toJson(), TXDATA);
+    tx.setTransactionData(testData.TXDATA);
+    should.equal(tx.toJson(), testData.TXDATA);
     should.throws(() => {
       tx.toBroadcastFormat();
     });
@@ -37,21 +26,21 @@ describe('Etc Transaction', function() {
   describe('should sign', function() {
     it('invalid', function() {
       const tx = new Transaction(coins.get('etc'));
-      return tx.sign(TEST_KEYPAIR).should.be.rejected();
+      return tx.sign(testData.KEYPAIR_PRV).should.be.rejected();
     });
     it('valid', function() {
       const tx = new Transaction(coins.get('etc'));
-      tx.setTransactionData(TXDATA);
-      return tx.sign(TEST_KEYPAIR).should.be.fulfilled();
+      tx.setTransactionData(testData.TXDATA);
+      return tx.sign(testData.KEYPAIR_PRV).should.be.fulfilled();
     });
   });
 
   describe('should return encoded tx', function() {
     it('valid sign', async function() {
       const tx = new Transaction(coins.get('etc'));
-      tx.setTransactionData(TXDATA);
-      await tx.sign(TEST_KEYPAIR);
-      should.equal(tx.toBroadcastFormat(), ENCODED_TRANSACTION);
+      tx.setTransactionData(testData.TXDATA);
+      await tx.sign(testData.KEYPAIR_PRV);
+      should.equal(tx.toBroadcastFormat(), testData.ENCODED_TRANSACTION);
     });
   });
 });
