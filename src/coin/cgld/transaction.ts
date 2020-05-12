afe3194ddef8223816ca832ee43dfbd18414b251
Fabian Esslinger <fesslinger@simtlix.com>
2020-05-12 19:26:05 -0300

    first implementation with new structure

--- src/coin/cgld/transaction.ts
+++ src/coin/cgld/transaction.ts
@@ -1,34 +1,9 @@
 import { BaseCoin as CoinConfig } from '@bitgo/statics';
 import { Eth } from '../../index';
-import { InvalidTransactionError } from '../baseCoin/errors';
-import { KeyPair, Utils } from './';
+import { TxData } from '../eth/iface';
 
 export class Transaction extends Eth.Transaction {
-  private _encodedTransaction?: string;
-
-  constructor(_coinConfig: Readonly<CoinConfig>) {
-    super(_coinConfig);
-  }
-
-  /**
-   * Sign the transaction with the provided key. It does not check if the signer is allowed to sign
-   * it or not.
-   *
-   * @param {KeyPair} keyPair The key to sign the transaction with
-   */
-  async sign(keyPair: KeyPair): Promise<void> {
-    // Check if there is a transaction to sign
-    if (!this._ethTransaction) {
-      throw new InvalidTransactionError('Empty transaction');
-    }
-    this._encodedTransaction = await Utils.sign(this._ethTransaction.toJson(), keyPair);
-  }
-
-  /** @inheritdoc */
-  toBroadcastFormat(): any {
-    if (!this._encodedTransaction) {
-      return super.toBroadcastFormat();
-    }
-    return this._encodedTransaction;
+  constructor(_coinConfig: Readonly<CoinConfig>, txData?: TxData) {
+    super(_coinConfig, txData);
   }
 }
