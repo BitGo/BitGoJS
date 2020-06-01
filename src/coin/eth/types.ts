3fc2a8cb167c4fef2ad6b302e31075cdec3c7641
Gonzalo Alvarez <gonzaalvarez70@gmail.com>
2020-06-01 17:53:56 -0300

    Added a single object for optional transaction parameters
    Ticket: BGA-129

--- src/coin/eth/types.ts
+++ src/coin/eth/types.ts
@@ -8,7 +8,7 @@ import { KeyPair } from './keyPair';
  * An Ethereum transaction with helpers for serialization and deserialization.
  */
 export class EthTransactionData implements EthLikeTransactionData {
-  constructor(private tx: EthereumTx, protected deployedAddress?: string, protected chainId?: string) {}
+  constructor(private tx: EthereumTx, protected args?: { deployedAddress?: string; chainId?: string }) {}
 
   /**
    * Build an ethereum transaction from its JSON representation
@@ -28,8 +28,7 @@ export class EthTransactionData implements EthLikeTransactionData {
         r: tx.r,
         s: tx.s,
       }),
-      tx.deployedAddress,
-      addHexPrefix(new BigNumber(Number(tx.chainId)).toString(16)),
+      { deployedAddress: tx.deployedAddress, chainId: addHexPrefix(new BigNumber(Number(tx.chainId)).toString(16)) },
     );
   }
 
@@ -69,12 +68,14 @@ export class EthTransactionData implements EthLikeTransactionData {
       result.s = bufferToHex(this.tx.s);
     }
 
-    if (this.chainId) {
-      result.chainId = this.chainId;
-    }
+    if (this.args) {
+      if (this.args.chainId) {
+        result.chainId = this.args.chainId;
+      }
 
-    if (this.deployedAddress) {
-      result.deployedAddress = this.deployedAddress;
+      if (this.args.deployedAddress) {
+        result.deployedAddress = this.args.deployedAddress;
+      }
     }
 
     return result;
