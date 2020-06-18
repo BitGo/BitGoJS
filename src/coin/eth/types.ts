5867a03e15f055309f5d8d860065910f2ac2489f
Fabian Esslinger <fesslinger@simtlix.com>
2020-06-18 18:13:25 -0300

    BGA-212 include signatures when tx id is calculated

--- src/coin/eth/types.ts
+++ src/coin/eth/types.ts
@@ -54,7 +54,7 @@ export class EthTransactionData implements EthLikeTransactionData {
       gasLimit: new BigNumber(bufferToHex(this.tx.gasLimit), 16).toString(10),
       value: this.tx.value.length === 0 ? '0' : new BigNumber(bufferToHex(this.tx.value), 16).toString(10),
       data: bufferToHex(this.tx.data),
-      id: addHexPrefix(bufferToHex(this.tx.hash())),
+      id: addHexPrefix(bufferToHex(this.tx.hash(true))),
     };
 
     if (this.tx.to && this.tx.to.length) {
