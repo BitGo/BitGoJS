84fe82d156b07112b4a714b36a0e0c979be0f4db
Gonzalo Alvarez <gonzaalvarez70@gmail.com>
2020-07-17 16:23:49 -0300

    Removed eslint tsconfig to prevent errors.

--- modules/account-lib/.eslintrc.js
+++ modules/account-lib/.eslintrc.js
@@ -13,7 +13,6 @@ module.exports =  {
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
     sourceType:  'module',  // Allows for the use of imports
-    project: '../../tsconfig.json',
   },
   rules:  {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
