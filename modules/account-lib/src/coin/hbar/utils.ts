13fcda43942d9aedebedf9e3bf82cf3da4491b99
Jorge Valdeiglesias <jorge@bitgo.com>
2020-07-21 23:48:34 -0700

    BG-23023 Add HBAR Utils
    Add validation utils methods for Hedera.
    
    TICKET: BG-23023

---
+++ modules/account-lib/src/coin/hbar/utils.ts
@@ -0,0 +1,42 @@
+import * as _ from 'lodash';
+
+import { AccountId } from '@hashgraph/sdk/lib/account/AccountId';
+import { TransactionId } from '@hashgraph/sdk';
+
+/**
+ * Returns whether or not the string is a valid Hedera account.
+ *
+ * In any form, `shard` and `realm` are assumed to be 0 if not provided.
+ *
+ * @param {string} address - the  address to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidAddress(address: string): boolean {
+  if (_.isEmpty(address)) {
+    return false;
+  }
+  try {
+    const acc = AccountId.fromString(address);
+    return !_.isNaN(acc.account);
+  } catch (e) {
+    return false;
+  }
+}
+
+/**
+ * Returns whether or not the string is a valid Hedera transaction id or not
+ *
+ * @param {string} txId - the transaction id to be validated
+ * @returns {boolean} - the validation result
+ */
+export function isValidTransactionHash(txId: string): boolean {
+  if (_.isEmpty(txId)) {
+    return false;
+  }
+  try {
+    const tx = TransactionId.fromString(txId);
+    return !_.isNaN(tx.accountId.account);
+  } catch (e) {
+    return false;
+  }
+}
