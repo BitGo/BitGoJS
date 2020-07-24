157d3555c894e2d421def4f1e75a2e9f4cb6846a
Thomas Taylor <ttaylor@bitgo.com>
2020-07-24 00:47:32 -0700

    Add hedera hashgraph skeleton to core module
    Ticket: BG-23027

---
+++ modules/core/src/v2/coins/thbar.ts
@@ -0,0 +1,20 @@
+/**
+ * @prettier
+ */
+import { BaseCoin } from '../baseCoin';
+import { BitGo } from '../../bitgo';
+import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
+import { Hbar } from './hbar';
+
+/**
+ * Tezos testnet.
+ */
+export class Thbar extends Hbar {
+  protected constructor(bitgo: BitGo, staticsCoin?: Readonly<StaticsBaseCoin>) {
+    super(bitgo, staticsCoin);
+  }
+
+  static createInstance(bitgo: BitGo, staticsCoin?: Readonly<StaticsBaseCoin>): BaseCoin {
+    return new Thbar(bitgo, staticsCoin);
+  }
+}
