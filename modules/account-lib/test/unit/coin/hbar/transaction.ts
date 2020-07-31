73f1b1cb3637f67f4c845ed63b6128c51ee074a5
Ismael <ipelaez@simtlix.com>
2020-07-31 16:47:23 -0300

    BGA-286 Add transaction sign unit test

---
+++ modules/account-lib/test/unit/coin/hbar/transaction.ts
@@ -0,0 +1,72 @@
+import should from 'should';
+import { coins } from '@bitgo/statics';
+import { Transaction } from '../../../../src/coin/hbar/transaction';
+import * as testData from '../../../resources/hbar/hbar';
+import { KeyPair } from '../../../../src/coin/hbar/keyPair';
+
+describe('Hbar Transaction', () => {
+  const coin = coins.get('thbar');
+
+  function getTransaction(): Transaction {
+    return new Transaction(coin);
+  }
+
+  it('should throw empty transaction', () => {
+    const tx = getTransaction();
+    should.throws(() => {
+      tx.toJson();
+    });
+    should.throws(() => {
+      tx.toBroadcastFormat();
+    });
+  });
+
+  describe('should sign', () => {
+    it('invalid', function() {
+      const tx = getTransaction();
+      return tx.sign(testData.INVALID_KEYPAIR_PRV).should.be.rejected();
+    });
+
+    it('valid', async () => {
+      const tx = getTransaction();
+      tx.bodyBytes(testData.WALLET_TXDATA);
+      const keypair = new KeyPair({ prv: testData.ACCOUNT_1.privateKey });
+      await tx.sign(keypair).should.be.fulfilled();
+      should.equal(
+        new Buffer(tx.hederaTx.sigMap!.sigPair![0].pubKeyPrefix!).toString('hex'),
+        testData.ACCOUNT_1.publicKey.slice(24),
+      );
+    });
+
+    it('multiple valid', async () => {
+      const tx = getTransaction();
+      tx.bodyBytes(testData.WALLET_TXDATA);
+      const keypair = new KeyPair({ prv: testData.ACCOUNT_1.privateKey });
+      const keypair2 = new KeyPair({ prv: testData.OPERATOR.privateKey });
+      await tx.sign(keypair).should.be.fulfilled();
+      should.equal(
+        new Buffer(tx.hederaTx.sigMap!.sigPair![0].pubKeyPrefix!).toString('hex'),
+        testData.ACCOUNT_1.publicKey.slice(24),
+      );
+      await tx.sign(keypair2).should.be.fulfilled();
+      should.equal(
+        new Buffer(tx.hederaTx.sigMap!.sigPair![0].pubKeyPrefix!).toString('hex'),
+        testData.ACCOUNT_1.publicKey.slice(24),
+      );
+      should.equal(
+        new Buffer(tx.hederaTx.sigMap!.sigPair![1].pubKeyPrefix!).toString('hex'),
+        testData.OPERATOR.publicKey.slice(24),
+      );
+    });
+
+  });
+
+  describe('should return encoded tx', function() {
+    it('valid sign', async function() {
+      const tx = getTransaction();
+      tx.bodyBytes(testData.WALLET_TXDATA);
+      await tx.sign(testData.KEYPAIR_PRV);
+      should.equal(tx.toBroadcastFormat(), testData.WALLET_SIGNED_TRANSACTION);
+    });
+  });
+});
