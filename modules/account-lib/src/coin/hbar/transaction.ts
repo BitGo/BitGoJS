3cf308f0742ac82e00d0bf7399cdac44e382bf07
silalvado <silvialvado@gmail.com>
2020-08-10 17:41:42 -0300

    BGA-297 WIP Removing proto from transaction update SDK version in package json

--- modules/account-lib/src/coin/hbar/transaction.ts
+++ modules/account-lib/src/coin/hbar/transaction.ts
@@ -2,17 +2,17 @@ import { BaseCoin as CoinConfig } from '@bitgo/statics/dist/src/base';
 import { hash } from '@stablelib/sha384';
 import BigNumber from 'bignumber.js';
 import { Writer } from 'protobufjs';
-import * as nacl from 'tweetnacl';
+// import * as nacl from 'tweetnacl';
 import { Transaction as SDKTransaction } from '@hashgraph/sdk';
 import { TransactionBody } from '@hashgraph/sdk/lib/generated/TransactionBody_pb';
-import { SignatureMap, SignaturePair } from '@hashgraph/sdk/lib/generated/BasicTypes_pb';
+// import { SignatureMap, SignaturePair } from '@hashgraph/sdk/lib/generated/BasicTypes_pb';
 import Long from 'long';
-import { proto } from '../../../resources/hbar/protobuf/hedera';
+// import { proto } from '../../../resources/hbar/protobuf/hedera';
 import { BaseTransaction } from '../baseCoin';
 import { BaseKey } from '../baseCoin/iface';
 import { SigningError } from '../baseCoin/errors';
 import { TxData } from './ifaces';
-import { stringifyAccountId, stringifyTxTime, toHex, toUint8Array } from './utils';
+import { stringifyAccountId, stringifyTxTime, toHex } from './utils';
 import { KeyPair } from './';
 
 export class Transaction extends BaseTransaction {
@@ -29,60 +29,47 @@ export class Transaction extends BaseTransaction {
   }
 
   async sign(keyPair: KeyPair): Promise<void> {
-    const keys = keyPair.getKeys(true);
+    const keys = keyPair.getSDKKeys();
     if (!keys.prv) {
       throw new SigningError('Missing private key');
     }
-    const secretKey = toUint8Array(keys.prv + keys.pub);
-    const signature = nacl.sign.detached(this._hederaTx._toProto().getBodybytes_asU8(), secretKey);
-    this.addSignature(toHex(signature), keyPair);
-  }
-
-  /**
-   * Add a signature to this transaction
-   *
-   * @param signature The signature to add, in string hex format
-   * @param key The key of the key that created the signature
-   */
-  addSignature(signature: string, key: KeyPair): void {
-    const sigPair = new SignaturePair();
-    sigPair.setPubkeyprefix(toUint8Array(key.getKeys(true).pub));
-    sigPair.setEd25519(toUint8Array(signature));
-
-    const sigMap = this._hederaTx._toProto().getSigmap() || new SignatureMap();
-    sigMap.getSigpairList()!.push(sigPair);
-    this._hederaTx._toProto().setSigmap(sigMap);
-    this._signatures.push(signature);
+    this.hederaTx.sign(keys.prv);
+    this._signatures = this.hederaTx
+      ._toProto()
+      .getSigmap()!
+      .getSigpairList()
+      .map(sigPair => {
+        return toHex(sigPair.getEd25519_asU8());
+      });
   }
 
   /** @inheritdoc */
   toBroadcastFormat(): string {
-    return toHex(this.encode(this._hederaTx));
+    // return toHex(this.encode(this._hederaTx));
+    return toHex(this.hederaTx.toBytes());
   }
 
   /** @inheritdoc */
   toJson(): TxData {
-    const [acc, time] = this.getTxIdParts();
+    const txData = this.hederaTx._toProto().toObject();
+    const acc = stringifyAccountId(txData.body!.transactionid!.accountid!);
+    const time = stringifyTxTime(txData.body!.transactionid!.transactionvalidstart!);
     const result: TxData = {
       id: acc + '@' + time,
-      hash: this.getTxHash(), // TODO: Update once hedera-sdk release this functionality BGA-284
-      data: toHex(this._hederaTx._toProto().getBodybytes_asU8()),
-      fee: new BigNumber(this._txBody.getTransactionfee().toString()).toNumber(),
+      hash: toHex(this.hederaTx.hash()),
+      data: toHex(this.hederaTx._toProto().getBodybytes_asU8()),
+      fee: new BigNumber(txData.body!.transactionId!.transactionfee).toNumber(),
       from: acc,
       startTime: time,
-      validDuration: this._txBody
-        .getTransactionvalidduration()!
-        .getSeconds()
-        .toString(),
-      node: stringifyAccountId(this._txBody.getNodeaccountid()!),
-      memo: this._txBody.getMemo(),
+      validDuration: txData.body!.transactionvalidduration!.seconds.toString(),
+      node: stringifyAccountId(txData.body!.nodeaccountid!),
+      memo: txData.body!.memo,
     };
 
-    // if (this._txBody.data === 'cryptoTransfer') {
     if (this._txBody.hasCryptotransfer()) {
-      const [recipient, amount] = this.getTransferData();
-      result.amount = amount;
-      result.to = recipient;
+      const transfers = txData.body!.cryptotransfer!.transfers.accountamountsList[0];
+      result.amount = transfers.amount;
+      result.to = stringifyAccountId(transfers.accountid!);
     }
     return result;
   }
@@ -124,7 +111,8 @@ export class Transaction extends BaseTransaction {
    * @param {SDKTransaction} tx body transaction
    */
   body(tx: SDKTransaction) {
-    this._txBody = TransactionBody.deserializeBinary(tx._toProto().getBodybytes_asU8());
+    // this._txBody = TransactionBody.deserializeBinary(tx._toProto().getBodybytes_asU8());
+    this._txBody = tx._toProto().getBody()!;
     this._hederaTx = tx;
     // this.loadPreviousSignatures();
   }
@@ -196,20 +184,6 @@ export class Transaction extends BaseTransaction {
       throw new Error('Missing transaction node id');
     }
     return this.getHashOf(this._hederaTx);
-  }
-
-  /**
-   * Encode an object using the given encoder class
-   *
-   * @param obj - the object to be encoded, it must be an proto namespace object
-   * @param encoder - Object encoder
-   * @returns {Uint8Array} - encoded object byte array
-   */
-  private encode<T extends { constructor: Function }>(obj: T, encoder?: { encode(arg: T): Writer }): Uint8Array {
-    if (encoder) {
-      return encoder.encode(obj).finish();
-    }
-    return this.encode(obj, proto[obj.constructor.name]);
   }
 
   /**
