d79ba05b3bbf020e319a37a8846efa6359e8caad
Ismael <ipelaez@simtlix.com>
2020-08-11 09:41:56 -0300

    BGA-297 Cleaning up transaction class

--- modules/account-lib/src/coin/hbar/transaction.ts
+++ modules/account-lib/src/coin/hbar/transaction.ts
@@ -34,6 +34,13 @@ export class Transaction extends BaseTransaction {
       throw new SigningError('Missing private key');
     }
     this.hederaTx.sign(keys.prv);
+    this.addSignature();
+  }
+
+  /**
+   * Add a signature to this transaction
+   */
+  addSignature(): void {
     this._signatures = this.hederaTx
       ._toProto()
       .getSigmap()!
@@ -52,48 +59,28 @@ export class Transaction extends BaseTransaction {
   /** @inheritdoc */
   toJson(): TxData {
     const txData = this.hederaTx._toProto().toObject();
-    const acc = stringifyAccountId(txData.body!.transactionid!.accountid!);
-    const time = stringifyTxTime(txData.body!.transactionid!.transactionvalidstart!);
+    const txBody = txData.body!;
+
+    const acc = stringifyAccountId(txBody.transactionid!.accountid!);
+    const time = stringifyTxTime(txBody.transactionid!.transactionvalidstart!);
     const result: TxData = {
       id: acc + '@' + time,
       hash: toHex(this.hederaTx.hash()),
       data: toHex(this.hederaTx._toProto().getBodybytes_asU8()),
-      fee: new BigNumber(txData.body!.transactionId!.transactionfee).toNumber(),
+      fee: new BigNumber(txBody.transactionfee).toNumber(),
       from: acc,
       startTime: time,
-      validDuration: txData.body!.transactionvalidduration!.seconds.toString(),
-      node: stringifyAccountId(txData.body!.nodeaccountid!),
-      memo: txData.body!.memo,
+      validDuration: txBody.transactionvalidduration!.seconds.toString(),
+      node: stringifyAccountId(txBody.nodeaccountid!),
+      memo: txBody.memo,
     };
 
     if (this._txBody.hasCryptotransfer()) {
-      const transfers = txData.body!.cryptotransfer!.transfers.accountamountsList[0];
+      const transfers = txBody.cryptotransfer!.transfers!.accountamountsList[0];
       result.amount = transfers.amount;
       result.to = stringifyAccountId(transfers.accountid!);
     }
     return result;
-  }
-
-  /**
-   * Get the recipient account and the amount
-   * transferred on this transaction
-   *
-   * @returns {[string, string]} first element is the recipient, second element is the amount
-   */
-  private getTransferData(): [string, string] {
-    let transferData;
-    this._txBody
-      .getCryptotransfer()!
-      .getTransfers()!
-      .getAccountamountsList()
-      .forEach(transfer => {
-        const amount = Long.fromValue(transfer.getAmount());
-        if (amount.isPositive()) {
-          transferData = [stringifyAccountId(transfer.getAccountid()!), amount.toString()];
-        }
-      });
-
-    return transferData;
   }
 
   //region getters & setters
@@ -150,60 +137,6 @@ export class Transaction extends BaseTransaction {
    */
   bodyBytes(bytes: Uint8Array) {
     this.body(SDKTransaction.fromBytes(bytes));
-  }
-  //endregion
-
-  //region helpers
-  /**
-   * Returns this hedera transaction id components in a readable format
-   *
-   * @returns {[string, string]} - transaction id parts [<account id>, <startTime in seconds>]
-   */
-  getTxIdParts(): [string, string] {
-    if (
-      this._txBody &&
-      this._txBody.getTransactionid() &&
-      this._txBody.getTransactionid()!.getAccountid() &&
-      this._txBody.getTransactionid()!.getTransactionvalidstart()
-    ) {
-      return [
-        stringifyAccountId(this._txBody.getTransactionid()!.getAccountid()!),
-        stringifyTxTime(this._txBody.getTransactionid()!.getTransactionvalidstart()!),
-      ];
-    }
-    throw new Error('Missing transaction id information');
-  }
-
-  /**
-   * Returns this transaction hash
-   *
-   * @returns {string} - The transaction hash
-   */
-  getTxHash(): string {
-    if (!this._txBody.getNodeaccountid()) {
-      throw new Error('Missing transaction node id');
-    }
-    return this.getHashOf(this._hederaTx);
-  }
-
-  /**
-   * Returns an sha-384 hash
-   *
-   * @param {Uint8Array} bytes - bytes to be hashed
-   * @returns {string} - the resulting hash string
-   */
-  sha(bytes: Uint8Array): string {
-    return toHex(hash(bytes));
-  }
-
-  /**
-   * Returns a hash of the given proto object.
-   *
-   * @param obj - The object to be hashed, it must be an proto namespace object
-   * @returns {string} - the resulting hash string
-   */
-  private getHashOf<T>(obj: T): string {
-    return this.sha(this.encode(obj));
   }
   //endregion
 }
