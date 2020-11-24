9bb748aa45b0be928b70ad801720d22ab454148d
silalvado <silvialvado@gmail.com>
2020-11-24 17:54:18 -0300

    BGA-655 Add test folder structure

---
+++ modules/account-lib/test/resources/cspr/cspr.ts
@@ -0,0 +1,5 @@
+export const ACCOUNT1 = {
+  accountHash: '019764f079d02768d704f286da630a1d3c7329330596c9561d403bed2d43c0e0c8',
+  publicKey: 'MCowBQYDK2VwAyEAl2TwedAnaNcE8obaYwodPHMpMwWWyVYdQDvtLUPA4Mg=',
+  privateKey: 'MC4CAQAwBQYDK2VwBCIEIG+yNyAN9sInKfXR2GOH5UN/f5a4bx/VL11lxW58wbRP',
+};
