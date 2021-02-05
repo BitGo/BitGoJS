d4ec8594d865640dcef677ca5b9d7564f0964073
Luis Rojas <luis@bitgo.com>
2021-02-05 08:55:41 -0600

    feat(account-lib): add pub key validation and address validation for CSPR
    Ticket: STLX-1668

---
+++ modules/account-lib/test/unit/coin/cspr/util.ts
@@ -0,0 +1,43 @@
+import * as should from 'should';
+import * as Utils from '../../../../src/coin/cspr/utils';
+
+describe('CSPR util library', function() {
+  describe('validation', function() {
+    it('should validate pub keys', function() {
+      const validPubKeys = [
+        'xpub661MyMwAqRbcGEkHtJzvg4PRy9i6YzYewjr6sqXhCU8gyrZtdH88BMrdzU3wo1Q3TodAyMU3aPaVAVTPvkgpA3aMvN9hQUu6x7xU2E68Drz',
+        'xpub661MyMwAqRbcFjmMuHyjyZsQYGD6m8JZseFXpjAc1djoKvnMiJvqCeGu4cvxDNXstgPTsP1yeXewvUf6rUubvuG8mi9yPEb7hHVVCqRnhu7',
+        '02620C05D663B1585616C79A482B8F1F27ED9FC24719EEB358E6A085C285732632',
+        '021E3BC1DE4255255BC799983F0A8384DAD84B3730BD95996AB95349C584B02B79',
+      ];
+
+      for (const pub of validPubKeys) {
+        Utils.isValidPublicKey(pub).should.be.true();
+      }
+    });
+
+    it('should validate addresses', function() {
+      const validAddresses = [
+        '03DC13CBBF29765C7745578D9E091280522F37684EF0E400B86B1C409BC454F1F3',
+        '0385D724A9A3E7E32BADF40F3279AF5A190CB2CFCAB6639BF532A0069E0E3824D0',
+      ];
+
+      for (const address of validAddresses) {
+        Utils.isValidAddress(address).should.be.true();
+      }
+    });
+
+    it('should fail to validate invalid addresses', function() {
+      const invalidAddresses = [
+        'xpub661MyMwAqRbcFjmMuHyjyZsQYGD6m8JZseFXpjAc1djoKvnMiJvqCeGu4cvxDNXstgPTsP1yeXewvUf6rUubvuG8mi9yPEb7hHVVCqRnhu7',
+        'X03DC13CBBF29765C7745578D9E091280522F37684EF0E400B86B1C409BC454F1F3',
+        'abc',
+      ];
+
+      for (const address of invalidAddresses) {
+        should.doesNotThrow(() => Utils.isValidAddress(address));
+        Utils.isValidAddress(address).should.be.false();
+      }
+    });
+  });
+});
