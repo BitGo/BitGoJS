48dcadc2c448456bb8df8122169afb27c0cb8597
Faizan Dastgir <faizan.dastgir@tintash.com>
2021-03-08 17:40:20 +0500

    test(account-lib): test stacks builder

--- modules/account-lib/test/unit/coin/stacks/util.ts
+++ modules/account-lib/test/unit/coin/stacks/util.ts
@@ -1,10 +1,12 @@
 import should from 'should';
-import * as testData from '../../../resources/stacks/stacks';
+import * as testData from '../../../resources/stacks/stacks'
 import * as Utils from '../../../../src/coin/stacks/utils';
+// import { defaultKeyPairFromPrv, defaultKeyPairFromPub } from '../../../resources/stacks/stacks';
+// import { HashType } from '../../../../src/coin/stacks/iface';
 
-describe('Stacks util library', function() {
-  describe('address', function() {
-    it('should validate addresses', function() {
+describe('Stacks util library', function () {
+  describe('address', function () {
+    it('should validate addresses', function () {
       const validAddresses = [
         'STB44HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6',
         'ST11NJTTKGVT6D1HY4NJRVQWMQM7TVAR091EJ8P2Y',
@@ -17,7 +19,7 @@ describe('Stacks util library', function() {
       }
     });
 
-    it('should fail to validate invalid addresses', function() {
+    it('should fail to validate invalid addresses', function () {
       const invalidAddresses = [
         'SP244HYPYAT2BB2QE513NSP81HTMYWBJP02HPGK6',
         'ST1T758K6T2YRKG9Q0TJ16B6FP5QQREWZSESRS0PY',
@@ -32,18 +34,18 @@ describe('Stacks util library', function() {
     });
   });
 
-  describe('amount', function() {
-    it('valid amount', function() {
+  describe('amount', function () {
+    it('valid amount', function () {
       Utils.isValidAmount('10').should.be.true();
     });
 
-    it('invalid amount', function() {
+    it('invalid amount', function () {
       Utils.isValidAmount('-10').should.be.false();
     });
   });
 
-  describe('private key', function() {
-    it('should validate proper keys', function() {
+  describe('private key', function () {
+    it('should validate proper keys', function () {
       const keys = [testData.secretKey1, testData.secretKey2, testData.ACCOUNT_1.prv];
 
       for (const key of keys) {
@@ -51,7 +53,7 @@ describe('Stacks util library', function() {
       }
     });
 
-    it('should not validate invalid keys', function() {
+    it('should not validate invalid keys', function () {
       const keys = [
         '66c88648116b721bb2f394e0007f9d348ea08017b6e604de51a3a7d957d5852409',
         '688648116b721bb2f394e0007f9d348ea08017b6e604de51a3a7d957d58524',
@@ -68,8 +70,8 @@ describe('Stacks util library', function() {
     });
   });
 
-  describe('public key', function() {
-    it('should validate proper keys', function() {
+  describe('public key', function () {
+    it('should validate proper keys', function () {
       const keys = [testData.pubKey1, testData.pubKey2, testData.pubKey2Compressed];
 
       for (const key of keys) {
@@ -77,7 +79,7 @@ describe('Stacks util library', function() {
       }
     });
 
-    it('should not validate invalid keys', function() {
+    it('should not validate invalid keys', function () {
       const keys = [
         '0421d6f42c97d23ec2c0dc21208a9c5edfce4e5bc7b63972e68e86e3cea6f41a94a9a7c24a1ccd83792173f475fdb590cc82f94ff615df39142766e759ce6387',
         '0321d6f42c99f7d23ec 2c0dc21208a9c5edfce4e5bc7b63972e68e6e3cea6f41a',
@@ -95,8 +97,8 @@ describe('Stacks util library', function() {
     });
   });
 
-  describe('transaction id', function() {
-    it('should validate proper ids', function() {
+  describe('transaction id', function () {
+    it('should validate proper ids', function () {
       const txIds = [
         '0x209a3e196195063b2e5195232087a71fe2329a6dc8d2fca531d48c5a7824f679',
         '6a590378c059f78fb698ec0af1ff610586cb1a52ee79fdae69e56430fde08cf4',
@@ -108,7 +110,7 @@ describe('Stacks util library', function() {
       }
     });
 
-    it('should not validate invalid ids', function() {
+    it('should not validate invalid ids', function () {
       const txIds = [
         '',
         'bitgo-stacks',
