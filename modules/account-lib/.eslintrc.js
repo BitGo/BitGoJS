648b8c3a53fe5b30617daac63582973b2d8a5443
Ryan Lee <ryanlee@bitgo.com>
2021-05-17 16:24:02 -0400

    chore(account-lib): update ecma version to 2021
    Updates ecma version to 2021 to make use of the BigInt type.
    
    ticket: BG-31991

--- modules/account-lib/.eslintrc.js
+++ modules/account-lib/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   ],
   plugins: ['import', 'jsdoc'],
   parserOptions: {
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
   rules: {
