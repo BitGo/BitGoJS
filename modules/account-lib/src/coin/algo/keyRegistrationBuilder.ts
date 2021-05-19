85e5204887d31e7af2df77e8c5d59bc9a5e15375
Gibran Gul <gibrangul@hotmail.com>
2021-05-19 18:36:04 +0500

    fix: vote last and Key Dilution Error Handlers

--- modules/account-lib/src/coin/algo/keyRegistrationBuilder.ts
+++ modules/account-lib/src/coin/algo/keyRegistrationBuilder.ts
@@ -1,9 +1,8 @@
+import BigNumber from 'bignumber.js';
 import { BaseCoin as CoinConfig } from '@bitgo/statics';
 import { NotImplementedError } from '../baseCoin/errors';
 import { BaseKey } from '../baseCoin/iface';
 import { TransactionBuilder } from './transactionBuilder';
-import { Transaction } from './transaction';
-
 export class KeyRegistrationBuilder extends TransactionBuilder {
   protected _voteKey?: string;
   protected _selectionKey?: string;
@@ -17,32 +16,42 @@ export class KeyRegistrationBuilder extends TransactionBuilder {
 
   voteKey(key: string): void {
     this._voteKey = key;
-    // return this;
   }
 
   selectionKey(key: string): void {
     this._selectionKey = key;
-    // return this;
   }
 
   voteFirst(round: number): void {
-    if (round < 1) {
-      throw new Error('First round of voting must be greater than 1');
-    }
+    this.validateValue(new BigNumber(round));
     this._voteFirst = round;
   }
 
+  /**
+   *
+   * @param {number} round No theoretical limit. A recommended range is 3,000,000 rounds.
+   * https://developer.algorand.org/docs/run-a-node/participate/generate_keys/
+   */
   voteLast(round: number = 3000000): void {
-    if (round < 1) {
-      throw new Error('First round of voting must be greater than 1');
+    this.validateValue(new BigNumber(round));
+    if (this._voteFirst && round < this._voteFirst) {
+      throw new Error('VoteKey last round must be greater than first round');
     }
-    this._voteFirst = round;
+    this._voteLast = round;
   }
 
+  /**
+   * @param {number} size Defaults to 10,000. To reduce the size of the participation key, set the key dilution value to roughly the square root of the range that the partkey is valid for.
+   * https://developer.algorand.org/docs/run-a-node/participate/generate_keys/#generate-the-participation-key-with-goal
+   */
   voteKeyDilution(size: number = 10000): void {
     if (this._voteFirst && this._voteLast) {
       if (size <= Math.sqrt(this._voteLast - this._voteFirst)) {
         this._voteKeyDilution = size;
+      } else {
+        throw new Error(
+          'Key dilution value must be less than or equal to the square root of the voteKey validity range.',
+        );
       }
     }
   }
