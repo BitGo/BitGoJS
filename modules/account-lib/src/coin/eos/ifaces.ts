10766fd0432487937ea5e7ccfb1d2978a118b38c
Gibran Gul <gibran.gul@tintash.com>
2021-06-29 14:13:01 +0500

    feat: (account-lib): eos transaction setup

---
+++ modules/account-lib/src/coin/eos/ifaces.ts
@@ -0,0 +1,99 @@
+import ser from 'eosjs/dist/eosjs-serialize';
+
+interface AuthKey {
+  key: string;
+  weight: number;
+}
+
+interface Permission {
+  actor: string;
+  permission: string;
+}
+
+interface AuthAccount {
+  permission: ser.Authorization;
+  weight: number;
+}
+
+interface Auth {
+  threshold: number;
+  accounts: AuthAccount[];
+  keys: AuthKey[];
+}
+
+interface Action {
+  account?: string;
+  name?: string;
+  authorization?: ser.Authorization[];
+  data?: {
+    from?: string; // transfer, delegatebw, undelegatebw
+    to?: string; // transfer
+    quantity?: string; // transfer
+    memo?: Uint8Array; // transfer
+    bytes?: number; // buyrambytes
+    transfer?: boolean; // delegatebw, undelegatebw
+    stake_net_quantity?: string; // delegatebw, undelegatebw
+    stake_cpu_quantity?: string; // delegatebw, undelegatebw
+    creator?: string; // newaccount
+    name?: string; // newaccount
+    owner?: {
+      // newaccount
+      threshold: number;
+      keys: AuthKey[];
+      accounts: string[];
+      waits: string[];
+    };
+    active?: {
+      // newaccount
+      threshold: number;
+      keys: AuthKey[];
+      accounts: string[];
+      waits: string[];
+    };
+    payer?: string; // buyrambytes
+    receiver?: string; // delegatebw, undelegatebw, buyrambytes
+    code?: string; // setcode, linkauth, unlinkauth
+    account?: string; // setcode, setabi, updateauth, deleteauth, linkauth, unlinkauth
+    abi?: string; // setabi
+    permission?: string; // updateauth, deleteauth
+    parent?: string; // updateauth
+    auth?: Auth; // updateauth
+    type?: string; // linkauth, unlinkauth
+    requirement?: string; // linkauth
+    proposer?: string; // approve, unapprove, propose, cancel, exec
+    proposal_name?: string; // approve, unapprove, propose, cancel, exec
+    level?: Permission; // approve, unapprove
+    canceler?: string; // cancel
+    executer?: string; // exec
+    voter?: string; // voteproducer
+    proxy?: string; // voteproducer
+    producers?: string[]; // voteproducer
+    // propose
+    requested?: Permission[];
+    trx?: {
+      expiration: string;
+      ref_block_num: number;
+      ref_block_prefix: number;
+      max_net_usage_words: number;
+      max_cpu_usage_ms: number;
+      delay_sec: number;
+      context_free_actions: [];
+      actions: ser.SerializedAction[];
+      transaction_extensions: [];
+    };
+  };
+  hex_data?: string;
+}
+
+export interface TxData {
+  actions: Action[];
+  expiration?: string;
+  ref_block_num?: number;
+  ref_block_prefix?: number;
+  max_net_usage_words?: number;
+  max_cpu_usage_ms?: number;
+  delay_sec?: number;
+  context_free_actions?: Action[];
+  context_free_data?: Uint8Array[];
+  transaction_extensions?: [number, string][];
+}
--- modules/account-lib/src/coin/eos/transaction.ts
+++ modules/account-lib/src/coin/eos/transaction.ts
@@ -1,20 +1,213 @@
-import { BaseTransaction } from '../baseCoin';
+import * as EosJS from 'eosjs';
+import { BaseCoin as CoinConfig } from '@bitgo/statics';
+import { BaseTransaction, TransactionType } from '../baseCoin';
+import { InvalidTransactionError, NotImplementedError } from '../baseCoin/errors';
 import { BaseKey } from '../baseCoin/iface';
-import { NotImplementedError } from '../baseCoin/errors';
-
+import { TxData } from './ifaces';
+// import { KeyPair } from './keyPair';
 export class Transaction extends BaseTransaction {
-  /** @inheritdoc */
-  canSign(key: BaseKey): boolean {
-    throw new NotImplementedError('canSign not implemented');
+  private _eosTransaction?: EosJS.ApiInterfaces.Transaction;
+  private _signedTransaction?: EosJS.RpcInterfaces.PushTransactionArgs;
+  private _serializedUnsignedTransaction?: EosJS.RpcInterfaces.PushTransactionArgs;
+  private _numberOfRequiredSigners: number;
+  private _sender: string;
+  private _signers: string[];
+  private _blocksBehind: number;
+  private _expireSeconds: number;
+
+  constructor(coinConfig: Readonly<CoinConfig>) {
+    super(coinConfig);
+    this._numberOfRequiredSigners = 0;
+    this._signers = [];
   }
 
   /** @inheritdoc */
-  toBroadcastFormat(): string {
-    throw new NotImplementedError('toBroadcastFormat not implemented');
+  canSign(key: BaseKey): boolean {
+    if (this._numberOfRequiredSigners === 0) {
+      return false;
+    }
+    // if (this._numberOfRequiredSigners === 1) {
+    //   const kp = new KeyPair({ prv: key });
+    //   const addr = kp.getAddress();
+    //   if (addr === this._sender) {
+    //     return true;
+    //   } else {
+    //     return false;
+    //   }
+    // } else {
+    //   return true;
+    // }
+    throw new NotImplementedError('canSign not implemented');
+  }
+
+  sender(address: string): void {
+    this._sender = address;
+  }
+
+  blocksBehind(blocksBehind: number): void {
+    this._blocksBehind = blocksBehind;
+  }
+
+  expireSeconds(expireSeconds: number): void {
+    this._expireSeconds = expireSeconds;
+  }
+
+  /**
+   * Set the transaction type.
+   *
+   * @param {TransactionType} transactionType The transaction type to be set.
+   */
+  setTransactionType(transactionType: TransactionType): void {
+    this._type = transactionType;
+  }
+
+  /**
+   * Signs transaction.
+   *
+   * @param {EosJS.Api} api Eos API.
+   * @param {string[]} requiredKeys signing keys.
+   */
+  async sign(api: EosJS.Api, requiredKeys: string[]): Promise<void> {
+    this._signedTransaction = await this.serializeTransaction(api, true, requiredKeys);
+  }
+
+  /**
+   * Serializes an unsigned transaction.
+   *
+   * @param {EosJS.Api} api Eos API.
+   * @param {string[]} requiredKeys signing keys.
+   */
+  async serializeUnsignedTransaction(api: EosJS.Api): Promise<void> {
+    this._serializedUnsignedTransaction = await this.serializeTransaction(api, false, []);
+  }
+
+  /**
+   * Serializes a transaction.
+   *
+   * @param {EosJS.Api} api Eos API.
+   * @param {boolean} sign sign the transaction or not.
+   * @param {string[]} requiredKeys signing keys.
+   * @returns {EosJS.RpcInterfaces.PushTransactionArgs} serialized transaction
+   */
+  private async serializeTransaction(
+    api: EosJS.Api,
+    sign: boolean,
+    requiredKeys: string[],
+  ): Promise<EosJS.RpcInterfaces.PushTransactionArgs> {
+    if (!this._eosTransaction) {
+      throw new InvalidTransactionError('Empty transaction');
+    }
+    return (await api.transact(this._eosTransaction, {
+      broadcast: false,
+      sign,
+      requiredKeys,
+      blocksBehind: this._blocksBehind,
+      expireSeconds: this._expireSeconds,
+    })) as EosJS.RpcInterfaces.PushTransactionArgs;
+  }
+
+  /**
+   * Set underlying eos transaction.
+   *
+   * @returns {void}
+   */
+
+  setEosTransaction(tx: EosJS.ApiInterfaces.Transaction): void {
+    this._eosTransaction = tx;
+  }
+
+  /**
+   * Get underlying eos transaction.
+   *
+   * @returns {EosJS.ApiInterfaces.Transaction}
+   */
+
+  getEosTransaction(): EosJS.ApiInterfaces.Transaction | undefined {
+    return this._eosTransaction;
+  }
+
+  /** @inheritdoc */
+  toBroadcastFormat(): EosJS.RpcInterfaces.PushTransactionArgs {
+    if (!this._eosTransaction) {
+      throw new InvalidTransactionError('Empty transaction');
+    }
+    if (this._signedTransaction) {
+      return this._signedTransaction;
+    }
+    if (!this._serializedUnsignedTransaction) {
+      throw new InvalidTransactionError('Please serialize the transaction first');
+    }
+    return this._serializedUnsignedTransaction;
   }
 
   /** @inheritdoc */
   toJson(): any {
-    throw new NotImplementedError('toJson not implemented');
+    // throw new NotImplementedError('toJson not implemented');
+    if (!this._eosTransaction) {
+      throw new InvalidTransactionError('Empty transaction');
+    }
+    const actions = this._eosTransaction.actions;
+    const result: TxData = {
+      actions: [],
+    };
+    if (this.type === TransactionType.Send) {
+      result.actions.push({
+        data: {
+          from: actions[0].data.from,
+          to: actions[0].data.to,
+          quantity: actions[0].data.quantity,
+          memo: actions[0].data.memo,
+        },
+      });
+    }
+    if (this.type === TransactionType.StakingActivate || this.type === TransactionType.StakingWithdraw) {
+      result.actions.push({
+        data: {
+          from: actions[0].data.from,
+          receiver: actions[0].data.receiver,
+          stake_net_quantity: actions[0].data.stake_net_quantity,
+          stake_cpu_quantity: actions[0].data.stake_cpu_quantity,
+          transfer: actions[0].data.transfer,
+        },
+      });
+    }
+
+    if (this.type === TransactionType.BuyRamBytes) {
+      result.actions.push({
+        data: {
+          payer: actions[0].data.payer,
+          receiver: actions[0].data.receiver,
+          bytes: actions[0].data.bytes,
+        },
+      });
+    }
+
+    if (this.type === TransactionType.WalletInitialization) {
+      result.actions.push({
+        data: {
+          creator: actions[0].data.creator,
+          name: actions[0].data.name,
+          owner: actions[0].data.owner,
+          active: actions[0].data.active,
+        },
+      });
+      result.actions.push({
+        data: {
+          payer: actions[1].data.payer,
+          receiver: actions[1].data.receiver,
+          bytes: actions[1].data.bytes,
+        },
+      });
+      result.actions.push({
+        data: {
+          from: actions[2].data.from,
+          receiver: actions[2].data.receiver,
+          stake_net_quantity: actions[2].data.stake_net_quantity,
+          stake_cpu_quantity: actions[2].data.stake_cpu_quantity,
+          transfer: actions[2].data.transfer,
+        },
+      });
+    }
+    return result;
   }
 }
