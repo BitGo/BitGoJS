e8121d4a08d1a2cd0b37c777da3e6f5d37e5c27d
Gibran Gul <gibran.gul@tintash.com>
2021-07-02 16:46:02 +0500

    feat(account-lib): algo removal

--- modules/account-lib/src/coin/algo/keyPair.ts
+++ modules/account-lib/src/coin/algo/keyPair.ts
@@ -1,16 +1,9 @@
-/* eslint-disable no-unused-vars */
-import { encodeAddress } from 'algosdk';
 import { Ed25519KeyPair } from '../baseCoin';
+import { AddressFormat } from '../baseCoin/enum';
 import { NotImplementedError } from '../baseCoin/errors';
 import { DefaultKeys, KeyPairOptions } from '../baseCoin/iface';
-import utils from './utils';
 
 export class KeyPair extends Ed25519KeyPair {
-  /**
-   * Public constructor. By default, creates a key pair with a random master seed.
-   *
-   * @param { KeyPairOptions } source Either a master seed, a private key, or a public key
-   */
   constructor(source?: KeyPairOptions) {
     super(source);
   }
@@ -26,28 +19,12 @@ export class KeyPair extends Ed25519KeyPair {
   }
 
   /** @inheritdoc */
-  getAddress(): string {
-    return encodeAddress(utils.hexStringToUInt8Array(this.keyPair.pub));
+  getAddress(format: AddressFormat): string {
+    throw new NotImplementedError('getAddress not implemented');
   }
 
   /** @inheritdoc */
-  getKeys(): DefaultKeys {
-    const result: DefaultKeys = { pub: this.keyPair.pub };
-
-    if (this.keyPair.prv) {
-      result.prv = this.keyPair.prv;
-    }
-
-    return result;
-  }
-
-  /**
-   * Getting the base64 private key for signing an algo transaction.
-   *
-   * @returns {Uint8Array} base64 private key
-   * @see https://developer.algorand.org/docs/features/accounts/#transformation-private-key-to-base64-private-key
-   */
-  getSigningKey(): Uint8Array {
-    return utils.toUint8Array(this.keyPair.prv + this.keyPair.pub);
+  getKeys(): void {
+    throw new NotImplementedError('getKeys not implemented');
   }
 }
