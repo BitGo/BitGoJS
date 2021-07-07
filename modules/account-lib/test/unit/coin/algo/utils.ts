de0ebb680e8ad689105ea8957b4577db589d58c4
Omar Fornaroli <ofornaroli@simtlix.com>
2021-07-07 15:52:31 -0300

    feat(account-lib): added Algo encodeObj support on account lib
    added Algo encodeObj support on account lib
    
    STLX-4477

---
+++ modules/account-lib/test/unit/coin/algo/utils.ts
@@ -0,0 +1,24 @@
+import should from 'should';
+import algosdk from 'algosdk';
+import utils from '../../../../src/coin/algo/utils';
+import { Algo } from '../../../../src';
+import * as AlgoResources from '../../../resources/algo';
+
+describe('utils', () => {
+  const {
+    accounts: { account1, account2, account3 },
+  } = AlgoResources;
+
+  it('should properly encode an algorand address from an ed25519 public key', () => {
+    should.equal(utils.publicKeyToAlgoAddress(account1.pubKey), account1.address);
+    should.equal(utils.publicKeyToAlgoAddress(account2.pubKey), account2.address);
+    should.equal(utils.publicKeyToAlgoAddress(account3.pubKey), account3.address);
+  });
+
+  it('should encode an object from a string message', () => {
+    const note = Buffer.from('hola mundo', 'base64');
+    const objEncoded = Algo.algoUtils.encodeObj(note);
+    const objEncodedByAlgo = algosdk.encodeObj(note);
+    should.deepEqual(objEncoded, objEncodedByAlgo);
+  });
+});
