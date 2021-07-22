75f6fab78ee3d1d0493be407e4c05257712dfddd
Otto Allmendinger <otto@bitgo.com>
2021-07-22 14:20:13 +0200

    fix(utxolib): use path package for path operations
    Issue: BG-34148

--- modules/utxo-lib/test/integration_local_rpc/generate/fixtures.ts
+++ modules/utxo-lib/test/integration_local_rpc/generate/fixtures.ts
@@ -1,14 +1,16 @@
 /**
  * @prettier
  */
-import { Network } from './types';
 import * as fs from 'fs/promises';
+import * as path from 'path';
+
+import { Network } from './types';
 
 const coins = require('../../../src/coins');
 
 export function getFixtureDir(network: Network): string {
   const networkName = coins.getNetworkName(network);
-  return `${__dirname}/../fixtures/${networkName}`;
+  return path.join(__dirname, '..', 'fixtures', networkName);
 }
 
 export async function wipeFixtures(network: Network): Promise<void> {
@@ -23,9 +25,9 @@ export async function wipeFixtures(network: Network): Promise<void> {
 
 export async function writeFixture(network: Network, filename: string, content: unknown): Promise<void> {
   await fs.mkdir(getFixtureDir(network), { recursive: true });
-  await fs.writeFile(`${getFixtureDir(network)}/${filename}`, JSON.stringify(content, null, 2));
+  await fs.writeFile(path.join(getFixtureDir(network), filename), JSON.stringify(content, null, 2));
 }
 
 export async function readFixture<T>(network: Network, filename: string): Promise<T> {
-  return JSON.parse(await fs.readFile(`${getFixtureDir(network)}/${filename}`, 'utf8'));
+  return JSON.parse(await fs.readFile(path.join(getFixtureDir(network), filename), 'utf8'));
 }
