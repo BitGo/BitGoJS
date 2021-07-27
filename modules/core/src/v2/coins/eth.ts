6e12861670343a515a5f8e15a34241d052241bab
Karan Kurbur <karankurbur@bitgo.com>
2021-07-27 16:39:56 -0700

    test(eth sign transaction eip1559): check that eip1559 params are returned in eth.signTransaction

--- modules/core/src/v2/coins/eth.ts
+++ modules/core/src/v2/coins/eth.ts
@@ -611,6 +611,9 @@ export class Eth extends BaseCoin {
     const self = this;
     return co<SignedTransaction>(function* () {
       const txPrebuild = params.txPrebuild;
+
+      console.log(txPrebuild);
+
       const userPrv = params.prv;
       const EXPIRETIME_DEFAULT = 60 * 60 * 24 * 7; // This signature will be valid for 1 week
 
