27c562fa1d557f50c7128308666987dab5c48231
Santiago Jose Maria <smaria@simtlix.com>
2021-08-03 11:13:41 -0300

    feat(account-lib): avax key pair support
    implemented key pair class and unit tests for avax
    
    stlx-5078

--- modules/account-lib/src/coin/avaxc/keyPair.ts
+++ modules/account-lib/src/coin/avaxc/keyPair.ts
@@ -1,31 +1,61 @@
-import { KeyPairOptions, DefaultKeys } from '../baseCoin/iface';
-import { NotImplementedError } from '../baseCoin/errors';
+import { randomBytes } from 'crypto';
+import { HDNode } from '@bitgo/utxo-lib';
+import { addHexPrefix, pubToAddress } from 'ethereumjs-util';
+import { DefaultKeys, isPrivateKey, isPublicKey, isSeed, KeyPairOptions } from '../baseCoin/iface';
 import { Secp256k1ExtendedKeyPair } from '../baseCoin/secp256k1ExtendedKeyPair';
 
+const DEFAULT_SEED_SIZE_BYTES = 16;
+
+/**
+ * Avalanche keys and address management for the C-Chain.
+ */
 export class KeyPair extends Secp256k1ExtendedKeyPair {
   /**
    * Public constructor. By default, creates a key pair with a random master seed.
    *
-   * @param { KeyPairOptions } source Either a master seed, a private key, or a public key
+   * @param { KeyPairOptions } source Either a master seed, a private key (extended or raw), or a public key
+   *     (extended, compressed, or uncompressed)
    */
   constructor(source?: KeyPairOptions) {
     super(source);
-    // TODO(STLX-5078): Implement random seed and key pair generation
+    if (!source) {
+      const seed = randomBytes(DEFAULT_SEED_SIZE_BYTES);
+      this.hdNode = HDNode.fromSeedBuffer(seed);
+    } else if (isSeed(source)) {
+      this.hdNode = HDNode.fromSeedBuffer(source.seed);
+    } else if (isPrivateKey(source)) {
+      this.recordKeysFromPrivateKey(source.prv);
+    } else if (isPublicKey(source)) {
+      this.recordKeysFromPublicKey(source.pub);
+    } else {
+      throw new Error('Invalid key pair options');
+    }
+
+    if (this.hdNode) {
+      this.keyPair = this.hdNode.keyPair;
+    }
   }
 
   /**
-   * Avalanche default keys format is CB58 strings with prefixes
+   * Avalanche C-Chain default keys format is the same as ethereum: raw private and uncompressed public key
    *
    * @returns { DefaultKeys } The keys in the protocol default key format
    */
   getKeys(): DefaultKeys {
-    throw new NotImplementedError('getKeys not implemented');
+    const keyPair = this.hdNode ? this.hdNode.keyPair : this.keyPair;
+    return {
+      pub: keyPair.Q.getEncoded(false).toString('hex').toUpperCase(),
+      prv: keyPair.d ? this.keyPair.d.toBuffer(32).toString('hex').toUpperCase() : undefined,
+    };
   }
 
   /**
-   * Avalanche C-chain address is an Eth-like hex value starting with 0x prefix
+   * Get an Avalanche C-Chain public address in ethereum format
+   *
+   * @returns {string} The address derived from the public key
    */
   getAddress(): string {
-    throw new NotImplementedError('getAddress not implemented');
+    const publicKey = Buffer.from(this.getKeys().pub, 'hex'); // first two characters identify a public key
+    return addHexPrefix(pubToAddress(publicKey, true).toString('hex'));
   }
 }
--- modules/account-lib/src/coin/avaxc/utils.ts
+++ modules/account-lib/src/coin/avaxc/utils.ts
@@ -1,4 +1,5 @@
 import { NetworkType } from '@bitgo/statics';
+import { isValidAddress, isValidPrivate, isValidPublic } from 'ethereumjs-util';
 import { KeyPair } from '../eth';
 import { TxData } from '../eth/iface';
 
@@ -14,10 +15,44 @@ export async function sign(transactionData: TxData, keyPair: KeyPair): Promise<a
 }
 
 /**
- * @param network
+ * @param {NetworkType} network either mainnet or testnet
  */
 export function getCommon(network: NetworkType): any {
   // TODO
+}
+
+/**
+ * Returns whether or not the string is a valid C-Chain address in Eth format
+ *
+ * @param {string} address - the address string
+ * @returns {boolean} - the validation result
+ */
+export function isValidEthAddress(address: string): boolean {
+  return isValidAddress(address);
+}
+
+/**
+ * Returns whether or not the string is a valid C-Chain private key in Eth format
+ *
+ * @param {string} privateKey - the string formatted key
+ * @returns {boolean} - the validation result
+ */
+export function isValidEthPrivateKey(privateKey: string): boolean {
+  const privateKeyBuffer = Buffer.from(privateKey, 'hex');
+  return isValidPrivate(privateKeyBuffer);
+}
+
+/**
+ * Returns whether or not the string is a valid C-Chain public key in Eth format
+ *
+ * @param {string} publicKey - the uncompressed public key string
+ * @returns {boolean} - the validation result
+ */
+export function isValidEthPublicKey(publicKey: string): boolean {
+  // Uncompressed Eth Public Keys have a 04 prefix that needs to be removed in order to validate it.
+  const publicKeyWithoutPrefix = publicKey.slice(2);
+  const publicKeyBuffer = Buffer.from(publicKeyWithoutPrefix, 'hex');
+  return isValidPublic(publicKeyBuffer);
 }
 
 // TODO: add walletSimpleByteCode
