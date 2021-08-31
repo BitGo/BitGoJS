4a2e2769f6e8c9281e050e3a6e2df3ce498bf68b
Otto Allmendinger <otto@bitgo.com>
2021-08-31 14:28:28 +0200

    feat(utxo-lib): expose lower-level signature validation methods
    We have instances where we need more fine-grained access to signature
    validation results.
    
    Issue: BG-34381

--- modules/utxo-lib/src/bitgo/index.ts
+++ modules/utxo-lib/src/bitgo/index.ts
@@ -1,4 +1,7 @@
+/**
+ * @prettier
+ */
 export * as keyutil from './keyutil';
 export * as outputScripts from './outputScripts';
-export { getDefaultSigHash, verifySignature, parseSignatureScript } from './signature';
+export { getDefaultSigHash, verifySignature, parseSignatureScript, getSignatureVerifications } from './signature';
 export * from './transaction';
