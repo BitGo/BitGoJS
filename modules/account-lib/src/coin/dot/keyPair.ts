8b95a64a17e066c186b9c280b41a0bc02c6c6550
Gibran Gul <gibran.gul@tintash.com>
2021-10-27 16:42:39 +0500

    feat(account-lib): keypair comments

--- modules/account-lib/src/coin/dot/keyPair.ts
+++ modules/account-lib/src/coin/dot/keyPair.ts
@@ -15,27 +15,37 @@ export class KeyPair extends Ed25519KeyPair {
    *
    * @param { KeyPairOptions } source Either a master seed, a private key, or a public key
    */
-
-  polkadotKeyPair: KeyringPair;
-
   constructor(source?: KeyPairOptions) {
     super(source);
   }
 
+  /**
+   * Helper function to create the KeyringPair for signing a dot transaction.
+   *
+   * @returns {KeyringPair} dot KeyringPair
+   * @see https://polkadot.js.org/docs/api/start/keyring
+   */
   protected createPolkadotPair(): KeyringPair {
     const secretKey = this.keyPair.prv ? new Uint8Array(Buffer.from(this.keyPair.prv, 'hex')) : new Uint8Array();
     const publicKey = new Uint8Array(Buffer.from(this.keyPair.pub, 'hex'));
     return createPair({ toSS58: keyring.encodeAddress, type: TYPE }, { secretKey, publicKey });
   }
 
+  /** @inheritdoc */
   getAddress(format?: AddressFormat): string {
     return this.createPolkadotPair().address;
   }
 
+  /**
+   * Getting the KeyringPair for signing a dot transaction.
+   *
+   * @returns {KeyringPair} dot KeyringPair
+   */
   getSigningKeyPair(): KeyringPair {
     return this.createPolkadotPair();
   }
 
+  /** @inheritdoc */
   getKeys(): DefaultKeys {
     const result: DefaultKeys = { pub: this.keyPair.pub };
 
@@ -46,12 +56,14 @@ export class KeyPair extends Ed25519KeyPair {
     return result;
   }
 
+  /** @inheritdoc */
   recordKeysFromPrivateKeyInProtocolFormat(prv: string): DefaultKeys {
     const decodedSeed = utils.decodeSeed(prv);
     const bufferFromSeed = Buffer.from(decodedSeed.seed);
     return utils.keyPairFromSeed(bufferFromSeed).keyPair;
   }
 
+  /** @inheritdoc */
   recordKeysFromPublicKeyInProtocolFormat(pub: string): DefaultKeys {
     const publicKey = keyring.addFromPair({
       publicKey: new Uint8Array(Buffer.from(pub, 'hex')),
