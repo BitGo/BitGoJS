24fa2ae6a443a08e5b4b7c7f9d88f746105f009b
Nimra Arif <nimra.arif@tintash.com>
2021-10-28 19:01:05 +0500

    feat(account-lib): dot transfer builder
    feat(account-lib): transaction and transaction builder
    
    feat(account-lib): package version fix
    
    feat(account-lib): transaction builder and transfer builder
    
    feat(account-lib): transaction and transaction builder
    
    feat(account-lib): transfer builder
    
    feat(account-lib): statics fix

---
+++ modules/account-lib/test/unit/coin/dot/transactionBuilder/transferBuilder.ts
@@ -0,0 +1,132 @@
+import { coins } from '@bitgo/statics';
+import should from 'should';
+import sinon, { assert } from 'sinon';
+import { TransferBuilder } from '../../../../../src/coin/dot';
+import * as DotResources from '../../../../resources/dot';
+
+describe('Dot Transfer Builder', () => {
+  let builder: TransferBuilder;
+
+  const sender = DotResources.accounts.account1;
+  const receiver = DotResources.accounts.account2;
+
+  beforeEach(() => {
+    const config = coins.get('dot');
+    builder = new TransferBuilder(config);
+  });
+  describe('setter validation', () => {
+    it('should validate transfer amount', () => {
+      const spy = sinon.spy(builder, 'validateValue');
+      should.throws(
+        () => builder.amount('-1'),
+        (e: Error) => e.message === 'Value cannot be less than zero',
+      );
+      should.doesNotThrow(() => builder.amount('1000'));
+      assert.calledTwice(spy);
+    });
+  });
+
+  describe('build transfer transaction', () => {
+    it('should build a transfer transaction', async () => {
+      builder
+        .testnet()
+        .amount('90034235235322')
+        .dest(receiver.address)
+        .sender(sender.address)
+        .blockNumber(3933)
+        .blockHash('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
+        .nonce(200)
+        .tip(0)
+        .transactionVersion(7)
+        .eraPeriod(64);
+      builder.sign({ key: sender.secretKey });
+      const tx = await builder.build();
+      const txJson = tx.toJson();
+      should.deepEqual(txJson.amount, '90034235235322');
+      should.deepEqual(txJson.dest, receiver.address);
+      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.blockNumber, 3933);
+      should.deepEqual(txJson.blockHash, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
+      should.deepEqual(txJson.genesisHash, '0x2b8d4fdbb41f4bc15b8a7ec8ed0687f2a1ae11e0fc2dc6604fa962a9421ae349');
+      should.deepEqual(txJson.specVersion, 9100);
+      should.deepEqual(txJson.nonce, 200);
+      should.deepEqual(txJson.tip, 0);
+      should.deepEqual(txJson.transactionVersion, 7);
+      should.deepEqual(txJson.chainName, 'Polkadot');
+      should.deepEqual(txJson.eraPeriod, 64);
+    });
+
+    it('should build an unsigned transfer transaction', async () => {
+      builder
+        .testnet()
+        .amount('90034235235322')
+        .dest(receiver.address)
+        .sender(sender.address)
+        .blockNumber(3933)
+        .blockHash('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
+        .nonce(200)
+        .tip(0)
+        .transactionVersion(7)
+        .eraPeriod(64);
+      const tx = await builder.build();
+      const txJson = tx.toJson();
+      should.deepEqual(txJson.amount, '90034235235322');
+      should.deepEqual(txJson.dest, receiver.address);
+      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.blockNumber, 3933);
+      should.deepEqual(txJson.blockHash, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
+      should.deepEqual(txJson.genesisHash, '0x2b8d4fdbb41f4bc15b8a7ec8ed0687f2a1ae11e0fc2dc6604fa962a9421ae349');
+      should.deepEqual(txJson.specVersion, 9100);
+      should.deepEqual(txJson.nonce, 200);
+      should.deepEqual(txJson.tip, 0);
+      should.deepEqual(txJson.transactionVersion, 7);
+      should.deepEqual(txJson.chainName, 'Polkadot');
+      should.deepEqual(txJson.eraPeriod, 64);
+    });
+
+    it('should build from raw signed tx', async () => {
+      builder.testnet().from(DotResources.rawTx.transfer.signed);
+      builder
+        .blockNumber(3933)
+        .blockHash('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
+        .transactionVersion(7);
+      const tx = await builder.build();
+      const txJson = tx.toJson();
+      should.deepEqual(txJson.amount, '1000000000000');
+      should.deepEqual(txJson.dest, receiver.address);
+      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.blockNumber, 3933);
+      should.deepEqual(txJson.blockHash, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
+      should.deepEqual(txJson.genesisHash, '0x2b8d4fdbb41f4bc15b8a7ec8ed0687f2a1ae11e0fc2dc6604fa962a9421ae349');
+      should.deepEqual(txJson.specVersion, 9100);
+      should.deepEqual(txJson.nonce, 200);
+      should.deepEqual(txJson.tip, 0);
+      should.deepEqual(txJson.transactionVersion, 7);
+      should.deepEqual(txJson.chainName, 'Polkadot');
+      should.deepEqual(txJson.eraPeriod, 64);
+    });
+
+    it('should build from raw unsigned tx', async () => {
+      builder.testnet().from(DotResources.rawTx.transfer.unsigned);
+      builder
+        .blockNumber(3933)
+        .blockHash('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
+        .sender(sender.address)
+        .sign({ key: sender.secretKey });
+      const tx = await builder.build();
+      const txJson = tx.toJson();
+      should.deepEqual(txJson.amount, '1000000000000');
+      should.deepEqual(txJson.dest, receiver.address);
+      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.blockNumber, 3933);
+      should.deepEqual(txJson.blockHash, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
+      should.deepEqual(txJson.genesisHash, '0x2b8d4fdbb41f4bc15b8a7ec8ed0687f2a1ae11e0fc2dc6604fa962a9421ae349');
+      should.deepEqual(txJson.specVersion, 9100);
+      should.deepEqual(txJson.nonce, 200);
+      should.deepEqual(txJson.eraPeriod, 64);
+      should.deepEqual(txJson.tip, 0);
+      should.deepEqual(txJson.transactionVersion, 7);
+      should.deepEqual(txJson.chainName, 'Polkadot');
+    });
+  });
+});
