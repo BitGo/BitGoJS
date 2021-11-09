d733cc53aa26af60f5565ad719646c56612b24c7
Otto Allmendinger <otto@bitgo.com>
2021-11-09 11:20:58 +0100

    test(core): add tests for verifySignature/explainTransaction
    Issue: BG-38773

--- modules/core/test/v2/unit/coins/utxo/util/index.ts
+++ modules/core/test/v2/unit/coins/utxo/util/index.ts
@@ -2,5 +2,5 @@ export { utxoCoins, getUtxoCoin, getUtxoCoinForNetwork } from './utxoCoins';
 export { getFixture, shouldEqualJSON } from './fixtures';
 export { keychains, Triple } from './keychains';
 export { getUtxoWallet } from './wallet';
-export { InputScriptType, mockUnspent, mockUnspentReplayProtection } from './unspents';
-export { transactionToObj, transactionHexToObj } from './transaction';
+export { InputScriptType, mockUnspent, mockUnspentReplayProtection, deriveKey } from './unspents';
+export { TransactionObj, transactionToObj, transactionHexToObj } from './transaction';
