a09b5cdd37bd9d32babbe427fe26943de1f0e8b7
Abrar Hussain <abrarhussain@bitgo.com>
2021-11-24 15:02:21 -0800

    copy over from other branch

--- modules/core/src/mpc/tss.ts
+++ modules/core/src/mpc/tss.ts
@@ -1,102 +1,133 @@
+/**
+ * Module provides functions for MPC using threshold signature scheme (TSS). It contains
+ * functions for key generation and message signing with EdDSA.
+ *
+ *
+ * ======================
+ * EdDSA Key Generation
+ * ======================
+ * 1. Each signer generates their own key share, which involves a private u-share and a public y-share.
+ * 2. Signers distribute their y-share to other signers.
+ * 3. After exhanging y-shares the next phase is to combine key shares. Each signer combines their u-share
+ *    with the y-shares received from other signers in order to generate a p-share for themselves. We
+ *    also save j-shares for other signers.
+ * 4. At this point the players do not distribute any shares and the first phase of the
+ *    signing protocol is complete.
+ *
+ * ======================
+ * EdDSA Signing
+ * ======================
+ * 1. The parties from key generation decide they want to sign something. They begin the signing protocol
+ *    by generating shares of an ephemereal key.
+ *
+ *    a) Each signer uses his p-share and the j-shares stored for other players to generate his signing share.
+ *    b) This results in each signer having a private x-share and public r-shares.
+ *
+ * 2. Signers distribute their r-shares to other signers.
+ * 3. After exchanging r-shares, each signer signs their share of the ephemereal key using their private
+ *    x-share with the r-shares from other signers.
+ * 4. This results in each signer having a public g-share which they send to the other signers.
+ * 5. After the signers broadcast their g-shares, the final signature can be reconstructed indepently.
+ */
 const assert = require('assert');
 import Ed25519Curve from './curves';
 import { sha512 } from 'js-sha512';
 import Shamir from './shamir';
 import { randomBytes as cryptoRandomBytes } from 'crypto';
-
+ 
 const convertObjectHexToBuffer = (object: Record<string, any>, keys: string[]) => {
   const result: any = object;
-  keys.forEach(key => {
+  keys.forEach((key) => {
     result[key] = Buffer.from(object[key], 'hex');
   });
   return result;
 };
-
-interface PrivateKeyShare {
-  i: string,
-  y: string,
-  u: string,
-  prefix: string,
-}
-
-interface PublicKeyShare {
-  i: string,
-  j: string,
-  y: string,
-  u: string,
-}
-
-interface KeyShare {
-  private: PrivateKeyShare,
-  public: Record<string, PublicKeyShare>
-}
-
-interface PrivateKeyCombine {
-  i: string,
-  x: string,
-  y: string,
-  prefix: string,
-}
-
-interface PublicKeyCombine {
-  i: string,
-  j: string,
-}
-
-interface KeyCombine {
-  private: PrivateKeyCombine,
-  public: Record<string, PublicKeyCombine>
-}
-
-interface PrivateSignShare {
-  i: string,
-  y: string,
-  x: string,
-  r: string,
-  R: string,
-}
-
-interface PublicSignShare {
-  i: string,
-  j: string,
-  r: string,
-  R: string,
-}
-
-interface SignShare {
-  private: PrivateSignShare,
-  public: Record<string, PublicSignShare>,
-}
-
-interface GShare {
-  i: string,
-  y: string,
-  gamma: string,
-  R: string,
-}
-
-interface Signature {
-  y: string,
-  R: string,
-  sigma: string,
-}
-
+ 
+ interface PlayerKeyShare {
+   i: string;
+   y: string;
+   u: string;
+   prefix: string;
+ }
+ 
+ interface DistributedKeyShare {
+   i: string;
+   j: string;
+   y: string;
+   u: string;
+ }
+ 
+ interface KeyShare {
+   player: PlayerKeyShare;
+   distributed: Record<string, DistributedKeyShare>;
+ }
+ 
+ interface PlayerKeyCombine {
+   i: string;
+   x: string;
+   y: string;
+   prefix: string;
+ }
+ 
+ interface DistributedKeyCombine {
+   i: string;
+   j: string;
+ }
+ 
+ interface KeyCombine {
+   player: PlayerKeyCombine;
+   distributed: Record<string, DistributedKeyCombine>;
+ }
+ 
+ interface PlayerSignShare {
+   i: string;
+   y: string;
+   x: string;
+   r: string;
+   R: string;
+ }
+ 
+ interface DistributedSignShare {
+   i: string;
+   j: string;
+   r: string;
+   R: string;
+ }
+ 
+ interface SignShare {
+   player: PlayerSignShare;
+   distributed: Record<string, DistributedSignShare>;
+ }
+ 
+ interface GShare {
+   i: string;
+   y: string;
+   gamma: string;
+   R: string;
+ }
+ 
+ interface Signature {
+   y: string;
+   R: string;
+   sigma: string;
+ }
+ 
 const Eddsa = async () => {
   const ed25519 = await Ed25519Curve();
   const shamir = Shamir(ed25519);
-
+ 
   const keyShare = (index: number, threshold: number, numShares: number): KeyShare => {
     assert(index > 0 && index <= numShares);
     const sk = cryptoRandomBytes(32);
     const h = Buffer.from(sha512.digest(sk));
     const zeroBuffer = Buffer.alloc(64 - h.length);
     const combinedBuffer = Buffer.concat([zeroBuffer, h]);
-
+ 
     let uBuffer = combinedBuffer.slice(0, 32);
     uBuffer[0] &= 248;
     uBuffer[31] &= 63;
     uBuffer[31] |= 64;
-
+ 
     // using big endian
     const zeroBuffer32 = Buffer.alloc(32);
     uBuffer = Buffer.concat([zeroBuffer32, uBuffer]);
@@ -104,29 +135,29 @@ const Eddsa = async () => {
     const u = Buffer.from(ed25519.scalarReduce(uBuffer));
     const y = Buffer.from(ed25519.basePointMult(u));
     const split_u = shamir.split(u, threshold, numShares);
-    
+ 
     // using big endian
     let prefixBuffer = combinedBuffer.subarray(32, combinedBuffer.length);
     prefixBuffer = Buffer.concat([zeroBuffer32, prefixBuffer]);
     prefixBuffer.reverse();
     const prefix = Buffer.from(ed25519.scalarReduce(prefixBuffer));
-
-    const P_i: PrivateKeyShare = {
+ 
+    const P_i: PlayerKeyShare = {
       i: index.toString(),
       y: y.toString('hex'),
       u: split_u[index.toString()].toString('hex'),
       prefix: prefix.toString('hex'),
     };
     const shares: KeyShare = {
-      private: P_i,
-      public: {},
+      player: P_i,
+      distributed: {},
     };
-
+ 
     for (const ind in split_u) {
       if (ind === index.toString()) {
         continue;
       }
-      shares['public'][ind] = {
+      shares['distributed'][ind] = {
         i: ind,
         j: P_i['i'],
         y: y.toString('hex'),
@@ -135,112 +166,113 @@ const Eddsa = async () => {
     }
     return shares;
   };
-  const keyCombine = (privateShare: PrivateKeyShare, publicShares: PublicKeyShare[]): KeyCombine => {
+  const keyCombine = (playerShare: PlayerKeyShare, distributedShares: DistributedKeyShare[]): KeyCombine => {
     // u-shares and y-shares required buffer format for curve functions
-    const shares = [privateShare, ...publicShares].map(share => convertObjectHexToBuffer(share, ['y', 'u']));
-    const yShares = shares.map(share => share['y']);
-    const uShares = shares.map(share => share['u']);
+    const shares = [playerShare, ...distributedShares].map((share) => convertObjectHexToBuffer(share, ['y', 'u']));
+    const yShares = shares.map((share) => share['y']);
+    const uShares = shares.map((share) => share['u']);
     const y = Buffer.from(yShares.reduce((partial, share) => ed25519.pointAdd(partial, share)));
     const x = Buffer.from(uShares.reduce((partial, share) => ed25519.scalarAdd(partial, share)));
-
-    const P_i: PrivateKeyCombine = {
-      i: privateShare['i'],
+ 
+    const P_i: PlayerKeyCombine = {
+      i: playerShare['i'],
       y: y.toString('hex'),
       x: x.toString('hex'),
-      prefix: privateShare['prefix'],
+      prefix: playerShare['prefix'],
     };
     const players: KeyCombine = {
-      private: P_i,
-      public: {},
+      player: P_i,
+      distributed: {},
     };
-
-    for (let ind = 0; ind < publicShares.length; ind++) {
-      const P_j = publicShares[ind];
-      players.public[P_j['j']] = {
+ 
+    for (let ind = 0; ind < distributedShares.length; ind++) {
+      const P_j = distributedShares[ind];
+      players.distributed[P_j['j']] = {
         i: P_j['j'],
         j: P_i['i'],
       };
     }
     return players;
   };
-
-  const signShare = (message: Buffer, privateShare: PrivateKeyCombine,
-    publicShares: PublicKeyCombine[]): SignShare => {
-
+ 
+  const signShare = (
+    message: Buffer,
+    playerShare: PlayerKeyCombine,
+    distributedShares: DistributedKeyCombine[],
+  ): SignShare => {
     // x-shares and y-shares required buffer format for curve functions
-    const shares = [convertObjectHexToBuffer(privateShare, ['x', 'y']), ...publicShares];
+    const shares = [convertObjectHexToBuffer(playerShare, ['x', 'y']), ...distributedShares];
     const indices = shares.map((share) => share['i']);
-
+ 
     const randomBuffer = cryptoRandomBytes(32);
-    const prefix_reference = Buffer.from(privateShare['prefix'], 'hex');
+    const prefix_reference = Buffer.from(playerShare['prefix'], 'hex');
     prefix_reference.reverse();
     const combinedBuffer = Buffer.concat([prefix_reference, message, randomBuffer]);
-
+ 
     // using big endian
     const digest = Buffer.from(sha512.digest(combinedBuffer));
     digest.reverse();
-
+ 
     const r = Buffer.from(ed25519.scalarReduce(digest));
     const R = Buffer.from(ed25519.basePointMult(r));
     const split_r = shamir.split(r, shares.length, shares.length, indices);
-
-    const P_i: PrivateSignShare = {
-      i: privateShare['i'],
-      y: privateShare['y'],
-      x: privateShare['x'],
-      r: split_r[privateShare['i']].toString('hex'),
+ 
+    const P_i: PlayerSignShare = {
+      i: playerShare['i'],
+      y: playerShare['y'],
+      x: playerShare['x'],
+      r: split_r[playerShare['i']].toString('hex'),
       R: R.toString('hex'),
     };
-
+ 
     const resultShares: SignShare = {
-      private: P_i,
-      public: {},
+      player: P_i,
+      distributed: {},
     };
-
-    for (let ind = 0; ind < publicShares.length; ind++) {
-      const S_j = publicShares[ind];
-      resultShares['public'][S_j['i']] = {
+ 
+    for (let ind = 0; ind < distributedShares.length; ind++) {
+      const S_j = distributedShares[ind];
+      resultShares['distributed'][S_j['i']] = {
         i: S_j['i'],
-        j: privateShare['i'],
+        j: playerShare['i'],
         r: split_r[S_j['i']].toString('hex'),
         R: R.toString('hex'),
       };
     }
     return resultShares;
   };
-
-  const sign = (message: Buffer, privateShare: PrivateSignShare, publicShares: PublicSignShare[]): GShare => {
+ 
+  const sign = (message: Buffer, playerShare: PlayerSignShare, distributedShares: DistributedSignShare[]): GShare => {
     // x, y, r, R required buffer format for curve functions
-    const shares = [privateShare, ...publicShares].map(share => convertObjectHexToBuffer(share, ['R', 'r']));
-    const S_i = privateShare;
-
-    const rShares = shares.map(share => share['R']);
-    const littleRShares = shares.map(share => share['r']);
-
+    const shares = [playerShare, ...distributedShares].map((share) => convertObjectHexToBuffer(share, ['R', 'r']));
+    const S_i = playerShare;
+ 
+    const rShares = shares.map((share) => share['R']);
+    const littleRShares = shares.map((share) => share['r']);
+ 
     const R = Buffer.from(rShares.reduce((partial, share) => ed25519.pointAdd(partial, share)));
     const r = Buffer.from(littleRShares.reduce((partial, share) => ed25519.scalarAdd(partial, share)));
-
+ 
     const combinedBuffer = Buffer.concat([R, Buffer.from(S_i['y'], 'hex'), message]);
     const digest = Buffer.from(sha512.digest(combinedBuffer));
     const k = Buffer.from(ed25519.scalarReduce(digest));
-
-    const gamma = Buffer.from(ed25519.scalarAdd(r,
-      ed25519.scalarMult(k, Buffer.from(S_i['x'], 'hex'))));
+ 
+    const gamma = Buffer.from(ed25519.scalarAdd(r, ed25519.scalarMult(k, Buffer.from(S_i['x'], 'hex'))));
     const result = {
-      i: privateShare['i'],
-      y: privateShare['y'],
+      i: playerShare['i'],
+      y: playerShare['y'],
       gamma: gamma.toString('hex'),
       R: R.toString('hex'),
     };
     return result;
   };
-
+ 
   const signCombine = (shares: GShare[]): Signature => {
-    shares = shares.map(share => convertObjectHexToBuffer(share, ['y', 'gamma', 'R']));
+    shares = shares.map((share) => convertObjectHexToBuffer(share, ['y', 'gamma', 'R']));
     const keys = Object.keys(shares);
     const y = shares[keys[0]]['y'];
     const R = shares[keys[0]]['R'];
-
+ 
     const resultShares = {};
     for (const ind in shares) {
       const S_i = shares[ind];
@@ -255,15 +287,19 @@ const Eddsa = async () => {
     };
     return result;
   };
-
+ 
   const verify = (message: Buffer, signature: Signature): Buffer => {
     const publicKey = Buffer.from(signature['y'], 'hex');
-    const signedMessage = Buffer.concat([Buffer.from(signature['R'], 'hex'),
-      Buffer.from(signature['sigma'], 'hex'), message]);
+    const signedMessage = Buffer.concat([
+      Buffer.from(signature['R'], 'hex'),
+      Buffer.from(signature['sigma'], 'hex'),
+      message,
+    ]);
     return Buffer.from(ed25519.verify(publicKey, signedMessage));
   };
-
+ 
   return { keyShare, keyCombine, signShare, sign, signCombine, verify };
 };
-
+ 
 export default Eddsa;
+ 
--- modules/core/test/v2/unit/tss.ts
+++ modules/core/test/v2/unit/tss.ts
@@ -12,17 +12,17 @@ describe('TSS EDDSA key generation and signing', () => {
     const B = MPC.keyShare(2, 2, 3);
     const C = MPC.keyShare(3, 2, 3);
 
-    const A_combine = MPC.keyCombine(A.private, [B.public[1], C.public[1]]);
-    const B_combine = MPC.keyCombine(B.private, [A.public[2], C.public[2]]);
+    const A_combine = MPC.keyCombine(A.player, [B.distributed[1], C.distributed[1]]);
+    const B_combine = MPC.keyCombine(B.player, [A.distributed[2], C.distributed[2]]);
     // const C_combine = MPC.keyCombine([A[3], B[3], C[3]]);
 
     const message = 'MPC on a Friday night';
     const message_buffer = Buffer.from(message, 'utf-8');
-    const A_sign_share = MPC.signShare(message_buffer, A_combine.private, [A_combine.public[2]]);
-    const B_sign_share = MPC.signShare(message_buffer, B_combine.private, [B_combine.public[1]]);
+    const A_sign_share = MPC.signShare(message_buffer, A_combine.player, [A_combine.distributed[2]]);
+    const B_sign_share = MPC.signShare(message_buffer, B_combine.player, [B_combine.distributed[1]]);
 
-    const A_sign = MPC.sign(message_buffer, A_sign_share.private, [B_sign_share.public[1]]);
-    const B_sign = MPC.sign(message_buffer, B_sign_share.private, [A_sign_share.public[2]]);
+    const A_sign = MPC.sign(message_buffer, A_sign_share.player, [B_sign_share.distributed[1]]);
+    const B_sign = MPC.sign(message_buffer, B_sign_share.player, [A_sign_share.distributed[2]]);
 
     const signature = MPC.signCombine([A_sign, B_sign]);
     const result = MPC.verify(message_buffer, signature).toString();
