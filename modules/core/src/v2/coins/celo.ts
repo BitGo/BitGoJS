1fa5e517809a7604ec157beb16d6156201e3a29a
Luis Covarrubias <luiscovarrubias@bitgo.com>
2021-11-29 21:53:37 -0800

    feat(core): override celo verifyAddress
    Overriding this method since for celo the address make
    take a few seconds to generate the address. therefore
    we make try to verify an address that is not present yet
    and will error out.
    
    TICKET: BG-39201

--- modules/core/src/v2/coins/celo.ts
+++ modules/core/src/v2/coins/celo.ts
@@ -1,11 +1,12 @@
 /**
  * @prettier
  */
-import { BaseCoin } from '../baseCoin';
+import { BaseCoin, VerifyAddressOptions } from '../baseCoin';
 import { BitGo } from '../../bitgo';
 import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
 import { AbstractEthLikeCoin } from './abstractEthLikeCoin';
 import { Celo as CeloAccountLib } from '@bitgo/account-lib';
+import { InvalidAddressError } from '../../errors';
 
 export class Celo extends AbstractEthLikeCoin {
   protected constructor(bitgo: BitGo, staticsCoin?: Readonly<StaticsBaseCoin>) {
@@ -24,5 +25,18 @@ export class Celo extends AbstractEthLikeCoin {
       valid = false;
     }
     return valid;
+  }
+
+  verifyAddress({ address, coinSpecific }: VerifyAddressOptions): boolean {
+    // for celo if the coin is pending init then an error will be thrown
+    // returning true to 'skip' verification
+    if (coinSpecific && coinSpecific.pendingChainInitialization) {
+      return true;
+    }
+
+    if (!this.isValidAddress(address)) {
+      throw new InvalidAddressError(`invalid address: ${address}`);
+    }
+    return true;
   }
 }
--- modules/core/src/v2/coins/tcelo.ts
+++ modules/core/src/v2/coins/tcelo.ts
@@ -4,9 +4,10 @@
 import { BaseCoin } from '../baseCoin';
 import { BitGo } from '../../bitgo';
 import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
-import { AbstractEthLikeCoin } from './abstractEthLikeCoin';
+import { Celo } from './celo';
+// import { AbstractEthLikeCoin } from './abstractEthLikeCoin';
 
-export class Tcelo extends AbstractEthLikeCoin {
+export class Tcelo extends Celo {
   protected constructor(bitgo: BitGo, staticsCoin?: Readonly<StaticsBaseCoin>) {
     super(bitgo, staticsCoin);
   }
