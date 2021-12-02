fedcb7e6c4c4078cc1425f74f068f6933a64861b
Otto Allmendinger <otto@bitgo.com>
2021-12-02 15:54:43 +0100

    refactor(core): unwrap explainTransaction
    Remove bluebird wrapping
    
    Issue: BG-35154

--- modules/core/src/v2/coins/abstractUtxoCoin.ts
+++ modules/core/src/v2/coins/abstractUtxoCoin.ts
@@ -1236,136 +1236,127 @@ export abstract class AbstractUtxoCoin extends BaseCoin {
    * Decompose a raw transaction into useful information, such as the total amounts,
    * change amounts, and transaction outputs.
    * @param params
-   * @param callback
    */
-  explainTransaction(
-    params: ExplainTransactionOptions,
-    callback?: NodeCallback<TransactionExplanation>
-  ): Bluebird<TransactionExplanation> {
-    const self = this;
-    return co<TransactionExplanation>(function* () {
-      const txHex = _.get(params, 'txHex');
-      if (!txHex || !_.isString(txHex) || !txHex.match(/^([a-f0-9]{2})+$/i)) {
-        throw new Error('invalid transaction hex, must be a valid hex string');
-      }
+  explainTransaction(params: ExplainTransactionOptions): Promise<TransactionExplanation> {
+    const txHex = _.get(params, 'txHex');
+    if (!txHex || !_.isString(txHex) || !txHex.match(/^([a-f0-9]{2})+$/i)) {
+      throw new Error('invalid transaction hex, must be a valid hex string');
+    }
 
-      let transaction;
-      try {
-        transaction = self.createTransactionFromHex(txHex);
-      } catch (e) {
-        throw new Error('failed to parse transaction hex');
-      }
+    let transaction;
+    try {
+      transaction = this.createTransactionFromHex(txHex);
+    } catch (e) {
+      throw new Error('failed to parse transaction hex');
+    }
 
-      const id = transaction.getId();
-      let changeAddresses: string[] = [];
-      let spendAmount = 0;
-      let changeAmount = 0;
-      const txInfo = _.get(params, 'txInfo');
-      if (txInfo && txInfo.changeAddresses) {
-        changeAddresses = txInfo.changeAddresses;
-      }
-      const explanation: any = {
-        displayOrder: ['id', 'outputAmount', 'changeAmount', 'outputs', 'changeOutputs'],
-        id: id,
-        outputs: [],
-        changeOutputs: [],
-      };
+    const id = transaction.getId();
+    let changeAddresses: string[] = [];
+    let spendAmount = 0;
+    let changeAmount = 0;
+    const txInfo = _.get(params, 'txInfo');
+    if (txInfo && txInfo.changeAddresses) {
+      changeAddresses = txInfo.changeAddresses;
+    }
+    const explanation: any = {
+      displayOrder: ['id', 'outputAmount', 'changeAmount', 'outputs', 'changeOutputs'],
+      id: id,
+      outputs: [],
+      changeOutputs: [],
+    };
 
-      transaction.outs.forEach((currentOutput) => {
-        const currentAddress = utxolib.address.fromOutputScript(currentOutput.script, self.network);
-        const currentAmount = currentOutput.value;
+    transaction.outs.forEach((currentOutput) => {
+      const currentAddress = utxolib.address.fromOutputScript(currentOutput.script, this.network);
+      const currentAmount = currentOutput.value;
 
-        if (changeAddresses.indexOf(currentAddress) !== -1) {
-          // this is change
-          changeAmount += currentAmount;
-          explanation.changeOutputs.push({
-            address: currentAddress,
-            amount: currentAmount,
-          });
-          return;
-        }
-
-        spendAmount += currentAmount;
-        explanation.outputs.push({
+      if (changeAddresses.indexOf(currentAddress) !== -1) {
+        // this is change
+        changeAmount += currentAmount;
+        explanation.changeOutputs.push({
           address: currentAddress,
           amount: currentAmount,
         });
+        return;
+      }
+
+      spendAmount += currentAmount;
+      explanation.outputs.push({
+        address: currentAddress,
+        amount: currentAmount,
       });
-      explanation.outputAmount = spendAmount;
-      explanation.changeAmount = changeAmount;
+    });
+    explanation.outputAmount = spendAmount;
+    explanation.changeAmount = changeAmount;
 
-      // add fee info if available
-      if (params.feeInfo) {
-        explanation.displayOrder.push('fee');
-        explanation.fee = params.feeInfo;
+    // add fee info if available
+    if (params.feeInfo) {
+      explanation.displayOrder.push('fee');
+      explanation.fee = params.feeInfo;
+    }
+
+    if (_.isInteger(transaction.locktime) && transaction.locktime > 0) {
+      explanation.locktime = transaction.locktime;
+      explanation.displayOrder.push('locktime');
+    }
+
+    const unspentValues = {};
+
+    // get information on tx inputs
+    const inputSignatures = transaction.ins.map((input, idx) => {
+      const hasSigScript = !_.isEmpty(input.script);
+      const hasWitnessScript = !_.isEmpty(input.witness);
+
+      if (!hasSigScript && !hasWitnessScript) {
+        // no sig script or witness data for this input
+        debug('no signature script or witness script data for input %s', idx);
+        return 0;
       }
 
-      if (_.isInteger(transaction.locktime) && transaction.locktime > 0) {
-        explanation.locktime = transaction.locktime;
-        explanation.displayOrder.push('locktime');
+      let parsedSigScript;
+      try {
+        parsedSigScript = utxolib.bitgo.parseSignatureScript(transaction.ins[idx]);
+      } catch (e) {
+        return false;
       }
 
-      const unspentValues = {};
-
-      // get information on tx inputs
-      const inputSignatures = transaction.ins.map((input, idx) => {
-        const hasSigScript = !_.isEmpty(input.script);
-        const hasWitnessScript = !_.isEmpty(input.witness);
-
-        if (!hasSigScript && !hasWitnessScript) {
-          // no sig script or witness data for this input
-          debug('no signature script or witness script data for input %s', idx);
+      if (hasWitnessScript) {
+        if (!txInfo || !txInfo.unspents) {
+          // segwit txs require input values, cannot validate signatures
+          debug('unable to retrieve input amounts from unspents - cannot validate segwit input signatures');
           return 0;
         }
 
-        let parsedSigScript;
-        try {
-          parsedSigScript = utxolib.bitgo.parseSignatureScript(transaction.ins[idx]);
-        } catch (e) {
+        // lazily populate unspent values
+        if (_.isEmpty(unspentValues)) {
+          txInfo.unspents.forEach((unspent) => {
+            unspentValues[unspent.id] = unspent.value;
+          });
+        }
+      }
+
+      const nonEmptySignatures = parsedSigScript.signatures?.filter((sig) => !_.isEmpty(sig)) ?? [];
+      const validSignatures = nonEmptySignatures.map((sig, sigIndex) => {
+        if (_.isEmpty(sig)) {
           return false;
         }
 
-        if (hasWitnessScript) {
-          if (!txInfo || !txInfo.unspents) {
-            // segwit txs require input values, cannot validate signatures
-            debug('unable to retrieve input amounts from unspents - cannot validate segwit input signatures');
-            return 0;
-          }
+        const parentTxId = (Buffer.from(input.hash).reverse() as Buffer).toString('hex');
+        const inputId = `${parentTxId}:${input.index}`;
+        const amount = unspentValues[inputId];
 
-          // lazily populate unspent values
-          if (_.isEmpty(unspentValues)) {
-            txInfo.unspents.forEach((unspent) => {
-              unspentValues[unspent.id] = unspent.value;
-            });
-          }
+        try {
+          return utxolib.bitgo.verifySignature(transaction, idx, amount, { signatureIndex: sigIndex });
+        } catch (e) {
+          return false;
         }
-
-        const nonEmptySignatures = parsedSigScript.signatures?.filter((sig) => !_.isEmpty(sig)) ?? [];
-        const validSignatures = nonEmptySignatures.map((sig, sigIndex) => {
-          if (_.isEmpty(sig)) {
-            return false;
-          }
-
-          const parentTxId = (Buffer.from(input.hash).reverse() as Buffer).toString('hex');
-          const inputId = `${parentTxId}:${input.index}`;
-          const amount = unspentValues[inputId];
-
-          try {
-            return utxolib.bitgo.verifySignature(transaction, idx, amount, { signatureIndex: sigIndex });
-          } catch (e) {
-            return false;
-          }
-        });
-
-        return validSignatures.reduce((validCount, isValid) => (isValid ? validCount + 1 : validCount), 0);
       });
 
-      explanation.inputSignatures = inputSignatures;
-      explanation.signatures = _.max(inputSignatures);
-      return explanation;
-    })
-      .call(this)
-      .asCallback(callback);
+      return validSignatures.reduce((validCount, isValid) => (isValid ? validCount + 1 : validCount), 0);
+    });
+
+    explanation.inputSignatures = inputSignatures;
+    explanation.signatures = _.max(inputSignatures);
+    return explanation;
   }
 
   /**
