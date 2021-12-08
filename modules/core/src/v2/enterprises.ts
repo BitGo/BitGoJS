a1959b9a1f185e9ae454f9a92c134099e579e174
Tyler Levine <tyler@bitgo.com>
2021-12-07 21:44:22 -0800

    chore(core): remove coroutines from v2/enterprises
    Ticket: BG-31214

--- modules/core/src/v2/enterprises.ts
+++ modules/core/src/v2/enterprises.ts
@@ -1,15 +1,11 @@
 /**
  * @prettier
  */
-import * as Bluebird from 'bluebird';
 import * as _ from 'lodash';
 import { BitGo } from '../bitgo';
 
 import { BaseCoin } from './baseCoin';
 import { Enterprise } from './enterprise';
-import { NodeCallback } from './types';
-
-const co = Bluebird.coroutine;
 
 export interface GetEnterpriseOptions {
   id?: string;
@@ -27,57 +23,39 @@ export class Enterprises {
   /**
    * List all enterprises available to the current user
    * @param params unused
-   * @param callback
    */
-  public list(params: Record<string, never> = {}, callback?: NodeCallback<Enterprise[]>): Bluebird<Enterprise[]> {
-    const self = this;
-    return co<Enterprise[]>(function* () {
-      const response = (yield self.bitgo.get(self.bitgo.url('/enterprise')).result()) as any;
-      return response.enterprises.map((e) => {
-        // instantiate a new object for each enterprise
-        return new Enterprise(self.bitgo, self.baseCoin, e);
-      });
-    })
-      .call(this)
-      .asCallback(callback);
+  public async list(params: Record<string, never> = {}): Promise<Enterprise[]> {
+    const response = (await this.bitgo.get(this.bitgo.url('/enterprise')).result()) as any;
+    return response.enterprises.map((e) => {
+      // instantiate a new object for each enterprise
+      return new Enterprise(this.bitgo, this.baseCoin, e);
+    });
   }
 
   /**
    * Fetch an enterprise from BitGo
    * @param params
-   * @param callback
    */
-  public get(params: GetEnterpriseOptions = {}, callback?: NodeCallback<Enterprise>): Bluebird<Enterprise> {
-    const self = this;
-    return co<Enterprise>(function* () {
-      const enterpriseId = params.id;
-      if (_.isUndefined(enterpriseId)) {
-        throw new Error('id must not be empty');
-      }
-      if (!_.isString(enterpriseId)) {
-        throw new Error('id must be hexadecimal enterprise ID');
-      }
+  public async get(params: GetEnterpriseOptions = {}): Promise<Enterprise> {
+    const enterpriseId = params.id;
+    if (_.isUndefined(enterpriseId)) {
+      throw new Error('id must not be empty');
+    }
+    if (!_.isString(enterpriseId)) {
+      throw new Error('id must be hexadecimal enterprise ID');
+    }
 
-      const enterpriseData = (yield self.bitgo.get(self.bitgo.url(`/enterprise/${enterpriseId}`)).result()) as any;
-      return new Enterprise(self.bitgo, self.baseCoin, enterpriseData);
-    })
-      .call(this)
-      .asCallback(callback);
+    const enterpriseData = (await this.bitgo.get(this.bitgo.url(`/enterprise/${enterpriseId}`)).result()) as any;
+    return new Enterprise(this.bitgo, this.baseCoin, enterpriseData);
   }
 
   /**
    * Create a new enterprise
    * @param params
-   * @param callback
    */
   // TODO: (CT-686) Flesh out params object with valid enterprise creation parameters
-  public create(params: any = {}, callback?: NodeCallback<Enterprise>): Bluebird<Enterprise> {
-    const self = this;
-    return co<Enterprise>(function* () {
-      const enterpriseData = (yield self.bitgo.post(self.bitgo.url(`/enterprise`)).send(params).result()) as any;
-      return new Enterprise(self.bitgo, self.baseCoin, enterpriseData);
-    })
-      .call(this)
-      .asCallback(callback);
+  public async create(params: any = {}): Promise<Enterprise> {
+    const enterpriseData = (await this.bitgo.post(this.bitgo.url(`/enterprise`)).send(params).result()) as any;
+    return new Enterprise(this.bitgo, this.baseCoin, enterpriseData);
   }
 }
