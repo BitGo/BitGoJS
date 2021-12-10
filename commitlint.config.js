ecb08a90937e9697fb9395246323e576dbe0d29e
Tyler Levine <tyler@bitgo.com>
2021-12-09 18:52:00 -0800

    chore(root): restrict commit scopes to module names and `root`
    Ticket: BG-39083

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,11 @@
+const { readdir } = require('fs').promises;
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   ignores: [
     (commit) => /^Merge commit '[a-f0-9]{40}'$/m.test(commit),
   ],
+  rules: {
+    'scope-enum': async () => [2, 'always', (await readdir('modules')).concat('root')],
+  },
 };
