3a8ce3045d8095ae8502f8d8437ae111dde308d5
Abrar Hussain <abrarhussain@bitgo.com>
2021-12-21 09:58:01 -0800

    fix tests

--- modules/core/test/v2/unit/tss.ts
+++ modules/core/test/v2/unit/tss.ts
@@ -18,46 +18,41 @@ describe('TSS EDDSA key generation and signing', () => {
 
     const message = 'MPC on a Friday night';
     const message_buffer = Buffer.from(message, 'utf-8');
-    // signing with A and B
-    let A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[2]]);
-    let B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [B_combine.jShares[1]]);
-
-    let A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [B_sign_share.rShares[1]]);
-    let B_sign = MPC.sign(message_buffer, B_sign_share.xShare, [A_sign_share.rShares[2]]);
-
-    let signature = MPC.signCombine([A_sign, B_sign]);
+    // signing with 3-3 signatures
+    let A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[2], A_combine.jShares[3]]);
+    let B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [B_combine.jShares[1], B_combine.jShares[3]]);
+    let C_sign_share = MPC.signShare(message_buffer, C_combine.pShare, [C_combine.jShares[1], C_combine.jShares[2]]);
+    let A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [B_sign_share.rShares[1], C_sign_share.rShares[1]]);
+    let B_sign = MPC.sign(message_buffer, B_sign_share.xShare, [A_sign_share.rShares[2], C_sign_share.rShares[2]]);
+    let C_sign = MPC.sign(message_buffer, C_sign_share.xShare, [A_sign_share.rShares[3], B_sign_share.rShares[3]]);
+    let signature = MPC.signCombine([A_sign, B_sign, C_sign]);
     let result = MPC.verify(message_buffer, signature).toString();
     result.should.equal(message);
 
-    // signing with A and C
-    A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[3]]);
-    let C_sign_share = MPC.signShare(message_buffer, C_combine.pShare, [C_combine.jShares[1]]);
-
-    A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [C_sign_share.rShares[1]]);
-    let C_sign = MPC.sign(message_buffer, C_sign_share.xShare, [A_sign_share.rShares[3]]);
-    signature = MPC.signCombine([A_sign, C_sign]);
-    result = MPC.verify(message_buffer, signature).toString();
-    result.should.equal(message);
-
-    // signing with B and C
+    // signing with A and B
     A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[2]]);
-    B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [C_combine.jShares[1]]);
-
+    B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [B_combine.jShares[1]]);
     A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [B_sign_share.rShares[1]]);
     B_sign = MPC.sign(message_buffer, B_sign_share.xShare, [A_sign_share.rShares[2]]);
     signature = MPC.signCombine([A_sign, B_sign]);
     result = MPC.verify(message_buffer, signature).toString();
     result.should.equal(message);
 
-    // signing with 3-3 signatures
-    A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[2], A_combine.jShares[3]]);
-    B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [B_combine.jShares[1], B_combine.jShares[3]]);
-    C_sign_share = MPC.signShare(message_buffer, C_combine.pShare, [C_combine.jShares[1], C_combine.jShares[2]]);
+    // signing with A and C
+    A_sign_share = MPC.signShare(message_buffer, A_combine.pShare, [A_combine.jShares[3]]);
+    C_sign_share = MPC.signShare(message_buffer, C_combine.pShare, [C_combine.jShares[1]]);
+    A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [C_sign_share.rShares[1]]);
+    C_sign = MPC.sign(message_buffer, C_sign_share.xShare, [A_sign_share.rShares[3]]);
+    signature = MPC.signCombine([A_sign, C_sign]);
+    result = MPC.verify(message_buffer, signature).toString();
+    result.should.equal(message);
 
-    A_sign = MPC.sign(message_buffer, A_sign_share.xShare, [B_sign_share.rShares[1], C_sign_share.rShares[1]]);
-    B_sign = MPC.sign(message_buffer, B_sign_share.xShare, [A_sign_share.rShares[2], C_sign_share.rShares[2]]);
-    C_sign = MPC.sign(message_buffer, C_sign_share.xShare, [A_sign_share.rShares[3], B_sign_share.rShares[3]]);
-    signature = MPC.signCombine([A_sign, B_sign, C_sign]);
+    // signing with B and C
+    B_sign_share = MPC.signShare(message_buffer, B_combine.pShare, [B_combine.jShares[3]]);
+    C_sign_share = MPC.signShare(message_buffer, C_combine.pShare, [C_combine.jShares[2]]);
+    B_sign = MPC.sign(message_buffer, B_sign_share.xShare, [C_sign_share.rShares[2]]);
+    C_sign = MPC.sign(message_buffer, C_sign_share.xShare, [B_sign_share.rShares[3]]);
+    signature = MPC.signCombine([B_sign, C_sign]);
     result = MPC.verify(message_buffer, signature).toString();
     result.should.equal(message);
   });
