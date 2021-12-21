597734f364fe575f4cd361daaf2257551155ef54
silalvado <silvialvado@gmail.com>
2021-12-21 14:51:13 -0300

    feat(account-lib): export Solana builders and transaction
    Export Solana builders and transaction
    
    stlx-11064

--- modules/account-lib/src/coin/sol/index.ts
+++ modules/account-lib/src/coin/sol/index.ts
@@ -1,5 +1,8 @@
 import * as Utils from './utils';
 
 export { KeyPair } from './keyPair';
+export { Transaction } from './transaction';
 export { TransactionBuilderFactory } from './transactionBuilderFactory';
+export { TransferBuilder } from './transferBuilder';
+export { WalletInitializationBuilder } from './walletInitializationBuilder';
 export { Utils };
