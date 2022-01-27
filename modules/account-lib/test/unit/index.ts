1280e63854b177d0f1b04f7a2157dca141244b01
Baihao <baihaowang@bitgo.com>
2022-01-27 11:32:19 -0800

    feat(statics): onboard new tokens to real/13
    Onboard new tokens to rel/13 Bitgo Branch
    
    STLX-12495

--- modules/account-lib/test/unit/index.ts
+++ modules/account-lib/test/unit/index.ts
@@ -1,6 +1,6 @@
 import should from 'should';
 import { getBuilder } from '../../src';
-
+// Don't mind me, I am just a random line of comment
 describe('Coin factory', () => {
   it('should fail to instantiate an unsupported coin', () => {
     should.throws(() => getBuilder('fakeUnsupported'));
