e6bd9ae79f2017dca62d1fa6f528f4fb2a35bf2a
Jichao Li <jichaoli@bitgo.com>
2022-02-09 17:25:42 -0500

    feat(core): enable hop transactions in avaxc Ticket: STLX-12577

--- modules/core/src/v2/coins/avaxc.ts
+++ modules/core/src/v2/coins/avaxc.ts
@@ -2,10 +2,15 @@
  * @prettier
  */
 import { BigNumber } from 'bignumber.js';
+import * as bip32 from 'bip32';
+import * as Keccak from 'keccak';
+import * as secp256k1 from 'secp256k1';
+import * as _ from 'lodash';
 // import * as debugLib from 'debug';
 
 import {
   BaseCoin,
+  FeeEstimateOptions,
   FullySignedTransaction,
   HalfSignedAccountTransaction,
   KeyPair,
@@ -15,7 +20,7 @@ import {
   VerifyAddressOptions,
   VerifyTransactionOptions,
   TransactionFee,
-  TransactionRecipient as Recipient,
+  // TransactionRecipient,
   TransactionPrebuild as BaseTransactionPrebuild,
   TransactionExplanation,
 } from '../baseCoin';
@@ -24,6 +29,11 @@ import { BitGo } from '../../bitgo';
 import { InvalidAddressError, MethodNotImplementedError } from '../../errors';
 import { BaseCoin as StaticsBaseCoin, CoinFamily } from '@bitgo/statics';
 import { getBuilder, Eth, AvaxC as AvaxCAccountLib } from '@bitgo/account-lib';
+import * as common from '../../common';
+
+import { optionalDeps } from './eth';
+import { Erc20Token } from './erc20Token';
+import { Wallet } from '../wallet';
 
 // const debug = debugLib('bitgo:v2:avaxc');
 
@@ -34,6 +44,63 @@ export interface ExplainTransactionOptions {
     txHex: string;
   };
   feeInfo: TransactionFee;
+}
+
+// For createHopTransactionParams
+interface HopTransactionBuildOptions {
+  wallet: Wallet;
+  recipients: Recipient[];
+  walletPassphrase: string;
+}
+
+interface Recipient {
+  address: string;
+  amount: string;
+  data?: string;
+}
+
+// For getExtraPrebuildParams
+interface BuildOptions {
+  hop?: boolean;
+  wallet?: Wallet;
+  recipients?: Recipient[];
+  walletPassphrase?: string;
+  [index: string]: unknown;
+}
+
+// For FeeEstimate
+interface FeeEstimate {
+  gasLimitEstimate: number;
+  feeEstimate: number;
+}
+
+/**
+ * The extra parameters to send to platform build route for hop transactions
+ */
+interface HopParams {
+  hopParams: {
+    gasPriceMax: number;
+    userReqSig: string;
+    paymentId: string;
+  };
+  gasLimit: number;
+}
+
+/**
+ * The prebuilt hop transaction returned from the HSM
+ */
+interface HopPrebuild {
+  tx: string;
+  id: string;
+  signature: string;
+  paymentId: string;
+  gasPrice: number;
+  gasLimit: number;
+  amount: number;
+  recipient: string;
+  nonce: number;
+  userReqSig: string;
+  gasPriceMax: number;
 }
 
 // For txPreBuild
@@ -218,6 +285,71 @@ export class AvaxC extends BaseCoin {
   }
 
   /**
+   * Above is standard BaseCoins functions
+   * ================================================================================================================
+   * ================================================================================================================
+   * Below is transaction functions
+   */
+
+  /**
+   * Validates that the hop prebuild from the HSM is valid and correct
+   * @param wallet The wallet that the prebuild is for
+   * @param hopPrebuild The prebuild to validate
+   * @param originalParams The original parameters passed to prebuildTransaction
+   * @returns void
+   * @throws Error if The prebuild is invalid
+   */
+  async validateHopPrebuild(
+    wallet: Wallet,
+    hopPrebuild: HopPrebuild,
+    originalParams?: { recipients: Recipient[] }
+  ): Promise<void> {
+    const { tx, id, signature } = hopPrebuild;
+
+    // first, validate the HSM signature
+    const serverXpub = common.Environments[this.bitgo.getEnv()].hsmXpub;
+    const serverPubkeyBuffer: Buffer = bip32.fromBase58(serverXpub).publicKey;
+    const signatureBuffer: Buffer = Buffer.from(optionalDeps.ethUtil.stripHexPrefix(signature), 'hex');
+    const messageBuffer: Buffer = Buffer.from(optionalDeps.ethUtil.stripHexPrefix(id), 'hex');
+
+    const sig = new Uint8Array(signatureBuffer.slice(1));
+    const isValidSignature: boolean = secp256k1.ecdsaVerify(sig, messageBuffer, serverPubkeyBuffer);
+    if (!isValidSignature) {
+      throw new Error(`Hop txid signature invalid`);
+    }
+
+    const builtHopTx = optionalDeps.EthTx.TransactionFactory.fromSerializedData(optionalDeps.ethUtil.toBuffer(tx));
+    // If original params are given, we can check them against the transaction prebuild params
+    if (!_.isNil(originalParams)) {
+      const { recipients } = originalParams;
+
+      // Then validate that the tx params actually equal the requested params
+      const originalAmount = new BigNumber(recipients[0].amount);
+      const originalDestination: string = recipients[0].address;
+
+      const hopAmount = new BigNumber(optionalDeps.ethUtil.bufferToHex(builtHopTx.value));
+      if (!builtHopTx.to) {
+        throw new Error(`Transaction does not have a destination address`);
+      }
+      const hopDestination = builtHopTx.to.toString();
+      if (!hopAmount.eq(originalAmount)) {
+        throw new Error(`Hop amount: ${hopAmount} does not equal original amount: ${originalAmount}`);
+      }
+      if (hopDestination.toLowerCase() !== originalDestination.toLowerCase()) {
+        throw new Error(`Hop destination: ${hopDestination} does not equal original recipient: ${hopDestination}`);
+      }
+    }
+
+    if (!builtHopTx.verifySignature()) {
+      // We dont want to continue at all in this case, at risk of ETH being stuck on the hop address
+      throw new Error(`Invalid hop transaction signature, txid: ${id}`);
+    }
+    if (optionalDeps.ethUtil.addHexPrefix(builtHopTx.hash().toString('hex')) !== id) {
+      throw new Error(`Signed hop txid does not equal actual txid`);
+    }
+  }
+
+  /**
    * Assemble half-sign prebuilt transaction
    * @param params
    */
@@ -236,6 +368,132 @@ export class AvaxC extends BaseCoin {
         expiration: params.txPrebuild.expireTime,
       },
     };
+  }
+
+  /**
+   * Modify prebuild before sending it to the server. Add things like hop transaction params
+   * @param buildParams The whitelisted parameters for this prebuild
+   * @param buildParams.hop True if this should prebuild a hop tx, else false
+   * @param buildParams.recipients The recipients array of this transaction
+   * @param buildParams.wallet The wallet sending this tx
+   * @param buildParams.walletPassphrase the passphrase for this wallet
+   */
+  async getExtraPrebuildParams(buildParams: BuildOptions): Promise<BuildOptions> {
+    if (
+      !_.isUndefined(buildParams.hop) &&
+      buildParams.hop &&
+      !_.isUndefined(buildParams.wallet) &&
+      !_.isUndefined(buildParams.recipients) &&
+      !_.isUndefined(buildParams.walletPassphrase)
+    ) {
+      if (this instanceof Erc20Token) {
+        throw new Error(
+          `Hop transactions are not enabled for ERC-20 tokens, nor are they necessary. Please remove the 'hop' parameter and try again.`
+        );
+      }
+      return (await this.createHopTransactionParams({
+        wallet: buildParams.wallet,
+        recipients: buildParams.recipients,
+        walletPassphrase: buildParams.walletPassphrase,
+      })) as any;
+    }
+    return {};
+  }
+
+  /**
+   * Creates the extra parameters needed to build a hop transaction
+   * @param buildParams The original build parameters
+   * @returns extra parameters object to merge with the original build parameters object and send to the platform
+   */
+  async createHopTransactionParams(buildParams: HopTransactionBuildOptions): Promise<HopParams> {
+    const wallet = buildParams.wallet;
+    const recipients = buildParams.recipients;
+    const walletPassphrase = buildParams.walletPassphrase;
+
+    const userKeychain = await this.keychains().get({ id: wallet.keyIds()[0] });
+    const userPrv = wallet.getUserPrv({ keychain: userKeychain, walletPassphrase });
+    const userPrvBuffer = bip32.fromBase58(userPrv).privateKey;
+    if (!userPrvBuffer) {
+      throw new Error('invalid userPrv');
+    }
+    if (!recipients || !Array.isArray(recipients)) {
+      throw new Error('expecting array of recipients');
+    }
+
+    // Right now we only support 1 recipient
+    if (recipients.length !== 1) {
+      throw new Error('must send to exactly 1 recipient');
+    }
+    const recipientAddress = recipients[0].address;
+    const recipientAmount = recipients[0].amount;
+    const feeEstimateParams = {
+      recipient: recipientAddress,
+      amount: recipientAmount,
+      hop: true,
+    };
+    const feeEstimate: FeeEstimate = await this.feeEstimate(feeEstimateParams);
+
+    const gasLimit = feeEstimate.gasLimitEstimate;
+    const gasPrice = Math.round(feeEstimate.feeEstimate / gasLimit);
+    const gasPriceMax = gasPrice * 5;
+    // Payment id a random number so its different for every tx
+    const paymentId = Math.floor(Math.random() * 10000000000).toString();
+    const hopDigest: Buffer = AvaxC.getHopDigest([
+      recipientAddress,
+      recipientAmount,
+      gasPriceMax.toString(),
+      gasLimit.toString(),
+      paymentId,
+    ]);
+
+    const userReqSig = optionalDeps.ethUtil.addHexPrefix(
+      Buffer.from(secp256k1.ecdsaSign(hopDigest, userPrvBuffer).signature).toString('hex')
+    );
+
+    return {
+      hopParams: {
+        gasPriceMax,
+        userReqSig,
+        paymentId,
+      },
+      gasLimit,
+    };
+  }
+
+  /**
+   * Fetch fee estimate information from the server
+   * @param {Object} params The params passed into the function
+   * @param {Boolean} [params.hop] True if we should estimate fee for a hop transaction
+   * @param {String} [params.recipient] The recipient of the transaction to estimate a send to
+   * @param {String} [params.data] The ETH tx data to estimate a send for
+   * @returns {Object} The fee info returned from the server
+   */
+  async feeEstimate(params: FeeEstimateOptions): Promise<FeeEstimate> {
+    const query: FeeEstimateOptions = {};
+    if (params && params.hop) {
+      query.hop = params.hop;
+    }
+    if (params && params.recipient) {
+      query.recipient = params.recipient;
+    }
+    if (params && params.data) {
+      query.data = params.data;
+    }
+    if (params && params.amount) {
+      query.amount = params.amount;
+    }
+
+    return await this.bitgo.get(this.url('/tx/fee')).query(query).result();
+  }
+
+  /**
+   * Gets the hop digest for the user to sign. This is validated in the HSM to prove that the user requested this tx
+   * @param paramsArr The parameters to hash together for the digest
+   */
+  private static getHopDigest(paramsArr: string[]): Buffer {
+    const hash = new Keccak('keccak256');
+    hash.update([AvaxC.hopTransactionSalt, ...paramsArr].join('$'));
+    return hash.digest();
   }
 
   // async getExtraPreBuildParams(buildParams: )
