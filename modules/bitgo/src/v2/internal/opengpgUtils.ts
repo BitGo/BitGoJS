c07b2dc78d47042ed1edbaac1f49d29fe6971c95
Joey Bai <joeybai@bitgo.com>
2022-04-28 13:19:22 -0700

    feat: support opengpg signatures
    add util functions to
    - encrypt and sign
    - decrypt and verify
    
    this will be used for TSS key creation
    
    Ticket: BG-45374

--- modules/bitgo/src/v2/internal/opengpgUtils.ts
+++ modules/bitgo/src/v2/internal/opengpgUtils.ts
@@ -1,4 +1,12 @@
-import { createMessage, encrypt, Key, readKey } from 'openpgp';
+import {
+  createMessage,
+  decrypt,
+  encrypt,
+  Key,
+  readKey,
+  readMessage,
+  readPrivateKey,
+} from 'openpgp';
 import { BitGo } from '../../bitgo';
 
 /**
@@ -18,9 +26,13 @@ export async function getBitgoGpgPubKey(bitgo: BitGo): Promise<Key> {
 
 /**
  * Encrypts string using gpg key
+ * @DEPRECATED - should use encryptAndSignText instead for added security
+ *
  * @param text string to encrypt
  * @param key encryption key
  * @return {string} encrypted string
+ *
+ * TODO(BG-47170): Delete once gpg signatures are fully supported
  */
 export async function encryptText(text: string, key: Key): Promise<string> {
   const messageToEncrypt = await createMessage({
@@ -36,4 +48,54 @@ export async function encryptText(text: string, key: Key): Promise<string> {
       showComment: false,
     },
   });
+}
+
+
+/**
+ * Encrypts and signs a string
+ * @param text string to encrypt and sign
+ * @param publicArmor public key to encrypt with
+ * @param privateArmor private key to sign with
+ */
+export async function encryptAndSignText(text: string, publicArmor: string, privateArmor: string): Promise<string> {
+  const publicKey = await readKey({ armoredKey: publicArmor });
+  const privateKey = await readPrivateKey({ armoredKey: privateArmor });
+
+  const message = await createMessage({ text });
+
+  const signedMessage = await encrypt({
+    message,
+    encryptionKeys: publicKey,
+    signingKeys: privateKey,
+    format: 'armored',
+    config: {
+      rejectCurves: new Set(),
+      showVersion: false,
+      showComment: false,
+    },
+  });
+
+  return signedMessage;
+}
+
+/**
+ * Reads a signed and encrypted message
+ *
+ * @param signed signed and encrypted message
+ * @param publicArmor public key to verify signature
+ * @param privateArmor private key to decrypt message
+ */
+export async function readSignedMessage(signed: string, publicArmor: string, privateArmor: string): Promise<string> {
+  const publicKey = await readKey({ armoredKey: publicArmor });
+  const privateKey = await readPrivateKey({ armoredKey: privateArmor });
+
+  const message = await readMessage({ armoredMessage: signed });
+  const decrypted = await decrypt({
+    message,
+    verificationKeys: publicKey,
+    decryptionKeys: privateKey,
+    expectSigned: true,
+  });
+
+  return decrypted.data;
 }
