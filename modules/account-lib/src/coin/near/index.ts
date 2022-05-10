a29f593797f6bda268a5e13e3ab2c2a870bee125
Abhay Shekhawat <abhayshekhawat@bitgo.com>
2022-05-10 18:15:29 +0530

    feat(account-lib): added missing exports for Near staking related builders BG-39200

--- modules/account-lib/src/coin/near/index.ts
+++ modules/account-lib/src/coin/near/index.ts
@@ -4,5 +4,9 @@ export { KeyPair } from './keyPair';
 export { Transaction } from './transaction';
 export { TransactionBuilder } from './transactionBuilder';
 export { TransferBuilder } from './transferBuilder';
+export { StakingActivateBuilder } from './stakingActivateBuilder';
+export { StakingDeactivateBuilder } from './stakingDeactivateBuilder';
+export { StakingWithdrawBuilder } from './stakingWithdrawBuilder';
+
 export { TransactionBuilderFactory } from './transactionBuilderFactory';
 export { Utils };
