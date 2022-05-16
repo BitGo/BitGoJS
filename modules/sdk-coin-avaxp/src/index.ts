328d546897d5df645d5bcbf6ca22c56d045bc306
Samuel S Kim <samuelkim@bitgo.com>
2022-05-17 03:39:39 +0700

    feat(sdk-coin-avaxp): add new sdk coin avaxp
    add avax-p module decoupled from account lib.  Avaxp is a utxo based
    coin but uses features of account lib, this now follows the modularized
    syntax of the SDK
    
    Ticket: STLX-16393

---
+++ modules/sdk-coin-avaxp/src/index.ts
@@ -0,0 +1,6 @@
+import * as Utils from './utils';
+export { KeyPair } from './keyPair';
+export { Transaction } from './transaction';
+export { TransactionBuilder } from './transactionBuilder';
+export { TransactionBuilderFactory } from './transactionBuilderFactory';
+export { Utils };
