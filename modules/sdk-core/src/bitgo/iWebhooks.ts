85989e8b61255af7654134ec0696d1e7774fc8fa
Aaron Loe <aaronloe@bitgo.com>
2022-05-17 13:37:22 -0700

    chore(sdk-core): extract webhooks interface
    - adds interface for iWebhooks
    
    Ticket: BG-48407

--- modules/sdk-core/src/bitgo/iWebhooks.ts
+++ modules/sdk-core/src/bitgo/iWebhooks.ts
@@ -1,3 +1,29 @@
+export interface AddOptions {
+  url: string;
+  type: 'block' | 'wallet_confirmation';
+  label?: string;
+  numConfirmations?: number;
+}
+
+export interface RemoveOptions {
+  url: string;
+  type: 'block' | 'wallet_confirmation';
+}
+
+export interface ListNotificationsOptions {
+  prevId?: string;
+  limit?: number;
+}
+
+export interface SimulateOptions {
+  webhookId: string;
+  blockId: string;
+}
+
 export interface IWebhooks {
-  placehold: unknown;
+  list(): Promise<any>;
+  add(url: undefined, type: undefined, label: undefined, numConfirmations: undefined): Promise<any>;
+  remove(params: RemoveOptions): Promise<any>;
+  listNotifications(params: ListNotificationsOptions): Promise<any>;
+  simulate(params: SimulateOptions): Promise<any>;
 }
