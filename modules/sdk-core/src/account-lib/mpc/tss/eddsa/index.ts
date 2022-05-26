662c3daea0e1d10e94f23ebeadd49c7c2237a153
Duc Nguyen <ducnguyen@bitgo.com>
2022-05-26 18:38:13 -0400

    chore(sdk-core): remove mpc/tss from sdk-core
    - Removed mpc/tss from sdk-core in order to use git mv.
    
    Ticket: BG-48167

--- modules/sdk-core/src/account-lib/mpc/tss/eddsa/index.ts
+++
@@ -1,6 +0,0 @@
-import Eddsa from './eddsa';
-
-// EDDSA Specific types
-export * as EDDSA from './types';
-
-export default Eddsa;
--- modules/sdk-core/src/account-lib/mpc/tss/index.ts
+++
@@ -1,7 +0,0 @@
-import Eddsa, { EDDSA } from './eddsa';
-
-// exporting all eddsa types and making eddsa as default for backward compatibility
-export * from './eddsa/types';
-export default Eddsa;
-
-export { Eddsa, EDDSA };
