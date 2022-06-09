63c83f7eede67e4cecf731435f243b3c7c27fec5
Akash Gianchandani <akashgianchandani@bitgo.com>
2022-06-09 10:31:24 +0530

    feat(bitgo): add explain tx polygon BG-41729

--- modules/bitgo/src/v2/coins/tpolygon.ts
+++ modules/bitgo/src/v2/coins/tpolygon.ts
@@ -3,14 +3,18 @@
  */
 import { BaseCoin, BitGoBase } from '@bitgo/sdk-core';
 import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
-import { AbstractEthLikeCoin } from './abstractEthLikeCoin';
+import { Polygon } from './polygon';
 
-export class Tpolygon extends AbstractEthLikeCoin {
+export class Tpolygon extends Polygon {
   protected constructor(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>) {
     super(bitgo, staticsCoin);
   }
 
   static createInstance(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>): BaseCoin {
     return new Tpolygon(bitgo, staticsCoin);
+  }
+
+  getChain(): string {
+    return 'tpolygon';
   }
 }
