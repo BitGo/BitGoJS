8c1ce90c582be7cbca744f3452a15d9fff6a224c
Akash Gianchandani <akashgianchandani@bitgo.com>
2022-06-09 22:04:48 +0530

    feat(bitgo): implements sign tx for polygon BG-41727

--- modules/bitgo/src/v2/coins/polygon.ts
+++ modules/bitgo/src/v2/coins/polygon.ts
@@ -1,18 +1,35 @@
 /**
  * @prettier
  */
-import { BaseCoin, BitGoBase } from '@bitgo/sdk-core';
-import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
-import { AbstractEthLikeCoin } from './abstractEthLikeCoin';
-import { Polygon as PolygonAccountLib } from '@bitgo/account-lib';
+import { BaseCoin, BitGoBase, TransactionExplanation } from '@bitgo/sdk-core';
+import { BaseCoin as StaticsBaseCoin, EthereumNetwork } from '@bitgo/statics';
+import { Eth, Recipient, GetSendMethodArgsOptions, SendMethodArgs, optionalDeps } from './eth';
+import { getBuilder, Polygon as PolygonAccountLib } from '@bitgo/account-lib';
+import BigNumber from 'bignumber.js';
+import { ExplainTransactionOptions } from './abstractEthLikeCoin';
 
-export class Polygon extends AbstractEthLikeCoin {
+export class Polygon extends Eth {
   protected constructor(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>) {
     super(bitgo, staticsCoin);
   }
 
   static createInstance(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>): BaseCoin {
     return new Polygon(bitgo, staticsCoin);
+  }
+
+  getChain(): string {
+    return 'polygon';
+  }
+
+  getNetwork(): EthereumNetwork | undefined {
+    return this.staticsCoin?.network as EthereumNetwork;
+  }
+
+  /**
+   * Get the base chain that the coin exists on.
+   */
+  getBaseChain(): string {
+    return this.getChain();
   }
 
   isValidPub(pub: string): boolean {
@@ -23,5 +40,109 @@ export class Polygon extends AbstractEthLikeCoin {
       valid = false;
     }
     return valid;
+  }
+
+  /**
+   * Get transfer operation for coin
+   * @param recipient recipient info
+   * @param expireTime expiry time
+   * @param contractSequenceId sequence id
+   * @returns {Array} operation array
+   */
+  getOperation(recipient: Recipient, expireTime: number, contractSequenceId: number): (string | Buffer)[][] {
+    return [
+      ['string', 'address', 'uint256', 'bytes', 'uint256', 'uint256'],
+      [
+        'POLYGON',
+        new optionalDeps.ethUtil.BN(optionalDeps.ethUtil.stripHexPrefix(recipient.address), 16),
+        recipient.amount,
+        Buffer.from(optionalDeps.ethUtil.stripHexPrefix(recipient.data) || '', 'hex'),
+        expireTime,
+        contractSequenceId,
+      ],
+    ];
+  }
+
+  /**
+   * Build arguments to call the send method on the wallet contract
+   * @param txInfo
+   */
+  getSendMethodArgs(txInfo: GetSendMethodArgsOptions): SendMethodArgs[] {
+    // Method signature is
+    // sendMultiSig(address toAddress, uint256 value, bytes data, uint256 expireTime, uint256 sequenceId, bytes signature)
+    return [
+      {
+        name: 'toAddress',
+        type: 'address',
+        value: txInfo.recipient.address,
+      },
+      {
+        name: 'value',
+        type: 'uint256',
+        value: txInfo.recipient.amount,
+      },
+      {
+        name: 'data',
+        type: 'bytes',
+        value: optionalDeps.ethUtil.toBuffer(optionalDeps.ethUtil.addHexPrefix(txInfo.recipient.data || '')),
+      },
+      {
+        name: 'expireTime',
+        type: 'uint256',
+        value: txInfo.expireTime,
+      },
+      {
+        name: 'sequenceId',
+        type: 'uint256',
+        value: txInfo.contractSequenceId,
+      },
+      {
+        name: 'signature',
+        type: 'bytes',
+        value: optionalDeps.ethUtil.toBuffer(optionalDeps.ethUtil.addHexPrefix(txInfo.signature)),
+      },
+    ];
+  }
+
+  /**
+   * Explain a transaction from txHex
+   * @param params The options with which to explain the transaction
+   */
+  async explainTransaction(params: ExplainTransactionOptions): Promise<TransactionExplanation> {
+    const txHex = params.txHex || (params.halfSigned && params.halfSigned.txHex);
+    if (!txHex || !params.feeInfo) {
+      throw new Error('missing explain tx parameters');
+    }
+    const txBuilder = this.getTransactionBuilder();
+    txBuilder.from(txHex);
+    const tx = await txBuilder.build();
+    const outputs = tx.outputs.map((output) => {
+      return {
+        address: output.address,
+        amount: output.value,
+      };
+    });
+
+    const displayOrder = ['id', 'outputAmount', 'changeAmount', 'outputs', 'changeOutputs', 'fee'];
+
+    return {
+      displayOrder,
+      id: tx.id,
+      outputs: outputs,
+      outputAmount: outputs
+        .reduce((accumulator, output) => accumulator.plus(output.amount), new BigNumber('0'))
+        .toFixed(0),
+      changeOutputs: [], // account based does not use change outputs
+      changeAmount: '0', // account base does not make change
+      fee: params.feeInfo,
+    };
+  }
+
+  /**
+   * Create a new transaction builder for the current chain
+   * @return a new transaction builder
+   */
+  protected getTransactionBuilder(): PolygonAccountLib.TransactionBuilder {
+    return getBuilder(this.getBaseChain()) as PolygonAccountLib.TransactionBuilder;
   }
 }
--- modules/bitgo/test/v2/unit/coins/polygon.ts
+++ modules/bitgo/test/v2/unit/coins/polygon.ts
@@ -1,20 +1,139 @@
 import { TestBitGo } from '../../../lib/test_bitgo';
 import { Polygon } from '../../../../src/v2/coins/polygon';
 import { Tpolygon } from '../../../../src/v2/coins/tpolygon';
+import * as should from 'should';
+import { getBuilder, Polygon as PolygonAccountLib } from '@bitgo/account-lib';
+import { TransactionType } from '@bitgo/sdk-core';
 
 describe('Polygon', function () {
   let bitgo;
+  let basecoin;
+
+  /**
+   * Build an unsigned account-lib multi-signature send transactino
+   * @param destination The destination address of the transaction
+   * @param contractAddress The address of the smart contract processing the transaction
+   * @param contractSequenceId The sequence id of the contract
+   * @param nonce The nonce of the sending address
+   * @param expireTime The expire time of the transaction
+   * @param amount The amount to send to the recipient
+   * @param gasPrice The gas price of the transaction
+   * @param gasLimit The gas limit of the transaction
+   */
+  const buildUnsignedTransaction = async function ({
+    destination,
+    contractAddress,
+    contractSequenceId = 1,
+    nonce = 0,
+    expireTime = Math.floor(new Date().getTime() / 1000),
+    amount = '100000',
+    gasPrice = '10000',
+    gasLimit = '20000',
+  }) {
+    const txBuilder: PolygonAccountLib.TransactionBuilder = getBuilder('tpolygon') as PolygonAccountLib.TransactionBuilder;
+    txBuilder.type(TransactionType.Send);
+    txBuilder.fee({
+      fee: gasPrice,
+      gasLimit: gasLimit,
+    });
+    txBuilder.counter(nonce);
+    txBuilder.contract(contractAddress);
+    const transferBuilder = txBuilder.transfer() as PolygonAccountLib.TransferBuilder;
+
+    transferBuilder
+      .coin('tpolygon')
+      .expirationTime(expireTime)
+      .amount(amount)
+      .to(destination)
+      .contractSequenceId(contractSequenceId);
+
+    return await txBuilder.build();
+  };
 
   before(function () {
     bitgo = new TestBitGo({ env: 'mock' });
     bitgo.initializeTestVars();
+    basecoin = bitgo.coin('tpolygon');
   });
 
-  it('should instantiate the coin', function () {
-    let localBasecoin = bitgo.coin('tpolygon');
-    localBasecoin.should.be.an.instanceof(Tpolygon);
+  describe('Instantiate', () => {
 
-    localBasecoin = bitgo.coin('polygon');
-    localBasecoin.should.be.an.instanceof(Polygon);
+    it('should instantiate the coin', function () {
+      let localBasecoin = bitgo.coin('tpolygon');
+      localBasecoin.should.be.an.instanceof(Tpolygon);
+
+      localBasecoin = bitgo.coin('polygon');
+      localBasecoin.should.be.an.instanceof(Polygon);
+    });
+
   });
+
+  describe('Explain transaction:', () => {
+
+    it('should fail if the params object is missing parameters', async function () {
+      const explainParams = {
+        feeInfo: { fee: 1 },
+        txHex: null,
+      };
+      await basecoin.explainTransaction(explainParams).should.be.rejectedWith('missing explain tx parameters');
+    });
+
+    it('explain a transfer transaction', async function () {
+      const destination = '0xfaa8f14f46a99eb439c50e0c3b835cc21dad51b4';
+      const contractAddress = '0x9e2c5712ab4caf402a98c4bf58c79a0dfe718ad1';
+
+      const unsignedTransaction = await buildUnsignedTransaction({
+        destination,
+        contractAddress,
+      });
+
+      const explainParams = {
+        halfSigned: {
+          txHex: unsignedTransaction.toBroadcastFormat(),
+        },
+        feeInfo: { fee: 1 },
+      };
+      const explanation = await basecoin.explainTransaction(explainParams);
+      should.exist(explanation.id);
+    });
+
+  });
+
+  describe('EIP1559', function () {
+    it('should sign a transaction with EIP1559 fee params', async function () {
+      const coin = bitgo.coin('tpolygon');
+
+      const userKeychain = {
+        prv: 'xprv9s21ZrQH143K3hekyNj7TciR4XNYe1kMj68W2ipjJGNHETWP7o42AjDnSPgKhdZ4x8NBAvaL72RrXjuXNdmkMqLERZza73oYugGtbLFXG8g',
+        pub: 'xpub661MyMwAqRbcGBjE5QG7pkf9cZD33UUD6K46q7ELrbuG7FqXfLNGiXYGHeEnGBb5AWREnk1eA28g8ArZvURbhshXWkTtddHRo54fgyVvLdb',
+        rawPub: '023636e68b7b204573abda2616aff6b584910dece2543f1cc6d842caac7d74974b',
+        rawPrv: '7438a50010ce7b1dfd86e68046cc78ba1ebd242d6d85d9904d3fcc08734bc172',
+      };
+
+      const halfSignedTransaction = await coin.signTransaction({
+        txPrebuild: {
+          eip1559: { maxPriorityFeePerGas: 10, maxFeePerGas: 10 },
+          isBatch: false,
+          recipients: [{
+            amount: '42',
+            address: '0xc93b13642d93b4218bb85f67317d6b37286e8028',
+          }],
+          expireTime: 1627949214,
+          contractSequenceId: 12,
+          gasLimit: undefined,
+          gasPrice: undefined,
+          hopTransaction: undefined,
+          backupKeyNonce: undefined,
+          sequenceId: undefined,
+          nextContractSequenceId: 0,
+        },
+        prv: userKeychain.prv,
+      });
+
+      halfSignedTransaction.halfSigned.eip1559.maxPriorityFeePerGas.should.equal(10);
+      halfSignedTransaction.halfSigned.eip1559.maxFeePerGas.should.equal(10);
+
+    });
+  });
+
 });
