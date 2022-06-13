2baadffb0e5d84196d9a59ec74ef6cf7a0a66476
Sachu Shaji Abraham <sachuabraham@bitgo.com>
2022-06-13 09:37:59 +0530

    feat(account-lib): add support for ecdsa sigining and verification tss
    this change add support for ecdsa signing and verification for TSS
    
    TICKET: BG-48874

--- modules/account-lib/test/unit/mpc/tss/ecdsa/ecdsa.ts
+++ modules/account-lib/test/unit/mpc/tss/ecdsa/ecdsa.ts
@@ -1,20 +1,25 @@
-import { Ecdsa } from '@bitgo/sdk-core';
+import { Ecdsa, EDDSA } from '@bitgo/sdk-core';
 /**
  * @prettier
  */
 
 describe('TSS ECDSA key generation', function () {
   const MPC = new Ecdsa();
-  it('should generate keys with correct threshold and share number', async function () {
+  let keyShares: EDDSA.KeyCombined[];
+  let commonPublicKey: string;
+
+  before(async () => {
     this.timeout(50000);
     const [A, B, C] = await Promise.all([MPC.keyShare(1, 2, 3), MPC.keyShare(2, 2, 3), MPC.keyShare(3, 2, 3)]);
 
     const A_combine = MPC.keyCombine(A.pShare, [B.nShares[1], C.nShares[1]]);
     const B_combine = MPC.keyCombine(B.pShare, [A.nShares[2], C.nShares[2]]);
     const C_combine = MPC.keyCombine(C.pShare, [A.nShares[3], B.nShares[3]]);
-    const keyShares = [A_combine, B_combine, C_combine];
-    const commonPublicKey = A_combine.xShare.y;
+    keyShares = [A_combine, B_combine, C_combine];
+    commonPublicKey = A_combine.xShare.y;
+  });
 
+  it('should generate keys with correct threshold and share number', async function () {
     for (let index = 0; index < 3; index++) {
       const participantOne = (index % 3) + 1;
       const participantTwo = ((index + 1) % 3) + 1;
@@ -52,5 +57,90 @@ describe('TSS ECDSA key generation', function () {
         e.should.equal('Invalid KeyShare Config');
       }
     }
+  });
+
+  it('should properly sign the message', async function () {
+    // Step One
+    // A, B have decided to sign the message
+    const [A, B] = keyShares;
+
+    // Step Two
+    // Sign Shares are created by one of the participants (A)
+    // with its private XShare and YShare corresponding to the other participant (B)
+    // This step produces a private WShare which A saves and KShare which A sends to B
+    const signSharesAB: EDDSA.SignShareRT = MPC.signShare(A.xShare, A.yShares['2']);
+
+    // Step Three
+    // B receives the KShare from A and uses it produce private
+    // BShare (Beta Share) which B saves and AShare (Alpha Share)
+    // which is sent to A
+    let signConvertBA: EDDSA.SignConvertRT = MPC.signConvert({
+      xShare: B.xShare,
+      yShare: B.yShares['1'], // YShare corresponding to the other participant A
+      kShare: signSharesAB.kShare,
+    });
+
+    // Step Four
+    // A receives the AShare from B and A using the private WShare from step two
+    // uses it produce private GShare (Gamma Share) and MUShare (Mu Share) which
+    // is sent to B to produce its Gamma Share
+    const signConvertAB: EDDSA.SignConvertRT = MPC.signConvert({
+      aShare: signConvertBA.aShare,
+      wShare: signSharesAB.wShare,
+    });
+
+    // Step Five
+    // B receives the MUShare from A and A using the private BShare from step three
+    // uses it produce private GShare (Gamma Share)
+
+    signConvertBA = MPC.signConvert({
+      muShare: signConvertAB.muShare,
+      bShare: signConvertBA.bShare,
+    });
+
+    // Step Six
+    // A and B both have successfully generated GShares and they use
+    // the sign combine function to generate their private omicron shares and
+    // delta shares which they share to each other
+
+    const [signCombineA, signCombineB] = [
+      MPC.signCombine({
+        gShares: signConvertAB.gShare,
+        giveMeANameShare: {
+          i: signConvertAB.muShare.i,
+          j: signConvertAB.muShare.j,
+        },
+      }),
+      MPC.signCombine({
+        gShares: signConvertBA.gShare,
+        giveMeANameShare: {
+          i: signConvertBA.muShare.i,
+          j: signConvertBA.muShare.j,
+        },
+      }),
+    ];
+
+    const MESSAGE = Buffer.from('I AM SATOSHI NAKAMOTO');
+
+    // Step Seven
+    // A and B shares the delta share from each other
+    // and finally signs the message using their private OShare
+    // and delta share received from the other participant
+
+    const [signA, signB] = [
+      MPC.sign(MESSAGE, signCombineA.oShare, signCombineB.dShare),
+      MPC.sign(MESSAGE, signCombineB.oShare, signCombineA.dShare),
+    ];
+
+    // Step Eight
+    // Construct the final signature
+
+    const signature = MPC.constructSignature([signA, signB]);
+
+    // Step Nine
+    // Verify signature
+
+    const isValid = MPC.verify(MESSAGE, signature);
+    isValid.should.equal(false); // @todo Need to recheck
   });
 });
--- modules/sdk-core/src/account-lib/mpc/tss/ecdsa/ecdsa.ts
+++ modules/sdk-core/src/account-lib/mpc/tss/ecdsa/ecdsa.ts
@@ -1,10 +1,34 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as paillierBigint from 'paillier-bigint';
-import { randomBytes } from 'crypto';
+import * as secp from '@noble/secp256k1';
+import { randomBytes, createHash } from 'crypto';
 import { bigIntToHex } from '../../../util/crypto';
-import { bigIntFromBufferBE, bigIntToBufferBE } from '../../util';
+import { bigIntFromBufferBE, bigIntToBufferBE, bigIntFromU8ABE, getPaillierPublicKey } from '../../util';
 import { Secp256k1Curve } from '../../curves';
 import Shamir from '../../shamir';
-import { NShare, PShare, KeyShare, KeyCombined } from './types';
+import {
+  NShare,
+  PShare,
+  KeyShare,
+  KeyCombined,
+  BShare,
+  AShare,
+  Signature,
+  SignConvertRT,
+  SignConvert,
+  GShare,
+  MUShare,
+  SignCombine,
+  SignCombineRT,
+  DShare,
+  OShare,
+  SignRT,
+  SShare,
+  SignShareRT,
+  KShare,
+  XShare,
+  YShare,
+} from './types';
 
 export default class Ecdsa {
   static curve: Secp256k1Curve = new Secp256k1Curve();
@@ -98,5 +122,252 @@ export default class Ecdsa {
       };
     }
     return participants;
+  }
+
+  /**
+   * Create signing shares.
+   * @param {xShare} xShare Private xShare of current participant signer
+   * @param {YShare} yShare yShare corresponding to the other participant signer
+   * @returns {SignShareRT} Returns the participant private w-share
+   * and k-share to be distributed to other participant signer
+   */
+  signShare(xShare: XShare, yShare: YShare): SignShareRT {
+    const pk = getPaillierPublicKey(BigInt(xShare.n));
+
+    const k = Ecdsa.curve.scalarRandom();
+    const gamma = Ecdsa.curve.scalarRandom();
+
+    const sharesIndex = [BigInt(xShare.i), BigInt(yShare.j)];
+
+    // @todo give a good name
+    const d = sharesIndex.reduce((acc, share) => {
+      return Ecdsa.curve.scalarMult(Ecdsa.curve.scalarSub(share, BigInt(xShare.i)), acc);
+    });
+
+    // @todo give a good name
+    const li = sharesIndex.reduce((acc, share) => {
+      return Ecdsa.curve.scalarMult(share, acc);
+    });
+
+    // @todo give a good name
+    const w = [li, BigInt(xShare['x']), Ecdsa.curve.scalarInvert(d)].reduce(Ecdsa.curve.scalarMult);
+
+    const signers: SignShareRT = {
+      wShare: {
+        i: xShare.i,
+        l: xShare.l,
+        m: xShare.m,
+        n: xShare.n,
+        y: xShare.y,
+        k: bigIntToHex(k),
+        w: bigIntToHex(w),
+        gamma: bigIntToHex(gamma),
+      },
+      kShare: {} as KShare,
+    };
+
+    signers.kShare = {
+      i: yShare.j,
+      j: xShare.i,
+      n: bigIntToHex(pk.n),
+      k: bigIntToHex(pk.encrypt(k)),
+    };
+
+    return signers;
+  }
+
+  /**
+   * Perform multiplicative-to-additive (MtA) share conversion with another
+   * signer by skipping computation of wShare and directly computing
+   * beta and alpha share (Short cut method for 2:n signing threshold)
+   * @param {SignConvert}
+   * @returns {SignConvertRT}
+   */
+  signConvert(shares: SignConvert): SignConvertRT {
+    // @todo fix return types give meaning full names add comments
+    let shareParticipant: BShare | GShare, shareToBeSend: AShare | MUShare;
+    let isGammaShare = false;
+    if (shares.xShare && shares.yShare && shares.kShare) {
+      const xShare = shares.xShare; // currentParticipant secret xShare
+      const yShare = shares.yShare;
+      const signShare = this.signShare(xShare, yShare);
+      shareToBeSend = { ...shares.kShare, alpha: '', mu: '' } as AShare;
+      shareParticipant = { ...signShare.wShare, beta: '', nu: '' } as BShare;
+    } else if ((shares.bShare && shares.muShare) || (shares.aShare && shares.wShare)) {
+      // w and a  ==> g and m // b and mu ==> g
+      isGammaShare = true;
+      shareToBeSend = shares.aShare ? ({ ...shares.aShare } as MUShare) : ({ ...shares.muShare } as any); // @todo recheck here , change shareToBESend use a type
+      shareParticipant = shares.wShare ? ({ ...shares.wShare } as GShare) : ({ ...shares.bShare } as GShare);
+    } else {
+      throw new Error('Invalid config for Sign Convert');
+    }
+    if (shareParticipant.i !== shareToBeSend.i) {
+      throw new Error('Shares from same participant');
+    }
+    if (shareToBeSend['alpha']) {
+      const pk = getPaillierPublicKey(BigInt(shareParticipant.n));
+      const sk = new paillierBigint.PrivateKey(BigInt(shareParticipant.l), BigInt(shareParticipant.m), pk);
+      const alpha = sk.decrypt(BigInt(shareToBeSend.alpha));
+      shareParticipant['alpha'] = Ecdsa.curve.scalarReduce(alpha);
+      const mu = sk.decrypt(BigInt(shareToBeSend.mu || BigInt(-1))); // Todo recheck
+      shareParticipant['mu'] = Ecdsa.curve.scalarReduce(mu);
+      delete shareParticipant['p'];
+      delete shareParticipant['q'];
+      delete shareToBeSend['alpha'];
+      delete shareToBeSend['mu'];
+    }
+    if (shareToBeSend['k']) {
+      const n = BigInt(shareToBeSend['n']); // Pallier pub from other signer
+      let pk = new paillierBigint.PublicKey(n, n + BigInt(1));
+      const k = pk.encrypt(BigInt(shareToBeSend['k']));
+      // MtA $k_j, \gamma_i$.
+      const beta0 = BigInt(100); // Todo Bring random int here
+      shareParticipant.beta = bigIntToHex(Ecdsa.curve.scalarNegate(beta0));
+      const alpha = BigInt(shareParticipant.gamma) * k + pk.encrypt(beta0);
+      shareToBeSend.alpha = bigIntToHex(alpha);
+      // MtA $k_j, w_i$.
+      const nu0 = BigInt(100); // Todo Bring random int here
+      shareParticipant.nu = bigIntToHex(Ecdsa.curve.scalarNegate(nu0));
+      const mu = BigInt(shareParticipant.w) * k + pk.encrypt(nu0);
+      shareToBeSend.mu = bigIntToHex(mu);
+      if (shareParticipant['alpha']) {
+        delete shareToBeSend['n'];
+        delete shareToBeSend['k'];
+      } else {
+        pk = getPaillierPublicKey(BigInt(shareParticipant.n));
+        shareToBeSend['n'] = bigIntToHex(pk.n);
+        shareToBeSend['k'] = bigIntToHex(pk.encrypt(BigInt(shareParticipant.k)));
+      }
+    }
+    if (!('alpha' in shareToBeSend) && !('k' in shareToBeSend)) {
+      shareToBeSend = {
+        i: shareToBeSend['i'],
+        j: shareToBeSend['j'],
+      };
+    }
+    [shareToBeSend['i'], shareToBeSend['j']] = [shareToBeSend['j'], shareToBeSend['i']];
+    if (isGammaShare) {
+      return {
+        muShare: shareToBeSend as MUShare,
+        gShare: shareParticipant as GShare,
+      };
+    }
+    return {
+      aShare: shareToBeSend,
+      bShare: shareParticipant,
+    };
+  }
+
+  /**
+   * Combine gamma shares to get the private omicron / delta shares
+   * @param {SignCombine} shares
+   * @returns {SignCombineRT}
+   */
+  signCombine(shares: SignCombine): SignCombineRT {
+    const gShare = shares.gShares;
+    const S = shares.giveMeANameShare;
+    const gamma = BigInt(gShare.gamma);
+    const alpha = BigInt(gShare.alpha);
+    const beta = BigInt(gShare.beta);
+    const mu = BigInt(gShare.mu);
+    const nu = BigInt(gShare.nu);
+    const k = BigInt(gShare.k);
+    const w = BigInt(gShare.w);
+
+    // Compute $\delta_i = k_i \gamma_i + \sum_{j \ne i} \alpha_{ij}
+    //                                  + \sum_{j \ne i} \beta_{ji}$.
+    const delta = Ecdsa.curve.scalarAdd(Ecdsa.curve.scalarMult(k, gamma), Ecdsa.curve.scalarAdd(alpha, beta));
+    // Compute $\omicron_i = k_i \w_i + \sum_{j \ne i} \mu_{ij}
+    //                                + \sum_{j \ne i} \nu_{ji}$.
+    const omicron = Ecdsa.curve.scalarAdd(Ecdsa.curve.scalarMult(k, w), Ecdsa.curve.scalarAdd(mu, nu));
+    const Gamma = Ecdsa.curve.basePointMult(gamma);
+    return {
+      oShare: {
+        i: gShare.i,
+        y: gShare.y,
+        k: bigIntToHex(k),
+        omicron: bigIntToHex(omicron),
+        delta: bigIntToHex(delta),
+        Gamma: bigIntToHex(Gamma),
+      },
+      dShare: {
+        i: S.i,
+        j: gShare.i,
+        delta: bigIntToHex(delta),
+        Gamma: bigIntToHex(Gamma),
+      },
+    };
+  }
+
+  /**
+   * Sign a message.
+   * @param {Buffer} M Message to be signed
+   * @param {OShare} oShare private omicron share of current participant
+   * @param {DShare} dShare delta share received from the other participant
+   * @returns {SignRT}
+   */
+  sign(M: Buffer, oShare: OShare, dShare: DShare): SignRT {
+    // S_i ==> Om
+    const m = createHash('sha256').update(M).digest();
+
+    const delta = Ecdsa.curve.scalarAdd(BigInt(oShare.delta), BigInt(dShare.delta));
+
+    // Compute $R = [\Si_{i \in S} \Gamma_i]^\delta^{-1}$ and $r = H'(R)$.
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const R = Ecdsa.curve.pointMultiply(
+      Ecdsa.curve.pointAdd(BigInt(oShare.Gamma), BigInt(dShare.Gamma)),
+      Ecdsa.curve.scalarInvert(delta)
+    );
+    const pointR = secp.Point.fromHex(bigIntToBufferBE(R));
+    const r = pointR.x;
+    // Compute $s_i = m k_i + r \omicron_i$.
+    const s = Ecdsa.curve.scalarAdd(
+      Ecdsa.curve.scalarMult(bigIntFromU8ABE(m), BigInt(oShare.k)),
+      Ecdsa.curve.scalarMult(r, BigInt(oShare.omicron))
+    );
+    return {
+      i: oShare.i,
+      y: oShare.y,
+      r: bigIntToHex(r),
+      s: bigIntToHex(s),
+    };
+  }
+
+  /**
+   * Construct full signature by combining SShared
+   * @param {SShare[]} shares
+   * @returns {Signature}
+   */
+  constructSignature(shares: SShare[]): Signature {
+    // Every r must match.
+    const r = shares[0]['r'];
+    const isRMatching = shares.map((share) => share['r'] === r).reduce((a, b) => a && b);
+    if (!isRMatching) {
+      throw new Error('r value should be consistent across all shares');
+    }
+
+    let s = shares.map((share) => BigInt(share['s'])).reduce(Ecdsa.curve.scalarAdd);
+
+    // Normalize s.
+    s = s > Ecdsa.curve.order() ? Ecdsa.curve.order() - s : s;
+    return {
+      y: shares[0]['y'],
+      r: r,
+      s: bigIntToHex(s),
+    };
+  }
+
+  /**
+   * Verify ecdsa signatures
+   * @param {Buffer} message
+   * @param {Signature } signature
+   * @returns {boolean} True if signature is valid; False otherwise
+   */
+  verify(message: Buffer, signature: Signature): boolean {
+    return Ecdsa.curve.verify(
+      message,
+      Buffer.concat([bigIntToBufferBE(BigInt(signature['r']), 32), bigIntToBufferBE(BigInt(signature['s']), 32)]),
+      BigInt(signature['y'])
+    );
   }
 }
