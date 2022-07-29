f357df9770af64311dd95475f100fc9df1d39967
Otto Allmendinger <otto@bitgo.com>
2022-07-29 14:56:21 +0200

    feat(utxo-bin): add command `parseAddress`
    Add address parsing to utxo-bin.
    
    Features:
    
    - script type detection
    - conversion to other networks
    
    Issue: BG-60608

---
+++ modules/utxo-bin/test/parseAddress.ts
@@ -0,0 +1,93 @@
+import * as assert from 'assert';
+import * as crypto from 'crypto';
+import * as yargs from 'yargs';
+import { BIP32Interface } from 'bip32';
+import * as utxolib from '@bitgo/utxo-lib';
+
+import { cmdParseAddress, getAddressParser } from '../src/commands';
+import { getFixtureString, formatTreeNoColor } from './fixtures';
+
+type Triple<T> = [T, T, T];
+
+type KeyTriple = Triple<BIP32Interface>;
+
+const scriptTypesSingleSig = ['p2pkh', 'p2wkh'] as const;
+const scriptTypes = [...utxolib.bitgo.outputScripts.scriptTypes2Of3, ...scriptTypesSingleSig] as const;
+type ScriptType = typeof scriptTypes[number];
+
+function getKey(seed: string): BIP32Interface {
+  return utxolib.bip32.fromSeed(crypto.createHash('sha256').update(seed).digest());
+}
+export function getKeyTriple(seed: string): KeyTriple {
+  return [getKey(seed + '.0'), getKey(seed + '.1'), getKey(seed + '.2')];
+}
+
+export function isSupportedDepositType(network: utxolib.Network, scriptType: ScriptType): boolean {
+  if (scriptType === 'p2pkh') {
+    return true;
+  }
+
+  if (scriptType === 'p2wkh') {
+    return utxolib.supportsSegwit(network);
+  }
+
+  return utxolib.bitgo.outputScripts.isSupportedScriptType(network, scriptType);
+}
+
+/**
+ *
+ * @param keys - Pubkeys to use for generating the address.
+ *               If scriptType is single-sig, the first key will be used.
+ * @param scriptType
+ * @param network
+ * @return {Buffer} scriptPubKey
+ */
+function createScriptPubKey(keys: KeyTriple, scriptType: ScriptType, network: utxolib.Network): Buffer {
+  const pubkeys = keys.map((k) => k.publicKey);
+
+  switch (scriptType) {
+    case 'p2sh':
+    case 'p2shP2wsh':
+    case 'p2wsh':
+    case 'p2tr':
+      return utxolib.bitgo.outputScripts.createOutputScript2of3(pubkeys, scriptType).scriptPubKey;
+    case 'p2pkh':
+      return utxolib.payments.p2pkh({ pubkey: keys[0].publicKey }).output as Buffer;
+    case 'p2wkh':
+      return utxolib.payments.p2wpkh({ pubkey: keys[0].publicKey }).output as Buffer;
+    default:
+      throw new Error(`unsupported output type ${scriptType}`);
+  }
+}
+
+function getAddresses(n: utxolib.Network): string[] {
+  const keys = getKeyTriple('parseAddress');
+  return scriptTypes
+    .filter((t) => isSupportedDepositType(n, t))
+    .map((t) => utxolib.address.fromOutputScript(createScriptPubKey(keys, t, n), n));
+}
+
+function parse(address: string, args: string[]) {
+  return getAddressParser(yargs.command(cmdParseAddress).parseSync(args)).parse(address);
+}
+
+function testParseAddress(address: string, args: string[], suffix: string) {
+  describe(`parse address ${address} with arguments ${args.join(' ')}`, function () {
+    it(`formats address`, async function () {
+      const formatted = formatTreeNoColor(parse(address, args));
+      assert.strictEqual(
+        await getFixtureString(`test/fixtures/formatAddress_${address}${suffix}`, formatted),
+        formatted
+      );
+    });
+  });
+}
+
+utxolib.getNetworkList().forEach((n) => {
+  getAddresses(n).forEach((address, i) => {
+    testParseAddress(address, [], '.txt');
+    if (n === utxolib.networks.bitcoin && i === 0) {
+      testParseAddress(address, ['--all'], '.all.txt');
+    }
+  });
+});
