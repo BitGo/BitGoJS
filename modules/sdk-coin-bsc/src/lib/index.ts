e297107f8f5e233acb1ef2d5f3c2bf5ade460f64
Dulhan Waduge <dulhanwaduge@bitgo.com>
2022-08-16 10:41:48 -0700

    feat(sdk-coin-bsc): replace bsc keypair with eth keypair
    Ticket: BG-55263

--- modules/sdk-coin-bsc/src/lib/index.ts
+++ modules/sdk-coin-bsc/src/lib/index.ts
@@ -1,7 +1,6 @@
 import * as Utils from './utils';
 
-export { KeyPair } from './keyPair';
 export { TransactionBuilder } from './transactionBuilder';
 export { TransferBuilder } from './transferBuilder';
-export { Transaction } from '@bitgo/sdk-coin-eth';
+export { Transaction, KeyPair } from '@bitgo/sdk-coin-eth';
 export { Utils };
