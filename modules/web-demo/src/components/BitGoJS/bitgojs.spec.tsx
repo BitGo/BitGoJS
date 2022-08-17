1a951a42fb39053a3f4252639aa84ffc0f130d7f
Aaron Loe <aaronloe@bitgo.com>
2022-08-16 17:02:37 -0700

    chore(bitgo): set latest buffer polyfil
    - webpack will use latest buffer explicitly instead of implicit dep
    
    Ticket: BG-54987

--- modules/web-demo/src/components/BitGoJS/bitgojs.spec.tsx
+++ modules/web-demo/src/components/BitGoJS/bitgojs.spec.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { mount } from '@cypress/react';
 import BitGoJSComponent from './index';
+import type { Buffer as WebBuffer } from 'buffer/index';
+import { Buffer } from 'buffer';
+import { expect } from 'chai';
 
 it('renders the BitGoJS SDK', () => {
   mount(<BitGoJSComponent />);
   cy.get('h3').contains('BitGoJS SDK');
   cy.get('div').contains('https://app.bitgo-test.com');
+});
+
+it('CR-686: uses the correct version of Buffer', () => {
+  const webBuffer: WebBuffer = new Buffer('') as unknown as WebBuffer;
+  expect(webBuffer.readBigUInt64BE).not.to.be.undefined;
 });
