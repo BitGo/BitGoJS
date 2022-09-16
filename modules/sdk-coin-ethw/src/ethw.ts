6b9eddd102bf152c5001532063aa462e4c85f4de
Flavio Peralta <fperalta@simtlix.com>
2022-09-16 17:59:51 -0300

    feat(sdk-coin-ethw): use ETHw full node RPC queries to recover funds
    For ETHw use RPC queries against a full node instead of a blockchain explorer
    
    BG-55611

--- modules/sdk-coin-ethw/src/ethw.ts
+++ modules/sdk-coin-ethw/src/ethw.ts
@@ -1,5 +1,6 @@
 import request from 'superagent';
 import BigNumber from 'bignumber.js';
+import { BN } from 'ethereumjs-util';
 
 import {
   BaseCoin,
@@ -13,8 +14,18 @@ import {
   VerifyTransactionOptions,
 } from '@bitgo/sdk-core';
 import { BaseCoin as StaticsBaseCoin, coins } from '@bitgo/statics';
-import { Eth, SignedTransaction, SignTransactionOptions, TransactionBuilder } from '@bitgo/sdk-coin-eth';
+import { Eth, optionalDeps, SignedTransaction, SignTransactionOptions, TransactionBuilder } from '@bitgo/sdk-coin-eth';
 import { ExplainTransactionOptions } from '@bitgo/abstract-eth';
+
+type FullNodeResponseBody = {
+  jsonrpc: string;
+  id: string;
+  result?: string;
+  error?: {
+    code: string;
+    message: string;
+  };
+};
 
 export class Ethw extends Eth {
   protected constructor(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>) {
@@ -51,25 +62,74 @@ export class Ethw extends Eth {
   }
 
   /**
-   * Make a query to Etherscan for information such as balance, token balance, solidity calls
-   * @param query {Object} key-value pairs of parameters to append after /api
-   * @returns {Object} response from Etherscan
+   * Query full node for the balance of an address
+   * @param {string} address the ETHw address
+   * @returns {Promise<BN>} address balance
    */
-  async recoveryBlockchainExplorerQuery(query: Record<string, string>): Promise<any> {
-    const token = common.Environments[this.bitgo.getEnv()].etherscanApiToken;
-    if (token) {
-      query.apikey = token;
+  async queryAddressBalance(address: string): Promise<BN> {
+    const result = await this.recoveryFullNodeRPCQuery('eth_getBalance', [address, 'latest']);
+    // throw if the result does not exist or the result is not a valid number
+    if (!result || !result.result) {
+      throw new Error(`Could not obtain address balance for ${address} from full node, got: ${result.result}`);
     }
-    const response = await request
-      .get(common.Environments[this.bitgo.getEnv()].ethwExplorerBaseUrl + '/api')
-      .query(query);
+    return new optionalDeps.ethUtil.BN(result.result.slice(2), 16);
+  }
+
+  /**
+   * Queries the contract (via RPC) for the next sequence ID
+   * @param {string} address address of the contract
+   * @returns {Promise<number>} sequence ID
+   */
+  async querySequenceId(address: string): Promise<number> {
+    // Get sequence ID using contract call
+    const sequenceIdMethodSignature = optionalDeps.ethAbi.methodID('getNextSequenceId', []);
+    const sequenceIdArgs = optionalDeps.ethAbi.rawEncode([], []);
+    const sequenceIdData = Buffer.concat([sequenceIdMethodSignature, sequenceIdArgs]).toString('hex');
+    const sequenceIdDataHex = optionalDeps.ethUtil.addHexPrefix(sequenceIdData);
+    const result = await this.recoveryFullNodeRPCQuery('eth_call', [
+      { to: address, data: sequenceIdDataHex },
+      'latest',
+    ]);
+    if (!result || !result.result) {
+      throw new Error('Could not obtain sequence ID from full node, got: ' + result.result);
+    }
+    const sequenceIdHex = result.result;
+    return new optionalDeps.ethUtil.BN(sequenceIdHex.slice(2), 16).toNumber();
+  }
+
+  /**
+   * Queries public full node to get the next ETHw nonce that should be used for the given ETH address
+   * @param {string} address
+   * @returns {Promise<number>} next ETHw nonce
+   */
+  async getAddressNonce(address: string): Promise<number> {
+    const result = await this.recoveryFullNodeRPCQuery('eth_getTransactionCount', [address, 'latest']);
+    if (!result || !result.result) {
+      throw new Error('Unable to find next nonce from full node, got: ' + JSON.stringify(result));
+    }
+    return new optionalDeps.ethUtil.BN(result.result.slice(2), 16).toNumber();
+  }
+
+  /**
+   * Make a RPC query to full node for information such as balance, token balance, solidity calls
+   * @param {string} method RPC method to execute
+   * @param {Array} params params to include in the RPC request
+   * @returns {Promise<FullNodeResponseBody>} response from full node
+   */
+  async recoveryFullNodeRPCQuery(method: string, params: Array<unknown>): Promise<FullNodeResponseBody> {
+    const response = await request.post(common.Environments[this.bitgo.getEnv()].ethwFullNodeRPCBaseUrl).send({
+      method,
+      params,
+      id: 0,
+      jsonrpc: '2.0',
+    });
 
     if (!response.ok) {
-      throw new Error('could not reach ETHW Explorer');
+      throw new Error('could not reach ETHW full node');
     }
 
-    if (response.body.status === '0' && response.body.message === 'NOTOK') {
-      throw new Error('ETHW Explorer rate limit reached');
+    if (response.body.error) {
+      throw new Error(`ETHW full node error: ${response.body.error.code} - ${response.body.error.message}`);
     }
     return response.body;
   }
