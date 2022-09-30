12e4102cf4ec4810e8bc28d01e551a329477cbdd
Michael Gallegos <michaelgallegos@bitgo.com>
2022-09-30 13:47:08 -0600

    fix(statics): update tokenAddress for tsol:orca
    Fixes the tsol orca mint address to the properly generated one,
    the old/current one is accidentally the same as USDT.
    
    TICKET: STLX-19257

--- modules/statics/src/coins.ts
+++ modules/statics/src/coins.ts
@@ -1904,7 +1904,7 @@ export const coins = CoinMap.fromCoins([
     'tsol:orca',
     'ORCA',
     9,
-    '9cgpBeNZ2HnLda7NWaaU1i3NyTstk2c4zCMUcoAGsi9C',
+    '4yQY4kNGCCM5rCWiQMWHFe5q3b5o7AqGWbx3XyeTti5h',
     UnderlyingAsset.ORCA,
     AccountCoin.DEFAULT_FEATURES
   ),
