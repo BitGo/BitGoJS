1cadc2d3fdbfbcd14e890e83838482947a25c00f
anshuldoshi <anshuldoshi@bitgo.com>
2022-10-04 15:26:05 -0400

    chore(account-lib): modify signing unit test for eth
    Ticket: bg-00000

--- modules/account-lib/test/unit/mpc/tss/ecdsa/ecdsa.ts
+++ modules/account-lib/test/unit/mpc/tss/ecdsa/ecdsa.ts
@@ -4,10 +4,17 @@ import createKeccakHash from 'keccak';
 import * as paillierBigint from 'paillier-bigint';
 import { paillerKeys, mockNShares, mockPShare, mockDKeyShare, mockEKeyShare, mockFKeyShare } from '../fixtures/ecdsa';
 import { Hash, randomBytes } from 'crypto';
+import { KeyPair as EthKeyPair } from '@bitgo/sdk-coin-eth';
+
+import { addHexPrefix, BN, bufferToHex, ecrecover } from 'ethereumjs-util';
+import { TransactionFactory, FeeMarketEIP1559Transaction, Transaction as LegacyTransaction } from '@ethereumjs/tx';
+import { default as Common } from '@ethereumjs/common';
+import { Signature } from '../../../../../../sdk-core/src/account-lib/mpc/tss/ecdsa/types';
+
 /**
  * @prettier
  */
-
+// testmeandonlyme
 describe('TSS ECDSA TESTS', function () {
   const MPC = new Ecdsa();
   const base = BigInt('0x010000000000000000000000000000000000000000000000000000000000000000'); // 2^256
@@ -48,6 +55,9 @@ describe('TSS ECDSA TESTS', function () {
     const dKeyCombine = MPC.keyCombine(D.pShare, [E.nShares[1], F.nShares[1]]);
     const eKeyCombine = MPC.keyCombine(E.pShare, [D.nShares[2], F.nShares[2]]);
     const fKeyCombine = MPC.keyCombine(F.pShare, [D.nShares[3], E.nShares[3]]);
+
+    // these keyshares are the private x share + 2 y shares that each party now has
+    // a, b, c are one group of signers, d, e, f are another
     keyShares = [aKeyCombine, bKeyCombine, cKeyCombine, dKeyCombine, eKeyCombine, fKeyCombine];
     commonPublicKey = aKeyCombine.xShare.y;
     pallierMock.reset();
@@ -122,9 +132,11 @@ describe('TSS ECDSA TESTS', function () {
 
   describe('ECDSA Signing', async function () {
     let config: { signerOne: ECDSA.KeyCombined; signerTwo: ECDSA.KeyCombined; hash?: string; shouldHash?: boolean }[];
+    let DKeyCombine;
 
     before(async () => {
       const [A, B, C, D, E, F] = keyShares;
+      DKeyCombine = D;
 
       config = [
         { signerOne: A, signerTwo: B },
@@ -144,7 +156,7 @@ describe('TSS ECDSA TESTS', function () {
       ];
     });
 
-    for (let index = 0; index < 8; index++) {
+    for (let index = 7; index < 8; index++) {
       it(`should properly sign the message case ${index}`, async function () {
         // Step One
         // signerOne, signerTwo have decided to sign the message
@@ -152,13 +164,15 @@ describe('TSS ECDSA TESTS', function () {
         const signerOneIndex = config[index].signerOne.xShare.i;
         const signerTwo = config[index].signerTwo;
         const signerTwoIndex = config[index].signerTwo.xShare.i;
+        console.log(`signerOne ${JSON.stringify(signerOne)}`);
+        console.log(`signerTwo ${JSON.stringify(signerTwo)}`);
 
         // Step Two
         // Sign Shares are created by one of the participants (signerOne)
         // with its private XShare and YShare corresponding to the other participant (signerTwo)
         // This step produces a private WShare which signerOne saves and KShare which signerOne sends to signerTwo
         const signShares: ECDSA.SignShareRT = MPC.signShare(signerOne.xShare, signerOne.yShares[signerTwoIndex]);
-
+        console.log(`signShares ${JSON.stringify(signShares)}`);
         // Step Three
         // signerTwo receives the KShare from signerOne and uses it produce private
         // BShare (Beta Share) which signerTwo saves and AShare (Alpha Share)
@@ -168,6 +182,7 @@ describe('TSS ECDSA TESTS', function () {
           yShare: signerTwo.yShares[signerOneIndex], // YShare corresponding to the other participant signerOne
           kShare: signShares.kShare,
         });
+        console.log(`signConvertS21 ${JSON.stringify(signConvertS21)}`);
 
         // Step Four
         // signerOne receives the AShare from signerTwo and signerOne using the private WShare from step two
@@ -177,6 +192,7 @@ describe('TSS ECDSA TESTS', function () {
           aShare: signConvertS21.aShare,
           wShare: signShares.wShare,
         });
+        console.log(`signConvertS12 ${JSON.stringify(signConvertS12)}`);
 
         // Step Five
         // signerTwo receives the MUShare from signerOne and signerOne using the private BShare from step three
@@ -186,6 +202,7 @@ describe('TSS ECDSA TESTS', function () {
           muShare: signConvertS12.muShare,
           bShare: signConvertS21.bShare,
         });
+        console.log(`signConvertS21 after signconvert ${JSON.stringify(signConvertS21)}`);
 
         // Step Six
         // signerOne and signerTwo both have successfully generated GShares and they use
@@ -208,8 +225,12 @@ describe('TSS ECDSA TESTS', function () {
             },
           }),
         ];
+        console.log(`signCombineOne ${JSON.stringify(signCombineOne)}`);
+        console.log(`signCombineTwo ${JSON.stringify(signCombineTwo)}`);
 
-        const MESSAGE = Buffer.from('TOO MANY SECRETS');
+        // const MESSAGE = Buffer.from('random message');//Buffer.from(txHex, 'hex');
+        const txData = getTxBuffer();
+        const MESSAGE = txData;
 
         // Step Seven
         // signerOne and signerTwo shares the delta share from each other
@@ -235,18 +256,123 @@ describe('TSS ECDSA TESTS', function () {
             config[index].shouldHash,
           ),
         ];
+        console.log(`signA ${JSON.stringify(signA)}`);
+        console.log(`signB ${JSON.stringify(signB)}`);
 
         // Step Eight
         // Construct the final signature
 
         const signature = MPC.constructSignature([signA, signB]);
+        console.log(`signature ${JSON.stringify(signature)}`);
 
         // Step Nine
         // Verify signature
 
         const isValid = MPC.verify(MESSAGE, signature, hashGenerator(config[index].hash), config[index].shouldHash);
         isValid.should.equal(true);
+
+        validateEthereumSignature(
+          signature,
+          '02eb0580847735940084b2d05e0083030d4094ab100912e133aa06ceb921459aaddbd62381f5a30180c0808080',
+          DKeyCombine,
+        );
       });
     }
   });
+
+  const common = new Common({ chain: 5, hardfork: 'london' });
+
+  enum EthereumJsTransactionType {
+    LEGACY = '0x00', // Set by default when building a tx
+    ACCESS_LIST_EIP2930 = '0x01',
+    FEE_MARKET_EIP1559 = '0x02',
+  }
+
+  function stripHexPrefix(str: string): string {
+    return str.replace(/^0x/i, '');
+  }
+
+  function getTxBuffer(): Buffer {
+    const txHex = '02eb0580847735940084b2d05e0083030d4094ab100912e133aa06ceb921459aaddbd62381f5a30180c0808080';
+    const parsedTxn = TransactionFactory.fromSerializedData(Buffer.from(txHex, 'hex'));
+
+    return parsedTxn.getMessageToSign(true);
+  }
+
+  function validateEthereumSignature(signature: Signature, txHex: string, DKeyCombine: any) {
+    console.log(`Validating signature ${JSON.stringify(signature)}`);
+    const parsedTxn = TransactionFactory.fromSerializedData(Buffer.from(txHex, 'hex'), { common });
+    const txData = parsedTxn.toJSON();
+    console.log(`unsigned tx ${JSON.stringify(txData)}`);
+
+    // const yParity = signature.recid;
+    const b = parsedTxn.getMessageToSign(true).toString('hex');
+    const message = Buffer.from(b, 'hex');
+    const publicKeyBuffer = ecrecover(message, 27, Buffer.from(signature.r, 'hex'), Buffer.from(signature.s, 'hex'));
+    const publicKey = bufferToHex(publicKeyBuffer);
+    const yParity = publicKey.slice(2, 66) === signature.y.slice(2) ? 0 : 1;
+
+    const baseParams = {
+      to: txData.to,
+      nonce: new BN(stripHexPrefix(txData.nonce!), 'hex'),
+      value: new BN(stripHexPrefix(txData.value!), 'hex'),
+      gasLimit: new BN(stripHexPrefix(txData.gasLimit!), 'hex'),
+      data: txData.data,
+      r: addHexPrefix(signature.r),
+      s: addHexPrefix(signature.s),
+    };
+
+    let tx: LegacyTransaction | FeeMarketEIP1559Transaction;
+    if (txData.maxFeePerGas && txData.maxPriorityFeePerGas) {
+      tx = FeeMarketEIP1559Transaction.fromTxData(
+        {
+          ...baseParams,
+          maxPriorityFeePerGas: new BN(stripHexPrefix(txData.maxPriorityFeePerGas!), 'hex'),
+          maxFeePerGas: new BN(stripHexPrefix(txData.maxFeePerGas!), 'hex'),
+          v: new BN(yParity.toString()),
+        },
+        { common },
+      );
+    } else if (txData.gasPrice) {
+      // v is simple replay attack protection that's calculated using the following formula
+      // v = {0,1} + CHAIN_ID * 2 + 35 where {0, 1} is the remainder of the signature's public key mod by 2
+      // for more details on this read EIP-155 https://eips.ethereum.org/EIPS/eip-155
+      const v = BigInt(35) + BigInt(yParity) + BigInt(common!.chainIdBN().toNumber()) * BigInt(2);
+      tx = LegacyTransaction.fromTxData(
+        {
+          ...baseParams,
+          v: new BN(v.toString()),
+          gasPrice: new BN(stripHexPrefix(txData.gasPrice!.toString()), 'hex'),
+          type: EthereumJsTransactionType.LEGACY,
+        },
+        { common },
+      );
+    } else {
+      throw new Error(`Expected either a legacy or an EIP1559 tx, received ${JSON.stringify(txData)}`);
+    }
+
+    const printSerializedTxHex = tx.serialize().toString('hex');
+    console.log(`******** getSignedTx 2 serializedTxHex ${printSerializedTxHex}`);
+
+    const final = {
+      id: addHexPrefix(tx.hash().toString('hex')),
+      tx: addHexPrefix(tx.serialize().toString('hex')),
+    };
+    console.log(`signed tx ${JSON.stringify(final)}`);
+
+    const commonKeychain = DKeyCombine.xShare.y;
+    const mypublicKey = commonKeychain.slice(0, 66);
+    const keyPair = new EthKeyPair({ pub: mypublicKey });
+    const senderAddress = keyPair.getAddress();
+    console.log(`expected sender address from user share ${senderAddress}`);
+
+    const address = bufferToHex(tx.getSenderAddress().toBuffer());
+    console.log(`actual sender address from tx obj ${address.toString()}`);
+
+    console.log(`expected public key from user share ${DKeyCombine.xShare.y}`);
+    console.log(`actual public key from signature ${signature.y}`);
+
+    const txPublicKey = bufferToHex(tx.getSenderPublicKey());
+    console.log(`actual public key from tx obj ${txPublicKey.toString()}`);
+  }
 });
