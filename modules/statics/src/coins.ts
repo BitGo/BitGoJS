918b47edd351be4f7720fbfeeb2340ffd5f934b4
Sachu Shaji Abraham <sachuabraham@bitgo.com>
2022-10-07 17:58:00 +0530

    feat(statics): introduce deprecation coin feature
    this change introduce a new coin deprecation feature.
    This helps to distinguish coins which are
    currently being deprecated.This is especially helpful
    when coins have multiple testnets which were previously
    supported but deprecated now. This change also deprecates
    teth.
    
    TICKET: BG-59177

--- modules/statics/src/coins.ts
+++ modules/statics/src/coins.ts
@@ -99,7 +99,10 @@ export const coins = CoinMap.fromCoins([
   account('dot', 'Polkadot', Networks.main.dot, 10, UnderlyingAsset.DOT, DOT_FEATURES, KeyCurve.Ed25519),
   account('tdot', 'Testnet Polkadot', Networks.test.dot, 12, UnderlyingAsset.DOT, DOT_FEATURES, KeyCurve.Ed25519),
   account('eth', 'Ethereum', Networks.main.ethereum, 18, UnderlyingAsset.ETH, ETH_FEATURES_WITH_STAKING),
-  account('teth', 'Kovan Testnet Ethereum (Deprecated)', Networks.test.kovan, 18, UnderlyingAsset.ETH, ETH_FEATURES),
+  account('teth', 'Kovan Testnet Ethereum (Deprecated)', Networks.test.kovan, 18, UnderlyingAsset.ETH, [
+    ...ETH_FEATURES,
+    CoinFeature.DEPRECATED,
+  ]),
   account(
     'gteth',
     'Goerli Testnet Ethereum',
