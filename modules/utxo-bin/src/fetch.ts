4f9660278aaa5c26aba69877faa853a047819b7d
Otto Allmendinger <otto@bitgo.com>
2022-11-03 09:54:20 +0100

    feat(utxo-bin): add parameter `--cache`
    Uses CachingHttpClient when set
    
    Issue: BG-60311

--- modules/utxo-bin/src/fetch.ts
+++ modules/utxo-bin/src/fetch.ts
@@ -1,6 +1,7 @@
 import * as utxolib from '@bitgo/utxo-lib';
 import * as blockapis from '@bitgo/blockapis';
 import { coins, UtxoCoin } from '@bitgo/statics';
+import { HttpClient } from '@bitgo/blockapis';
 
 function getCoinName(network: utxolib.Network): string {
   const networkName = utxolib.getNetworkName(network);
@@ -26,42 +27,56 @@ function getCoinName(network: utxolib.Network): string {
   }
 }
 
-function getApi(network: utxolib.Network): blockapis.UtxoApi {
+function getApi(httpClient: HttpClient, network: utxolib.Network): blockapis.UtxoApi {
   const coinName = getCoinName(network);
   switch (coinName) {
     case 'btc':
     case 'tbtc':
-      return blockapis.BlockstreamApi.forCoin(coinName);
+      return blockapis.BlockstreamApi.forCoin(coinName, { httpClient });
     default:
-      return blockapis.BlockchairApi.forCoin(coinName);
+      return blockapis.BlockchairApi.forCoin(coinName, { httpClient });
   }
 }
 
-export async function fetchTransactionHex(txid: string, network: utxolib.Network): Promise<string> {
-  return await getApi(network).getTransactionHex(txid);
+export async function fetchTransactionHex(
+  httpClient: HttpClient,
+  txid: string,
+  network: utxolib.Network
+): Promise<string> {
+  return await getApi(httpClient, network).getTransactionHex(txid);
 }
 
 export async function fetchTransactionStatus(
+  httpClient: HttpClient,
   txid: string,
   network: utxolib.Network
 ): Promise<blockapis.TransactionStatus> {
-  return await getApi(network).getTransactionStatus(txid);
+  return await getApi(httpClient, network).getTransactionStatus(txid);
 }
 
-export async function fetchPrevOutputs(tx: utxolib.bitgo.UtxoTransaction): Promise<utxolib.TxOutput[]> {
-  return await blockapis.fetchInputs(tx.ins, getApi(tx.network), tx.network);
+export async function fetchPrevOutputs(
+  httpClient: HttpClient,
+  tx: utxolib.bitgo.UtxoTransaction
+): Promise<utxolib.TxOutput[]> {
+  return await blockapis.fetchInputs(tx.ins, getApi(httpClient, tx.network), tx.network);
 }
 
-export async function fetchPrevOutputSpends(tx: utxolib.bitgo.UtxoTransaction): Promise<blockapis.OutputSpend[]> {
+export async function fetchPrevOutputSpends(
+  httpClient: HttpClient,
+  tx: utxolib.bitgo.UtxoTransaction
+): Promise<blockapis.OutputSpend[]> {
   return await blockapis.fetchTransactionSpends(
     tx.ins.map((i) => utxolib.bitgo.getOutputIdForInput(i)),
-    getApi(tx.network)
+    getApi(httpClient, tx.network)
   );
 }
 
-export async function fetchOutputSpends(tx: utxolib.bitgo.UtxoTransaction): Promise<blockapis.OutputSpend[]> {
+export async function fetchOutputSpends(
+  httpClient: HttpClient,
+  tx: utxolib.bitgo.UtxoTransaction
+): Promise<blockapis.OutputSpend[]> {
   try {
-    return await getApi(tx.network).getTransactionSpends(tx.getId());
+    return await getApi(httpClient, tx.network).getTransactionSpends(tx.getId());
   } catch (e) {
     return [];
   }
