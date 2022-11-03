c60495d7460d7938c26d80222afe07c79981d2bd
Otto Allmendinger <otto@bitgo.com>
2022-11-03 09:31:02 +0100

    fix(utxo-bin): fix getCoinName for bitcoincash
    When looking up bitcoincash transaction, default to coin `bch` (ignore
    `bcha`)
    
    Issue: BG-60311

--- modules/utxo-bin/src/fetch.ts
+++ modules/utxo-bin/src/fetch.ts
@@ -4,13 +4,26 @@ import { coins, UtxoCoin } from '@bitgo/statics';
 
 function getCoinName(network: utxolib.Network): string {
   const networkName = utxolib.getNetworkName(network);
-  const matches = [...coins].flatMap(([, coin]) =>
-    coin instanceof UtxoCoin && coin.network.utxolibName === networkName ? [coin] : []
-  );
-  if (matches.length === 1) {
-    return matches[0].name;
+
+  // the bitcoincash network actually has two coins: bch and bcha - hardcode bch by default here
+  switch (networkName) {
+    case 'bitcoincash':
+      return 'bch';
+    case 'bitcoincashTestnet':
+      return 'tbch';
   }
-  throw new Error(`no coin for network ${networkName}`);
+
+  const matches = [...coins].flatMap(([, coin]) =>
+    coin instanceof UtxoCoin && coin.network.utxolibName === networkName ? [coin.name] : []
+  );
+  switch (matches.length) {
+    case 0:
+      throw new Error(`no coin for network ${networkName}`);
+    case 1:
+      return matches[0];
+    default:
+      throw new Error(`more than one coin for ${networkName}: ${matches}`);
+  }
 }
 
 function getApi(network: utxolib.Network): blockapis.UtxoApi {
