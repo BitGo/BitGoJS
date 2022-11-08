a49e552fdda627dbf7c224b4910869782a81240e
Rushil Kapoor <rushilkapoor@bitgo.com>
2022-11-08 15:36:12 +0530

    feat(sdk-coin-ltc): remove canonicalising LTC addresses
    BG-60666
    
    BREAKING CHANGE: this drops support for the legacy Litecoin format.
    Litecoin addresses starting with 3 will not be supported anymore.

--- modules/sdk-coin-ltc/src/ltc.ts
+++ modules/sdk-coin-ltc/src/ltc.ts
@@ -1,5 +1,5 @@
 import { AbstractUtxoCoin, UtxoNetwork } from '@bitgo/abstract-utxo';
-import { BitGoBase, BaseCoin, InvalidAddressError } from '@bitgo/sdk-core';
+import { BitGoBase, BaseCoin } from '@bitgo/sdk-core';
 import * as utxolib from '@bitgo/utxo-lib';
 
 export class Ltc extends AbstractUtxoCoin {
@@ -29,46 +29,5 @@ export class Ltc extends AbstractUtxoCoin {
 
   supportsBlockTarget(): boolean {
     return false;
-  }
-
-  /**
-   * Canonicalize a Litecoin address for a specific scriptHash version
-   * @param address
-   * @param scriptHashVersion 1 or 2, where 1 is the old version and 2 is the new version
-   * @returns {*} address string
-   */
-  canonicalAddress(address: string, scriptHashVersion = 2): string {
-    if (!this.isValidAddress(address, true)) {
-      throw new InvalidAddressError();
-    }
-    try {
-      // try deserializing as bech32
-      utxolib.address.fromBech32(address);
-      // address may be all uppercase, but canonical bech32 addresses are all lowercase
-      return address.toLowerCase();
-    } catch (e) {
-      // not a valid bech32, try to decode as base58
-    }
-    const addressDetails = utxolib.address.fromBase58Check(address, this.network);
-    if (addressDetails.version === this.network.pubKeyHash) {
-      // the pub keys never changed
-      return address;
-    }
-    if ([1, 2].indexOf(scriptHashVersion) === -1) {
-      throw new Error('scriptHashVersion needs to be either 1 or 2');
-    }
-    const scriptHashMap = {
-      // altScriptHash is the old one
-      1: this.altScriptHash,
-      // by default we're using the new one
-      2: this.network.scriptHash,
-    };
-    const newScriptHash = scriptHashMap[scriptHashVersion];
-    return utxolib.address.toBase58Check(addressDetails.hash, newScriptHash, this.network);
-  }
-
-  calculateRecoveryAddress(scriptHashScript: Buffer): string {
-    const bitgoAddress = utxolib.address.fromOutputScript(scriptHashScript, this.network);
-    return this.canonicalAddress(bitgoAddress, 1);
   }
 }
