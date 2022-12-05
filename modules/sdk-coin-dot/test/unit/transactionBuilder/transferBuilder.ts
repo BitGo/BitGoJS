d6fa3973e28beff3abf464b76819d35407ee59a7
Michael Gallegos <michaelgallegos@bitgo.com>
2022-12-05 14:33:53 -0500

    feat(sdk-coin-dot): add keepAlive flag to DOT sweep constructor
    Adds the keepAlive flag as an optional parameter to the
    sweep() constructor used when building DOT transactions.
    
    TICKET: BG-62062

--- modules/sdk-coin-dot/test/unit/transactionBuilder/transferBuilder.ts
+++ modules/sdk-coin-dot/test/unit/transactionBuilder/transferBuilder.ts
@@ -386,6 +386,38 @@ describe('Dot Transfer Builder', () => {
       should.deepEqual(outputs.value, consolidationValue);
     });
 
+    it('should build an unsigned sweep transaction with keepAlive as false', async () => {
+      builder
+        .sweep(false)
+        .to({ address: receiver.address })
+        .amount('90034235235322')
+        .sender({ address: sender.address })
+        .validity({ firstValid: 3933, maxDuration: 64 })
+        .referenceBlock('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
+        .sequenceId({ name: 'Nonce', keyword: 'nonce', value: 200 })
+        .fee({ amount: 0, type: 'tip' });
+      const tx = await builder.build();
+      const txJson = tx.toJson();
+      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.blockNumber, 3933);
+      should.deepEqual(txJson.referenceBlock, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
+      should.deepEqual(txJson.genesisHash, genesisHash);
+      should.deepEqual(txJson.specVersion, specVersion);
+      should.deepEqual(txJson.nonce, 200);
+      should.deepEqual(txJson.tip, 0);
+      should.deepEqual(txJson.transactionVersion, txVersion);
+      should.deepEqual(txJson.chainName, chainName);
+      should.deepEqual(txJson.eraPeriod, 64);
+
+      const inputs = tx.inputs[0];
+      should.deepEqual(inputs.address, sender.address);
+      should.deepEqual(inputs.value, consolidationValue);
+
+      const outputs = tx.outputs[0];
+      should.deepEqual(outputs.address, receiver.address);
+      should.deepEqual(outputs.value, consolidationValue);
+    });
+
     it('should build from raw signed sweep transaction', async () => {
       builder.from(rawTx.transferAll.signed);
       builder
