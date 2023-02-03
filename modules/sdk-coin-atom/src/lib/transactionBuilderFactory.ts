94bf5c7bd1bbd10619fc99a21498746e04f2e5cd
Michael Gallegos <michaelgallegos@bitgo.com>
2023-02-03 11:36:26 -0600

    feat(sdk-coin-atom): add Atom transaction builder skeleton
    TICKET: BG-64364

---
+++ modules/sdk-coin-atom/src/lib/transactionBuilderFactory.ts
@@ -0,0 +1,61 @@
+import { BaseTransactionBuilderFactory, InvalidTransactionError, TransactionType } from '@bitgo/sdk-core';
+import { TransactionBuilder } from './transactionBuilder';
+import { TransferBuilder } from './transferBuilder';
+import { BaseCoin as CoinConfig } from '@bitgo/statics';
+import { Transaction } from './transaction';
+
+export class TransactionBuilderFactory extends BaseTransactionBuilderFactory {
+  constructor(_coinConfig: Readonly<CoinConfig>) {
+    super(_coinConfig);
+  }
+
+  /** @inheritdoc */
+  from(raw: string): TransactionBuilder {
+    const tx = this.parseTransaction(raw);
+    try {
+      switch (tx.type) {
+        case TransactionType.Send:
+          return this.getTransferBuilder(tx);
+        default:
+          throw new InvalidTransactionError('Invalid transaction');
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  /** @inheritdoc */
+  getTransferBuilder(tx?: Transaction): TransferBuilder {
+    return this.initializeBuilder(tx, new TransferBuilder(this._coinConfig));
+  }
+
+  /** @inheritdoc */
+  getWalletInitializationBuilder(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  /**
+   * Initialize the builder with the given transaction
+   *
+   * @param {Transaction | undefined} tx - the transaction used to initialize the builder
+   * @param {TransactionBuilder} builder - the builder to be initialized
+   * @returns {TransactionBuilder} the builder initialized
+   */
+  private initializeBuilder<T extends TransactionBuilder>(tx: Transaction | undefined, builder: T): T {
+    if (tx) {
+      builder.initBuilder(tx);
+    }
+    return builder;
+  }
+
+  /** Parse the transaction from a raw transaction
+   *
+   * @param {string} rawTransaction - the raw tx
+   * @returns {Transaction} parsedtransaction
+   */
+  private parseTransaction(rawTransaction: string): Transaction {
+    const tx = new Transaction(this._coinConfig);
+    tx.fromRawTransaction(rawTransaction);
+    return tx;
+  }
+}
