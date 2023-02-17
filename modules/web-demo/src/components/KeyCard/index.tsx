218a53f627500758f3b74aedc1c8f26438fe2444
Leo Kotschenreuther <leo@bitgo.com>
2023-02-17 15:08:29 +0100

    feat(web-demo): add manual e2e test for key card creation
    Ticket: BG-68545

--- modules/web-demo/src/components/KeyCard/index.tsx
+++ modules/web-demo/src/components/KeyCard/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {
+  createNewSMHAWalletAndDownloadKeycard,
   downloadKeycardForHotEthTSSWallet,
   downloadKeycardForHotLtcWallet,
   downloadKeycardForSelfManagedHotAdvancedPolygonWallet,
@@ -9,6 +10,7 @@ const KeyCard = () => {
   return (
     <React.Fragment>
       <h3>Key Card</h3>
+      <h4>Static Key Cards</h4>
       <br />
       <button onClick={downloadKeycardForHotLtcWallet}>
         Download for Hot LTC Wallet
@@ -18,6 +20,15 @@ const KeyCard = () => {
       </button>
       <button onClick={downloadKeycardForSelfManagedHotAdvancedPolygonWallet}>
         Download for Self Managed Hot Advanced Polygon Wallet
+      </button>
+      <h4>Create new SMAH Wallet with Key Card</h4>
+      <label htmlFor="accessToken">Access Token:</label>
+      <input type="text" id="accessToken" name="accessToken" />
+      <label htmlFor="accessToken">Enterprise ID:</label>
+      <input type="text" id="enterpriseId" name="enterpriseId" />
+      <br />
+      <button onClick={createNewSMHAWalletAndDownloadKeycard}>
+        Create Wallet and Download Key Card
       </button>
     </React.Fragment>
   );
