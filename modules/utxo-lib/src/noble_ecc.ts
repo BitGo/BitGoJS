0eb8fef554d1dda3d6d836b3d942552a7ae452ac
Brandon Black <brandonblack@bitgo.com>
2023-03-28 07:32:22 -0700

    chore(utxo-lib): update noble and musig libraries
    Issue: BG-63187

--- modules/utxo-lib/src/noble_ecc.ts
+++ modules/utxo-lib/src/noble_ecc.ts
@@ -1,24 +1,12 @@
-import * as createHash from 'create-hash';
-import * as createHmac from 'create-hmac';
+import { sha256 } from '@noble/hashes/sha256';
 import { ECPairAPI, ECPairFactory, ECPairInterface } from 'ecpair';
-import * as necc from '@noble/secp256k1';
+import { ProjPointType } from '@noble/curves/abstract/weierstrass';
+import { secp256k1, schnorr } from '@noble/curves/secp256k1';
 import { BIP32API, BIP32Factory, BIP32Interface } from 'bip32';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore base_crypto is exported as a subPath export, ignoring since compiler complains about importing like this
 import * as baseCrypto from '@brandonblack/musig/base_crypto';
 import { MuSig, MuSigFactory } from '@brandonblack/musig';
-
-necc.utils.sha256Sync = (...messages: Uint8Array[]): Uint8Array => {
-  const sha256 = createHash('sha256');
-  for (const message of messages) sha256.update(message);
-  return sha256.digest();
-};
-
-necc.utils.hmacSha256Sync = (key: Uint8Array, ...messages: Uint8Array[]): Uint8Array => {
-  const hash = createHmac('sha256', Buffer.from(key));
-  messages.forEach((m) => hash.update(m));
-  return Uint8Array.from(hash.digest());
-};
 
 const defaultTrue = (param?: boolean): boolean => param !== false;
 
@@ -30,80 +18,82 @@ function throwToNull<Type>(fn: () => Type): Type | null {
   }
 }
 
-function isPoint(p: Uint8Array, xOnly: boolean): boolean {
-  if ((p.length === 32) !== xOnly) return false;
-  try {
-    return !!necc.Point.fromHex(p);
-  } catch (e) {
-    return false;
-  }
-}
-
-function toBigInt(b: Uint8Array | Buffer): bigint {
-  const buff = Buffer.from(b);
-  if (buff.length !== 32) {
-    throw new Error('Invalid size ${buff.length}');
-  }
-  return BigInt(`0x${buff.toString('hex')}`);
+function pointAddTweak(P: ProjPointType<bigint>, tweak: Uint8Array): ProjPointType<bigint> {
+  const t = baseCrypto.readSecret(tweak);
+  const Q = secp256k1.ProjectivePoint.BASE.multiplyAndAddUnsafe(P, t, BigInt(1));
+  if (!Q) throw new Error('Unexpected point at infinity while tweaking');
+  return Q;
 }
 
 const ecc = {
-  isPoint: (p: Uint8Array): boolean => isPoint(p, false),
-  isPrivate: (d: Uint8Array): boolean => necc.utils.isValidPrivateKey(d),
-  isXOnlyPoint: (p: Uint8Array): boolean => isPoint(p, true),
+  isPoint: baseCrypto.isPoint,
+  isPrivate: secp256k1.utils.isValidPrivateKey,
+  isXOnlyPoint: baseCrypto.isXOnlyPoint,
 
   xOnlyPointAddTweak: (p: Uint8Array, tweak: Uint8Array): { parity: 0 | 1; xOnlyPubkey: Uint8Array } | null =>
     throwToNull(() => {
-      const P = necc.utils.pointAddScalar(p, tweak, true);
-      const parity = P[0] % 2 === 1 ? 1 : 0;
-      return { parity, xOnlyPubkey: P.slice(1) };
+      const X = schnorr.utils.bytesToNumberBE(p);
+      const Q = pointAddTweak(schnorr.utils.lift_x(X), tweak);
+      return {
+        parity: Q.hasEvenY() ? 0 : 1,
+        xOnlyPubkey: schnorr.utils.pointToBytes(Q),
+      };
     }),
 
   pointFromScalar: (sk: Uint8Array, compressed?: boolean): Uint8Array | null =>
-    throwToNull(() => necc.getPublicKey(sk, defaultTrue(compressed))),
+    throwToNull(() => secp256k1.getPublicKey(sk, defaultTrue(compressed))),
 
-  pointCompress: (p: Uint8Array, compressed?: boolean): Uint8Array => {
-    return necc.Point.fromHex(p).toRawBytes(defaultTrue(compressed));
-  },
+  pointCompress: (p: Uint8Array, compressed?: boolean): Uint8Array =>
+    secp256k1.ProjectivePoint.fromHex(p).toRawBytes(defaultTrue(compressed)),
 
   pointMultiply: (a: Uint8Array, tweak: Uint8Array, compressed?: boolean): Uint8Array | null =>
-    throwToNull(() => necc.utils.pointMultiply(a, tweak, defaultTrue(compressed))),
+    throwToNull(() => {
+      const product = secp256k1.ProjectivePoint.fromHex(a).multiplyAndAddUnsafe(
+        secp256k1.ProjectivePoint.ZERO,
+        BigInt(`0x${Buffer.from(tweak).toString('hex')}`),
+        BigInt(1)
+      );
+      if (!product) return null;
+      return product.toRawBytes(defaultTrue(compressed));
+    }),
 
   pointAdd: (a: Uint8Array, b: Uint8Array, compressed?: boolean): Uint8Array | null =>
     throwToNull(() => {
-      const A = necc.Point.fromHex(a);
-      const B = necc.Point.fromHex(b);
+      const A = secp256k1.ProjectivePoint.fromHex(a);
+      const B = secp256k1.ProjectivePoint.fromHex(b);
       return A.add(B).toRawBytes(defaultTrue(compressed));
     }),
 
   pointAddScalar: (p: Uint8Array, tweak: Uint8Array, compressed?: boolean): Uint8Array | null =>
-    throwToNull(() => necc.utils.pointAddScalar(p, tweak, defaultTrue(compressed))),
+    throwToNull(() => pointAddTweak(secp256k1.ProjectivePoint.fromHex(p), tweak).toRawBytes(defaultTrue(compressed))),
 
   privateAdd: (d: Uint8Array, tweak: Uint8Array): Uint8Array | null =>
     throwToNull(() => {
-      const res = necc.utils.privateAdd(d, tweak);
+      const res = baseCrypto.scalarAdd(d, tweak);
       // tiny-secp256k1 returns null rather than allowing a 0 private key to be returned
       // ECPair.testEcc() requires that behavior.
       if (res?.every((i) => i === 0)) return null;
       return res;
     }),
 
-  privateNegate: (d: Uint8Array): Uint8Array => necc.utils.privateNegate(d),
+  privateNegate: baseCrypto.scalarNegate,
 
   sign: (h: Uint8Array, d: Uint8Array, e?: Uint8Array): Uint8Array => {
-    return necc.signSync(h, d, { der: false, extraEntropy: e });
+    return secp256k1.sign(h, d, { extraEntropy: e }).toCompactRawBytes();
   },
 
   signSchnorr: (h: Uint8Array, d: Uint8Array, e: Uint8Array = Buffer.alloc(32, 0x00)): Uint8Array => {
-    return necc.schnorr.signSync(h, d, e);
+    return schnorr.sign(h, d, e);
   },
 
   verify: (h: Uint8Array, Q: Uint8Array, signature: Uint8Array, strict?: boolean): boolean => {
-    return necc.verify(signature, h, Q, { strict });
+    const s = secp256k1.Signature.fromCompact(signature);
+    if (strict && s.r > secp256k1.CURVE.n / BigInt(2)) return false;
+    return secp256k1.verify(s, h, Q);
   },
 
   verifySchnorr: (h: Uint8Array, Q: Uint8Array, signature: Uint8Array): boolean => {
-    return necc.schnorr.verifySync(signature, h, Q);
+    return schnorr.verify(signature, h, Q);
   },
 };
 
@@ -111,7 +101,11 @@ const crypto = {
   ...baseCrypto,
   pointMultiplyUnsafe(p: Uint8Array, a: Uint8Array, compress: boolean): Uint8Array | null {
     try {
-      const product = necc.Point.fromHex(p).multiplyAndAddUnsafe(necc.Point.ZERO, toBigInt(a), BigInt(1));
+      const product = secp256k1.ProjectivePoint.fromHex(p).multiplyAndAddUnsafe(
+        secp256k1.ProjectivePoint.ZERO,
+        schnorr.utils.bytesToNumberBE(a),
+        BigInt(1)
+      );
       if (!product) return null;
       return product.toRawBytes(compress);
     } catch {
@@ -120,8 +114,12 @@ const crypto = {
   },
   pointMultiplyAndAddUnsafe(p1: Uint8Array, a: Uint8Array, p2: Uint8Array, compress: boolean): Uint8Array | null {
     try {
-      const p2p = necc.Point.fromHex(p2);
-      const p = necc.Point.fromHex(p1).multiplyAndAddUnsafe(p2p, toBigInt(a), BigInt(1));
+      const p2p = secp256k1.ProjectivePoint.fromHex(p2);
+      const p = secp256k1.ProjectivePoint.fromHex(p1).multiplyAndAddUnsafe(
+        p2p,
+        schnorr.utils.bytesToNumberBE(a),
+        BigInt(1)
+      );
       if (!p) return null;
       return p.toRawBytes(compress);
     } catch {
@@ -130,16 +128,16 @@ const crypto = {
   },
   pointAdd(a: Uint8Array, b: Uint8Array, compress: boolean): Uint8Array | null {
     try {
-      return necc.Point.fromHex(a).add(necc.Point.fromHex(b)).toRawBytes(compress);
+      return secp256k1.ProjectivePoint.fromHex(a).add(secp256k1.ProjectivePoint.fromHex(b)).toRawBytes(compress);
     } catch {
       return null;
     }
   },
   pointAddTweak(p: Uint8Array, tweak: Uint8Array, compress: boolean): Uint8Array | null {
     try {
-      const P = necc.Point.fromHex(p);
+      const P = secp256k1.ProjectivePoint.fromHex(p);
       const t = baseCrypto.readSecret(tweak);
-      const Q = necc.Point.BASE.multiplyAndAddUnsafe(P, t, BigInt(1));
+      const Q = secp256k1.ProjectivePoint.BASE.multiplyAndAddUnsafe(P, t, BigInt(1));
       if (!Q) throw new Error('Tweaked point at infinity');
       return Q.toRawBytes(compress);
     } catch {
@@ -147,27 +145,27 @@ const crypto = {
     }
   },
   pointCompress(p: Uint8Array, compress = true): Uint8Array {
-    return necc.Point.fromHex(p).toRawBytes(compress);
+    return secp256k1.ProjectivePoint.fromHex(p).toRawBytes(compress);
   },
   liftX(p: Uint8Array): Uint8Array | null {
     try {
-      return necc.Point.fromHex(p).toRawBytes(false);
+      return secp256k1.ProjectivePoint.fromHex(p).toRawBytes(false);
     } catch {
       return null;
     }
   },
   getPublicKey(s: Uint8Array, compress: boolean): Uint8Array | null {
     try {
-      return necc.getPublicKey(s, compress);
+      return secp256k1.getPublicKey(s, compress);
     } catch {
       return null;
     }
   },
-  taggedHash: necc.utils.taggedHashSync,
+  taggedHash: schnorr.utils.taggedHash,
   sha256(...messages: Uint8Array[]): Uint8Array {
-    const sha256 = createHash('sha256');
-    for (const message of messages) sha256.update(message);
-    return sha256.digest();
+    const h = sha256.create();
+    for (const message of messages) h.update(message);
+    return h.digest();
   },
 };
 
