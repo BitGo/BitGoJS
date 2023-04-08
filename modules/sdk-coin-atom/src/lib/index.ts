6723ecc36441409d34fd4b8563bd04fe3f048fdb
Vijay-Jagannathan <vijayjagannathan@bitgo.com>
2023-04-08 14:58:32 +0530

    chore(sdk-coin-atom): export atom staking builders
    Ticket: BG-72595

--- modules/sdk-coin-atom/src/lib/index.ts
+++ modules/sdk-coin-atom/src/lib/index.ts
@@ -6,4 +6,7 @@ export { Transaction } from './transaction';
 export { TransactionBuilder } from './transactionBuilder';
 export { TransferBuilder } from './transferBuilder';
 export { TransactionBuilderFactory } from './transactionBuilderFactory';
+export { StakingActivateBuilder } from './StakingActivateBuilder';
+export { StakingDeactivateBuilder } from './StakingDeactivateBuilder';
+export { StakingWithdrawRewardsBuilder } from './StakingWithdrawRewardsBuilder';
 export { Interface, Utils };
