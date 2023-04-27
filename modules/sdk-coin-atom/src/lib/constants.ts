0e4cfc74201f9a44ec3b4b9c7591a6fbd3f2efb6
Vijay-Jagannathan <vijayjagannathan@bitgo.com>
2023-04-27 16:06:19 +0530

    feat(sdk-coin-atom): add recover mechanism for ATOM
    Ticket: BG-69146

--- modules/sdk-coin-atom/src/lib/constants.ts
+++ modules/sdk-coin-atom/src/lib/constants.ts
@@ -7,3 +7,5 @@ export const validDenoms = ['natom', 'uatom', 'matom', 'atom'];
 export const accountAddressRegex = /^(cosmos)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l']+)$/;
 export const validatorAddressRegex = /^(cosmosvaloper)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l']+)$/;
 export const UNAVAILABLE_TEXT = 'UNAVAILABLE';
+export const GAS_AMOUNT = '100000';
+export const GAS_LIMIT = 200000;
