dcfa9392726db45a5a5c4de751a19fe7154ffa2f
Evan Zheng <evanzheng@bitgo.com>
2023-06-21 12:46:25 -0700

    feat(sdk-coin-ada): parse partially signed tx hex
    Support parsing partially signed tx hex; bump CSL version; expose signature in ADA txn wrapper
    
    EA-621

--- modules/sdk-coin-ada/test/unit/stakingPledgeBuilder.ts
+++ modules/sdk-coin-ada/test/unit/stakingPledgeBuilder.ts
@@ -15,18 +15,18 @@ describe('ADA Staking Pledge Transaction Builder', async () => {
     const tx = (await txBuilder.build()) as Transaction;
     should.equal(tx.type, TransactionType.StakingPledge);
     const txData = tx.toJson();
-    txData.inputs.length.should.equal(1);
+    txData.inputs.length.should.equal(2);
     txData.outputs.length.should.equal(1);
     txData.certs.length.should.equal(2);
     txData.certs[0].type.should.equal(3);
     txData.certs[1].type.should.equal(1);
     should.exist(txData.certs[1].poolKeyHash);
-    txData.certs[0].poolKeyHash!.should.equal('76044d3aa9caafbb213ae45f9a3f5ad3cc6c836f375ebbcfa5c83681');
+    txData.certs[0].poolKeyHash!.should.equal('10324dc34187735de46f6260d94620cdcc819f7ed1f93e3fc58d06a0');
     txData.withdrawals.length.should.equal(0);
     txData.witnesses.length.should.equal(0);
 
     const fee = tx.getFee;
-    fee.should.equal('185609');
+    fee.should.equal('1000000');
     tx.toBroadcastFormat().should.equal(rawTx.unsignedNewPledgeTx);
     should.equal(tx.id, rawTx.unsignedNewPledgeTxHash);
 
@@ -42,17 +42,17 @@ describe('ADA Staking Pledge Transaction Builder', async () => {
     const tx = (await txBuilder.build()) as Transaction;
     should.equal(tx.type, TransactionType.StakingPledge);
     const txData = tx.toJson();
-    txData.inputs.length.should.equal(1);
+    txData.inputs.length.should.equal(2);
     txData.outputs.length.should.equal(1);
     txData.certs.length.should.equal(1);
     txData.certs[0].type.should.equal(3);
     should.exist(txData.certs[0].poolKeyHash);
-    txData.certs[0].poolKeyHash!.should.equal('f61c42cbf7c8c53af3f520508212ad3e72f674f957fe23ff0acb4973');
+    txData.certs[0].poolKeyHash!.should.equal('10324dc34187735de46f6260d94620cdcc819f7ed1f93e3fc58d06a0');
     txData.withdrawals.length.should.equal(0);
     txData.witnesses.length.should.equal(0);
 
     const fee = tx.getFee;
-    fee.should.equal('42');
+    fee.should.equal('1000000');
     tx.toBroadcastFormat().should.equal(rawTx.unsignedUpdatePledgeTx);
     should.equal(tx.id, rawTx.unsignedUpdatePledgeTxHash);
 
@@ -67,7 +67,35 @@ describe('ADA Staking Pledge Transaction Builder', async () => {
     txBuilder.initBuilder(prebuiltTx);
     txBuilder.addNodeKeyWitness(rawTx.pledgeNodeKeyPubkey, rawTx.pledgeNodeWitnessSignature);
     const tx = (await txBuilder.build()) as Transaction;
+    tx.signature.length.should.equal(1);
     const txData = tx.toJson();
     txData.witnesses.length.should.equal(1);
+
+    const rebuiltTx = new Transaction(coins.get('tada'));
+    rebuiltTx.fromRawTransaction(tx.toBroadcastFormat());
+    rebuiltTx.signature.length.should.equal(1);
+    rebuiltTx.toBroadcastFormat().should.equal(tx.toBroadcastFormat());
+    rebuiltTx.toBroadcastFormat().should.not.equal(rawTx.unsignedUpdatePledgeTx);
+  });
+
+  it('should init partially signed txn hex and preserver the signature', async () => {
+    const txnBuilderFactory = new TransactionBuilderFactory(coins.get('tada'));
+    const txnBuilder = txnBuilderFactory.from(rawTx.partiallySignedPledgeTx);
+    let tx = (await txnBuilder.build()) as Transaction;
+    tx.type.should.equal(TransactionType.StakingPledge);
+    let txData = tx.toJson();
+    txData.witnesses.length.should.equal(1);
+    txData.witnesses[0].publicKey.should.equal(rawTx.pledgeNodeKeyPubkey);
+    txData.witnesses[0].signature.should.equal(rawTx.pledgeNodeWitnessSignature);
+
+    txnBuilder.addSignature({ pub: rawTx.pledgeNodeKeyPubkey }, Buffer.from(rawTx.pledgeNodeWitnessSignature, 'hex'));
+    tx = (await txnBuilder.build()) as Transaction;
+    tx.type.should.equal(TransactionType.StakingPledge);
+    txData = tx.toJson();
+    txData.witnesses.length.should.equal(2);
+    txData.witnesses[0].publicKey.should.equal(rawTx.pledgeNodeKeyPubkey);
+    txData.witnesses[0].signature.should.equal(rawTx.pledgeNodeWitnessSignature);
+    txData.witnesses[1].publicKey.should.equal(rawTx.pledgeNodeKeyPubkey);
+    txData.witnesses[1].signature.should.equal(rawTx.pledgeNodeWitnessSignature);
   });
 });
