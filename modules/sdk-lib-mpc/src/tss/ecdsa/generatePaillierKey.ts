75f5f447d449db6148ac529f0d33a13d95d04d8e
John Driscoll <john@bitgo.com>
2023-07-28 16:17:40 -0500

    feat(sdk-lib-mpc): add implementation of paillier-blum for paillier proof
    TICKET: HSM-102

---
+++ modules/sdk-lib-mpc/src/tss/ecdsa/generatePaillierKey.ts
@@ -0,0 +1,28 @@
+import * as bcu from 'bigint-crypto-utils';
+import { PublicKey, PrivateKey, KeyPair } from 'paillier-bigint';
+import { prove } from './paillierBlumProof';
+
+export interface KeyPairWithProof {
+  keyPair: KeyPair;
+  w: bigint;
+  x: Array<bigint>;
+  z: Array<bigint>;
+}
+
+// Implementation based on paillier-bigint's generateRandomKeys
+export async function generatePaillierKey(bitlength = 3072): Promise<KeyPairWithProof> {
+  let p, q, n;
+  do {
+    p = await bcu.prime(Math.floor(bitlength / 2) + 1);
+    q = await bcu.prime(Math.floor(bitlength / 2));
+    n = p * q;
+  } while (q === p || q % BigInt(4) !== BigInt(3) || p % BigInt(4) !== BigInt(3) || bcu.bitLength(n) !== bitlength);
+  const { w, x, z } = await prove(p, q);
+  const g = n + BigInt(1);
+  const lambda = (p - BigInt(1)) * (q - BigInt(1));
+  const mu = bcu.modInv(lambda, n);
+  const publicKey = new PublicKey(n, g);
+  const privateKey = new PrivateKey(lambda, mu, publicKey, p, q);
+  const keyPair: KeyPair = { publicKey, privateKey };
+  return { keyPair, w, x, z };
+}
