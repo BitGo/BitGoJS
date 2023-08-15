e224ca306608e9618d080fdb623db09307a91910
Otto Allmendinger <otto@bitgo.com>
2023-08-15 13:49:08 +0200

    feat(sdk-core): extend build param codec
    Issue: BTC-368

--- modules/sdk-core/test/unit/bitgo/wallet/SendTransactionRequest.ts
+++ modules/sdk-core/test/unit/bitgo/wallet/SendTransactionRequest.ts
@@ -5,9 +5,25 @@ import { getCodecProperties } from '../../../../src/bitgo/utils/codecProps';
 
 describe('SendTransactionRequest', function () {
   it('has expected property count', function () {
-    assert.strictEqual(getCodecProperties(BuildParams).length, 54);
+    assert.strictEqual(getCodecProperties(BuildParams).length, 72);
   });
+
   it('enforces codec', function () {
+    assert.deepStrictEqual(
+      BuildParams.encode({
+        addressType: 'p2sh',
+        txFormat: 'psbt',
+        comment: 'foo',
+        unknown: 'bar',
+      } as any),
+      {
+        addressType: 'p2sh',
+        txFormat: 'psbt',
+        comment: 'foo',
+        // drops unknown properties
+      }
+    );
+
     assert.deepStrictEqual(SendTransactionRequest.encode({ txHex: '00' }), { txHex: '00' });
     assert.deepStrictEqual(SendTransactionRequest.encode({ txHex: '00', addressType: 'p2sh', bar: 'omit' } as any), {
       txHex: '00',
