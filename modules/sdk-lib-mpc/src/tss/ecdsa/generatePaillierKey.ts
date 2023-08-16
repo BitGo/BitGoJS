e38c7eef63fd8e952d8f4aa2a9c3315cedda44d4
Zahin Mohammad <zahinmohammad@bitgo.com>
2023-08-16 12:35:30 -0400

    test(sdk-lib-mpc): add paillierBlumProof tests
    TICKET: WP-132

--- modules/sdk-lib-mpc/src/tss/ecdsa/generatePaillierKey.ts
+++ modules/sdk-lib-mpc/src/tss/ecdsa/generatePaillierKey.ts
@@ -1,22 +1,32 @@
 import * as bcu from 'bigint-crypto-utils';
 import { PublicKey, PrivateKey, KeyPair } from 'paillier-bigint';
 import { prove } from './paillierBlumProof';
-import { KeyPairWithDeserializedPaillierBlumProof } from './types';
+import { DeserializedKeyPairWithPaillierBlumProof, RawPaillierKey } from './types';
 
 // Implementation based on paillier-bigint's generateRandomKeys
-export async function generatePaillierKey(bitlength = 3072): Promise<KeyPairWithDeserializedPaillierBlumProof> {
-  let p, q, n;
+export async function generatePaillierKey(bitlength = 3072): Promise<RawPaillierKey> {
+  let [p, q, n] = [BigInt(0), BigInt(0), BigInt(0)];
   do {
     p = await bcu.prime(Math.floor(bitlength / 2) + 1);
     q = await bcu.prime(Math.floor(bitlength / 2));
     n = p * q;
   } while (q === p || q % BigInt(4) !== BigInt(3) || p % BigInt(4) !== BigInt(3) || bcu.bitLength(n) !== bitlength);
-  const { w, x, z } = await prove(p, q);
-  const g = n + BigInt(1);
   const lambda = (p - BigInt(1)) * (q - BigInt(1));
   const mu = bcu.modInv(lambda, n);
+  return { n, lambda, mu, p, q };
+}
+
+export async function generatePaillierKeyWithProof(
+  bitlength = 3072
+): Promise<DeserializedKeyPairWithPaillierBlumProof> {
+  const key = await generatePaillierKey(bitlength);
+  const proof = await prove(key.p, key.q);
+  return { ...key, ...proof };
+}
+
+export function rawPaillierKeyToPaillierKey(n: bigint, lambda: bigint, mu: bigint, p: bigint, q: bigint): KeyPair {
+  const g = n + BigInt(1);
   const publicKey = new PublicKey(n, g);
   const privateKey = new PrivateKey(lambda, mu, publicKey, p, q);
-  const keyPair: KeyPair = { publicKey, privateKey };
-  return { keyPair, w, x, z };
+  return { publicKey, privateKey };
 }
--- modules/sdk-lib-mpc/src/tss/ecdsa/index.ts
+++ modules/sdk-lib-mpc/src/tss/ecdsa/index.ts
@@ -2,5 +2,5 @@ export * as EcdsaTypes from './types';
 export * as EcdsaRangeProof from './rangeproof';
 export * as EcdsaPaillierProof from './paillierproof';
 export * as EcdsaZkVProof from './zkVProof';
-
+export * from './generatePaillierKey';
 export const minModulusBitLength = 3072;
