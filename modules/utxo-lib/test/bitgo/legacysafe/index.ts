4ac75f0031a40aa17de37f176e3493284cba4cac
Rushil Kapoor <rushilkapoor@bitgo.com>
2023-08-17 13:01:52 +0530

    feat(utxo-lib): add tests for v1 safe wallets with uncompressed public keys
    BTC-335

--- modules/utxo-lib/test/bitgo/legacysafe/index.ts
+++ modules/utxo-lib/test/bitgo/legacysafe/index.ts
@@ -1,5 +1,5 @@
 import { toUncompressedPub, toCompressedPub, createLegacySafeOutputScript2of3 } from '../../../src/bitgo/legacysafe';
-import { getKey, getKeyTriple } from '../../../src/testutil';
+import { getKey, getKeyTriple, getUncompressedKeyTriple } from '../../../src/testutil';
 import * as assert from 'assert';
 import { networks } from '../../../src';
 import { fromOutputScript } from '../../../src/address';
@@ -50,5 +50,22 @@ describe('legacy safe scripts creation', function () {
     assert.strictEqual(script.redeemScript.length, uncompressedPubKeysScriptSize); // bytes
     assert.strictEqual(script.scriptPubKey.length, 23); // bytes;
     assert.strictEqual(fromOutputScript(script.scriptPubKey, networks.bitcoin), uncompressedPubKeysAddress);
+  });
+
+  it('creates deterministic uncompressed public keys and address for v1 safe wallet', function () {
+    const uncompressedKeyPairs = getUncompressedKeyTriple([12414, 546456, 65856546]);
+    const uncompressedPubKeys = uncompressedKeyPairs.map((keyPair) => keyPair.publicKey);
+    const outputScript = createLegacySafeOutputScript2of3(uncompressedPubKeys, networks.testnet);
+
+    assert.strictEqual(outputScript.scriptPubKey.length, 23);
+    assert.strictEqual(outputScript.redeemScript.length, 201);
+    assert.strictEqual(outputScript.scriptPubKey.toString('hex'), 'a9146e9fb0db60f4fc5974885c392815b58e6505a30d87');
+    assert.strictEqual(
+      outputScript.redeemScript.toString('hex'),
+      '524104b9aea0bf6be18d3ed48d1cc3495e9af5e499ad90a84930990ba58b198ee81b5e83f6aa2f617c04401616e844f51802c0f1d827422efc1f3856dc222dd8beed16410435e9d7c48e3a5254d5881b60abf004cf6eedc6ab842393caa2fdd20d6d0ad170cc32c4664603de3e9b9d1fd01b070d192fad570fcbb3b185c034ec5a4a0b1fb44104466d7fcae563e5cb09a0d1870bb580344804617879a14949cf22285f1bae3f276728176c3c6431f8eeda4538dc37c865e2784f3a9e77d044f33e407797e1278a53ae'
+    );
+
+    const addressTestnet = fromOutputScript(outputScript.scriptPubKey, networks.testnet);
+    assert.strictEqual(addressTestnet, '2N3L9cu9WN2Df7Xvb1Y8owokuDVj5Hdyv4i');
   });
 });
