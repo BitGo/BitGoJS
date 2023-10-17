75f8c87e8b7f185a1ae383aba9516b1980a31a39
David Kaplan <davidkaplan@bitgo.com>
2023-10-17 08:25:07 -0400

    test(utxo-lib): expand tests to other coins
    BTC-316

--- modules/utxo-lib/test/bitgo/psbt/signingAndValidation.ts
+++ modules/utxo-lib/test/bitgo/psbt/signingAndValidation.ts
@@ -2,20 +2,9 @@ import * as assert from 'assert';
 import { describe, it } from 'mocha';
 import * as bs58check from 'bs58check';
 
-import { getDefaultWalletKeys, mockReplayProtectionUnspent, mockUnspents } from '../../../src/testutil';
-import {
-  addReplayProtectionUnspentToPsbt,
-  addWalletOutputToPsbt,
-  addWalletUnspentToPsbt,
-  createPsbtForNetwork,
-  getInternalChainCode,
-  KeyName,
-  outputScripts,
-  UtxoPsbt,
-  WalletUnspent,
-} from '../../../src/bitgo';
+import { constructPsbt, getDefaultWalletKeys } from '../../../src/testutil';
+import { getSignatureValidationArrayPsbt, KeyName, outputScripts, UtxoPsbt } from '../../../src/bitgo';
 import { getNetworkName, Network, networks } from '../../../src';
-import { createOutputScriptP2shP2pk } from '../../../src/bitgo/outputScripts';
 
 function getScriptTypes(): outputScripts.ScriptType[] {
   return [...outputScripts.scriptTypes2Of3, 'p2shP2pk'];
@@ -39,11 +28,20 @@ function runTest(scriptType: outputScripts.ScriptType, signerName: KeyName, cosi
     `signer=${signerName}`,
     `cosigner=${cosignerName}`,
   ].join(',')}`, function () {
-    let psbt: UtxoPsbt;
+    let psbtBase: UtxoPsbt;
     before('create transaction', async function () {
       // Build a fully hydrated UtxoPsbt
-      psbt = createPsbtForNetwork({ network });
-      psbt.updateGlobal({
+      psbtBase = constructPsbt(
+        [{ scriptType, value: BigInt(1e8) }],
+        [{ scriptType: 'p2sh', value: BigInt(1e8 - 10000) }],
+        network,
+        walletKeys,
+        'unsigned',
+        {
+          signers: { signerName, cosignerName },
+        }
+      );
+      psbtBase.updateGlobal({
         globalXpub: walletKeys.triple.map((bip32) => {
           const extendedPubkey = bip32.neutered().toBase58();
           return {
@@ -53,36 +51,23 @@ function runTest(scriptType: outputScripts.ScriptType, signerName: KeyName, cosi
           };
         }),
       });
-
-      // Add the inputs
-      if (scriptType === 'p2shP2pk') {
-        const unspent = mockReplayProtectionUnspent(network, BigInt(1e8), { key: signer });
-        const { redeemScript } = createOutputScriptP2shP2pk(signer.publicKey);
-        assert(redeemScript);
-        addReplayProtectionUnspentToPsbt(psbt, unspent, redeemScript);
-      } else {
-        const unspents = mockUnspents(walletKeys, [scriptType], BigInt(1e8), network) as WalletUnspent<bigint>[];
-        unspents.forEach((unspent) => addWalletUnspentToPsbt(psbt, unspent, walletKeys, signerName, cosignerName));
-      }
-
-      // Add the outputs
-      addWalletOutputToPsbt(psbt, walletKeys, getInternalChainCode('p2sh'), 0, BigInt(1e8 - 10000));
     });
 
     it('can go from unsigned to fully signed', async function () {
+      const psbt = psbtBase.clone();
       if (scriptType === 'p2trMusig2' && signerName === 'user' && cosignerName === 'bitgo') {
         psbt.setAllInputsMusig2NonceHD(signer);
         psbt.setAllInputsMusig2NonceHD(cosigner);
       }
       assert.ok(psbt.getSignatureValidationArray(0).every((res) => !res));
-      if (scriptType === 'p2shP2pk') {
-        psbt.signAllInputs(signer);
-      } else {
-        psbt.signAllInputsHD(signer);
+      psbt.signAllInputsHD(signer);
+      if (scriptType !== 'p2shP2pk') {
         psbt.signAllInputsHD(cosigner);
       }
       assert(psbt.validateSignaturesOfAllInputs());
-      assert.deepStrictEqual(psbt.getSignatureValidationArray(0), signingKeys);
+      if (scriptType !== 'p2shP2pk') {
+        assert.deepStrictEqual(getSignatureValidationArrayPsbt(psbt, walletKeys)[0], [0, signingKeys]);
+      }
       psbt.finalizeAllInputs();
       const tx = psbt.extractTransaction();
       assert(tx);
@@ -93,4 +78,11 @@ function runTest(scriptType: outputScripts.ScriptType, signerName: KeyName, cosi
 getScriptTypes().forEach((t) => {
   runTest(t, 'user', 'bitgo', networks.bitcoin);
   runTest(t, 'backup', 'user', networks.bitcoin);
+  if (t === 'p2sh') {
+    runTest(t, 'user', 'backup', networks.litecoin);
+    runTest(t, 'user', 'backup', networks.zcash);
+    runTest(t, 'user', 'backup', networks.bitcoincash);
+    runTest(t, 'user', 'backup', networks.bitcoingold);
+    runTest(t, 'user', 'backup', networks.dash);
+  }
 });
