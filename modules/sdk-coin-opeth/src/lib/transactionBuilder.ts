df6eea5d299c415b30263d1713335c14e5abef4a
Akash Gianchandani <akashgianchandani@bitgo.com>
2023-11-07 23:59:59 +0530

    refactor(abstract-eth): add common method to abstract-eth WIN-1087
    BREAKING CHANGE: Type of nextContractSequenceId field in TransactionPrebuild
    interface is changed from string to number in AbstractEthLikeCoin and AbstractEthLikeNewCoins classes.
    getCustomChainName method is removed from Polygon class because a common
    method getCustomChainCommon has been added to AbstractEthLikeNewCoins
    class for all EthLike coins. replayProtectionOptions is not optional in buildTransaction method in AbstractEthLikeNewCoins
    and needs to be passed to derive the Eth common object from the chainId.
    signFinalPolygon method name from Polygon class is updated to signFinalEthLike so that
    it can be used for other EthLike coins. getBaseFactor method in Eth
    and Polygon class returns number instead of string just to align with
    AbstractEthLikeCoin
    Ticket: WIN-1012

--- modules/sdk-coin-opeth/src/lib/transactionBuilder.ts
+++ modules/sdk-coin-opeth/src/lib/transactionBuilder.ts
@@ -1,12 +1,6 @@
 import { BaseCoin as CoinConfig } from '@bitgo/statics';
-import EthereumAbi from 'ethereumjs-abi';
 import { BuildTransactionError, TransactionType } from '@bitgo/sdk-core';
-import {
-  KeyPair,
-  Transaction,
-  TransactionBuilder as EthLikeTransactionBuilder,
-  walletSimpleConstructor,
-} from '@bitgo/abstract-eth';
+import { KeyPair, Transaction, TransactionBuilder as EthLikeTransactionBuilder } from '@bitgo/abstract-eth';
 import { getCommon } from './utils';
 import { TransferBuilder } from './transferBuilder';
 import { walletSimpleByteCode } from './walletUtil';
@@ -18,20 +12,7 @@ export class TransactionBuilder extends EthLikeTransactionBuilder {
     super(_coinConfig);
     this._common = getCommon(this._coinConfig.network.type);
     this.transaction = new Transaction(this._coinConfig, this._common);
-  }
-
-  /**
-   * Returns the smart contract encoded data
-   *
-   * @param {string[]} addresses - the contract signers
-   * @returns {string} - the smart contract encoded data
-   */
-  protected getContractData(addresses: string[]): string {
-    const params = [addresses];
-    const resultEncodedParameters = EthereumAbi.rawEncode(walletSimpleConstructor, params)
-      .toString('hex')
-      .replace('0x', '');
-    return walletSimpleByteCode + resultEncodedParameters;
+    this._walletSimpleByteCode = walletSimpleByteCode;
   }
 
   /** @inheritdoc */
