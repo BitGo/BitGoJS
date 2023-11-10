6a0e5c74d27d4a7ed5e9972e184fb9744b15793e
Aaron Loe <aaronloe@bitgo.com>
2023-11-10 09:46:42 -0800

    fix: use public types for tx send
    - use the official codec from @bitgo/public-types
      when forming API calls to the /tx/send endpoint
    
    Ticket: CE-3212

--- modules/sdk-core/test/unit/bitgo/wallet/SendTransactionRequest.ts
+++ modules/sdk-core/test/unit/bitgo/wallet/SendTransactionRequest.ts
@@ -1,16 +1,10 @@
 import * as assert from 'assert';
-import { SendTransactionRequest } from '../../../../src/bitgo/wallet/SendTransactionRequest';
-import { BuildParams } from '../../../../src/bitgo/wallet/BuildParams';
-import { getCodecProperties } from '../../../../src/bitgo/utils/codecProps';
+import { TxSendBody } from '@bitgo/public-types';
 
 describe('SendTransactionRequest', function () {
-  it('has expected property count', function () {
-    assert.strictEqual(getCodecProperties(BuildParams).length, 74);
-  });
-
   it('enforces codec', function () {
     assert.deepStrictEqual(
-      BuildParams.encode({
+      TxSendBody.encode({
         addressType: 'p2sh',
         txFormat: 'psbt',
         comment: 'foo',
@@ -24,8 +18,8 @@ describe('SendTransactionRequest', function () {
       }
     );
 
-    assert.deepStrictEqual(SendTransactionRequest.encode({ txHex: '00' }), { txHex: '00' });
-    assert.deepStrictEqual(SendTransactionRequest.encode({ txHex: '00', addressType: 'p2sh', bar: 'omit' } as any), {
+    assert.deepStrictEqual(TxSendBody.encode({ txHex: '00' }), { txHex: '00' });
+    assert.deepStrictEqual(TxSendBody.encode({ txHex: '00', addressType: 'p2sh', bar: 'omit' } as any), {
       txHex: '00',
       addressType: 'p2sh',
     });
