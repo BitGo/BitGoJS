cb1d616ae81d7a69191705944bb42345ac00fe3f
John Driscoll <john@bitgo.com>
2023-12-04 14:19:19 -0600

    chore(deser-lib): move current code to cbor namespace
    TICKET: HSM-236

---
+++ modules/deser-lib/src/cbor.ts
@@ -0,0 +1,192 @@
+import { decodeFirstSync, encodeCanonical } from 'cbor';
+
+/**
+ * Return a string describing value as a type.
+ * @param value - Any javascript value to type.
+ * @returns String describing value type.
+ */
+function getType(value: unknown): string {
+  if (value === null || value === undefined) {
+    return 'null';
+  }
+  if (value instanceof Array) {
+    const types = value.map(getType);
+    if (!types.slice(1).every((value) => value === types[0])) {
+      throw new Error('Array elements are not of the same type');
+    }
+    return JSON.stringify([types[0]]);
+  }
+  if (value instanceof Object) {
+    const properties = Object.getOwnPropertyNames(value);
+    properties.sort();
+    return JSON.stringify(
+      properties.reduce((acc, name) => {
+        acc[name] = getType(value[name]);
+        return acc;
+      }, {})
+    );
+  }
+  if (typeof value === 'string') {
+    if (value.startsWith('0x')) {
+      return 'bytes';
+    }
+    return 'string';
+  }
+  return JSON.stringify(typeof value);
+}
+
+/**
+ * Compare two buffers for sorting.
+ * @param a - left buffer to compare to right buffer.
+ * @param b - right buffer to compare to left buffer.
+ * @returns Negative if a < b, positive if b > a, 0 if equal.
+ */
+function bufferCompare(a: Buffer, b: Buffer): number {
+  let i = 0;
+  while (i < a.length && i < b.length && a[i] == b[i]) {
+    i++;
+  }
+  if (i === a.length && i === b.length) {
+    return 0;
+  }
+  if (i === a.length || i === b.length) {
+    return a.length - b.length;
+  }
+  return a[i] - b[i];
+}
+
+/** A sortable array element. */
+interface Sortable {
+  weight: number;
+  value?: unknown;
+}
+
+/**
+ * Type check for sortable array element.
+ * @param value - Value to type check.
+ * @returns True if value is a sortable array element.
+ */
+function isSortable(value: unknown): value is Sortable {
+  return value instanceof Object && 'weight' in value;
+}
+
+/**
+ * Compare two array elements for sorting.
+ * @param a - left element to compare to right element.
+ * @param b - right element to compare to left element.
+ * @returns Negative if a < b, positive if b > a, 0 if equal.
+ */
+function elementCompare(a: unknown, b: unknown): number {
+  if (!isSortable(a) || !isSortable(b)) {
+    throw new Error('Array elements must be sortable');
+  }
+  if (a.weight === b.weight) {
+    if ('value' in a && 'value' in b) {
+      const aVal = transform(a.value);
+      const bVal = transform(b.value);
+      if (
+        (!Buffer.isBuffer(aVal) && typeof aVal !== 'string' && typeof aVal !== 'number') ||
+        (!Buffer.isBuffer(bVal) && typeof bVal !== 'string' && typeof bVal !== 'number')
+      ) {
+        throw new Error('Array element value cannot be compared');
+      }
+      let aBuf, bBuf;
+      if (typeof aVal === 'number') {
+        aBuf = Buffer.from([aVal]);
+      } else {
+        aBuf = Buffer.from(aVal);
+      }
+      if (typeof bVal === 'number') {
+        bBuf = Buffer.from([bVal]);
+      } else {
+        bBuf = Buffer.from(bVal);
+      }
+      return bufferCompare(aBuf, bBuf);
+    }
+    throw new Error('Array elements must be sortable');
+  }
+  return a.weight - b.weight;
+}
+
+/**
+ * Transform value into its canonical, serializable form.
+ * @param value - Value to transform.
+ * @returns Canonical, serializable form of value.
+ */
+export function transform<T>(value: T): T | Buffer {
+  if (value === null || value === undefined) {
+    return value;
+  }
+  if (typeof value === 'string') {
+    // Transform hex strings to buffers.
+    if (value.startsWith('0x')) {
+      if (!value.match(/^0x([0-9a-fA-F]{2})*$/)) {
+        throw new Error('0x prefixed string contains non-hex characters.');
+      }
+      return Buffer.from(value.slice(2), 'hex');
+    }
+  } else if (value instanceof Array) {
+    // Enforce array elements are same type.
+    getType(value);
+    value = [...value] as unknown as T;
+    (value as unknown as Array<unknown>).sort(elementCompare);
+    return (value as unknown as Array<unknown>).map(transform) as unknown as T;
+  } else if (value instanceof Object) {
+    const properties = Object.getOwnPropertyNames(value);
+    properties.sort();
+    return properties.reduce((acc, name) => {
+      acc[name] = transform(value[name]);
+      return acc;
+    }, {}) as unknown as T;
+  }
+  return value;
+}
+
+/**
+ * Untransform value into its human readable form.
+ * @param value - Value to untransform.
+ * @returns Untransformed, human readable form of value.
+ */
+export function untransform<T>(value: T): T | string {
+  if (Buffer.isBuffer(value)) {
+    return '0x' + value.toString('hex');
+  }
+  if (value instanceof Array && value.length > 1) {
+    for (let i = 1; i < value.length; i++) {
+      if (value[i - 1].weight > value[i].weight) {
+        throw new Error('Array elements are not in canonical order');
+      }
+    }
+    return value.map(untransform) as unknown as T;
+  } else if (value instanceof Object) {
+    const properties = Object.getOwnPropertyNames(value);
+    for (let i = 1; i < properties.length; i++) {
+      if (properties[i - 1].localeCompare(properties[i]) > 0) {
+        throw new Error('Object properties are not in caonical order');
+      }
+    }
+    return properties.reduce((acc, name) => {
+      acc[name] = untransform(value[name]);
+      return acc;
+    }, {}) as unknown as T;
+  }
+  return value;
+}
+
+/**
+ * Serialize a value.
+ * @param value - Value to serialize.
+ * @returns Buffer representing serialized value.
+ */
+export function serialize<T>(value: T): Buffer {
+  return encodeCanonical(transform(value));
+}
+
+/**
+ * Deserialize a value.
+ * @param value - Buffer to deserialize.
+ * @returns Deserialized value.
+ */
+export function deserialize(value: Buffer): unknown {
+  return untransform(decodeFirstSync(value));
+}
--- modules/deser-lib/test/unit/deser-lib.ts
+++ modules/deser-lib/test/unit/deser-lib.ts
@@ -1,312 +1,314 @@
-import { transform, untransform, serialize, deserialize } from '../..';
-import * as fixtures from '../fixtures.json';
+import { cbor } from '../..';
+import * as cborFixtures from '../cbor/fixtures.json';
 
 describe('deser-lib', function () {
-  describe('transform', function () {
-    it('orders object properties canonically', function () {
-      const res = transform({ b: 'second', a: 'first' }) as any;
-      const properties = Object.getOwnPropertyNames(res);
-      properties[0].should.equal('a');
-      properties[1].should.equal('b');
-      res.a.should.equal('first');
-      res.b.should.equal('second');
-    });
-
-    describe('canonical ordering', function () {
-      it('orders by weight', function () {
-        const res = transform([
-          { weight: 2, value: null },
-          { weight: 1, value: null },
-        ]) as any;
-        res[0].weight.should.equal(1);
-        res[1].weight.should.equal(2);
+  describe('cbor', function () {
+    describe('transform', function () {
+      it('orders object properties canonically', function () {
+        const res = cbor.transform({ b: 'second', a: 'first' }) as any;
+        const properties = Object.getOwnPropertyNames(res);
+        properties[0].should.equal('a');
+        properties[1].should.equal('b');
+        res.a.should.equal('first');
+        res.b.should.equal('second');
       });
 
-      it('groups equal elements', function () {
-        const res = transform([
-          {
-            weight: 2,
-            value: 'b',
-          },
-          {
-            weight: 1,
-            value: 'a',
-          },
-          {
-            weight: 3,
-            value: 'c',
-          },
-          {
-            weight: 2,
-            value: 'b',
-          },
-        ]) as any;
-        res[0].weight.should.equal(1);
-        res[1].weight.should.equal(2);
-        res[2].weight.should.equal(2);
-        res[3].weight.should.equal(3);
-      });
-
-      it('orders number values', function () {
-        const res = transform([
-          { weight: 1, value: 2 },
-          { weight: 1, value: 1 },
-        ]) as any;
-        res[0].value.should.equal(1);
-        res[1].value.should.equal(2);
-      });
-
-      it('orders string values', function () {
-        const res = transform([
-          { weight: 1, value: 'ab' },
-          { weight: 1, value: 'aa' },
-        ]) as any;
-        res[0].value.should.equal('aa');
-        res[1].value.should.equal('ab');
-      });
-
-      it('orders byte values', function () {
-        const res = transform([
-          { weight: 1, value: '0x0b' },
-          { weight: 1, value: '0x0a' },
-        ]) as any;
-        res[0].value.equals(Buffer.from([0x0a])).should.equal(true);
-        res[1].value.equals(Buffer.from([0x0b])).should.equal(true);
-      });
-
-      it('orders string values of different lengths', function () {
-        const res = transform([
-          { weight: 1, value: 'ab' },
-          { weight: 1, value: 'a' },
-        ]) as any;
-        res[0].value.should.equal('a');
-        res[1].value.should.equal('ab');
-      });
-
-      it('throws for elements without weight', function () {
-        (() => transform([{}, {}])).should.throw();
-      });
-
-      it('throws for elements without value', function () {
-        (() => transform([{ weight: 1 }, { weight: 1 }])).should.throw();
-      });
-
-      it('throws for values that cannot be compared', function () {
-        (() =>
-          transform([
-            { weight: 1, value: {} },
-            { weight: 1, value: 1 },
-          ])).should.throw();
-        (() =>
-          transform([
-            { weight: 1, value: undefined },
+      describe('canonical ordering', function () {
+        it('orders by weight', function () {
+          const res = cbor.transform([
+            { weight: 2, value: null },
             { weight: 1, value: null },
-          ])).should.throw();
-      });
+          ]) as any;
+          res[0].weight.should.equal(1);
+          res[1].weight.should.equal(2);
+        });
 
-      it('throws for elements of mixed type', function () {
-        (() =>
-          transform([
-            { weight: 0, value: '0' },
-            { weight: 0, value: 0 },
-          ])).should.throw();
-      });
-    });
-
-    it('preserves null values', function () {
-      const res = transform({ value: null }) as any;
-      res.should.have.property('value').which.is.null();
-    });
-
-    it('replaces prefixed hex strings with Buffers', function () {
-      const hex = '00010203';
-      const res = transform({ value: '0x' + hex }) as any;
-      Buffer.isBuffer(res.value).should.equal(true);
-      res.value.equals(Buffer.from(hex, 'hex')).should.equal(true);
-    });
-
-    it('preserves non-prefixed hex strings', function () {
-      const string = '00010203';
-      const res = transform({ value: string }) as any;
-      res.value.should.equal(string);
-    });
-
-    it('transforms object recursively', function () {
-      const res = transform({ value: { b: 'second', a: 'first' } }) as any;
-      const properties = Object.getOwnPropertyNames(res.value);
-      properties[0].should.equal('a');
-      properties[1].should.equal('b');
-      res.value.a.should.equal('first');
-      res.value.b.should.equal('second');
-    });
-
-    it('transforms array recursively', function () {
-      const res = transform([{ weight: 0, value: { b: 'second', a: 'first' } }]) as any;
-      const properties = Object.getOwnPropertyNames(res[0].value);
-      properties[0].should.equal('a');
-      properties[1].should.equal('b');
-      res[0].value.a.should.equal('first');
-      res[0].value.b.should.equal('second');
-    });
-
-    it('throws for invalid hex strings', function () {
-      (() => transform('0x0g')).should.throw();
-    });
-  });
-
-  describe('untransform', function () {
-    it('untransforms object', function () {
-      const res = untransform({ a: 'first', b: 'second' }) as any;
-      const properties = Object.getOwnPropertyNames(res);
-      properties[0].should.equal('a');
-      properties[1].should.equal('b');
-      res.a.should.equal('first');
-      res.b.should.equal('second');
-    });
-
-    it('enforces canonical object property order', function () {
-      (() => untransform({ b: 'second', a: 'first' })).should.throw();
-    });
-
-    it('enforces canonical array element order', function () {
-      (() => untransform([{ weight: 2 }, { weight: 1 }])).should.throw();
-    });
-
-    it('replaces Buffers with prefixed hex strings', function () {
-      const hex = '00010203';
-      const res = untransform({ value: Buffer.from(hex, 'hex') }) as any;
-      res.value.should.equal('0x' + hex);
-    });
-
-    it('preserves non-prefixed hex strings', function () {
-      const string = '00010203';
-      const res = untransform({ value: string }) as any;
-      res.value.should.equal(string);
-    });
-
-    it('untransforms object recursively', function () {
-      const hex = '00010203';
-      const res = untransform({ value: { value: Buffer.from(hex, 'hex') } }) as any;
-      res.value.value.should.equal('0x' + hex);
-    });
-
-    it('untransforms array recursively', function () {
-      const hex = '00010203';
-      const res = untransform([{ value: Buffer.from(hex, 'hex'), weight: 0 }]) as any;
-      res[0].value.should.equal('0x' + hex);
-    });
-  });
-
-  describe('fixtures', function () {
-    xit('creates test vectors', function () {
-      const { writeFileSync } = require('fs');
-      const deserialized = [
-        {
-          keys: [
-            {
-              key: '0x010203',
-              weight: 0,
-            },
-            {
-              key: '0x040506',
-              weight: 1,
-            },
-          ],
-        },
-        {
-          a: '0xffffffff',
-          b: '0x00000000',
-          c: '0xffffffff',
-          d: [
-            {
-              weight: 0,
-            },
-            {
-              weight: 1,
-            },
+        it('groups equal elements', function () {
+          const res = cbor.transform([
             {
               weight: 2,
+              value: 'b',
+            },
+            {
+              weight: 1,
+              value: 'a',
             },
             {
               weight: 3,
-            },
-          ],
-        },
-        {
-          a: [
-            {
-              value: 'a',
-              weight: 0,
-            },
-            {
-              value: 'b',
-              weight: 0,
-            },
-            {
               value: 'c',
-              weight: 0,
-            },
-          ],
-        },
-        {
-          a: [
-            {
-              weight: 0,
-              value: '0x0a',
             },
             {
-              weight: 0,
-              value: '0x0b',
+              weight: 2,
+              value: 'b',
             },
-            {
-              weight: 0,
-              value: '0x0c',
-            },
-          ],
-        },
-        {
-          a: [
-            {
-              weight: 0,
-              value: 1,
-            },
-            {
-              weight: 0,
-              value: 2,
-            },
-            {
-              weight: 0,
-              value: 3,
-            },
-          ],
-        },
-      ];
-      const serialized = deserialized.map((x) => serialize(x).toString('hex'));
-      writeFileSync(
-        'test/fixtures.json',
-        JSON.stringify(
-          deserialized.map((deserialized, i) => ({
-            deserialized: untransform(transform(deserialized)),
-            serialized: serialized[i],
-          })),
-          null,
-          2
-        )
-      );
+          ]) as any;
+          res[0].weight.should.equal(1);
+          res[1].weight.should.equal(2);
+          res[2].weight.should.equal(2);
+          res[3].weight.should.equal(3);
+        });
+
+        it('orders number values', function () {
+          const res = cbor.transform([
+            { weight: 1, value: 2 },
+            { weight: 1, value: 1 },
+          ]) as any;
+          res[0].value.should.equal(1);
+          res[1].value.should.equal(2);
+        });
+
+        it('orders string values', function () {
+          const res = cbor.transform([
+            { weight: 1, value: 'ab' },
+            { weight: 1, value: 'aa' },
+          ]) as any;
+          res[0].value.should.equal('aa');
+          res[1].value.should.equal('ab');
+        });
+
+        it('orders byte values', function () {
+          const res = cbor.transform([
+            { weight: 1, value: '0x0b' },
+            { weight: 1, value: '0x0a' },
+          ]) as any;
+          res[0].value.equals(Buffer.from([0x0a])).should.equal(true);
+          res[1].value.equals(Buffer.from([0x0b])).should.equal(true);
+        });
+
+        it('orders string values of different lengths', function () {
+          const res = cbor.transform([
+            { weight: 1, value: 'ab' },
+            { weight: 1, value: 'a' },
+          ]) as any;
+          res[0].value.should.equal('a');
+          res[1].value.should.equal('ab');
+        });
+
+        it('throws for elements without weight', function () {
+          (() => cbor.transform([{}, {}])).should.throw();
+        });
+
+        it('throws for elements without value', function () {
+          (() => cbor.transform([{ weight: 1 }, { weight: 1 }])).should.throw();
+        });
+
+        it('throws for values that cannot be compared', function () {
+          (() =>
+            cbor.transform([
+              { weight: 1, value: {} },
+              { weight: 1, value: 1 },
+            ])).should.throw();
+          (() =>
+            cbor.transform([
+              { weight: 1, value: undefined },
+              { weight: 1, value: null },
+            ])).should.throw();
+        });
+
+        it('throws for elements of mixed type', function () {
+          (() =>
+            cbor.transform([
+              { weight: 0, value: '0' },
+              { weight: 0, value: 0 },
+            ])).should.throw();
+        });
+      });
+
+      it('preserves null values', function () {
+        const res = cbor.transform({ value: null }) as any;
+        res.should.have.property('value').which.is.null();
+      });
+
+      it('replaces prefixed hex strings with Buffers', function () {
+        const hex = '00010203';
+        const res = cbor.transform({ value: '0x' + hex }) as any;
+        Buffer.isBuffer(res.value).should.equal(true);
+        res.value.equals(Buffer.from(hex, 'hex')).should.equal(true);
+      });
+
+      it('preserves non-prefixed hex strings', function () {
+        const string = '00010203';
+        const res = cbor.transform({ value: string }) as any;
+        res.value.should.equal(string);
+      });
+
+      it('transforms object recursively', function () {
+        const res = cbor.transform({ value: { b: 'second', a: 'first' } }) as any;
+        const properties = Object.getOwnPropertyNames(res.value);
+        properties[0].should.equal('a');
+        properties[1].should.equal('b');
+        res.value.a.should.equal('first');
+        res.value.b.should.equal('second');
+      });
+
+      it('transforms array recursively', function () {
+        const res = cbor.transform([{ weight: 0, value: { b: 'second', a: 'first' } }]) as any;
+        const properties = Object.getOwnPropertyNames(res[0].value);
+        properties[0].should.equal('a');
+        properties[1].should.equal('b');
+        res[0].value.a.should.equal('first');
+        res[0].value.b.should.equal('second');
+      });
+
+      it('throws for invalid hex strings', function () {
+        (() => cbor.transform('0x0g')).should.throw();
+      });
     });
 
-    for (let i = 0; i < fixtures.length; i++) {
-      it(`deserializes vector[${i}]`, function () {
-        const { deserialized, serialized } = fixtures[i];
-        serialize(deserialized).equals(Buffer.from(serialized, 'hex')).should.equal(true);
+    describe('untransform', function () {
+      it('untransforms object', function () {
+        const res = cbor.untransform({ a: 'first', b: 'second' }) as any;
+        const properties = Object.getOwnPropertyNames(res);
+        properties[0].should.equal('a');
+        properties[1].should.equal('b');
+        res.a.should.equal('first');
+        res.b.should.equal('second');
       });
-    }
 
-    for (let i = 0; i < fixtures.length; i++) {
-      it(`serializes vector[${i}]`, function () {
-        const { deserialized, serialized } = fixtures[i];
-        JSON.stringify(deserialize(Buffer.from(serialized, 'hex'))).should.equal(JSON.stringify(deserialized));
+      it('enforces canonical object property order', function () {
+        (() => cbor.untransform({ b: 'second', a: 'first' })).should.throw();
       });
-    }
+
+      it('enforces canonical array element order', function () {
+        (() => cbor.untransform([{ weight: 2 }, { weight: 1 }])).should.throw();
+      });
+
+      it('replaces Buffers with prefixed hex strings', function () {
+        const hex = '00010203';
+        const res = cbor.untransform({ value: Buffer.from(hex, 'hex') }) as any;
+        res.value.should.equal('0x' + hex);
+      });
+
+      it('preserves non-prefixed hex strings', function () {
+        const string = '00010203';
+        const res = cbor.untransform({ value: string }) as any;
+        res.value.should.equal(string);
+      });
+
+      it('untransforms object recursively', function () {
+        const hex = '00010203';
+        const res = cbor.untransform({ value: { value: Buffer.from(hex, 'hex') } }) as any;
+        res.value.value.should.equal('0x' + hex);
+      });
+
+      it('untransforms array recursively', function () {
+        const hex = '00010203';
+        const res = cbor.untransform([{ value: Buffer.from(hex, 'hex'), weight: 0 }]) as any;
+        res[0].value.should.equal('0x' + hex);
+      });
+    });
+
+    describe('fixtures', function () {
+      xit('creates test vectors', function () {
+        const { writeFileSync } = require('fs');
+        const deserialized = [
+          {
+            keys: [
+              {
+                key: '0x010203',
+                weight: 0,
+              },
+              {
+                key: '0x040506',
+                weight: 1,
+              },
+            ],
+          },
+          {
+            a: '0xffffffff',
+            b: '0x00000000',
+            c: '0xffffffff',
+            d: [
+              {
+                weight: 0,
+              },
+              {
+                weight: 1,
+              },
+              {
+                weight: 2,
+              },
+              {
+                weight: 3,
+              },
+            ],
+          },
+          {
+            a: [
+              {
+                value: 'a',
+                weight: 0,
+              },
+              {
+                value: 'b',
+                weight: 0,
+              },
+              {
+                value: 'c',
+                weight: 0,
+              },
+            ],
+          },
+          {
+            a: [
+              {
+                weight: 0,
+                value: '0x0a',
+              },
+              {
+                weight: 0,
+                value: '0x0b',
+              },
+              {
+                weight: 0,
+                value: '0x0c',
+              },
+            ],
+          },
+          {
+            a: [
+              {
+                weight: 0,
+                value: 1,
+              },
+              {
+                weight: 0,
+                value: 2,
+              },
+              {
+                weight: 0,
+                value: 3,
+              },
+            ],
+          },
+        ];
+        const serialized = deserialized.map((x) => cbor.serialize(x).toString('hex'));
+        writeFileSync(
+          'test/cbor/fixtures.json',
+          JSON.stringify(
+            deserialized.map((deserialized, i) => ({
+              deserialized: cbor.untransform(cbor.transform(deserialized)),
+              serialized: serialized[i],
+            })),
+            null,
+            2
+          )
+        );
+      });
+
+      for (let i = 0; i < cborFixtures.length; i++) {
+        it(`deserializes vector[${i}]`, function () {
+          const { deserialized, serialized } = cborFixtures[i];
+          cbor.serialize(deserialized).equals(Buffer.from(serialized, 'hex')).should.equal(true);
+        });
+      }
+
+      for (let i = 0; i < cborFixtures.length; i++) {
+        it(`serializes vector[${i}]`, function () {
+          const { deserialized, serialized } = cborFixtures[i];
+          JSON.stringify(cbor.deserialize(Buffer.from(serialized, 'hex'))).should.equal(JSON.stringify(deserialized));
+        });
+      }
+    });
   });
 });
