4243c1d02a59793f30b50a9efb80d1da8709aa4c
Aaron Loe <aaronloe@bitgo.com>
2023-12-20 11:43:41 -0800

    fix: remove unused dynamic headers
    - removes encoding errors from request headers
    
    Ticket: CE-00000

--- modules/sdk-core/src/bitgo/utils/postWithCodec.ts
+++ modules/sdk-core/src/bitgo/utils/postWithCodec.ts
@@ -2,37 +2,6 @@ import * as t from 'io-ts';
 import { BitGoBase } from '../bitgoBase';
 import { BitGoRequest } from '../../api';
 import { SuperAgent, SuperAgentRequest } from 'superagent';
-import { isLeft } from 'fp-ts/Either';
-
-/**
- * @param body
- * @param encodedBody
- * @returns a list of unknown properties that are present in the body but not the codec.
- */
-function getUnknownProperties(body: Record<string, unknown>, encodedBody: Record<string, unknown>): string[] {
-  const unknownProperties: string[] = [];
-  if (body && encodedBody) {
-    const bodyKeys = Object.keys(body);
-    const encodedBodyKeys = Object.keys(encodedBody);
-    const unknownKeys = bodyKeys.filter((key) => !encodedBodyKeys.includes(key));
-    unknownProperties.push(...unknownKeys);
-  }
-  return unknownProperties;
-}
-
-function getDecodeErrorKeys<A extends Record<string, unknown>, O extends Record<string, unknown>>(
-  codec: t.Type<A, O>,
-  body: A
-): string[] {
-  function toKeyPath(context: t.Context): string {
-    return context.flatMap((c) => (c.key ? [c.key] : [])).join('.');
-  }
-  const errors = codec.decode(body);
-  if (isLeft(errors)) {
-    return errors.left.map((error) => toKeyPath(error.context));
-  }
-  return [];
-}
 
 /**
  * Try to encode the body with the codec and send the request.
@@ -60,25 +29,12 @@ export function postWithCodec<
   } = {}
 ): TAgent extends BitGoBase ? BitGoRequest : SuperAgentRequest {
   let encodedBody: O | undefined;
-  let codecError;
   try {
     encodedBody = codec.encode(body);
-    codecError = false;
   } catch (e) {
     console.error('error encoding request body for url', url, e);
-    codecError = true;
   }
-  const postRequest = agent.post(url).set('io-ts-codec-encode-error', codecError ? 'true' : 'false');
-
-  if (codec) {
-    const decodeErrorKeys = getDecodeErrorKeys(codec, body).join(',');
-    if (decodeErrorKeys.trim() !== '') {
-      postRequest.set('io-ts-codec-decode-error', decodeErrorKeys);
-    }
-  }
-
-  const unknownProperties = encodedBody ? getUnknownProperties(body, encodedBody).join(',') : 'NA';
-  postRequest.set('io-ts-unknown-properties', unknownProperties || 'NA');
+  const postRequest = agent.post(url);
 
   return postRequest.send(useEncodedBody && encodedBody ? encodedBody : body);
 }
