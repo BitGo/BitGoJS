806c8747ce4df0c21c11bfb6e5a310e133f5861a
Akash Gianchandani <akashgianchandani@bitgo.com>
2024-02-22 20:45:59 +0530

    refactor(sdk-coin-polygon): refactor recover token related methods WIN-2117
    This commit refactors token recovery related methods for polygon
    TICKET: WIN-2117

--- modules/sdk-coin-polygon/src/polygon.ts
+++ modules/sdk-coin-polygon/src/polygon.ts
@@ -1,8 +1,7 @@
 /**
  * @prettier
  */
-import request from 'superagent';
-import { AbstractEthLikeNewCoins } from '@bitgo/abstract-eth';
+import { AbstractEthLikeNewCoins, recoveryBlockchainExplorerQuery } from '@bitgo/abstract-eth';
 import { BaseCoin, BitGoBase, common, MPCAlgorithm } from '@bitgo/sdk-core';
 import { BaseCoin as StaticsBaseCoin, coins } from '@bitgo/statics';
 import { TransactionBuilder } from './lib';
@@ -25,27 +24,14 @@ export class Polygon extends AbstractEthLikeNewCoins {
   }
 
   /**
-   * Make a query to Polygonscan for information such as balance, token balance, solidity calls
+   * Make a query to Polygon explorer for information such as balance, token balance, solidity calls
    * @param {Object} query key-value pairs of parameters to append after /api
-   * @returns {Promise<Object>} response from Polygonscan
+   * @returns {Promise<Object>} response from Polygon
    */
-  async recoveryBlockchainExplorerQuery(query: Record<string, string>): Promise<any> {
-    const token = common.Environments[this.bitgo.getEnv()].polygonscanApiToken;
-    if (token) {
-      query.apikey = token;
-    }
-    const response = await request
-      .get(common.Environments[this.bitgo.getEnv()].polygonscanBaseUrl + '/api')
-      .query(query);
-
-    if (!response.ok) {
-      throw new Error('could not reach Polygonscan');
-    }
-
-    if (response.body.status === '0' && response.body.message === 'NOTOK') {
-      throw new Error('Polygonscan rate limit reached');
-    }
-    return response.body;
+  async recoveryBlockchainExplorerQuery(query: Record<string, string>): Promise<Record<string, unknown>> {
+    const apiToken = common.Environments[this.bitgo.getEnv()].polygonscanApiToken;
+    const explorerUrl = common.Environments[this.bitgo.getEnv()].polygonscanBaseUrl;
+    return await recoveryBlockchainExplorerQuery(query, explorerUrl as string, apiToken);
   }
 
   /** @inheritDoc */
