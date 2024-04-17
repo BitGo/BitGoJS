eb0824dfaf7e9c2c9bb85df08b2e9012d1bf657b
Islam Amin <islamamin@bitgo.com>
2024-04-17 19:42:02 -0400

    feat(sdk-lib-mpc): add utility to verify dkls signatures
    Ticket: HSM-341

--- modules/sdk-lib-mpc/src/tss/ecdsa-dkls/util.ts
+++ modules/sdk-lib-mpc/src/tss/ecdsa-dkls/util.ts
@@ -3,6 +3,10 @@ import { Secp256k1Curve } from '../../curves';
 import { bigIntFromBufferBE, bigIntToBufferBE } from '../../util';
 import { DeserializedDklsSignature } from './types';
 import { decode } from 'cbor';
+import * as secp256k1 from 'secp256k1';
+import { Hash, createHash } from 'crypto';
+
+const delimeter = ':';
 
 /**
  * Combines partial signatures from parties participating in DSG.
@@ -26,4 +30,37 @@ export function combinePartialSignatures(round4MessagePayloads: Uint8Array[], rH
     R: new Uint8Array(bigIntToBufferBE(normalizedSig.r)),
     S: new Uint8Array(bigIntToBufferBE(normalizedSig.s)),
   };
+}
+
+/**
+ * Verify a DKLs Signature and serialize it to recid:r:s:publickey format.
+ * @param message - message that was signed.
+ * @param dklsSignature - R and S values of the ECDSA signature.
+ * @param commonKeychain - public key appended to chaincode in hex.
+ * @param hash - optional hash function to apply on message before verifying. Default is sha256.
+ * @param shouldHash - flag to determine whether message should be hashed before verifying.
+ * @returns {string} - serialized signature in `recid:r:s:publickey` format
+ */
+export function verifyAndConvertDklsSignature(
+  message: Buffer,
+  dklsSignature: DeserializedDklsSignature,
+  commonKeychain: string,
+  hash?: Hash,
+  shouldHash = true
+): string {
+  const messageToVerify = shouldHash ? (hash || createHash('sha256')).update(message).digest() : message;
+  const pub0 = secp256k1.ecdsaRecover(Buffer.concat([dklsSignature.R, dklsSignature.S]), 0, messageToVerify, true);
+  const pub1 = secp256k1.ecdsaRecover(Buffer.concat([dklsSignature.R, dklsSignature.S]), 1, messageToVerify, true);
+  const truePub = commonKeychain.slice(0, 66);
+  let recId: number;
+  if (truePub === Buffer.from(pub0).toString('hex')) {
+    recId = 0;
+  } else if (truePub === Buffer.from(pub1).toString('hex')) {
+    recId = 1;
+  } else {
+    throw Error('Invalid Signature');
+  }
+  return `${recId}${delimeter}${Buffer.from(dklsSignature.R).toString('hex')}${delimeter}${Buffer.from(
+    dklsSignature.S
+  ).toString('hex')}${delimeter}${truePub}`;
 }
