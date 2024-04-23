50bd01a28491457a57a73405f8f0e2dacc3b7027
Eric Crosson <ericcrosson@bitgo.com>
2024-04-23 15:31:26 -0500

    chore(sdk-coin-atom): remove duplicate char
    This commit removes a duplicate character in a regex
    character class, which has no effect when specified more than once.
    
    Ticket: VL-1904

--- modules/sdk-coin-atom/src/lib/constants.ts
+++ modules/sdk-coin-atom/src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const validDenoms = ['natom', 'uatom', 'matom', 'atom'];
-export const accountAddressRegex = /^(cosmos)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l']{38})$/;
-export const validatorAddressRegex = /^(cosmosvaloper)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l']{38})$/;
-export const contractAddressRegex = /^(cosmos)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l']+)$/;
+export const accountAddressRegex = /^(cosmos)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l]{38})$/;
+export const validatorAddressRegex = /^(cosmosvaloper)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l]{38})$/;
+export const contractAddressRegex = /^(cosmos)1(['qpzry9x8gf2tvdw0s3jn54khce6mua7l]+)$/;
 export const GAS_AMOUNT = '100000';
 export const GAS_LIMIT = 200000;
