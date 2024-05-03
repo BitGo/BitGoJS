af048f76709aa89c000b7bf43cdb2931cb00d7fa
Michael <michaelmcshinsky@bitgo.com>
2024-05-03 14:52:24 -0600

    feat: breakout encryption logic functions
    * Move encryption functions to seperate files
    * Add browser compatible rsa encryption functions
    
    PX-3786
    
    TICKET: PX-3786

---
+++ modules/sdk-core/src/bitgo/trading/network/encrypt-rsa-browser.ts
@@ -0,0 +1,62 @@
+import crypto from 'crypto';
+import { ab2str, str2ab } from './utils';
+
+/**
+ * Import a public RSA key and encrypt a plaintext
+ * @param {string} publicKey PEM-formatted public RSA key
+ * @param {string} text The text to encrypt
+ * @returns {string} The encrypted text
+ */
+export async function _encryptBrowserRsa(publicKey: string, text: string): Promise<string> {
+  const pub = await importPublicKey(publicKey);
+  const encrypted = await encryptRSA(pub, new TextEncoder().encode(text));
+  return btoa(ab2str(encrypted));
+}
+
+/**
+ * Import a public RSA key from a PEM string
+ * @param {string} spkiPem
+ * @returns {CryptoKey}
+ */
+async function importPublicKey(spkiPem: string) {
+  return await crypto.subtle.importKey(
+    'spki',
+    getSpkiDer(spkiPem),
+    {
+      name: 'RSA-OAEP',
+      hash: 'SHA-256',
+    },
+    true,
+    ['encrypt']
+  );
+}
+
+/**
+ * Encrypt a plaintext using RSA-OAEP
+ * @param {CryptoKey} key The public key to use for encryption
+ * @param {ArrayBuffer} plaintext The plaintext to encrypt
+ * @returns {ArrayBuffer} The encrypted text
+ */
+async function encryptRSA(key: CryptoKey, plaintext: ArrayBuffer) {
+  const encrypted = await crypto.subtle.encrypt(
+    {
+      name: 'RSA-OAEP',
+    },
+    key,
+    plaintext
+  );
+  return encrypted;
+}
+
+/**
+ * Convert a PEM-formatted SPKI key to a DER-formatted ArrayBuffer
+ * @param {string} spkiPem PEM-formatted SPKI key
+ * @returns {ArrayBuffer} DER-formatted public key
+ */
+function getSpkiDer(spkiPem: string) {
+  const pemHeader = '-----BEGIN PUBLIC KEY-----';
+  const pemFooter = '-----END PUBLIC KEY-----';
+  const pemContents = spkiPem.substring(pemHeader.length, spkiPem.length - pemFooter.length);
+  const binaryDerString = atob(pemContents);
+  return str2ab(binaryDerString);
+}
--- modules/sdk-core/src/bitgo/trading/network/index.ts
+++ modules/sdk-core/src/bitgo/trading/network/index.ts
@@ -1,3 +1,4 @@
 export * from './encrypt';
+export * from './decrypt';
 export * from './network';
 export * from './types';
