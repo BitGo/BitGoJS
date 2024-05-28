191940a3f84be70d79a0bbfc532c2972952d8a91
Bhavi Dhingra <bhavidhingra@bitgo.com>
2024-05-28 11:30:31 +0530

    refactor(examples): key chains

--- examples/ts/get-keychains.ts
+++ examples/ts/get-keychains.ts
@@ -1,17 +1,23 @@
 import { BitGoAPI } from '@bitgo/sdk-api';
+import {Btc} from "@bitgo/sdk-coin-btc";
 require('dotenv').config({ path: '../../.env' });
 
+const ACCESS_TOKEN = '<YOUR_ACCESS_TOKEN>';
+
 const bitgo = new BitGoAPI({
-  accessToken: process.env.TESTNET_ACCESS_TOKEN,
+  accessToken: ACCESS_TOKEN,
   env: 'prod',
 });
 
+const coin = 'btc';
+bitgo.register(coin, Btc.createInstance);
+
 async function main() {
-  const keys = [
-    '634ee23072f6f100078232c8ca3c248d',
-    '634ee230aef5e00007843c2053f2ec3d',
-    '634ee22fd8f2360007113883647a5f4e',
-  ];
+  const keys= [
+    "65f09e34244e2f7ac804251494e530e1",
+    "65f09e3572c880cb959b423f1d987cbf",
+    "65f09e37e19cf9bba3339920aa83273f"
+  ]
   for (const id of keys) {
     const key = await bitgo.coin(coin).keychains().get({ id });
     console.log(key);
