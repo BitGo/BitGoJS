c913e9bffb98356028e964dcbcf0bcc23e925ca5
Eric Li <ericli@bitgo.com>
2024-07-23 17:35:41 -0400

    fix: register tbtcsig
    TICKET: BTC-0

--- modules/sdk-coin-btc/src/register.ts
+++ modules/sdk-coin-btc/src/register.ts
@@ -1,8 +1,10 @@
 import { BitGoBase } from '@bitgo/sdk-core';
 import { Btc } from './btc';
 import { Tbtc } from './tbtc';
+import { Tbtcsig } from './tbtcsig';
 
 export const register = (sdk: BitGoBase): void => {
   sdk.register('btc', Btc.createInstance);
   sdk.register('tbtc', Tbtc.createInstance);
+  sdk.register('tbtcsig', Tbtcsig.createInstance);
 };
