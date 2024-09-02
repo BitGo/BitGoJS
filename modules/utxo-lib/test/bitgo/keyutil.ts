38fff9dee1d95a73e42a23f788c53eee507c747a
Saravanan Mani <saravananmani@bitgo.com>
2024-09-02 20:10:37 +0530

    feat(utxo-lib): add convertExtendedKeyNetwork function
    To convert extended key from one network to another
    
    Ticket: BTC-1467

--- modules/utxo-lib/test/bitgo/keyutil.ts
+++ modules/utxo-lib/test/bitgo/keyutil.ts
@@ -1,9 +1,15 @@
+import { networks } from '../../src';
+
 const assert = require('assert');
 const crypto = require('crypto');
 
 import { ECPairInterface } from 'ecpair';
-import { privateKeyBufferFromECPair, privateKeyBufferToECPair } from '../../src/bitgo/keyutil';
-import { ECPair } from '../../src/noble_ecc';
+import {
+  convertExtendedKeyNetwork,
+  privateKeyBufferFromECPair,
+  privateKeyBufferToECPair,
+} from '../../src/bitgo/keyutil';
+import { bip32, ECPair } from '../../src/noble_ecc';
 
 describe('privateKeyBufferFromECPair', function () {
   it('pads short private keys', function () {
@@ -53,5 +59,45 @@ describe('privateKeyBufferToECPair', function () {
     assert.throws(function () {
       privateKeyBufferToECPair(Buffer.alloc(33, 0x00));
     }, new RegExp('invalid private key buffer'));
+  });
+});
+
+describe('convertExtendedKeyNetwork', function () {
+  const prvKeyBuffer = crypto.randomBytes(32);
+  const mainnetHdNode = bip32.fromSeed(prvKeyBuffer, networks.bitcoin);
+  const testnetHdNode = bip32.fromSeed(prvKeyBuffer, networks.testnet);
+
+  it('should return the same extended key if fromNetwork and targetNetwork are the same', () => {
+    const extendedKey = mainnetHdNode.toBase58();
+    const result = convertExtendedKeyNetwork(extendedKey, networks.bitcoin, networks.bitcoin);
+    assert.strictEqual(result, extendedKey);
+  });
+
+  it('should change the network from mainnet to testnet for a neutered (public) key', () => {
+    const extendedKey = mainnetHdNode.neutered().toBase58();
+    const expectedKey = convertExtendedKeyNetwork(extendedKey, networks.bitcoin, networks.testnet);
+    const testnetHdNodeFromExpected = bip32.fromBase58(expectedKey, networks.testnet);
+    assert.deepStrictEqual(testnetHdNodeFromExpected.publicKey, mainnetHdNode.neutered().publicKey);
+  });
+
+  it('should change the network from testnet to mainnet for a neutered (public) key', () => {
+    const extendedKey = testnetHdNode.neutered().toBase58();
+    const expectedKey = convertExtendedKeyNetwork(extendedKey, networks.testnet, networks.bitcoin);
+    const mainnetHdNodeFromExpected = bip32.fromBase58(expectedKey, networks.bitcoin);
+    assert.deepStrictEqual(mainnetHdNodeFromExpected.publicKey, testnetHdNode.neutered().publicKey);
+  });
+
+  it('should change the network from mainnet to testnet for a non-neutered (private) key', () => {
+    const extendedKey = mainnetHdNode.toBase58();
+    const expectedKey = convertExtendedKeyNetwork(extendedKey, networks.bitcoin, networks.testnet);
+    const testnetHdNodeFromExpected = bip32.fromBase58(expectedKey, networks.testnet);
+    assert.deepStrictEqual(testnetHdNodeFromExpected.privateKey, mainnetHdNode.privateKey);
+  });
+
+  it('should change the network from testnet to mainnet for a non-neutered (private) key', () => {
+    const extendedKey = testnetHdNode.toBase58();
+    const expectedKey = convertExtendedKeyNetwork(extendedKey, networks.testnet, networks.bitcoin);
+    const mainnetHdNodeFromExpected = bip32.fromBase58(expectedKey, networks.bitcoin);
+    assert.deepStrictEqual(mainnetHdNodeFromExpected.privateKey, testnetHdNode.privateKey);
   });
 });
