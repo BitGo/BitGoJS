89d1c710c95d114b6f0803c787466b7342cafebb
Otto Allmendinger <otto@bitgo.com>
2024-09-26 09:24:28 +0200

    refactor: move argToString to parseString
    Issue: BTC-1351

--- modules/utxo-bin/src/parseString.ts
+++ modules/utxo-bin/src/parseString.ts
@@ -1,3 +1,10 @@
+import * as process from 'process';
+import * as fs from 'fs';
+
+import clipboardy from 'clipboardy-cjs';
+
+import { readStdin } from './readStdin';
+
 type Format = 'hex' | 'base64';
 export function stringToBuffer(data: string, format: Format | Format[]): Buffer {
   if (typeof format !== 'string') {
@@ -24,4 +31,55 @@ export function stringToBuffer(data: string, format: Format | Format[]): Buffer 
     throw new Error(`invalid ${format}`);
   }
   return buf;
+}
+
+/**
+ * @param argv
+ * @param input - optional input data. If set, this function just ensures that nothing else is set.
+ * @return string from specified source
+ */
+export async function argToString(
+  argv: {
+    clipboard?: boolean;
+    path?: string;
+    data?: string;
+    stdin: boolean;
+  },
+  input?: string
+): Promise<string | undefined> {
+  if (argv.stdin || argv.path === '-') {
+    if (input) {
+      throw new Error(`conflicting arguments`);
+    }
+    console.log('Reading from stdin. Please paste hex-encoded transaction data.');
+    console.log('After inserting data, press Ctrl-D to finish. Press Ctrl-C to cancel.');
+    if (process.stdin.isTTY) {
+      input = await readStdin();
+    } else {
+      input = await fs.promises.readFile('/dev/stdin', 'utf8');
+    }
+  }
+
+  if (argv.clipboard) {
+    if (input) {
+      throw new Error(`conflicting arguments`);
+    }
+    input = await clipboardy.read();
+  }
+
+  if (argv.path) {
+    if (input) {
+      throw new Error(`conflicting arguments`);
+    }
+    input = (await fs.promises.readFile(argv.path, 'utf8')).toString();
+  }
+
+  if (argv.data) {
+    if (input) {
+      throw new Error(`conflicting arguments`);
+    }
+    input = argv.data;
+  }
+
+  return input;
 }
