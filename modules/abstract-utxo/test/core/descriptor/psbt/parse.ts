291aed8be2c5cf58f55158d4d2519e0497c88ab6
Otto Allmendinger <otto@bitgo.com>
2024-12-04 14:25:35 +0100

    refactor(abstract-utxo): improve parse method
    Rename and add tests
    
    TICKET: BTC-1450

---
+++ modules/abstract-utxo/test/core/descriptor/psbt/parse.ts
@@ -0,0 +1,34 @@
+import assert from 'assert';
+import { parse } from '../../../../src/core/descriptor';
+import { DescriptorTemplate, getDefaultXPubs, getDescriptorMap } from '../descriptor.utils';
+import { mockPsbtDefaultWithDescriptorTemplate } from './mock.utils';
+import { getFixture } from '../../fixtures.utils';
+import { ParsedDescriptorTransaction } from '../../../../src/core/descriptor/psbt/parse';
+import { toPlainObject } from '../../toPlainObject.utils';
+import { Descriptor } from '@bitgo/wasm-miniscript';
+
+function normalize(parsed: ParsedDescriptorTransaction) {
+  return toPlainObject(
+    parsed,
+    {
+      apply(v, path) {
+        if (v instanceof Descriptor) {
+          return v.toString();
+        }
+      },
+    },
+    []
+  );
+}
+
+function describeWithTemplate(t: DescriptorTemplate) {
+  describe(`parse ${t}`, function () {
+    it('parses a PSBT with descriptors', async function () {
+      const psbt = mockPsbtDefaultWithDescriptorTemplate(t);
+      const parsed = normalize(parse(psbt, getDescriptorMap(t, getDefaultXPubs('a')), psbt.network));
+      assert.deepStrictEqual(parsed, await getFixture(__dirname + `/fixtures/${t}.parse.json`, parsed));
+    });
+  });
+}
+
+describeWithTemplate('Wsh2Of3');
