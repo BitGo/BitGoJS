b92855e4c89840e5b2c9d114cb4181edef217d81
Otto Allmendinger <otto@bitgo.com>
2024-12-11 15:41:31 +0100

    feat(abstract-utxo): improve isDescriptorWallet family
    TICKET: BTC-1450

--- modules/abstract-utxo/src/descriptor/descriptorWallet.ts
+++ modules/abstract-utxo/src/descriptor/descriptorWallet.ts
@@ -2,20 +2,38 @@ import * as t from 'io-ts';
 import { NamedDescriptor } from './NamedDescriptor';
 import { AbstractUtxoCoinWalletData } from '../abstractUtxoCoin';
 import { DescriptorMap, toDescriptorMap } from '../core/descriptor';
+import { IWallet, WalletCoinSpecific } from '@bitgo/sdk-core';
 
-type DescriptorWalletData = AbstractUtxoCoinWalletData & {
-  coinSpecific: {
-    descriptors: NamedDescriptor[];
-  };
+type DescriptorWalletCoinSpecific = {
+  descriptors: NamedDescriptor[];
 };
 
+function isDescriptorWalletCoinSpecific(obj: unknown): obj is DescriptorWalletCoinSpecific {
+  return (
+    obj !== null && typeof obj === 'object' && 'descriptors' in obj && t.array(NamedDescriptor).is(obj.descriptors)
+  );
+}
+
+type DescriptorWalletData = AbstractUtxoCoinWalletData & {
+  coinSpecific: DescriptorWalletCoinSpecific;
+};
+
+interface IDescriptorWallet extends IWallet {
+  coinSpecific(): WalletCoinSpecific & DescriptorWalletCoinSpecific;
+}
+
 export function isDescriptorWalletData(obj: AbstractUtxoCoinWalletData): obj is DescriptorWalletData {
-  if ('coinSpecific' in obj && 'descriptors' in obj.coinSpecific) {
-    return t.array(NamedDescriptor).is(obj.coinSpecific.descriptors);
-  }
-  return false;
+  return isDescriptorWalletCoinSpecific(obj.coinSpecific);
+}
+
+export function isDescriptorWallet(obj: IWallet): obj is IDescriptorWallet {
+  return isDescriptorWalletCoinSpecific(obj.coinSpecific());
 }
 
 export function getDescriptorMapFromWalletData(wallet: DescriptorWalletData): DescriptorMap {
   return toDescriptorMap(wallet.coinSpecific.descriptors);
+}
+
+export function getDescriptorMapFromWallet(wallet: IDescriptorWallet): DescriptorMap {
+  return toDescriptorMap(wallet.coinSpecific().descriptors);
 }
