017ba2c1405f7d5ff0a9f81bc4c639b0fb7657b1
Otto Allmendinger <otto@bitgo.com>
2024-12-11 12:45:19 +0100

    feat(abstract-utxo): add isDescriptorWalletData
    TICKET: BTC-1450

--- modules/abstract-utxo/src/descriptor/index.ts
+++ modules/abstract-utxo/src/descriptor/index.ts
@@ -1,3 +1,4 @@
 export { Miniscript, Descriptor } from '@bitgo/wasm-miniscript';
 export { assertDescriptorWalletAddress } from './assertDescriptorWalletAddress';
 export { NamedDescriptor } from './NamedDescriptor';
+export { isDescriptorWalletData, getDescriptorMapFromWalletData } from './descriptorWallet';
