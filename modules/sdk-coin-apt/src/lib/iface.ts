48d7edf36b83df18a3c956ef1263abc534713520
Bhavi Dhingra <bhavidhingra@bitgo.com>
2024-12-27 19:42:37 +0530

    feat(sdk-coin-apt): transaction builder
    TICKET: COIN-2258

--- modules/sdk-coin-apt/src/lib/iface.ts
+++ modules/sdk-coin-apt/src/lib/iface.ts
@@ -1,6 +1,34 @@
+import {
+  TransactionExplanation as BaseTransactionExplanation,
+  TransactionType as BitGoTransactionType,
+} from '@bitgo/sdk-core';
+
+export enum AptTransactionType {
+  Transfer = 'Transfer',
+  TokenTransfer = 'TokenTransfer',
+}
+
+export interface TransactionExplanation extends BaseTransactionExplanation {
+  type: BitGoTransactionType;
+}
+
 /**
  * The transaction data returned from the toJson() function of a transaction
  */
 export interface TxData {
   id: string;
+  sender: string;
+  sequenceNumber: BigInt;
+  maxGasAmount: BigInt;
+  gasUnitPrice: BigInt;
+  expirationTime: BigInt;
+  payload: AptPayload;
+  chainId: number;
+}
+
+export interface AptPayload {
+  function: string;
+  typeArguments: string[];
+  arguments: string[];
+  type: string;
 }
