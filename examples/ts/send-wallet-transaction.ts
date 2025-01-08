8eff10a6b7c9ca12b6064a47dd6d985753efa5cb
Bhavi Dhingra <bhavidhingra@bitgo.com>
2025-01-08 13:17:47 +0530

    test: initial commit of new branch

--- examples/ts/send-wallet-transaction.ts
+++ examples/ts/send-wallet-transaction.ts
@@ -4,38 +4,36 @@
  * Copyright 2022, BitGo, Inc.  All Rights Reserved.
  */
 import { BitGoAPI } from '@bitgo/sdk-api';
-import { Tbtc } from '@bitgo/sdk-coin-btc';
+import { Trune } from '@bitgo/sdk-coin-rune';
 require('dotenv').config({ path: '../../.env' });
 
 const bitgo = new BitGoAPI({
   accessToken: process.env.TESTNET_ACCESS_TOKEN,
-  env: 'test',
+  env: 'staging',
 });
 
-const coin = 'tbtc';
-bitgo.register(coin, Tbtc.createInstance);
+const coin = 'tthorchain:rune';
+bitgo.register(coin, Trune.createInstance);
 
-const walletId = '';
-const walletPassphrase = '';
+const walletId = '67359b1a0892a94fb4ccb819ca0ae8d5';
+const walletPassphrase = process.env.PASS;
 
 async function main() {
+  bitgo.unlock({ otp: '000000' });
   const walletInstance = await bitgo.coin(coin).wallets().get({ id: walletId });
 
-  const newReceiveAddress1 = await walletInstance.createAddress();
-  const newReceiveAddress2 = await walletInstance.createAddress();
+  // const newReceiveAddress1 = await walletInstance.createAddress();
+  // const newReceiveAddress2 = await walletInstance.createAddress();
 
   const transaction = await walletInstance.sendMany({
     recipients: [
       {
-        amount: '12341234',
-        address: newReceiveAddress1.address,
-      },
-      {
-        amount: '13370000',
-        address: newReceiveAddress2.address,
+        amount: '1000000',
+        address: "sthor10w2wpnfvkzn948en3mgqlg3rjz6e40dgu5fdtn",
       },
     ],
     walletPassphrase: walletPassphrase,
+    type: 'transfer',
   });
   const explanation = await bitgo.coin(coin).explainTransaction({ txHex: transaction.tx });
 
