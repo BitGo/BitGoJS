140f0ce419a0a1093a463a0f955d094ee7c16334
Aditya Tiwari <adityatiwari@bitgo.com>
2025-01-09 15:01:48 +0530

    fix(sdk-coin-rune): add native transaction fees for wallet recovery
    Ticket: COIN-2417

--- modules/sdk-coin-rune/src/lib/constants.ts
+++ modules/sdk-coin-rune/src/lib/constants.ts
@@ -9,4 +9,6 @@ export const testnetValidatorAddressRegex = /^(sthor)1(['qpzry9x8gf2tvdw0s3jn54k
 export const TESTNET_ADDRESS_PREFIX = 'sthor';
 
 export const GAS_LIMIT = 200000;
-export const GAS_AMOUNT = '150000';
+export const GAS_AMOUNT = '0'; // Gas amount should be zero for RUNE transactions, as fees (0.02 RUNE) is cut from sender balance directly in the transaction
+export const RUNE_FEES = '2000000'; // https://dev.thorchain.org/concepts/fees.html#thorchain-native-rune
+export const ROOT_PATH = 'm/0';
