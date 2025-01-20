9afbcdd3eef638a6721bca592e5cf24eaace2550
Mohammed Ryaan <mohammedryaan@bitgo.com>
2025-01-20 10:35:11 +0530

    feat(statics): add sol:melania TICKET: COIN-2828

--- modules/statics/test/unit/coins.ts
+++ modules/statics/test/unit/coins.ts
@@ -268,6 +268,13 @@ const custodyFeatures: Record<string, { features: CoinFeature[] }> = {
       CoinFeature.CUSTODY_BITGO_SINGAPORE,
     ],
   },
+  'sol:melania': {
+    features: [
+      CoinFeature.CUSTODY_BITGO_FRANKFURT,
+      CoinFeature.CUSTODY_BITGO_GERMANY,
+      CoinFeature.CUSTODY_BITGO_SINGAPORE,
+    ],
+  },
   'xlm:ZUSD-GDF6VOEGRWLOZ64PQQGKD2IYWA22RLT37GJKS2EJXZHT2VLAGWLC5TOB': {
     features: [CoinFeature.CUSTODY_BITGO_FRANKFURT],
   },
