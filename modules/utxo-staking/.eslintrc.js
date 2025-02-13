4395971fcc21afaf55727f6badb26335c91cde19
Otto Allmendinger <otto@bitgo.com>
2025-02-13 10:29:05 +0100

    feat(utxo-staking): migrate test utils to utxo-core
    Move fixture loading to shared test utilities in utxo-core
    
    Issue: BTC-1829

---
+++ modules/utxo-staking/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ['../../.eslintrc.json'],
+  rules: {
+    // FIXME: enable
+    // 'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/no-internal-modules': 'off',
+  },
+};
--- modules/utxo-staking/test/unit/coreDao/utils.ts
+++ modules/utxo-staking/test/unit/coreDao/utils.ts
@@ -1,38 +1,5 @@
-import { promises as fs } from 'fs';
 import { Descriptor, Psbt } from '@bitgo/wasm-miniscript';
 import * as utxolib from '@bitgo/utxo-lib';
-
-function encode(path: string, defaultValue: unknown): string {
-  if (path.endsWith('.txt')) {
-    return String(defaultValue);
-  }
-  if (path.endsWith('.json')) {
-    return JSON.stringify(defaultValue, null, 2) + '\n';
-  }
-  throw new Error(`unrecognized path ${path}`);
-}
-
-function decode(path: string, v: string): unknown {
-  if (path.endsWith('.txt')) {
-    return v;
-  }
-  if (path.endsWith('.json')) {
-    return JSON.parse(v);
-  }
-  throw new Error(`unrecognized path ${path}`);
-}
-
-export async function getFixture(path: string, defaultValue: unknown): Promise<unknown> {
-  try {
-    return decode(path, await fs.readFile(path, 'utf8'));
-  } catch (e) {
-    if (e.code === 'ENOENT') {
-      await fs.writeFile(path, encode(path, defaultValue), 'utf8');
-      throw new Error(`wrote default value for ${path}`);
-    }
-    throw e;
-  }
-}
 
 export function updateInputWithDescriptor(psbt: utxolib.Psbt, inputIndex: number, descriptor: Descriptor): void {
   const wrappedPsbt = Psbt.deserialize(psbt.toBuffer());
