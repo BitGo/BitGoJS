0f8b1273a6e5683610a7131de828d6e21a5a4bc4
Otto Allmendinger <otto@bitgo.com>
2025-02-13 10:38:23 +0100

    feat(utxo-staking): enable import ordering rule
    Issue: BTC-1829

--- modules/utxo-staking/.eslintrc.js
+++ modules/utxo-staking/.eslintrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   extends: ['../../.eslintrc.json'],
   rules: {
-    // FIXME: enable
-    // 'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/order': ['error', { 'newlines-between': 'always' }],
     'import/no-internal-modules': 'off',
   },
 };
