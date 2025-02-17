512782b2da40b8e1b057ef64a2dbc408d6234f84
Otto Allmendinger <otto@bitgo.com>
2025-02-17 13:00:12 +0100

    feat(abstract-utxo): add multiple descriptor validation support
    Refactor descriptor validation to handle multiple descriptors at once and
    introduce native descriptor types. Add helper to convert string descriptors
    to native ones.
    
    This is required to enforce more complex policies where we want to make sure
    that all descriptors in a set belong to some particular group.
    
    Issue: BTC-1843

--- modules/abstract-utxo/src/descriptor/index.ts
+++ modules/abstract-utxo/src/descriptor/index.ts
@@ -1,7 +1,13 @@
 export { Miniscript, Descriptor } from '@bitgo/wasm-miniscript';
 export { DescriptorMap } from '@bitgo/utxo-core/descriptor';
 export { assertDescriptorWalletAddress } from './assertDescriptorWalletAddress';
-export { NamedDescriptor, createNamedDescriptorWithSignature, hasValidSignature } from './NamedDescriptor';
+export {
+  NamedDescriptor,
+  createNamedDescriptorWithSignature,
+  hasValidSignature,
+  NamedDescriptorNative,
+  toNamedDescriptorNative,
+} from './NamedDescriptor';
 export { isDescriptorWallet, getDescriptorMapFromWallet } from './descriptorWallet';
 export { getPolicyForEnv } from './validatePolicy';
 export * as createWallet from './createWallet';
--- modules/abstract-utxo/src/descriptor/validatePolicy.ts
+++ modules/abstract-utxo/src/descriptor/validatePolicy.ts
@@ -1,16 +1,16 @@
-import { Descriptor } from '@bitgo/wasm-miniscript';
 import { EnvironmentName, Triple } from '@bitgo/sdk-core';
 import * as utxolib from '@bitgo/utxo-lib';
 import { DescriptorMap, toDescriptorMap } from '@bitgo/utxo-core/descriptor';
 
 import { parseDescriptor } from './builder';
-import { hasValidSignature, NamedDescriptor } from './NamedDescriptor';
+import { hasValidSignature, NamedDescriptor, NamedDescriptorNative, toNamedDescriptorNative } from './NamedDescriptor';
 
 export type KeyTriple = Triple<utxolib.BIP32Interface>;
 
 export interface DescriptorValidationPolicy {
   name: string;
-  validate(d: Descriptor, walletKeys: KeyTriple, signatures: string[]): boolean;
+
+  validate(arr: NamedDescriptorNative[], walletKeys: KeyTriple): boolean;
 }
 
 export const policyAllowAll: DescriptorValidationPolicy = {
@@ -21,13 +21,15 @@ export const policyAllowAll: DescriptorValidationPolicy = {
 export function getValidatorDescriptorTemplate(name: string): DescriptorValidationPolicy {
   return {
     name: 'descriptorTemplate(' + name + ')',
-    validate(d: Descriptor, walletKeys: KeyTriple): boolean {
-      const parsed = parseDescriptor(d);
-      return (
-        parsed.name === name &&
-        parsed.keys.length === walletKeys.length &&
-        parsed.keys.every((k, i) => k.toBase58() === walletKeys[i].neutered().toBase58())
-      );
+    validate(arr: NamedDescriptorNative[], walletKeys: KeyTriple): boolean {
+      return arr.every((d) => {
+        const parsed = parseDescriptor(d.value);
+        return (
+          parsed.name === name &&
+          parsed.keys.length === walletKeys.length &&
+          parsed.keys.every((k, i) => k.toBase58() === walletKeys[i].neutered().toBase58())
+        );
+      });
     },
   };
 }
@@ -35,8 +37,8 @@ export function getValidatorDescriptorTemplate(name: string): DescriptorValidati
 export function getValidatorEvery(validators: DescriptorValidationPolicy[]): DescriptorValidationPolicy {
   return {
     name: 'every(' + validators.map((v) => v.name).join(',') + ')',
-    validate(d: Descriptor, walletKeys: KeyTriple, signatures: string[]): boolean {
-      return validators.every((v) => v.validate(d, walletKeys, signatures));
+    validate(arr: NamedDescriptorNative[], walletKeys: KeyTriple): boolean {
+      return validators.every((v) => v.validate(arr, walletKeys));
     },
   };
 }
@@ -44,8 +46,8 @@ export function getValidatorEvery(validators: DescriptorValidationPolicy[]): Des
 export function getValidatorSome(validators: DescriptorValidationPolicy[]): DescriptorValidationPolicy {
   return {
     name: 'some(' + validators.map((v) => v.name).join(',') + ')',
-    validate(d: Descriptor, walletKeys: KeyTriple, signatures: string[]): boolean {
-      return validators.some((v) => v.validate(d, walletKeys, signatures));
+    validate(arr: NamedDescriptorNative[], walletKeys: KeyTriple): boolean {
+      return validators.some((v) => v.validate(arr, walletKeys));
     },
   };
 }
@@ -57,27 +59,26 @@ export function getValidatorOneOfTemplates(names: string[]): DescriptorValidatio
 export function getValidatorSignedByUserKey(): DescriptorValidationPolicy {
   return {
     name: 'signedByUser',
-    validate(d: Descriptor, walletKeys: KeyTriple, signatures: string[]): boolean {
+    validate(arr: NamedDescriptorNative[], walletKeys: KeyTriple): boolean {
       // the first key is the user key, by convention
-      return hasValidSignature(d, walletKeys[0], signatures);
+      return arr.every((d) => hasValidSignature(d.value, walletKeys[0], d.signatures ?? []));
     },
   };
 }
 
 export class DescriptorPolicyValidationError extends Error {
-  constructor(descriptor: Descriptor, policy: DescriptorValidationPolicy) {
-    super(`Descriptor ${descriptor.toString()} does not match policy ${policy.name}`);
+  constructor(ds: NamedDescriptorNative[], policy: DescriptorValidationPolicy) {
+    super(`Descriptors ${ds.map((d) => d.value.toString())} does not match policy ${policy.name}`);
   }
 }
 
 export function assertDescriptorPolicy(
-  descriptor: Descriptor,
+  descriptors: NamedDescriptorNative[],
   policy: DescriptorValidationPolicy,
-  walletKeys: KeyTriple,
-  signatures: string[]
+  walletKeys: KeyTriple
 ): void {
-  if (!policy.validate(descriptor, walletKeys, signatures)) {
-    throw new DescriptorPolicyValidationError(descriptor, policy);
+  if (!policy.validate(descriptors, walletKeys)) {
+    throw new DescriptorPolicyValidationError(descriptors, policy);
   }
 }
 
@@ -86,13 +87,11 @@ export function toDescriptorMapValidate(
   walletKeys: KeyTriple,
   policy: DescriptorValidationPolicy
 ): DescriptorMap {
-  return toDescriptorMap(
-    descriptors.map((namedDescriptor) => {
-      const d = Descriptor.fromString(namedDescriptor.value, 'derivable');
-      assertDescriptorPolicy(d, policy, walletKeys, namedDescriptor.signatures ?? []);
-      return { name: namedDescriptor.name, value: d };
-    })
+  const namedDescriptorsNative: NamedDescriptorNative[] = descriptors.map((v) =>
+    toNamedDescriptorNative(v, 'derivable')
   );
+  assertDescriptorPolicy(namedDescriptorsNative, policy, walletKeys);
+  return toDescriptorMap(namedDescriptorsNative);
 }
 
 export function getPolicyForEnv(env: EnvironmentName): DescriptorValidationPolicy {
@@ -101,7 +100,7 @@ export function getPolicyForEnv(env: EnvironmentName): DescriptorValidationPolic
     case 'prod':
       return getValidatorSome([
         // allow all 2-of-3-ish descriptors where the keys match the wallet keys
-        getValidatorOneOfTemplates(['Wsh2Of3', 'Wsh2Of3CltvDrop', 'ShWsh2Of3CltvDrop']),
+        getValidatorOneOfTemplates(['Wsh2Of3', 'Wsh2Of3CltvDrop']),
         // allow all descriptors signed by the user key
         getValidatorSignedByUserKey(),
       ]);
