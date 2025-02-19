dba5411dea805ab3bf5e3937736d5c0d6f166e49
Eric Li <ericli@bitgo.com>
2025-02-19 15:20:25 -0500

    feat(abstract-lightning): wire up lnv2 invoices
    BTC-1773
    
    TICKET: BTC-0

--- modules/abstract-lightning/src/wallet/lightning.ts
+++ modules/abstract-lightning/src/wallet/lightning.ts
@@ -1,10 +1,15 @@
 import * as sdkcore from '@bitgo/sdk-core';
+import * as t from 'io-ts';
 import {
   createMessageSignature,
   LightningAuthKeychain,
   LightningKeychain,
   unwrapLightningCoinSpecific,
   UpdateLightningWalletSignedRequest,
+  CreateInvoiceRequest,
+  GetInvoicesQuery,
+  LightningInvoice,
+  InvoiceInfo,
 } from '../lightning';
 
 export interface ILightningWallet {
@@ -12,7 +17,13 @@ export interface ILightningWallet {
    * Creates a lightning invoice
    * @param params Invoice parameters (to be defined)
    */
-  createInvoice(params: unknown): Promise<unknown>;
+  createInvoice(params: CreateInvoiceRequest): Promise<LightningInvoice>;
+
+  /**
+   * Lists current lightning invoices
+   * @param params Invoice parameters (to be defined)
+   */
+  listInvoices(params: GetInvoicesQuery): Promise<InvoiceInfo[]>;
 
   /**
    * Pay a lightning invoice
@@ -59,12 +70,30 @@ export class SelfCustodialLightningWallet implements ILightningWallet {
     this.wallet = wallet;
   }
 
-  async createInvoice(params: unknown): Promise<unknown> {
-    throw new Error('Method not implemented.');
+  async createInvoice(params: CreateInvoiceRequest): Promise<LightningInvoice> {
+    const createInvoiceResponse = await this.wallet.bitgo
+      .post(this.wallet.baseCoin.url(`/wallet/${this.wallet.id()}/lightning/invoice`))
+      .send(CreateInvoiceRequest.encode(params))
+      .result();
+    return sdkcore.decodeOrElse(LightningInvoice.name, LightningInvoice, createInvoiceResponse, (_) => {
+      // DON'T throw errors from decodeOrElse. It could leak sensitive information.
+      throw new Error(`Invalid create invoice response`);
+    });
   }
 
   async payInvoice(params: unknown): Promise<unknown> {
     throw new Error('Method not implemented.');
+  }
+
+  async listInvoices(params: GetInvoicesQuery): Promise<InvoiceInfo[]> {
+    const returnCodec = t.array(InvoiceInfo);
+    const createInvoiceResponse = await this.wallet.bitgo
+      .get(this.wallet.baseCoin.url(`/wallet/${this.wallet.id()}/lightning/invoice`))
+      .query(GetInvoicesQuery.encode(params))
+      .result();
+    return sdkcore.decodeOrElse(returnCodec.name, returnCodec, createInvoiceResponse, (_) => {
+      throw new Error(`Invalid list invoices response`);
+    });
   }
 
   async getLightningKeychain(): Promise<LightningKeychain> {
@@ -74,7 +103,6 @@ export class SelfCustodialLightningWallet implements ILightningWallet {
     }
     const keychain = await this.wallet.baseCoin.keychains().get({ id: keyIds[0] });
     return sdkcore.decodeOrElse(LightningKeychain.name, LightningKeychain, keychain, (_) => {
-      // DON'T throw errors from decodeOrElse. It could leak sensitive information.
       throw new Error(`Invalid user key`);
     });
   }
