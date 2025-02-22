2c9eb1a005a14b1fd0dce5c0df1d7943d1f767ee
Bhavi Dhingra <bhavidhingra@bitgo.com>
2025-02-22 11:42:56 +0530

    chore(examples): get keychains

--- examples/ts/access-token.ts
+++ examples/ts/access-token.ts
@@ -6,14 +6,14 @@ import { BitGoAPI } from '@bitgo/sdk-api';
 require('dotenv').config({ path: '../../.env' });
 
 const bitgo = new BitGoAPI({
-  env: 'test', // Change this to env: 'production' when you are ready for production
+  env: 'prod', // Change this to env: 'production' when you are ready for production
 });
 
 async function main() {
   const auth_res = await bitgo.authenticate({
-    username: process.env.EMAIL!,
-    password: process.env.PASS!,
-    otp: '000000',
+    username: process.env.PROD_EMAIL!,
+    password: process.env.PROD_PASS!,
+    otp: '942384',
   });
 
   console.log(auth_res);
