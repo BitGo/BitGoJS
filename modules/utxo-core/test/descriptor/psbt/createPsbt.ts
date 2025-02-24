1cc79078f9cf97e4b3f1af14c7e75e4b0ede6c69
Otto Allmendinger <otto@bitgo.com>
2025-02-24 10:11:23 +0100

    test(utxo-core): support different descriptor for sending address
    Update test to allow separate descriptors for input and output scripts
    when creating PSBTs.
    
    Issue: BTC-1826

--- modules/utxo-core/test/descriptor/psbt/createPsbt.ts
+++ modules/utxo-core/test/descriptor/psbt/createPsbt.ts
@@ -3,11 +3,11 @@ import * as assert from 'assert';
 import * as utxolib from '@bitgo/utxo-lib';
 import { BIP32Interface } from '@bitgo/utxo-lib';
 
-import { DescriptorTemplate, getPsbtParams } from '../../../src/testutil/descriptor/descriptors';
+import { DescriptorTemplate, getDescriptor, getPsbtParams } from '../../../src/testutil/descriptor/descriptors';
 import { getFixture } from '../../../src/testutil/fixtures.utils';
 import { finalizePsbt } from '../../../src/descriptor';
 import { getKeyTriple } from '../../../src/testutil/key.utils';
-import { mockPsbtDefaultWithDescriptorTemplate } from '../../../src/testutil/descriptor/mock.utils';
+import { mockPsbtDefault } from '../../../src/testutil/descriptor/mock.utils';
 import { toPlainObjectFromPsbt, toPlainObjectFromTx } from '../../../src/testutil/descriptor/psbt.utils';
 
 async function assertEqualsFixture(t: DescriptorTemplate, filename: string, value: unknown) {
@@ -33,6 +33,7 @@ async function assertEqualsFixture(t: DescriptorTemplate, filename: string, valu
 function describeCreatePsbt(t: DescriptorTemplate) {
   describe(`createPsbt ${t}`, function () {
     const keysA = getKeyTriple('a');
+    const keysB = getKeyTriple('b');
 
     function getSignedPsbt(psbt: utxolib.bitgo.UtxoPsbt, keys: BIP32Interface[]) {
       const cloned = utxolib.bitgo.createPsbtFromBuffer(psbt.toBuffer(), psbt.network);
@@ -43,7 +44,11 @@ function describeCreatePsbt(t: DescriptorTemplate) {
     }
 
     it('creates psbt with expected properties', async function () {
-      const psbt = mockPsbtDefaultWithDescriptorTemplate(t, getPsbtParams(t));
+      const psbt = mockPsbtDefault({
+        descriptorSelf: getDescriptor(t, keysA),
+        descriptorOther: getDescriptor(t, keysB),
+        params: getPsbtParams(t),
+      });
       await assertEqualsFixture(t, 'createPsbt.json', psbt);
       const psbtSignedA = getSignedPsbt(psbt, keysA.slice(0, 1));
       await assertEqualsFixture(t, 'createPsbtSignedA.json', psbtSignedA);
