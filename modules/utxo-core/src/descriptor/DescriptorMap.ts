1dc56bb831721ff93e89d9ec096b1de04de94047
Otto Allmendinger <otto@bitgo.com>
2025-02-27 11:21:59 +0100

    feat(utxo-core): auto-detect descriptor type from string
    Makes `Descriptor.fromString` automatically detect derivable/definite type,
    removing need for manual specification.
    
    Issue: BTC-1826

--- modules/utxo-core/src/descriptor/DescriptorMap.ts
+++ modules/utxo-core/src/descriptor/DescriptorMap.ts
@@ -6,9 +6,6 @@ export type DescriptorMap = Map<string, Descriptor>;
 /** Convert an array of descriptor name-value pairs to a descriptor map */
 export function toDescriptorMap(descriptors: { name: string; value: Descriptor | string }[]): DescriptorMap {
   return new Map(
-    descriptors.map((d) => [
-      d.name,
-      d.value instanceof Descriptor ? d.value : Descriptor.fromString(d.value, 'derivable'),
-    ])
+    descriptors.map((d) => [d.name, d.value instanceof Descriptor ? d.value : Descriptor.fromStringDetectType(d.value)])
   );
 }
