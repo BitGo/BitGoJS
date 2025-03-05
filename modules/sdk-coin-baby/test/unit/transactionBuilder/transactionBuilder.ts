4e59dc67e80e0f5c608f85e67b389fc2885f349c
Eric Liu <ericliu539@bitgo.com>
2025-03-05 11:06:39 -0500

    feat(sdk-coin-baby): add sdk support for babylon native staking
    TICKET: SC-1276

--- modules/sdk-coin-baby/test/unit/transactionBuilder/transactionBuilder.ts
+++ modules/sdk-coin-baby/test/unit/transactionBuilder/transactionBuilder.ts
@@ -2,51 +2,82 @@ import { TransactionType } from '@bitgo/sdk-core';
 import should from 'should';
 
 import * as testData from '../../resources/baby';
-import { getBuilderFactory } from '../getBuilderFactory';
+import { TestBitGo, TestBitGoAPI } from '@bitgo/sdk-test';
+import { BitGoAPI } from '@bitgo/sdk-api';
+import { Baby, Tbaby } from '../../../src';
 
 describe('Babylon Transaction Builder', async () => {
-  const factory = getBuilderFactory('tbaby');
-  const testTxData = testData.TEST_SEND_TX;
+  let bitgo: TestBitGoAPI;
+  let basecoin;
+  let factory;
   let data;
 
-  beforeEach(() => {
+  before(function () {
+    bitgo = TestBitGo.decorate(BitGoAPI, { env: 'mock' });
+    bitgo.safeRegister('baby', Baby.createInstance);
+    bitgo.safeRegister('tbaby', Tbaby.createInstance);
+    bitgo.initializeTestVars();
+    basecoin = bitgo.coin('tbaby');
+    factory = basecoin.getBuilder();
     data = [
       {
         type: TransactionType.Send,
         testTx: testData.TEST_SEND_TX,
         builder: factory.getTransferBuilder(),
       },
+      {
+        type: TransactionType.StakingActivate,
+        testTx: testData.TEST_DELEGATE_TX,
+        builder: factory.getStakingActivateBuilder(),
+      },
+      {
+        type: TransactionType.StakingDeactivate,
+        testTx: testData.TEST_UNDELEGATE_TX,
+        builder: factory.getStakingDeactivateBuilder(),
+      },
+      {
+        type: TransactionType.StakingRedelegate,
+        testTx: testData.TEST_REDELEGATE_TX,
+        builder: factory.getStakingRedelegateBuilder(),
+      },
+      {
+        type: TransactionType.StakingWithdraw,
+        testTx: testData.TEST_WITHDRAW_REWARDS_TX,
+        builder: factory.getStakingWithdrawRewardsBuilder(),
+      },
     ];
   });
 
   it('should build a signed tx from signed tx data', async function () {
-    const txBuilder = factory.from(testTxData.signedTxBase64);
-    const tx = await txBuilder.build();
-    should.equal(tx.type, TransactionType.Send);
-    // Should recreate the same raw tx data when re-build and turned to broadcast format
-    const rawTx = tx.toBroadcastFormat();
-    should.equal(rawTx, testTxData.signedTxBase64);
+    for (const { type, testTx } of data) {
+      const txBuilder = factory.from(testTx.signedTxBase64);
+      const tx = await txBuilder.build();
+      should.equal(tx.type, type);
+      const rawTx = tx.toBroadcastFormat();
+      should.equal(rawTx, testTx.signedTxBase64);
+    }
   });
 
-  describe('gasBudget tests', async () => {
-    it('should succeed for valid gasBudget', function () {
-      for (const { builder } of data) {
-        should.doesNotThrow(() => builder.gasBudget(testTxData.gasBudget));
-      }
-    });
+  it('should succeed for valid gasBudget', function () {
+    for (const { testTx, builder } of data) {
+      should.doesNotThrow(() => builder.gasBudget(testTx.gasBudget));
+    }
+  });
 
-    it('should throw for invalid gasBudget', function () {
-      const invalidGasBudget = 0;
-      for (const { builder } of data) {
-        should(() => builder.gasBudget({ gasLimit: invalidGasBudget })).throw('Invalid gas limit ' + invalidGasBudget);
-      }
-    });
+  it('should throw for invalid gasBudget', function () {
+    const invalidGasBudget = 0;
+    for (const { builder } of data) {
+      should(() => builder.gasBudget({ gasLimit: invalidGasBudget })).throw('Invalid gas limit ' + invalidGasBudget);
+    }
   });
 
   it('validateAddress', function () {
     const invalidAddress = { address: 'randomString' };
-    for (const { builder } of data) {
-      should.doesNotThrow(() => builder.validateAddress({ address: testTxData.sender }));
+    for (const { testTx, builder } of data) {
+      should.doesNotThrow(() => builder.validateAddress({ address: testTx.from ?? testTx.delegator ?? testTx.sender }));
+      should.doesNotThrow(() =>
+        builder.validateAddress({ address: testTx.to ?? testTx.validator ?? testTx.recipient })
+      );
       should(() => builder.validateAddress(invalidAddress)).throwError(
         'transactionBuilder: address isValidAddress check failed: ' + invalidAddress.address
       );
