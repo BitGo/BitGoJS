d5df5bb94258eb82a7c981690596904d171ee59f
Bhavi Dhingra <bhavidhingra@bitgo.com>
2025-04-15 19:30:49 +0530

    feat(examples): send NFTs

--- examples/ts/nft/send-wallet-nfts.ts
+++ examples/ts/nft/send-wallet-nfts.ts
@@ -5,7 +5,7 @@
  * Copyright 2025, BitGo, Inc.  All Rights Reserved.
  */
 import { BitGoAPI } from '@bitgo/sdk-api';
-import { Tapt } from "@bitgo/sdk-coin-apt";
+import {Tapt} from "@bitgo/sdk-coin-apt";
 require('dotenv').config({ path: '../../../.env' });
 
 const bitgo = new BitGoAPI({
@@ -19,13 +19,26 @@ bitgo.register(coin, Tapt.createInstance);
 const walletId = '67ab1a85f51f11a09f9e919e4cbcd11c';
 
 async function main() {
-  const wallet = await bitgo.coin(coin).wallets().get({ id: walletId, allTokens: true });
+  const wallet = await bitgo.coin(coin).wallets().get({id: walletId, allTokens: true});
 
   console.log('\nWallet ID:', wallet.id());
   console.log('\nSupported NFTs:', );
   console.log(wallet.nftBalances());
-  console.log('\nUnsupported NFTs:', );
-  console.log(wallet.unsupportedNftBalances());
+
+  const resp = await wallet.sendNft({
+    walletPassphrase: 'Ghghjkg!455544llll',
+    otp: '000000',
+  }, {
+    entries: [
+      {
+        tokenId: '0x39f154ec595d28f3037596c98c0f3219c2dc5132afc0d3527286f791df9eb5c8',
+        amount: 1,
+      },
+    ],
+    type: 'Digital Asset',
+    tokenContractAddress: '0xbbc561fbfa5d105efd8dfb06ae3e7e5be46331165b99d518f094c701e40603b5',
+    recipientAddress: '0xc15acc27ee41f266877c8f0c61df5bcbc7997df6',
+  });
 }
 
 main().catch((e) => console.error(e));
