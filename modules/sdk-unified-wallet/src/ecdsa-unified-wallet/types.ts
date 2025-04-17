2b152555acb63cabc88083b5c5e23a9425e76bfa
KaustubhPatange <kaustubhpatange225@bitgo.com>
2025-04-17 12:08:23 +0530

    refactor(root): remove sdk-unified-wallet package
    BREAKING CHANGE: remove sdk-unified-wallet package
    
    TICKET: WP-3945

--- modules/sdk-unified-wallet/src/ecdsa-unified-wallet/types.ts
+++
@@ -1,8 +0,0 @@
-import { coins } from '@bitgo/statics';
-
-export const supportedCoins: string[] = [coins.get('eth').name, coins.get('polygon').name].map((e) => e.toLowerCase());
-export const supportedTestCoins: string[] = [
-  coins.get('gteth').name,
-  coins.get('hteth').name,
-  coins.get('tpolygon').name,
-].map((e) => e.toLowerCase());
--- modules/sdk-unified-wallet/src/eddsa-unified-wallet/types.ts
+++
@@ -1,4 +0,0 @@
-import { coins } from '@bitgo/statics';
-
-export const supportedEDDSACoins: string[] = [coins.get('sol').name].map((e) => e.toLowerCase());
-export const supportedEDDSATestCoins: string[] = [coins.get('tsol').name].map((e) => e.toLowerCase());
