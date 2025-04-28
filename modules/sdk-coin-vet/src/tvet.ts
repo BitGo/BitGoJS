ca144878db0eebf3ea5fbf4d8f5f8498d1108ccc
Mohit Khandelwal <mohitkhandelwal@bitgo.com>
2025-04-28 15:39:03 +0530

    feat(sdk-coin-vet): add sdk skeleton for vet
    Ticket: COIN-3934

---
+++ modules/sdk-coin-vet/src/tvet.ts
@@ -0,0 +1,23 @@
+/**
+ * Testnet Vechain
+ */
+import { BaseCoin, BitGoBase } from '@bitgo/sdk-core';
+import { Vet } from './vet';
+import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
+
+export class Tvet extends Vet {
+  protected readonly _staticsCoin: Readonly<StaticsBaseCoin>;
+  protected constructor(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>) {
+    super(bitgo, staticsCoin);
+
+    if (!staticsCoin) {
+      throw new Error('missing required constructor parameter staticsCoin');
+    }
+
+    this._staticsCoin = staticsCoin;
+  }
+
+  static createInstance(bitgo: BitGoBase, staticsCoin?: Readonly<StaticsBaseCoin>): BaseCoin {
+    return new Tvet(bitgo, staticsCoin);
+  }
+}
