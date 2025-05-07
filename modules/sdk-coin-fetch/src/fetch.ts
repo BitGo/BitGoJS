132abbe8897512bb1d1248e22da33cf68f715b08
Alok Baltiyal <alokbaltiyal@bitgo.com>
2025-05-07 18:58:04 +0530

    feat(sdk-coin-fetch): add SDK support for Fetch
    TICKET: COIN-4029

--- modules/sdk-coin-fetch/src/fetch.ts
+++ modules/sdk-coin-fetch/src/fetch.ts
@@ -1,8 +1,17 @@
-import { BaseCoin, BitGoBase } from '@bitgo/sdk-core';
-import { BaseCoin as StaticsBaseCoin } from '@bitgo/statics';
+import { BaseCoin, BitGoBase, Environments } from '@bitgo/sdk-core';
+import { BaseUnit, BaseCoin as StaticsBaseCoin, coins } from '@bitgo/statics';
 import { CosmosCoin, CosmosKeyPair, GasAmountDetails } from '@bitgo/abstract-cosmos';
-import { TransactionBuilderFactory } from './lib';
+import { KeyPair, TransactionBuilderFactory } from './lib';
+import { GAS_AMOUNT, GAS_LIMIT } from './lib/constants';
+import utils from './lib/utils';
 
+/**
+ *
+ * Full Name: Fetch
+ * Website: https://innovationlab.fetch.ai/
+ * Docs: https://website.prod.fetch-ai.com/docs/
+ * GitHub : https://github.com/fetchai
+ */
 export class Fetch extends CosmosCoin {
   protected readonly _staticsCoin: Readonly<StaticsBaseCoin>;
 
@@ -27,36 +36,39 @@ export class Fetch extends CosmosCoin {
 
   /** @inheritDoc **/
   getBuilder(): TransactionBuilderFactory {
-    throw new Error('Method not implemented.');
+    return new TransactionBuilderFactory(coins.get(this.getChain()));
   }
 
   /** @inheritDoc **/
   isValidAddress(address: string): boolean {
-    throw new Error('Method not implemented.');
+    return utils.isValidAddress(address) || utils.isValidValidatorAddress(address);
   }
 
   /** @inheritDoc **/
   getDenomination(): string {
-    throw new Error('Method not implemented');
+    return BaseUnit.FETCH;
   }
 
   /** @inheritDoc **/
   getGasAmountDetails(): GasAmountDetails {
-    throw new Error('Method not implemented');
+    return {
+      gasAmount: GAS_AMOUNT,
+      gasLimit: GAS_LIMIT,
+    };
   }
 
   /** @inheritDoc **/
   getKeyPair(publicKey: string): CosmosKeyPair {
-    throw new Error('Method not implemented');
+    return new KeyPair({ pub: publicKey });
   }
 
   /** @inheritDoc **/
   protected getPublicNodeUrl(): string {
-    throw new Error('Method not implemented');
+    return Environments[this.bitgo.getEnv()].fetchNodeUrl;
   }
 
   /** @inheritDoc **/
   getAddressFromPublicKey(pubKey: string): string {
-    throw new Error('Method not implemented');
+    return new KeyPair({ pub: pubKey }).getAddress();
   }
 }
