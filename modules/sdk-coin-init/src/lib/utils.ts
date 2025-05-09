c1f4db32fa0d239cac424a1e24410a37ef80d1d7
Alok Baltiyal <alokbaltiyal@bitgo.com>
2025-05-09 10:44:51 +0530

    feat(sdk-coin-init): add SDK support for Initia
    TICKET: COIN-4030

--- modules/sdk-coin-init/src/lib/utils.ts
+++ modules/sdk-coin-init/src/lib/utils.ts
@@ -1,25 +1,34 @@
 import { CosmosUtils } from '@bitgo/abstract-cosmos';
+import { InvalidTransactionError } from '@bitgo/sdk-core';
 import { Coin } from '@cosmjs/stargate';
+import BigNumber from 'bignumber.js';
+import * as constants from './constants';
 
 export class Utils extends CosmosUtils {
   /** @inheritdoc */
   isValidAddress(address: string): boolean {
-    throw new Error('Method not implemented.');
+    return this.isValidCosmosLikeAddressWithMemoId(address, constants.accountAddressRegex);
   }
 
   /** @inheritdoc */
   isValidValidatorAddress(address: string): boolean {
-    throw new Error('Method not implemented.');
+    return this.isValidBech32AddressMatchingRegex(address, constants.validatorAddressRegex);
   }
 
   /** @inheritdoc */
   isValidContractAddress(address: string): boolean {
-    throw new Error('Method not implemented.');
+    return this.isValidBech32AddressMatchingRegex(address, constants.contractAddressRegex);
   }
 
   /** @inheritdoc */
   validateAmount(amount: Coin): void {
-    throw new Error('Method not implemented.');
+    const amountBig = BigNumber(amount.amount);
+    if (amountBig.isLessThanOrEqualTo(0)) {
+      throw new InvalidTransactionError('transactionBuilder: validateAmount: Invalid amount: ' + amount.amount);
+    }
+    if (!constants.validDenoms.find((denom) => denom === amount.denom)) {
+      throw new InvalidTransactionError('transactionBuilder: validateAmount: Invalid denom: ' + amount.denom);
+    }
   }
 }
 
--- modules/sdk-coin-init/test/unit/init.ts
+++ modules/sdk-coin-init/test/unit/init.ts
@@ -1,33 +1,275 @@
-import 'should';
+import should = require('should');
+import BigNumber from 'bignumber.js';
+import sinon from 'sinon';
 import { TestBitGo, TestBitGoAPI } from '@bitgo/sdk-test';
 import { BitGoAPI } from '@bitgo/sdk-api';
 import { Init, Tinit } from '../../src/index';
+import utils from '../../src/lib/utils';
+import { TEST_SEND_MANY_TX, TEST_SEND_TX, TEST_TX_WITH_MEMO, address, TEST_ACCOUNT } from '../resources/init';
 
 const bitgo: TestBitGoAPI = TestBitGo.decorate(BitGoAPI, { env: 'test' });
-bitgo.safeRegister('tinit', Tinit.createInstance);
+const coinString = 'init';
+const testCoinString = 'tinit';
+const coinFullName = 'Initia';
+const testCoinFullName = 'Testnet Initia';
+const baseFactor = 1e6;
+const coinInstance = Init.createInstance;
+const testCoinInstance = Tinit.createInstance;
+bitgo.safeRegister(testCoinString, testCoinInstance);
 
-describe('Init', function () {
+describe('Initia', function () {
   let bitgo: TestBitGoAPI;
+  let basecoin;
 
   before(function () {
     bitgo = TestBitGo.decorate(BitGoAPI, { env: 'mock' });
-    bitgo.safeRegister('init', Init.createInstance);
-    bitgo.safeRegister('tinit', Tinit.createInstance);
+    bitgo.safeRegister(coinString, coinInstance);
+    bitgo.safeRegister(testCoinString, testCoinInstance);
     bitgo.initializeTestVars();
+    basecoin = bitgo.coin(testCoinString);
   });
 
   it('should return the right info', function () {
-    const init = bitgo.coin('init');
-    const tinit = bitgo.coin('tinit');
+    const init = bitgo.coin(coinString);
+    const tinit = bitgo.coin(testCoinString);
 
-    init.getChain().should.equal('init');
-    init.getFamily().should.equal('init');
-    init.getFullName().should.equal('Initia');
-    init.getBaseFactor().should.equal(1e6);
+    init.getChain().should.equal(coinString);
+    init.getFamily().should.equal(coinString);
+    init.getFullName().should.equal(coinFullName);
+    init.getBaseFactor().should.equal(baseFactor);
 
-    tinit.getChain().should.equal('tinit');
-    tinit.getFamily().should.equal('init');
-    tinit.getFullName().should.equal('Testnet Initia');
-    tinit.getBaseFactor().should.equal(1e6);
+    tinit.getChain().should.equal(testCoinString);
+    tinit.getFamily().should.equal(coinString);
+    tinit.getFullName().should.equal(testCoinFullName);
+    tinit.getBaseFactor().should.equal(baseFactor);
+  });
+  describe('Address Validation', () => {
+    it('should get address details without memoId', function () {
+      const addressDetails = basecoin.getAddressDetails(address.noMemoIdAddress);
+      addressDetails.address.should.equal(address.noMemoIdAddress);
+      should.not.exist(addressDetails.memoId);
+    });
+
+    it('should get address details with memoId', function () {
+      const addressDetails = basecoin.getAddressDetails(address.validMemoIdAddress);
+      addressDetails.address.should.equal(address.validMemoIdAddress.split('?')[0]);
+      addressDetails.memoId.should.equal('2');
+    });
+
+    it('should throw on invalid memo id address', () => {
+      (() => {
+        basecoin.getAddressDetails(address.invalidMemoIdAddress);
+      }).should.throw();
+    });
+
+    it('should throw on multiple memo id address', () => {
+      (() => {
+        basecoin.getAddressDetails(address.multipleMemoIdAddress);
+      }).should.throw();
+    });
+
+    it('should validate wallet receive address', async function () {
+      const memo = 'memoId=7';
+      const receiveAddress = {
+        address: `${TEST_ACCOUNT.pubAddress}?${memo}`,
+        coinSpecific: {
+          rootAddress: TEST_ACCOUNT.pubAddress,
+          memoID: '7',
+        },
+      };
+      const isValid = await basecoin.isWalletAddress(receiveAddress);
+      isValid.should.equal(true);
+    });
+
+    it('should validate account addresses correctly', () => {
+      should.equal(utils.isValidAddress(address.address1), true);
+      should.equal(utils.isValidAddress(address.address2), true);
+      should.equal(utils.isValidAddress(address.address3), false);
+      should.equal(utils.isValidAddress(address.address4), false);
+      should.equal(utils.isValidAddress('dfjk35y'), false);
+      should.equal(utils.isValidAddress(undefined as unknown as string), false);
+      should.equal(utils.isValidAddress(''), false);
+      should.equal(utils.isValidAddress(address.validMemoIdAddress), true);
+      should.equal(utils.isValidAddress(address.invalidMemoIdAddress), false);
+      should.equal(utils.isValidAddress(address.multipleMemoIdAddress), false);
+    });
+    it('should validate validator addresses correctly', () => {
+      should.equal(utils.isValidValidatorAddress(address.validatorAddress1), true);
+      should.equal(utils.isValidValidatorAddress(address.validatorAddress2), true);
+      should.equal(utils.isValidValidatorAddress(address.validatorAddress3), false);
+      should.equal(utils.isValidValidatorAddress(address.validatorAddress4), false);
+      should.equal(utils.isValidValidatorAddress('dfjk35y'), false);
+      should.equal(utils.isValidValidatorAddress(undefined as unknown as string), false);
+      should.equal(utils.isValidValidatorAddress(''), false);
+    });
+  });
+
+  describe('Verify transaction: ', () => {
+    it('should succeed to verify transaction', async function () {
+      const txPrebuild = {
+        txHex: TEST_SEND_TX.signedTxBase64,
+        txInfo: {},
+      };
+      const txParams = {
+        recipients: [
+          {
+            address: TEST_SEND_TX.recipient,
+            amount: TEST_SEND_TX.sendAmount,
+          },
+        ],
+      };
+      const verification = {};
+      const isTransactionVerified = await basecoin.verifyTransaction({ txParams, txPrebuild, verification });
+      isTransactionVerified.should.equal(true);
+    });
+
+    it('should succeed to verify sendMany transaction', async function () {
+      const txPrebuild = {
+        txHex: TEST_SEND_MANY_TX.signedTxBase64,
+        txInfo: {},
+      };
+      const txParams = {
+        recipients: [
+          {
+            address: TEST_SEND_MANY_TX.sendMessages[0].value.toAddress,
+            amount: TEST_SEND_MANY_TX.sendMessages[0].value.amount[0].amount,
+          },
+          {
+            address: TEST_SEND_MANY_TX.sendMessages[1].value.toAddress,
+            amount: TEST_SEND_MANY_TX.sendMessages[1].value.amount[0].amount,
+          },
+        ],
+      };
+      const verification = {};
+      const isTransactionVerified = await basecoin.verifyTransaction({ txParams, txPrebuild, verification });
+      isTransactionVerified.should.equal(true);
+    });
+
+    it('should fail to verify transaction with invalid param', async function () {
+      const txPrebuild = {};
+      const txParams = { recipients: undefined };
+      await basecoin
+        .verifyTransaction({
+          txParams,
+          txPrebuild,
+        })
+        .should.rejectedWith('missing required tx prebuild property txHex');
+    });
+  });
+
+  describe('Explain Transaction: ', () => {
+    it('should explain a transfer transaction', async function () {
+      const explainedTransaction = await basecoin.explainTransaction({
+        txHex: TEST_SEND_TX.signedTxBase64,
+      });
+      explainedTransaction.should.deepEqual({
+        displayOrder: ['id', 'outputs', 'outputAmount', 'changeOutputs', 'changeAmount', 'fee', 'type'],
+        id: TEST_SEND_TX.hash,
+        outputs: [
+          {
+            address: TEST_SEND_TX.recipient,
+            amount: TEST_SEND_TX.sendAmount,
+          },
+        ],
+        outputAmount: TEST_SEND_TX.sendAmount,
+        changeOutputs: [],
+        changeAmount: '0',
+        fee: { fee: TEST_SEND_TX.gasBudget.amount[0].amount },
+        type: 0,
+      });
+    });
+
+    it('should explain sendMany transfer transaction', async function () {
+      const explainedTransaction = await basecoin.explainTransaction({
+        txHex: TEST_SEND_MANY_TX.signedTxBase64,
+      });
+      explainedTransaction.should.deepEqual({
+        displayOrder: ['id', 'outputs', 'outputAmount', 'changeOutputs', 'changeAmount', 'fee', 'type'],
+        id: TEST_SEND_MANY_TX.hash,
+        outputs: [
+          {
+            address: TEST_SEND_MANY_TX.sendMessages[0].value.toAddress,
+            amount: TEST_SEND_MANY_TX.sendMessages[0].value.amount[0].amount,
+          },
+          {
+            address: TEST_SEND_MANY_TX.sendMessages[1].value.toAddress,
+            amount: TEST_SEND_MANY_TX.sendMessages[1].value.amount[0].amount,
+          },
+        ],
+        outputAmount: '20000',
+        changeOutputs: [],
+        changeAmount: '0',
+        fee: { fee: TEST_SEND_MANY_TX.gasBudget.amount[0].amount },
+        type: 0,
+      });
+    });
+
+    it('should explain a transfer transaction with memo', async function () {
+      const explainedTransaction = await basecoin.explainTransaction({
+        txHex: TEST_TX_WITH_MEMO.signedTxBase64,
+      });
+      explainedTransaction.should.deepEqual({
+        displayOrder: ['id', 'outputs', 'outputAmount', 'changeOutputs', 'changeAmount', 'fee', 'type'],
+        id: TEST_TX_WITH_MEMO.hash,
+        outputs: [
+          {
+            address: TEST_TX_WITH_MEMO.to,
+            amount: TEST_TX_WITH_MEMO.sendAmount,
+            memo: TEST_TX_WITH_MEMO.memo,
+          },
+        ],
+        outputAmount: TEST_TX_WITH_MEMO.sendAmount,
+        changeOutputs: [],
+        changeAmount: '0',
+        fee: { fee: TEST_TX_WITH_MEMO.gasBudget.amount[0].amount },
+        type: 0,
+      });
+    });
+
+    it('should fail to explain transaction with missing params', async function () {
+      try {
+        await basecoin.explainTransaction({});
+      } catch (error) {
+        should.equal(error.message, 'missing required txHex parameter');
+      }
+    });
+
+    it('should fail to explain transaction with invalid params', async function () {
+      try {
+        await basecoin.explainTransaction({ txHex: 'randomString' });
+      } catch (error) {
+        should.equal(error.message.startsWith('Invalid transaction:'), true);
+      }
+    });
+  });
+
+  describe('Parse Transactions: ', () => {
+    const transferInputsResponse = {
+      address: TEST_SEND_TX.recipient,
+      amount: new BigNumber(TEST_SEND_TX.sendAmount).plus(TEST_SEND_TX.gasBudget.amount[0].amount).toFixed(),
+    };
+
+    const transferOutputsResponse = {
+      address: TEST_SEND_TX.recipient,
+      amount: TEST_SEND_TX.sendAmount,
+    };
+
+    it('should parse a transfer transaction', async function () {
+      const parsedTransaction = await basecoin.parseTransaction({ txHex: TEST_SEND_TX.signedTxBase64 });
+
+      parsedTransaction.should.deepEqual({
+        inputs: [transferInputsResponse],
+        outputs: [transferOutputsResponse],
+      });
+    });
+
+    it('should fail to parse a transfer transaction when explainTransaction response is undefined', async function () {
+      const stub = sinon.stub(Init.prototype, 'explainTransaction');
+      stub.resolves(undefined);
+      await basecoin
+        .parseTransaction({ txHex: TEST_SEND_TX.signedTxBase64 })
+        .should.be.rejectedWith('Invalid transaction');
+      stub.restore();
+    });
   });
 });
