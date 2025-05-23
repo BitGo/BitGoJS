46e6ba3e46a3b2be56f6d1570e0e70d042a47315
Bhavi Dhingra <bhavidhingra@bitgo.com>
2025-05-23 15:34:41 +0530

    feat(statics): add new batch of tokens
    Ticket: COIN-1234

--- modules/statics/src/coins/erc20Coins.ts
+++ modules/statics/src/coins/erc20Coins.ts
@@ -10468,6 +10468,19 @@ export const erc20Coins = [
     '0xdc035d45d973e3ec169d2276ddab16f1e407384f',
     UnderlyingAsset['eth:usds']
   ),
+  erc20(
+    '3a173650-5a31-4025-a703-cf9d3f029d15',
+    'eth:newtoken',
+    'New Token',
+    18,
+    '0x1234567890123456789012345678901234567890',
+    UnderlyingAsset['eth:newtoken'],
+    [
+      ...AccountCoin.DEFAULT_FEATURES,
+      CoinFeature.CUSTODY_BITGO_FRANKFURT,
+      CoinFeature.CUSTODY_BITGO_GERMANY,
+    ],
+  ),
   terc20(
     '0205f0d6-0647-47c9-ad8b-c48d048e54f3',
     'fixed',
@@ -11380,6 +11393,18 @@ export const erc20Coins = [
     '0xebe8b46a42f05072b723b00013ff822b2af1b5cb',
     UnderlyingAsset.BGERCH,
     undefined,
+    undefined,
+    undefined,
+    Networks.test.holesky
+  ),
+  terc20(
+    '373e1a33-0bed-4b05-bc98-594cf991f5f6',
+    'hteth:testnewtoken',
+    'Test New Token',
+    18,
+    '0x1234567890123456789012345678901234567890',
+    UnderlyingAsset['hteth:testnewtoken'],
+    HTETH_TOKEN_FEATURES,
     undefined,
     undefined,
     Networks.test.holesky
--- modules/statics/src/coins/ofcErc20Coins.ts
+++ modules/statics/src/coins/ofcErc20Coins.ts
@@ -3153,6 +3153,7 @@ export const ofcErc20Coins = [
     underlyingAssetForSymbol('eth:gaia')
   ),
   ofcerc20('c3a5c5d2-5d6e-4c9b-9f3e-6d2a4e5b6c8d', 'ofceth:usds', 'USDS', 18, underlyingAssetForSymbol('eth:usds')),
+  ofcerc20('a0b6df4e-1ae8-48bd-882a-7ce7552fa2e1', 'ofcnewtoken', 'New Token', 18, UnderlyingAsset['eth:newtoken']),
 ];
 
 export const tOfcErc20Coins = [
@@ -3304,6 +3305,20 @@ export const tOfcErc20Coins = [
     'Holesky Testnet AllUnity',
     6,
     UnderlyingAsset['hteth:aut'],
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    'hteth'
+  ),
+  tofcerc20(
+    '71f96dbb-649b-4f5d-845f-8a8a88e89554',
+    'ofchtestnewtoken',
+    'Test New Token',
+    18,
+    UnderlyingAsset['hteth:testnewtoken'],
     undefined,
     undefined,
     undefined,
