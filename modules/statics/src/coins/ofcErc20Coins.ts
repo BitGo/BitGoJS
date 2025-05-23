af3cf880e8f424f4bfc0ded24e54575e32ed54a4
Bhavi Dhingra <bhavidhingra@bitgo.com>
2025-05-23 07:00:47 +0530

    feat(statics): add new batch of tokens
    Ticket: COIN-1234

--- modules/statics/src/coins/ofcErc20Coins.ts
+++ modules/statics/src/coins/ofcErc20Coins.ts
@@ -3153,6 +3153,7 @@ export const ofcErc20Coins = [
     underlyingAssetForSymbol('eth:gaia')
   ),
   ofcerc20('c3a5c5d2-5d6e-4c9b-9f3e-6d2a4e5b6c8d', 'ofceth:usds', 'USDS', 18, underlyingAssetForSymbol('eth:usds')),
+  ofcerc20('bcfc4bd6-645b-4688-b2ec-d9f8b6efc6ad', 'ofcnewtoken', 'New Token', 18, UnderlyingAsset['eth:newtoken']),
 ];
 
 export const tOfcErc20Coins = [
@@ -3304,6 +3305,20 @@ export const tOfcErc20Coins = [
     'Holesky Testnet AllUnity',
     6,
     UnderlyingAsset['hteth:aut'],
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    undefined,
+    'hteth'
+  ),
+  tofcerc20(
+    '3aa069d4-12f5-4a96-a60c-a7173f4d1991',
+    'ofchtestnewtoken',
+    'Test New Token',
+    18,
+    UnderlyingAsset['hteth:testnewtoken'],
     undefined,
     undefined,
     undefined,
