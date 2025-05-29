ca139ee47e205bf0c784e1ba106f5cbac0078836
Christopher Fong <christopherfong533+github@bitgo.com>
2025-05-29 10:49:11 -0400

    chore: wip added functions and tests
    TICKET: BTC-2047

--- modules/utxo-lib/src/bitgo/psbt/paygoAddressProof.ts
+++ modules/utxo-lib/src/bitgo/psbt/paygoAddressProof.ts
@@ -1,12 +1,17 @@
+import * as assert from 'assert';
 import * as bitcoinMessage from 'bitcoinjs-message';
 import { crypto } from 'bitcoinjs-lib';
 
+import { address } from '../..';
 import { networks } from '../../networks';
 import { toBase58Check } from '../../address';
 import { getPsbtOutputProprietaryKeyVals, ProprietaryKeySubtype, PSBT_PROPRIETARY_IDENTIFIER } from '../PsbtUtil';
 import { UtxoPsbt } from '../UtxoPsbt';
 
-/**
+/** The function consumes the signature as a parameter and adds the PayGo address to the 
+ * PSBT output at the output index where the signature is of the format:
+ * 0x18Bitcoin Signed Message:\n<varint_length><ENTROPY><ADDRESS><UUID> signed by
+ * the HSM beforehand.
  *
  * @param psbt - PSBT that we need to encode our paygo address into
  * @param outputIndex - the index of the address in our output
@@ -16,7 +21,7 @@ export function addPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, sig: B
   psbt.addProprietaryKeyValToOutput(outputIndex, {
     key: {
       identifier: PSBT_PROPRIETARY_IDENTIFIER,
-      subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_PROOF,
+      subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_ATTESTATION_PROOF,
       keydata: Buffer.from(pub)
     },
     value: sig,
@@ -33,7 +38,7 @@ export function addPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, sig: B
 export function verifyPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, pub: Buffer): void {
   const stored = psbt.getOutputProprietaryKeyVals(outputIndex, {
     identifier: PSBT_PROPRIETARY_IDENTIFIER,
-    subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_PROOF,
+    subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_ATTESTATION_PROOF,
   });
   if (!stored) {
     throw new Error('No address proof');
@@ -41,7 +46,7 @@ export function verifyPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, pub
 
   // assert stored length is 0 or 1
   if (stored.length === 0) {
-    throw new Error('There is no paygo address proof encoded in the PSBT.');
+    throw new Error(`There is no paygo address proof encoded in the PSBT at output ${outputIndex}.`);
   } else if (stored.length > 1) {
     throw new Error('There are multiple paygo address proofs encoded in the PSBT. Something went wrong.');
   }
@@ -49,12 +54,21 @@ export function verifyPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, pub
   const signature = stored[0].value;
   // It doesn't matter that this is bitcoin or not, we just need to convert the public key buffer into an address format
   // for the verification
-  const verifyingAddress = toBase58Check(crypto.hash160(pub), networks.bitcoin.pubKeyHash, networks.bitcoin);
+  const messageToVerify = toBase58Check(crypto.hash160(pub), networks.bitcoin.pubKeyHash, networks.bitcoin);
 
   // TODO: need to figure out what the message is in this context
   // Are we verifying the address of the PayGo? we can call getAddressFromScript given output index.
-  if (!bitcoinMessage.verify(message, verifyingAddress, signature)) {
+  if (!bitcoinMessage.verify(message, messageToVerify, signature)) {
     throw new Error('Cannot verify the paygo address signature with the provided pubkey.');
+  }
+
+  const out = psbt.txOutputs[outputIndex];
+  assert(out);
+  const addressFromOutput = address.fromOutputScript(out.script, psbt.network);
+  const addressFromProof = extractAddressFromPayGoAttestationProof(message, addressFromOutput.length);
+  
+  if (addressFromProof !== addressFromOutput) {
+    throw new Error(`The address from the output (${addressFromOutput}) does not match the address that is in the proof (${addressFromProof}).`)
   }
 }
 
@@ -65,11 +79,11 @@ export function verifyPaygoAddressProof(psbt: UtxoPsbt, outputIndex: number, pub
  * @param psbt
  * @returns number - the index of the output address
  */
-export function getPaygoAddressProofIndex(psbt: UtxoPsbt): number | undefined {
+export function getPaygoAddressProofOutputIndex(psbt: UtxoPsbt): number | undefined {
   const res = psbt.data.outputs.flatMap((output, outputIndex) => {
     const proprietaryKeyVals = getPsbtOutputProprietaryKeyVals(output, {
       identifier: PSBT_PROPRIETARY_IDENTIFIER,
-      subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_PROOF,
+      subtype: ProprietaryKeySubtype.PAYGO_ADDRESS_ATTESTATION_PROOF,
     });
 
     return proprietaryKeyVals.length === 0 ? [] : [outputIndex];
@@ -78,6 +92,6 @@ export function getPaygoAddressProofIndex(psbt: UtxoPsbt): number | undefined {
   return res.length === 0 ? undefined : res[0];
 }
 
-export function psbtIncludesPaygoAddressProof(psbt: UtxoPsbt): boolean {
-  return getPaygoAddressProofIndex(psbt) !== undefined;
+export function psbtOutputIncludesPaygoAddressProof(psbt: UtxoPsbt): boolean {
+  return getPaygoAddressProofOutputIndex(psbt) !== undefined;
 }
