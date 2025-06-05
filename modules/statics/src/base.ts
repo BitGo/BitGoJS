bdef145e0e4d6fefb38f765bea192257ead4f6f1
Sampras Saha <samsaha471@bitgo.com>
2025-06-05 19:07:06 +0530

    feat(statics): Add `rebase-token` coin feature
    This PR adds a new coin feature called `rebase-token` to enable a
    coin as a rebase token needed for rebase token flow
    
    TICKET: COIN-3676

--- modules/statics/src/base.ts
+++ modules/statics/src/base.ts
@@ -370,6 +370,11 @@ export enum CoinFeature {
    * This coin supports multisig wallets
    */
   MULTISIG = 'multisig',
+
+  /**
+   * This coin is a rebase token and should use the rebase token functionality
+   */
+  REBASE_TOKEN = 'rebase-token',
 }
 
 /**
