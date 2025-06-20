8dc18d1249eb7d798013fe66e2af303065a2111d
Hrishikesh Jain <hrishikeshjain@bitgo.com>
2025-06-20 17:25:19 +0530

    feat(statics): onboard tnear:usdc token
    TICKET: COIN-4614

--- modules/statics/src/coins/nep141Tokens.ts
+++ modules/statics/src/coins/nep141Tokens.ts
@@ -34,4 +34,14 @@ export const nep141Tokens = [
     UnderlyingAsset['tnear:tnep24dp'],
     NEAR_TOKEN_FEATURES
   ),
+  tnep141Token(
+    '272b824a-792a-444b-be3a-4783cd34ae59',
+    'tnear:usdc',
+    'USD Coin',
+    6,
+    '3e2210e1184b45b64c8a434c0a7e7b23cc04ea7eb7a6c3c32520d03d4afcb8af',
+    '1250000000000000000000',
+    UnderlyingAsset['tnear:usdc'],
+    NEAR_TOKEN_FEATURES
+  ),
 ];
