000a3c5f9f9b1cd3cf02de842fcf140fa7ce8f57
Otto Allmendinger <otto@bitgo.com>
2025-07-04 08:26:59 +0200

    test(utxo-core): move bip32utils tests from bitgo to utxo-core
    Issue: BTC-0
    
    Co-authored-by: llm-git <llm-git@ttll.de>

--- modules/utxo-core/test/bip32utils.ts
+++ modules/utxo-core/test/bip32utils.ts
@@ -1,19 +1,16 @@
-/**
- * @prettier
- */
-import { signMessage, verifyMessage } from '@bitgo/sdk-core';
 import * as crypto from 'crypto';
-import { bip32 } from '@bitgo/utxo-lib';
-import 'should';
 
 import * as utxolib from '@bitgo/utxo-lib';
+import 'should';
 
-describe('bip32util', function () {
+import { signMessage, verifyMessage } from '../src/bip32utils';
+
+describe('bip32utils', function () {
   function getSeedBuffers(length: number) {
     return Array.from({ length }).map((_, i) => crypto.createHash('sha256').update(`${i}`).digest());
   }
   it('signMessage/verifyMessage', function () {
-    const keys = getSeedBuffers(4).map((seed) => bip32.fromSeed(seed));
+    const keys = getSeedBuffers(4).map((seed) => utxolib.bip32.fromSeed(seed));
     const messages = ['hello', 'goodbye'];
     keys.forEach((key) => {
       messages.forEach((message) => {
