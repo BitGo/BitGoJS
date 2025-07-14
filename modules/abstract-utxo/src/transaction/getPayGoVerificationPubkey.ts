26d42d42ede422d3b4c4daeb55f9fa6b3c692ce4
Christopher Fong <christopherfong533+github@bitgo.com>
2025-07-14 12:36:31 -0400

    chore(abstract-utxo): function to get pub key
    TICKET: BTC-2118

---
+++ modules/abstract-utxo/src/transaction/getPayGoVerificationPubkey.ts
@@ -0,0 +1,18 @@
+import * as utxolib from '@bitgo/utxo-lib';
+import { Networks } from '@bitgo/statics';
+
+/**
+ * We want to return the verification pubkey from our statics that has our
+ * verification pubkey.
+ * @param network
+ * @returns
+ */
+export function getPayGoVerificationPubkey(network: utxolib.Network): string | undefined {
+  const networkName = utxolib.getNetworkName(network);
+  switch (networkName) {
+    case 'testnet':
+      return Networks.test.bitcoin.paygoAddressAttestationPubkey;
+    default:
+      return undefined;
+  }
+}
