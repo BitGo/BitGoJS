1f8af7d620b5d603a4e27c15276f518e6ff4f349
N V Rakesh Reddy <nvrakeshreddy@bitgo.com>
2025-08-11 19:42:31 +0530

    fix(sdk-coin-polyx): add test token and update the tests
    Ticket: WIN-6675
    
    TICKET: WIN-6675

--- modules/sdk-coin-polyx/test/unit/transactionBuilder/tokenTransferBuilder.ts
+++ modules/sdk-coin-polyx/test/unit/transactionBuilder/tokenTransferBuilder.ts
@@ -91,45 +91,51 @@ describe('Polyx token transfer Builder - Testnet', () => {
       should.deepEqual(txJson.eraPeriod, 64);
     });
 
-    it.skip('should build from raw signed tx', async () => {
-      builder.from(rawTx.cddTransaction.signed);
+    it('should build from raw signed tx', async () => {
+      builder.from(rawTx.tokenTransfer.signed);
       builder
         .validity({ firstValid: 3933, maxDuration: 64 })
         .referenceBlock('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
       const tx = await builder.build();
       const txJson = tx.toJson();
-      should.deepEqual(txJson.amount, '0');
-      should.deepEqual(txJson.to, '5EFWg5wKTgkFE9XCxigBYPYKQg173djwSmRbkALCdL1jFVUU');
-      should.deepEqual(txJson.sender, '5E7XWJRysj27EzibT4duRxrBQT9Qfa7Z5nAAvJmvd32nhkjH');
+      should.deepEqual(txJson.amount, '1000000');
+      should.deepEqual(txJson.toDID, '0xbc6f7ec808f361c1353ab9dc88c3cc54b98d9eb60fed9c063e67a40925b8ef61');
+      should.deepEqual(txJson.fromDID, '0x1208d7851e6698249aea40742701ee1ef6cdcced260a7c49c1cca1a9db836342');
+      should.deepEqual(txJson.sender, '5HK1b9uRQqkj422kbWmDYFgzoLcjtVZAgP9yXkQyAMNDJnpX');
+      should.deepEqual(txJson.assetId, '0x780602887b358cf48989d0d9aa6c8d28');
+      should.deepEqual(txJson.memo, '0x3000000000000000000000000000000000000000000000000000000000000000');
       should.deepEqual(txJson.blockNumber, 3933);
       should.deepEqual(txJson.referenceBlock, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
       should.deepEqual(txJson.genesisHash, genesisHash);
       should.deepEqual(txJson.specVersion, Number(testnetMaterial.specVersion));
-      should.deepEqual(txJson.nonce, 1);
+      should.deepEqual(txJson.nonce, 3);
       should.deepEqual(txJson.tip, 0);
       should.deepEqual(txJson.transactionVersion, Number(testnetMaterial.txVersion));
       should.deepEqual(txJson.chainName, chainName);
       should.deepEqual(txJson.eraPeriod, 64);
     });
 
-    it.skip('should build from raw unsigned tx', async () => {
-      builder.from(rawTx.cddTransaction.unsigned);
+    it('should build from raw unsigned tx', async () => {
+      builder.from(rawTx.tokenTransfer.unsigned);
       builder
         .validity({ firstValid: 3933, maxDuration: 64 })
         .referenceBlock('0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d')
-        .sender({ address: sender.address })
+        .sender({ address: '5HK1b9uRQqkj422kbWmDYFgzoLcjtVZAgP9yXkQyAMNDJnpX' })
         .addSignature({ pub: sender.publicKey }, Buffer.from(mockTssSignature, 'hex'));
 
       const tx = await builder.build();
       const txJson = tx.toJson();
-      should.deepEqual(txJson.amount, '0');
-      should.deepEqual(txJson.to, '5EFWg5wKTgkFE9XCxigBYPYKQg173djwSmRbkALCdL1jFVUU');
-      should.deepEqual(txJson.sender, sender.address);
+      should.deepEqual(txJson.amount, '1000000');
+      should.deepEqual(txJson.toDID, '0xbc6f7ec808f361c1353ab9dc88c3cc54b98d9eb60fed9c063e67a40925b8ef61');
+      should.deepEqual(txJson.fromDID, '0x1208d7851e6698249aea40742701ee1ef6cdcced260a7c49c1cca1a9db836342');
+      should.deepEqual(txJson.sender, '5HK1b9uRQqkj422kbWmDYFgzoLcjtVZAgP9yXkQyAMNDJnpX');
+      should.deepEqual(txJson.assetId, '0x780602887b358cf48989d0d9aa6c8d28');
+      should.deepEqual(txJson.memo, '0x3000000000000000000000000000000000000000000000000000000000000000');
       should.deepEqual(txJson.blockNumber, 3933);
       should.deepEqual(txJson.referenceBlock, '0x149799bc9602cb5cf201f3425fb8d253b2d4e61fc119dcab3249f307f594754d');
       should.deepEqual(txJson.genesisHash, genesisHash);
       should.deepEqual(txJson.specVersion, Number(testnetMaterial.specVersion));
-      should.deepEqual(txJson.nonce, 1);
+      should.deepEqual(txJson.nonce, 3);
       should.deepEqual(txJson.eraPeriod, 64);
       should.deepEqual(txJson.tip, 0);
       should.deepEqual(txJson.transactionVersion, Number(testnetMaterial.txVersion));
--- modules/statics/src/base.ts
+++ modules/statics/src/base.ts
@@ -2929,7 +2929,7 @@ export enum UnderlyingAsset {
   'ttao:apex' = 'ttao:apex',
 
   // Polymesh testnet tokens
-  'tpolyx:rbitgot' = 'tpolyx:rbitgot',
+  'tpolyx:nvbitgot' = 'tpolyx:nvbitgot',
 
   // Hbar tokens
   'hbar:karate' = 'hbar:karate',
