e84334c3a7df746f719121305ad56d7c7e0ff26b
Aditya Tiwari <adityatiwari@bitgo.com>
2025-08-21 17:52:23 +0530

    feat: add vetNFTCollection skeleton
    Ticket: COIN-5348

--- modules/sdk-coin-vet/src/register.ts
+++ modules/sdk-coin-vet/src/register.ts
@@ -2,11 +2,15 @@ import { BitGoBase } from '@bitgo/sdk-core';
 import { Vet } from './vet';
 import { Tvet } from './tvet';
 import { VetToken } from './vetToken';
+import { VetNFTCollection } from './vetNFTCollection';
 
 export const register = (sdk: BitGoBase): void => {
   sdk.register('vet', Vet.createInstance);
   sdk.register('tvet', Tvet.createInstance);
   VetToken.createTokenConstructors().forEach(({ name, coinConstructor }) => {
+    sdk.register(name, coinConstructor);
+  });
+  VetNFTCollection.createNFTCollectionConstructors().forEach(({ name, coinConstructor }) => {
     sdk.register(name, coinConstructor);
   });
 };
