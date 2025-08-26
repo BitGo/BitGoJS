b898f78dbc3b0766a64dccbebc245942a2c33c37
David Kaplan <davidkaplan@bitgo.com>
2025-08-26 10:41:01 -0400

    feat(utxo-core): add bip322 proof checker
    BTC-2408
    
    TICKET: BTC-2408

--- modules/utxo-core/src/bip322/utils.ts
+++ modules/utxo-core/src/bip322/utils.ts
@@ -33,3 +33,47 @@ export function getBip322ProofMessageAtIndex(psbt: utxolib.Psbt, inputIndex: num
   }
   return Buffer.from(proprietaryKeyVals[0].value);
 }
+
+/**
+ * checks if the transaction contains a BIP322 proof
+ * does not check if the proof is valid
+ * Source: https://github.com/bitcoin/bips/blob/master/bip-0322.mediawiki#user-content-Full
+ * @params tx The transaction or the PSBT to check
+ * @returns boolean
+ */
+export function isBip322ProofCheck(tx: utxolib.bitgo.UtxoPsbt | utxolib.bitgo.UtxoTransaction<bigint>): boolean {
+  if (tx instanceof utxolib.bitgo.UtxoPsbt) {
+    if (tx.version !== 0 || tx.locktime !== 0 || tx.data.outputs.length !== 1) {
+      return false;
+    }
+    const output = tx.txOutputs[0];
+    if (output.script.toString('hex') !== '6a' || output.value !== 0n) {
+      return false;
+    }
+
+    // Return true if there is a message encoded in every input in the proprietary field
+    return tx.data.inputs.every((input, index) => getBip322ProofMessageAtIndex(tx, index) !== undefined);
+  } else if (tx instanceof utxolib.bitgo.UtxoTransaction) {
+    if (tx.version !== 0 || tx.locktime !== 0 || tx.outs.length !== 1) {
+      return false;
+    }
+    if (tx.outs.length !== 1) {
+      return false;
+    }
+    const output = tx.outs[0];
+    // check that the only output is an OP_RETURN with 0 value
+    if (output.script.toString('hex') !== '6a' || output.value !== 0n) {
+      return false;
+    }
+
+    for (const input of tx.ins) {
+      if (input.index !== 0 || input.sequence !== 0) {
+        return false;
+      }
+    }
+
+    return true;
+  } else {
+    throw new Error('Unsupported transaction type for BIP322 proof check');
+  }
+}
