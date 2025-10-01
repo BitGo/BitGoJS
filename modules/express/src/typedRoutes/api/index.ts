ba0d21903c78eb1d137271d69407a79bf3ed028e
danielzhao122 <danielzhao122@bitgo.com>
2025-10-01 14:59:20 -0400

    feat(express): migrate shareWallet to typed routes
    TICKET: WP-5419

--- modules/express/src/typedRoutes/api/index.ts
+++ modules/express/src/typedRoutes/api/index.ts
@@ -26,6 +26,7 @@ import { PostCreateAddress } from './v2/createAddress';
 import { PutFanoutUnspents } from './v1/fanoutUnspents';
 import { PostOfcSignPayload } from './v2/ofcSignPayload';
 import { PostWalletRecoverToken } from './v2/walletRecoverToken';
+import { PostShareWallet } from './v2/shareWallet';
 
 export const ExpressApi = apiSpec({
   'express.ping': {
@@ -99,6 +100,9 @@ export const ExpressApi = apiSpec({
   },
   'express.v2.wallet.recovertoken': {
     post: PostWalletRecoverToken,
+  },
+  'express.v2.wallet.share': {
+    post: PostShareWallet,
   },
 });
 
