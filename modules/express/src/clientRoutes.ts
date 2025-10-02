d78bc30d21d3bcce64f8dd6255e7781980dcb69d
danielzhao122 <danielzhao122@bitgo.com>
2025-10-01 23:31:12 -0400

    refactor: added back check for keychain
    TICKET: WP-5417

--- modules/express/src/clientRoutes.ts
+++ modules/express/src/clientRoutes.ts
@@ -1049,6 +1049,10 @@ export async function handleKeychainChangePassword(
     reqId,
   });
 
+  if (!keychain) {
+    throw new ApiResponseError(`Keychain ${req.params.id} not found`, 404);
+  }
+
   const updatedKeychain = coin.keychains().updateSingleKeychainPassword({
     keychain,
     oldPassword,
