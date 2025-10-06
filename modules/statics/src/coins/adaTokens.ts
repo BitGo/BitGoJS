20fa5cf831fa6cdbf1d5d30acbb592308acedf7d
Ranjna Ganesh Ram <ranjnaganeshram694@bitgo.com>
2025-10-06 19:25:24 +0530

    feat(sdk-coin-ada): add prod ada token config
    Ticket: WIN-6465

--- modules/statics/src/coins/adaTokens.ts
+++ modules/statics/src/coins/adaTokens.ts
@@ -1,7 +1,6 @@
-import { tadaToken } from '../account';
+import { tadaToken, adaToken } from '../account';
 import { UnderlyingAsset } from '../base';
 import { ADA_TOKEN_FEATURES } from '../coinFeatures';
-import { Networks } from '../networks';
 
 export const adaTokens = [
   tadaToken(
@@ -13,7 +12,17 @@ export const adaTokens = [
     'WATER',
     'asset1n69xf60d0760xvn8v2ffd5frvsm0cl2r8hfjf6',
     UnderlyingAsset['tada:water'],
-    ADA_TOKEN_FEATURES,
-    Networks.test.ada
+    ADA_TOKEN_FEATURES
+  ),
+  adaToken(
+    '2d1f9c55-808d-4a6e-b494-62bdb54a16a4',
+    'ada:min',
+    'MIN ADA Token',
+    0, // Tokens are not divisible - https://cardano-ledger.readthedocs.io/en/latest/explanations/token-bundles.html
+    '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6',
+    'MIN',
+    'asset1d9v7aptfvpx7we2la8f25kwprkj2ma5rp6uwzv',
+    UnderlyingAsset['ada:min'],
+    ADA_TOKEN_FEATURES
   ),
 ];
