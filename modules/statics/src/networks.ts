01df6653518ab03b5a7a065309aced8b0599e7c3
Dinshaw Kothari <dinshawkothari@bitgo.com>
2025-10-08 12:32:08 +0530

    chore: added contracts for hypeevm in testnet
    Ticket: WIN-5788

--- modules/statics/src/networks.ts
+++ modules/statics/src/networks.ts
@@ -1429,6 +1429,11 @@ class HypeEVMTestnet extends Testnet implements EthereumNetwork {
   accountExplorerUrl = 'https://testnet.purrsec.com/address/';
   chainId = 998;
   nativeCoinOperationHashPrefix = '998';
+  forwarderFactoryAddress = '0x37996e762fa8b671869740c79eb33f625b3bf92a';
+  forwarderImplementationAddress = '0xd5fe1c1f216b775dfd30638fa7164d41321ef79b';
+  walletFactoryAddress = '0x809ee567e413543af1caebcdb247f6a67eafc8dd';
+  walletImplementationAddress = '0x944fef03af368414f29dc31a72061b8d64f568d2';
+  batcherContractAddress = '0x3e1e5d78e44f15593b3b61ed278f12c27f0ff33e';
 }
 
 class HypeEVM extends Mainnet implements EthereumNetwork {
