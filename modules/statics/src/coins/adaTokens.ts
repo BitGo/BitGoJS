4c0046c92923d740216b711b004abd910953839b
Ranjna Ganesh Ram <ranjnaganeshram694@bitgo.com>
2025-10-08 17:15:19 +0530

    feat(sdk-coin-ada): fix decimal places of minswap
    Ticket: WIN-6465

--- modules/statics/src/coins/adaTokens.ts
+++ modules/statics/src/coins/adaTokens.ts
@@ -7,7 +7,7 @@ export const adaTokens = [
     'a7678172-84b3-4c7e-ac46-9875d23a1cb7',
     'tada:water',
     'Test ADA Token',
-    0, // Tokens are not divisible - https://cardano-ledger.readthedocs.io/en/latest/explanations/token-bundles.html
+    0,
     '2533cca6eb42076e144e9f2772c390dece9fce173bc38c72294b3924',
     'WATER',
     'asset1n69xf60d0760xvn8v2ffd5frvsm0cl2r8hfjf6',
@@ -18,7 +18,7 @@ export const adaTokens = [
     '2d1f9c55-808d-4a6e-b494-62bdb54a16a4',
     'ada:min',
     'MIN ADA Token',
-    0, // Tokens are not divisible - https://cardano-ledger.readthedocs.io/en/latest/explanations/token-bundles.html
+    6,
     '29d222ce763455e3d7a09a665ce554f00ac89d2e99a1a83d267170c6',
     'MIN',
     'asset1d9v7aptfvpx7we2la8f25kwprkj2ma5rp6uwzv',
