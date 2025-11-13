debafb96b6b5eff36873a00591e0057a0a400de0
Harit Kapadia <haritkapadia@bitgo.com>
2025-11-12 21:02:41 -0500

    feat(statics): tapt staking coin feature
    TICKET: SC-3978

--- modules/statics/src/allCoinsAndTokens.ts
+++ modules/statics/src/allCoinsAndTokens.ts
@@ -1939,7 +1939,7 @@ export const allCoinsAndTokens = [
     8,
     UnderlyingAsset.APT,
     BaseUnit.APT,
-    APT_FEATURES,
+    [...APT_FEATURES, CoinFeature.STAKING],
     KeyCurve.Ed25519,
     80,
     200
