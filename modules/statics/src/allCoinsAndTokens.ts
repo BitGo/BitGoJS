09b38391cc5188f8b39224c1217f00037603d58c
Doddanna17 <doddannab@bitgo.com>
2025-12-11 18:31:04 +0530

    feat: enable seievm staking for production environment
    Enable staking functionality for SEIEVM in production
    
    Ticket: SC-4474

--- modules/statics/src/allCoinsAndTokens.ts
+++ modules/statics/src/allCoinsAndTokens.ts
@@ -833,6 +833,7 @@ export const allCoinsAndTokens = [
       CoinFeature.EVM_UNSIGNED_SWEEP_RECOVERY,
       CoinFeature.EVM_NON_BITGO_RECOVERY,
       CoinFeature.SUPPORTS_ERC20,
+      CoinFeature.STAKING,
     ]
   ),
   account(
