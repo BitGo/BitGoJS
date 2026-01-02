596a624d5b89584fcc4bcdc1fa914e93ddc43d22
Aditya Tiwari <adityatiwari@bitgo.com>
2026-01-02 22:41:47 +0530

    fix: add xdc token contructor
    Ticket: COIN-7060

--- modules/sdk-coin-xdc/src/register.ts
+++ modules/sdk-coin-xdc/src/register.ts
@@ -1,8 +1,15 @@
 import { BitGoBase } from '@bitgo/sdk-core';
+import { EthLikeErc20Token } from '@bitgo/sdk-coin-evm';
 import { Xdc } from './xdc';
 import { Txdc } from './txdc';
 
 export const register = (sdk: BitGoBase): void => {
   sdk.register('xdc', Xdc.createInstance);
   sdk.register('txdc', Txdc.createInstance);
+  EthLikeErc20Token.createTokenConstructors({
+    Mainnet: 'xdc',
+    Testnet: 'txdc',
+  }).forEach(({ name, coinConstructor }) => {
+    sdk.register(name, coinConstructor);
+  });
 };
