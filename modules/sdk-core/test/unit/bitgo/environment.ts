722a6451f8be714a0f4e0a9653673b9d98a825c0
Dinshaw Kothari <dinshawkothari@bitgo.com>
2026-02-02 14:02:57 +0530

    feat: add hypeevm bridging config details
    Ticket: WIN-8770

--- modules/sdk-core/test/unit/bitgo/environment.ts
+++ modules/sdk-core/test/unit/bitgo/environment.ts
@@ -16,3 +16,21 @@ describe('zkSync Era Environment Configuration', function () {
     testnetEnv.evm?.zksyncera.baseUrl.should.equal('https://api.etherscan.io/v2');
   });
 });
+
+describe('HypeEvm Bridging Environment Configuration', function () {
+  it('should have bridging config for mainnet Hypeevm', function () {
+    const mainnetEnv = Environments.prod;
+    should.exist(mainnetEnv.evm);
+    should.exist(mainnetEnv.evm?.hypeevm);
+    mainnetEnv.evm?.hypeevm.tokenId?.should.equal(150);
+    mainnetEnv.evm?.hypeevm.systemAddr?.should.equal('0x2222222222222222222222222222222222222222');
+  });
+
+  it('should have bridging config for testnet Hypeevm', function () {
+    const testnetEnv = Environments.test;
+    should.exist(testnetEnv.evm);
+    should.exist(testnetEnv.evm?.hypeevm);
+    testnetEnv.evm?.hypeevm.tokenId?.should.equal(1105);
+    testnetEnv.evm?.hypeevm.systemAddr?.should.equal('0x2222222222222222222222222222222222222222');
+  });
+});
