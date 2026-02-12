245cb80c16b17fc89d27a956f1d02f04adb41d0b
raksha-r7 <rakshar328@bitgo.com>
2026-02-12 13:21:42 +0530

    feat(irys): changes in statics for irys
    Ticket: SC-5393

--- modules/statics/src/allCoinsAndTokens.ts
+++ modules/statics/src/allCoinsAndTokens.ts
@@ -1891,8 +1891,6 @@ export const allCoinsAndTokens = [
     BaseUnit.ETH,
     [
       ...EVM_FEATURES,
-      CoinFeature.SHARED_EVM_SIGNING,
-      CoinFeature.SHARED_EVM_SDK,
       CoinFeature.EVM_COMPATIBLE_IMS,
       CoinFeature.EVM_COMPATIBLE_UI,
       CoinFeature.EVM_UNSIGNED_SWEEP_RECOVERY,
@@ -1909,12 +1907,11 @@ export const allCoinsAndTokens = [
     BaseUnit.ETH,
     [
       ...EVM_FEATURES,
-      CoinFeature.SHARED_EVM_SIGNING,
-      CoinFeature.SHARED_EVM_SDK,
       CoinFeature.EVM_COMPATIBLE_IMS,
       CoinFeature.EVM_COMPATIBLE_UI,
       CoinFeature.EVM_UNSIGNED_SWEEP_RECOVERY,
       CoinFeature.EVM_NON_BITGO_RECOVERY,
+      CoinFeature.STAKING,
     ]
   ),
   account(
