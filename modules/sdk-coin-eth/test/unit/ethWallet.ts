ac58a01dfb0b11edaf46e0ba61874bc860b830e2
Derran Wijesinghe <derranwijesinghe420@bitgo.com>
2026-03-03 12:12:30 -0500

    fix(sdk-coin-eth): update test to expect buildParams on prebuild result
    prebuildTransaction now preserves buildParams on the result instead of
    deleting them, so update the ETH hop transaction test assertion from
    should.not.exist to should.exist.
    
    WP-8015
    
    TICKET: WP-8015

--- modules/sdk-coin-eth/test/unit/ethWallet.ts
+++ modules/sdk-coin-eth/test/unit/ethWallet.ts
@@ -282,7 +282,7 @@ describe('Ethereum Hop Transactions', function () {
       should.exist(res.hopTransaction.id);
       should.exist(res.hopTransaction.signature);
       should.not.exist(res.wallet);
-      should.not.exist(res.buildParams);
+      should.exist(res.buildParams);
       feeScope.isDone().should.equal(true);
       const feeReq = (feeScope as any).interceptors[0].req;
       feeReq.path.should.containEql('hop=true');
