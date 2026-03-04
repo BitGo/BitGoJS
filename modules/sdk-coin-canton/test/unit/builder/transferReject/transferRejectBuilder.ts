d171c94a990e39f3a75dd52f16f033c34f853122
Ravi Hegde <ravihegde348@bitgo.com>
2026-03-04 19:57:53 +0530

    refactor(sdk-coin-canton): refactor the parseRawCantonTransactionData function to correctly handle all the cases
    Ticket: CHALO-270

--- modules/sdk-coin-canton/test/unit/builder/transferReject/transferRejectBuilder.ts
+++ modules/sdk-coin-canton/test/unit/builder/transferReject/transferRejectBuilder.ts
@@ -6,7 +6,13 @@ import { coins } from '@bitgo/statics';
 import { Transaction, TransferRejectionBuilder } from '../../../../src';
 import { CantonTransferAcceptRejectRequest } from '../../../../src/lib/iface';
 
-import { TransferRejection, TransferRejectionPrepareResponse } from '../../../resources';
+import {
+  CantonTokenRejectPrepareResponse,
+  CBTCTokenRejectionPrepareResponse,
+  TransferAcceptance,
+  TransferRejection,
+  TransferRejectionPrepareResponse,
+} from '../../../resources';
 
 describe('Transfer Rejection Builder', () => {
   it('should get the transfer rejection request object', function () {
@@ -23,6 +29,47 @@ describe('Transfer Rejection Builder', () => {
     assert.equal(requestObj.actAs.length, 1);
     const actAs = requestObj.actAs[0];
     assert.equal(actAs, partyId);
+  });
+
+  it('should get the token transfer rejection request object', function () {
+    const txBuilder = new TransferRejectionBuilder(coins.get('tcanton'));
+    const tx = new Transaction(coins.get('tcanton'));
+    txBuilder.initBuilder(tx);
+    txBuilder.setTransaction(CantonTokenRejectPrepareResponse);
+    const { commandId, contractId, partyId } = TransferRejection;
+    txBuilder.commandId(commandId).contractId(contractId).actAs(partyId);
+    const requestObj: CantonTransferAcceptRejectRequest = txBuilder.toRequestObject();
+    txBuilder.transaction.toJson();
+    should.exist(requestObj);
+    assert.equal(requestObj.commandId, commandId);
+    assert.equal(requestObj.contractId, contractId);
+    assert.equal(requestObj.actAs.length, 1);
+    const actAs = requestObj.actAs[0];
+    assert.equal(actAs, partyId);
+  });
+
+  it('should get the transfer acceptance request object for cbtc token', function () {
+    const txBuilder = new TransferRejectionBuilder(coins.get('tcanton'));
+    const transferAcceptanceTx = new Transaction(coins.get('tcanton'));
+    txBuilder.initBuilder(transferAcceptanceTx);
+    txBuilder.setTransaction(CBTCTokenRejectionPrepareResponse);
+    const commandId = '3935a06d-3b03-41be-99a5-95b2ecaabf7d';
+    const partyId = '12201::1220175583b704cbb493393c1dbe17b9909ee4cf55ef345e8147cd6900c5768f861d';
+    const { contractId } = TransferAcceptance;
+    txBuilder.commandId(commandId).contractId(contractId).actAs(partyId);
+    const requestObj: CantonTransferAcceptRejectRequest = txBuilder.toRequestObject();
+    const txData = txBuilder.transaction.toJson();
+    should.exist(requestObj);
+    assert.equal(requestObj.commandId, commandId);
+    assert.equal(requestObj.contractId, contractId);
+    assert.equal(requestObj.actAs.length, 1);
+    const actAs = requestObj.actAs[0];
+    assert.equal(actAs, partyId);
+    should.exist(txData);
+    assert.equal(txData.sender, '12203::1220307926a923431f0aed10609fff4519826a7032d26cb9360d4f67b3082d332f93');
+    assert.equal(txData.receiver, partyId);
+    assert.equal(txData.amount, '1000000');
+    assert.equal(txData.token, 'canton:cbtc');
   });
 
   it('should validate raw transaction', function () {
