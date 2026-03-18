46b529a0f81a795a6d1e6936c785e1239768e353
Hrishikesh Jain <hrishikeshjain@bitgo.com>
2026-03-18 11:02:23 +0530

    fix: add getAddressDetails method for canton
    TICKET: CHALO-77

--- modules/sdk-coin-canton/test/unit/index.ts
+++ modules/sdk-coin-canton/test/unit/index.ts
@@ -1,15 +1,45 @@
-import 'should';
+import should from 'should';
 import { BitGoAPI } from '@bitgo/sdk-api';
+import { InvalidAddressError } from '@bitgo/sdk-core';
 import { TestBitGo, TestBitGoAPI } from '@bitgo/sdk-test';
 import { Canton, Tcanton } from '../../src';
+import { CANTON_ADDRESSES } from '../resources';
 
 describe('Canton:', function () {
   let bitgo: TestBitGoAPI;
+  let basecoin: Canton;
 
   before(function () {
     bitgo = TestBitGo.decorate(BitGoAPI, { env: 'mock' });
     bitgo.safeRegister('canton', Canton.createInstance);
     bitgo.safeRegister('tcanton', Tcanton.createInstance);
     bitgo.initializeTestVars();
+    basecoin = bitgo.coin('canton') as Canton;
+  });
+
+  describe('getAddressDetails', function () {
+    it('should get address details without memoId', function () {
+      const addressDetails = basecoin.getAddressDetails(CANTON_ADDRESSES.VALID_ADDRESS);
+      addressDetails.address.should.equal(CANTON_ADDRESSES.VALID_ADDRESS);
+      should.not.exist(addressDetails.memoId);
+    });
+
+    it('should get address details with memoId', function () {
+      const addressWithMemoId = CANTON_ADDRESSES.VALID_MEMO_ID;
+      const addressDetails = basecoin.getAddressDetails(addressWithMemoId);
+      addressDetails.address.should.equal(CANTON_ADDRESSES.VALID_ADDRESS);
+      should.exist(addressDetails.memoId);
+      addressDetails.memoId!.should.equal('1');
+    });
+
+    it('should throw on multiple memoId query params', function () {
+      (() => basecoin.getAddressDetails(`${CANTON_ADDRESSES.VALID_ADDRESS}?memoId=1&memoId=2`)).should.throw(
+        InvalidAddressError
+      );
+    });
+
+    it('should throw on unknown query params', function () {
+      (() => basecoin.getAddressDetails(`${CANTON_ADDRESSES.VALID_ADDRESS}?foo=bar`)).should.throw(InvalidAddressError);
+    });
   });
 });
