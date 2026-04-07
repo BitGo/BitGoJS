7886f7feea3e1bdee4f863c410b40005b4bcb7d3
Lokesh Chandra <lokeshchandra956@bitgo.com>
2026-04-07 13:13:52 +0530

    docs(express): create keychain local v2
    Ticket: WP-7001

--- modules/express/src/typedRoutes/api/v2/keychainLocal.ts
+++ modules/express/src/typedRoutes/api/v2/keychainLocal.ts
@@ -7,12 +7,14 @@ import { BitgoExpressError } from '../../schemas/error';
  * @property {string} coin - Coin identifier (e.g. btc, tbtc, eth)
  */
 export const KeychainLocalRequestParams = {
-  /** Coin identifier (e.g. btc, tbtc, eth) */
+  /** A cryptocurrency or token ticker symbol. */
   coin: t.string,
 } as const;
 
 export const KeychainLocalRequestResponse200 = t.type({
+  /** Private key */
   prv: t.string,
+  /** public part of a key pair */
   pub: t.string,
 });
 
@@ -28,13 +30,12 @@ export const KeychainLocalResponse = {
 
 /**
  * Local client-side function to create a new keychain.
- * Creating your keychains is a critical step for safely securing your Bitcoin. When generating new keychains, this API uses a random number generator that adheres to industry standards. If you provide your own seed, you must take extreme caution when creating it.
- * Returns an object containing the xprv and xpub for the new chain. The created keychain is not known to the BitGo service. To use it with the BitGo service, use the ‘Store Keychain’ API call.
+ * Creating your keychains is a critical step for safely securing your Bitcoin. When generating new keychains, this API uses a random number generator that adheres to industry standards. If you provide your own seed, you must take extreme caution when creating it. Returns an object containing the xprv and xpub for the new chain. The created keychain is not known to the BitGo service. To use it with the BitGo service, use the ‘Store Keychain’ API call.
  *
  * For security reasons, it is highly recommended that you encrypt and destroy the original xprv immediately to prevent theft.
  *
  * @operationId express.keychain.local
- * @tag express
+ * @tag Express
  */
 export const PostKeychainLocal = httpRoute({
   path: '/api/v2/{coin}/keychain/local',
