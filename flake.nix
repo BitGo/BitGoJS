d01e1cb2c624938760bea5d26fb3dd8ff77665b6
Zahin Mohammad <zahinmohammad@bitgo.com>
2026-04-09 22:56:34 -0400

    chore(root): add commitlint as a husky commit-msg hook
    Add a commit-msg husky hook that runs `yarn check-commits` to validate
    commit messages locally. This ensures conventional commit format is
    enforced before commits are pushed, for all developers regardless of
    their shell setup.
    
    Reverts the nix-specific pre-commit-hooks approach in favor of the
    simpler husky-based hook which works universally after `yarn install`.
    
    WAL-565

--- flake.nix
+++ flake.nix
@@ -2,20 +2,12 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
     nixpkgs-nodejs.url = "github:nixos/nixpkgs/6ad174a6dc07c7742fc64005265addf87ad08615"; # Node.js 22.14.0
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      # Avoids pulling in a second version of `nixpkgs`
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
     nixpkgs-nodejs,
-    pre-commit-hooks,
   }: let
     forEachSystem = nixpkgs.lib.genAttrs [
       "aarch64-darwin"
@@ -24,24 +16,8 @@
       "x86_64-linux"
     ];
   in {
-    checks = forEachSystem (system: {
-      pre-commit-check = pre-commit-hooks.lib.${system}.run {
-        src = ./.;
-        hooks = {
-          commitlint = {
-            enable = true;
-            name = "commitlint";
-            entry = "npx --no -- commitlint --edit";
-            language = "system";
-            stages = ["commit-msg"];
-          };
-        };
-      };
-    });
-
     devShells = forEachSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
-      pre-commit-check = self.checks.${system}.pre-commit-check;
     in {
       default = pkgs.mkShell {
         packages = with nixpkgs-nodejs.legacyPackages.${system}; [
@@ -51,8 +27,6 @@
 
         shellHook = ''
           export PATH="$(pwd)/node_modules/.bin:$PATH"
-          ${pre-commit-check.shellHook}
-          pre-commit install --hook-type commit-msg
         '';
       };
     });
