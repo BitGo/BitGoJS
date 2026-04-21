9f39d85b01df545bfb02a6e00fa109531662eafd
Derran Wijesinghe <derranwijesinghe420@bitgo.com>
2026-04-21 13:52:40 -0400

    docs(express): remove custom field descriptions, use only platform.yaml
    TICKET: WP-7280

--- modules/express/src/typedRoutes/api/common/encrypt.ts
+++ modules/express/src/typedRoutes/api/common/encrypt.ts
@@ -2,17 +2,9 @@ import * as t from 'io-ts';
 import { httpRoute, httpRequest, optional } from '@api-ts/io-ts-http';
 import { BitgoExpressError } from '../../schemas/error';
 
-/**
- * Request body for encrypting data
- *
- * Symmetrically encrypt an arbitrary message with provided password
- */
 export const EncryptRequestBody = {
-  /** The plaintext string to encrypt */
   input: t.string,
-  /** The password used as the encryption key */
   password: optional(t.string),
-  /** Additional authenticated data included in encryption but not encrypted itself */
   adata: optional(t.string),
 };
 
