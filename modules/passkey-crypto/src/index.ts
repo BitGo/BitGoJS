942bd8444bb1b9b726f2e344ce9ddc0fbe718fbf
Derran Wijesinghe <derranwijesinghe420@bitgo.com>
2026-05-01 15:58:03 -0400

    feat(passkey-crypto): extend package with PRF helpers and WebAuthn types
    - add buildEvalByCredential and matchDeviceByCredentialId to @bitgo/passkey-crypto
    - add WebAuthnProvider, PasskeyAuthResult, PasskeyGetOptions types to passkey-crypto
    - re-export WebAuthnOtpDevice from @bitgo/public-types via passkey-crypto
    - add @bitgo/public-types dependency to passkey-crypto
    - add DOM lib to passkey-crypto tsconfig for WebAuthn browser types
    
    TICKET: WCN-187

--- modules/passkey-crypto/src/index.ts
+++ modules/passkey-crypto/src/index.ts
@@ -1,2 +1,4 @@
 export { derivePassword } from './derivePassword';
 export { deriveEnterpriseSalt } from './deriveEnterpriseSalt';
+export { buildEvalByCredential, matchDeviceByCredentialId } from './prfHelpers';
+export type { WebAuthnOtpDevice, PasskeyAuthResult, PasskeyGetOptions, WebAuthnProvider } from './webAuthnTypes';
