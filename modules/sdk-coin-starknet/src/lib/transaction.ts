c4f11510378857ee5651159f3252cff2f6427981
Shubham Damkondwar <shubhamdamkondwar036@bitgo.com>
2026-06-22 15:47:36 +0530

    feat(sdk-coin-starknet): add contractAddress field to transaction interface
    CECHO-1376
    
    TICKET: CECHO-1376

--- modules/sdk-coin-starknet/src/lib/transaction.ts
+++ modules/sdk-coin-starknet/src/lib/transaction.ts
@@ -69,6 +69,7 @@ export class Transaction extends BaseTransaction {
 
       this._starknetTransactionData = {
         senderAddress: parsed.senderAddress,
+        contractAddress: parsed.contractAddress,
         calls: parsed.calls || [],
         nonce: parsed.nonce,
         chainId: parsed.chainId,
